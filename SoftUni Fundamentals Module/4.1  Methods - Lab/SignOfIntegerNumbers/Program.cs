70d8ae16724610229df69bc2d538ac2e1ba7e453
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-09-26 18:23:29 +0300

    Adding folder 4.1 Methods - Lab + 2 Projects in

---
+++ SoftUni Fundamentals Module/4.1  Methods - Lab/SignOfIntegerNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignOfIntegerNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             printNumber(int.Parse(Console.ReadLine())); 
+        }
+
+        static void printNumber(int number)
+        {
+            if (number > 0)
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is zero.");
+            }
+        }
+    }
+}
