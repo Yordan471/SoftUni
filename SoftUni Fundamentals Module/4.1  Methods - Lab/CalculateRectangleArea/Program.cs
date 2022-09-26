2e713d2f47f7dff574279c55532477c08e9065a4
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-09-26 19:15:46 +0300

    Adding Project - CalculateRectangleArea

---
+++ SoftUni Fundamentals Module/4.1  Methods - Lab/CalculateRectangleArea/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateRectangleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int height = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+
+            double area =  CalculateAreaOfARectangle(height, width);
+            Console.WriteLine(area);
+        }
+
+        static double CalculateAreaOfARectangle(int height, int width)
+        {
+            return height * width;
+        }
+    }
+}
