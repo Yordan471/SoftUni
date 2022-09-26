98960e14b14745a32034cf8409434f7a954dd923
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-09-26 20:59:25 +0300

    Adding Projects - Greater Of Two Values and Math Power

---
+++ SoftUni Fundamentals Module/4.1  Methods - Lab/GreaterOfTwoValues/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreaterOfTwoValues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string typeVariable = Console.ReadLine();
+
+            switch (typeVariable)
+            {
+                case "int":
+                    int inputOne = int.Parse(Console.ReadLine());
+                    int inputTwo = int.Parse(Console.ReadLine());
+                    Console.WriteLine(GetMax(inputOne, inputTwo)); 
+                    break;
+                case "char":
+                    char charInputOne = char.Parse(Console.ReadLine());
+                    int charInputTwo = char.Parse(Console.ReadLine());
+                    Console.WriteLine(GetMax((char)charInputOne,(char)charInputTwo)); 
+                    break;
+                case "string":
+                    string stringInputOne = Console.ReadLine();
+                    string stringInputTwo = Console.ReadLine();
+                    Console.WriteLine(GetMax(stringInputOne, stringInputTwo)); 
+                    break;
+            }
+        }
+
+        static int GetMax(int inputOne, int inputTwo)
+        {
+            if (inputOne > inputTwo)
+            {
+                return inputOne;
+            }
+            else
+            {
+                return inputTwo;
+            }
+        }
+
+        static char GetMax(char inputOne, char inputTwo)
+        {
+            if (inputOne > inputTwo)
+            {
+                return inputOne;
+            }
+            else
+            {
+                return inputTwo;
+            }
+        }
+
+        static string GetMax(string inputOne, string inputTwo)
+        {
+            int result = inputOne.CompareTo(inputTwo);
+
+            if (result > 0)
+            {
+                return inputOne;
+            }
+            else
+            {
+                return inputTwo;
+            }              
+        }
+    }
+}
---
+++ SoftUni Fundamentals Module/4.1  Methods - Lab/MathPower/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathPower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseNumber = double.Parse(Console.ReadLine());
+            double power = double.Parse(Console.ReadLine());
+
+            double result = BaseNumberRaisedToPower(baseNumber, power);
+
+            Console.WriteLine(result);
+        }
+
+        static double BaseNumberRaisedToPower(double baseNumber, double power)
+        {
+            return Math.Pow(baseNumber, power);
+        }
+    }
+}
