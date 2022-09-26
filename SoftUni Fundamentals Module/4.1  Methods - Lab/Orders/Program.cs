2f185b8e19067ca3194100b22dba80b9b92509ad
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-09-26 19:10:34 +0300

    Adding Project - Orders

---
+++ SoftUni Fundamentals Module/4.1  Methods - Lab/Orders/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+
+            CalculatePriceForProduct(product, quantity);
+        }
+
+        static void CalculatePriceForProduct(string product, int quantity)
+        {
+            double priceSingleProduct = 0;
+
+            switch (product)
+            {
+                case "coffee":
+                    priceSingleProduct = 1.5;
+                    Console.WriteLine($"{(priceSingleProduct * quantity):f2}");
+                    break;
+                case "water":
+                    priceSingleProduct = 1.00;
+                    Console.WriteLine($"{(priceSingleProduct * quantity):f2}");
+                    break;
+                case "coke":
+                    priceSingleProduct = 1.40;
+                    Console.WriteLine($"{(priceSingleProduct * quantity):f2}");
+                    break;
+                case "snacks":
+                    priceSingleProduct = 2.00;
+                    Console.WriteLine($"{(priceSingleProduct * quantity):f2}");
+                    break;
+            }
+        }
+    }
+}
