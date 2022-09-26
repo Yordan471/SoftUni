da6b728b4fd94a81bbd702bcacceede4047891d2
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-09-26 19:28:52 +0300

    Adding Project - RepeatString

---
+++ SoftUni Fundamentals Module/4.1  Methods - Lab/RepeatString/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepeatString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int repeatWord = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(RepeatWord(word, repeatWord));
+        }
+
+        static string RepeatWord(string word, int repeatWord)
+        {
+            string result = String.Empty;
+
+            for (int i = 0; i < repeatWord; i++)
+            {
+                result += word;
+            }
+
+            return result;
+        }
+    }
+}
