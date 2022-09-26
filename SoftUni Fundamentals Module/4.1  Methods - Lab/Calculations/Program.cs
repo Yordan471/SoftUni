91d8a274e67d82fdabca719582caef16a71c2dbc
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-09-26 18:38:17 +0300

    Adding Project - Calculations

---
+++ SoftUni Fundamentals Module/4.1  Methods - Lab/Calculations/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string operation = Console.ReadLine();
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            switch (operation)
+            {
+                case "add":
+                    Add(firstNumber, secondNumber);
+                    break;
+                case "subtract":
+                    Subtract(firstNumber, secondNumber);
+                    break;
+                case "multiply":
+                    Multiply(firstNumber, secondNumber);
+                    break;
+                case "divide":
+                    Divide(firstNumber, secondNumber);
+                    break;
+            }
+        }
+
+        static void Add(int firstNumber, int secondNumber)
+        {
+            Console.WriteLine($"{firstNumber} + {secondNumber}");
+        }
+
+        static void Subtract(int firstNumber, int secondNumber)
+        {
+            Console.WriteLine(firstNumber - secondNumber);
+        }
+
+        static void Multiply(int firstNumber, int secondNumber)
+        {
+            Console.WriteLine(firstNumber * secondNumber);
+        }
+
+        static void Divide (int firstNumber, int secondNumber)
+        {
+            Console.WriteLine(firstNumber / secondNumber);
+        }
+    }
+}
