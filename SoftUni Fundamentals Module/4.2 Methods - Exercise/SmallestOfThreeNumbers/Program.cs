0de06c493e74e512cc5e5a5ff3b7580e75428849
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-09-27 20:43:39 +0300

    Adding source code for Project - SmallesOfThreeNumbers

--- SoftUni Fundamentals Module/4.2 Methods - Exercise/SmallestOfThreeNumbers/Program.cs
+++ SoftUni Fundamentals Module/4.2 Methods - Exercise/SmallestOfThreeNumbers/Program.cs
@@ -10,7 +10,27 @@ namespace SmallestOfThreeNumbers
     {
         static void Main(string[] args)
         {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());    
 
+            PringBiggestNumber(firstNumber, secondNumber, thirdNumber);
+        }
+
+        static void PringBiggestNumber(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            if (firstNumber < secondNumber && firstNumber < thirdNumber)
+            {
+                Console.WriteLine(firstNumber);
+            }
+            else if (secondNumber < firstNumber && secondNumber < thirdNumber)
+            {
+                Console.WriteLine(secondNumber);
+            }
+            else
+            {
+                Console.WriteLine(thirdNumber);
+            }
         }
     }
 }
