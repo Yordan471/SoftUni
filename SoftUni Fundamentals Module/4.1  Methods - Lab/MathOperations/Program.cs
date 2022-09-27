7db0d57e88573070a265f64407791315d63b6ea0
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-09-27 19:34:51 +0300

    Adding Projects - MultyplyEvenByOdds and MathOperations

---
+++ SoftUni Fundamentals Module/4.1  Methods - Lab/MathOperations/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            string operation = Console.ReadLine();
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Calculations(firstNumber, operation, secondNumber));
+        }
+
+        static int Calculations(int firstNumber, string operation, int secondNumber)
+        {
+            int result = 0;
+
+            switch (operation)
+            {
+                case "/":
+                    result = firstNumber / secondNumber;
+                    break;
+                case "*":
+                    result = firstNumber * secondNumber;
+                    break;
+                case "+":
+                    result = firstNumber + secondNumber; 
+                    break;
+                case "-":
+                    result = firstNumber - secondNumber;
+                    break;
+            }
+
+            return result;
+        }
+
+  //   static int Divide(int firstNumber, int secondNumber)
+  //   {
+  //       return firstNumber / secondNumber;
+  //   }
+  //
+  //   static int Multiply(int firstNumber, int secondNumber)
+  //   {
+  //       return firstNumber * secondNumber;
+  //   }
+  //
+  //   static int Add(int firstNumber, int secondNumber)
+  //   {
+  //       return firstNumber + secondNumber;
+  //   }
+  //
+  //   static int Subtraction(int firstNumber, int secondNumber)
+  //   {
+  //       return firstNumber - secondNumber;
+  //   }
+    }
+}
---
+++ SoftUni Fundamentals Module/4.1  Methods - Lab/MultiplyEvenByOdds/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplyEvenByOdds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            
+            number = Math.Abs(number);
+
+            int sumOddDigits = GetSumOfOddDigits(number);
+            int sumEvenDigits = GetSumOfEvenDigits(number);
+            int result = GetMultipleOfEvenAndOdd(sumEvenDigits, sumOddDigits);
+
+            Console.WriteLine(result);      
+        }
+
+        static int GetSumOfEvenDigits(int number)
+        {
+            int digit = 0;
+            int sumEvenDigits = 0;
+
+            while (number > 0)
+            {
+                digit = number % 10; 
+
+                if (digit % 2 == 0)
+                {
+                    sumEvenDigits += digit;
+                }
+
+                number /= 10;
+            }
+
+            return sumEvenDigits;
+        }
+
+        static int GetSumOfOddDigits(int number)
+        {
+            int digit = 0;
+            int sumOddDigits = 0;
+
+            while (number > 0)
+            {
+                digit = number % 10;
+
+                if (digit % 2 != 0)
+                {
+                    sumOddDigits += digit;
+                }
+
+                number /= 10;
+            }
+
+            return sumOddDigits;
+        }
+
+        static int GetMultipleOfEvenAndOdd(int sumEvenDigits, int sumOddDigits)
+        {
+            return sumEvenDigits * sumOddDigits;
+        }
+    }
+}
