5056e873811c177c7d93199fd621730f53e09359
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-09-27 20:56:38 +0300

    Adding Project - VowelsCount

---
+++ SoftUni Fundamentals Module/4.2 Methods - Exercise/VowelsCount/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VowelsCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+
+            Console.WriteLine(PrintNumberOfVowels(word));
+        }
+
+        static int PrintNumberOfVowels(string word)
+        {
+            char[] chars = word.ToCharArray();
+            int counterVowels = 0;
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (chars[i] == 'e' || chars[i] == 'i' || chars[i] == 'o' ||
+                    chars[i] == 'u' || chars[i] == 'a')
+                {
+                    counterVowels++;
+                }
+            }
+
+            return counterVowels;
+        }
+    }
+}
