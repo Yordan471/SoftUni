0ae5f9c685abc684d16eee98e2dbbcd5b0555de4
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-09-28 19:31:56 +0300

    Adding Source code for Project - MiddleCharachetrs

---
+++ SoftUni Fundamentals Module/4.2 Methods - Exercise/MiddleCharacters/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiddleCharacters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string wordOrNumber = Console.ReadLine();
+
+            MiddleChar(wordOrNumber);
+        }
+
+        static void MiddleChar (string wordOrNumber)
+        {
+            char[] array = wordOrNumber.ToCharArray();
+
+            if (array.Length % 2 == 0)
+            {
+                Console.WriteLine((char)array[(array.Length / 2) - 1] + "" + (char)array[array.Length / 2]);
+            }
+            else if (array.Length % 2 != 0)
+            {
+                Console.WriteLine((char)array[(array.Length / 2)]);
+            }    
+        }
+    }
+}
