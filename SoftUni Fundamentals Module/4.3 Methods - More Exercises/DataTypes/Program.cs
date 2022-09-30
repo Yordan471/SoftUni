bdc85c61e2ab15a3411e3884ff11d7e8724d77d4
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-09-30 20:37:07 +0300

    Changing folder Methods - More Exercises to 4.3 Methods - More Exercises; Adding Project + Source code - DataTypes; Adding Text for 4.2 Methods Projects

---
+++ SoftUni Fundamentals Module/4.3 Methods - More Exercises/DataTypes/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+ 
+            ReturnByType(type);
+        }
+
+        static void ReturnByType(string type)
+        {
+            switch (type)
+            {
+                case "int":
+                    ReturnInteger(type);
+                    break;
+                case "real":
+                    ReturnDouble(type);
+                    break;
+                case "string":
+                    ReturnString(type);
+                    break;
+            }
+        }
+
+        static void ReturnInteger(string type)
+        {
+            int input = int.Parse(Console.ReadLine());
+            Console.WriteLine(input * 2);
+        }
+
+        static void ReturnDouble(string type)
+        {
+            double input = double.Parse(Console.ReadLine());
+            double result = input * 1.5;
+            Console.WriteLine($"{result:f2}");
+        }
+
+        static void ReturnString(string type)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine($"${input}$");
+        }
+    }
+}
