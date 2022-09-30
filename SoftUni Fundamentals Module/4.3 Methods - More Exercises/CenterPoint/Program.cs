47ef1d023a96583e0429b014bf50e9036b0713a3
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-09-30 22:34:43 +0300

    Adding Source Code for Project - CenterPoint

---
+++ SoftUni Fundamentals Module/4.3 Methods - More Exercises/CenterPoint/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CenterPoint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstX = int.Parse(Console.ReadLine());
+            int firstY = int.Parse(Console.ReadLine()); 
+            int secondX = int.Parse(Console.ReadLine());
+            int secondY = int.Parse(Console.ReadLine());
+
+            ClosestToZero(firstX, firstY, secondX, secondY);
+        }
+
+        static void ClosestToZero(int firstX, int firstY, int secondX, int secondY)
+        {
+            int firstNumber = 0;
+            int secondNumber = 0;
+
+            firstNumber = Math.Max(Math.Abs(firstX), Math.Abs(firstY));
+            secondNumber = Math.Max(Math.Abs(secondX), Math.Abs(secondY));
+
+            if (firstNumber > secondNumber)
+            {
+                Console.WriteLine($"({secondX}, {secondY})");
+            }
+            else
+            {
+                Console.WriteLine($"({firstX}, {firstY})");
+            }
+        }
+    }
+}
