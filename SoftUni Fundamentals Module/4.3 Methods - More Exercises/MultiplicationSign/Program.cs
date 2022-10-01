3f00ca1ed1f3d2acafe12d0dc2977360b4f6013f
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-01 19:47:14 +0300

    Adding Source Code for Project - MultiplicationSign

--- SoftUni Fundamentals Module/4.3 Methods - More Exercises/MultiplicationSign/Program.cs
+++ SoftUni Fundamentals Module/4.3 Methods - More Exercises/MultiplicationSign/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,43 @@ namespace MultiplicationSign
     {
         static void Main(string[] args)
         {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            CheckIfResultIsPositiveOrNegative(num1, num2, num3);
+        }
+
+        static void CheckIfResultIsPositiveOrNegative(int num1,
+            int num2, int num3)
+        {
+            bool twoNegatives = false;
+
+            if ((num1 < 0 && num2 < 0) || (num1 < 0 && num3 < 0) ||
+                num2 < 0 && num3 < 0)
+            {
+                twoNegatives = true;
+
+                if ((num1 == 0 || num2 == 0 || num3 == 0) ||
+                    (num1 < 0 && num2 < 0 && num3 < 0))
+                {
+                    twoNegatives = false;
+                }
+
+            }
+
+            if ((num1 > 0 && num2 > 0 && num3 > 0) || twoNegatives)
+            {
+                Console.WriteLine("positive");
+            }
+            else if (num1 == 0 || num2 == 0 || num3 == 0)
+            {
+                Console.WriteLine("zero");
+            }
+            else
+            {
+                Console.WriteLine("negative");
+            }
         }
     }
 }
