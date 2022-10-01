ab59f1c6c008bd9355db9f5ce33608d767f3b70c
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-01 19:07:53 +0300

    Adding Source code for Project - TribonacciSequence

--- SoftUni Fundamentals Module/4.3 Methods - More Exercises/TribonacciSequence/Program.cs
+++ SoftUni Fundamentals Module/4.3 Methods - More Exercises/TribonacciSequence/Program.cs
@@ -10,6 +10,42 @@ namespace TribonacciSequence
     {
         static void Main(string[] args)
         {
+            int num = int.Parse(Console.ReadLine());
+
+            TribonacciSequence(num);
+        }
+
+        static void TribonacciSequence (int num)
+        {
+            int thirdNum = 1;
+            int secondNum = 0;
+            int firstNum = 0;
+            int sumForNextNum = 0;
+
+            if (num == 1)
+            {
+                Console.WriteLine(1);
+            }
+            else if (num == 2)
+            {
+                Console.WriteLine(1 + " " + 1);
+            }
+            else if (num >= 3)
+            {
+                for (int i = 1; i <= num; i++)
+                {
+                    sumForNextNum = firstNum + secondNum + thirdNum;
+
+                    Console.Write($"{sumForNextNum} ");
+
+                    if (i >= 2)
+                    {
+                        firstNum = secondNum;
+                        secondNum = thirdNum;
+                        thirdNum = sumForNextNum;
+                    }               
+                }
+            }
         }
     }
 }
