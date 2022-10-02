8560b49896d3e241a255fd27a0da245720d0a05e
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-02 09:52:36 +0300

    Adding Source code for Project - RemoveNegativesAndReverse

--- SoftUni Fundamentals Module/5.1 Lists - Lab/RemoveNegativesAndReverse/Program.cs
+++ SoftUni Fundamentals Module/5.1 Lists - Lab/RemoveNegativesAndReverse/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,36 @@ namespace RemoveNegativesAndReverse
     {
         static void Main(string[] args)
         {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> newList = new List<int>();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] < 0)
+                {
+                    numbers.RemoveAt(i);
+                    i--;
+                }
+                else
+                {
+                    newList.Add(numbers[i]);
+                }
+            }
+
+            if (newList.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                newList.Reverse();
+
+                Console.WriteLine(String.Join(" ", newList));
+            }
         }
     }
 }
