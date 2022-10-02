d209511c9032fe5720b22e2088e27342f7113bda
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-02 09:41:25 +0300

    Adding Project - RemoveNegativesAndReverse

---
+++ SoftUni Fundamentals Module/5.1 Lists - Lab/RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveNegativesAndReverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
