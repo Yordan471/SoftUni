c6f2bf01676eda723cf8261c42186e9d1f70c13c
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-02 12:18:40 +0300

    Adding Source code for Project - ListManipulation|Basics

--- SoftUni Fundamentals Module/5.1 Lists - Lab/ListmanipulationBasics/Program.cs
+++ SoftUni Fundamentals Module/5.1 Lists - Lab/ListmanipulationBasics/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,47 @@ namespace ListmanipulationBasics
     {
         static void Main(string[] args)
         {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            string command = Console.ReadLine();
+            while (command != "end")
+            {
+                List<string> strings = new List<string>();
+                strings = command.Split().ToList();
+
+                string operation = strings[0];
+                int number = int.Parse(strings[1]);
+                int index = 0;
+
+                if (strings.Count == 3)
+                {
+                    index = int.Parse(strings[2]);
+                }
+
+                switch (operation)
+                {
+                    case "Add":
+                        numbers.Add(number);
+                        break;
+                    case "Remove":
+                        numbers.Remove(number);
+                        break;
+                    case "RemoveAt":
+                        numbers.RemoveAt(number);
+                        break;
+                    case "Insert":
+                        numbers.Insert(index, number);
+                        break;
+                }
+
+                command = Console.ReadLine();
+
+            }
+
+                Console.WriteLine(String.Join(" ", numbers));
         }
     }
 }
