56d8945228a1e3d305727120771d989c1ed42b2a
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-02 09:22:43 +0300

    Adding Source code for Project - MergingLists

--- SoftUni Fundamentals Module/5.1 Lists - Lab/MergingLists/Program.cs
+++ SoftUni Fundamentals Module/5.1 Lists - Lab/MergingLists/Program.cs
@@ -10,6 +10,40 @@ namespace MergingLists
     {
         static void Main(string[] args)
         {
+            List<int> firstListNumbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            List<int> secondListNumbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> MergingList = new List<int>();
+            int counter = -1;
+
+            for (int i = 0; i < Math.Min(firstListNumbers.Count, secondListNumbers.Count); i++)
+            {
+                counter++;
+
+                 MergingList.Add(firstListNumbers[0 + i]);
+                 MergingList.Add(secondListNumbers[0 + i]);                                                                       
+            }
+
+            for (int i = 0; i < Math.Max(firstListNumbers.Count, secondListNumbers.Count) - counter - 1; i++)
+            {
+                if (firstListNumbers.Count > secondListNumbers.Count)
+                {
+                    MergingList.Add(firstListNumbers[counter + 1 + i]);
+                }
+                else
+                {
+                    MergingList.Add(secondListNumbers[counter + 1 + i]);
+                }
+
+            }
+
+            Console.WriteLine(String.Join(" ", MergingList));
         }
     }
 }
