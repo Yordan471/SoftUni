c5e14658559dc95a7174d6c065ddfe0498667807
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-03 20:07:33 +0300

    Adding Source code to Project - Train

--- SoftUni Fundamentals Module/Lists - Exercises/Train/Program.cs
+++ SoftUni Fundamentals Module/Lists - Exercises/Train/Program.cs
@@ -10,6 +10,53 @@ namespace Train
     {
         static void Main(string[] args)
         {
-        }
+            List<int> wagons = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            int maxPassangers = int.Parse(Console.ReadLine());
+
+            while (true)
+            {
+                string addWagonWithPassangersOrOnlyPassangers = Console.ReadLine();
+
+                if (addWagonWithPassangersOrOnlyPassangers == "end")
+                {
+                    break;
+                }
+
+                List<string> addWagonWithPeople = addWagonWithPassangersOrOnlyPassangers.Split().ToList();
+                string add = addWagonWithPeople[0];
+
+                int numberOfPassangers = 0;
+
+                if (add == "Add")
+                {
+                    numberOfPassangers = int.Parse(addWagonWithPeople[1]);
+
+                    wagons.Add(numberOfPassangers);
+                }
+                else
+                {
+                    int passangers = int.Parse(addWagonWithPassangersOrOnlyPassangers);
+
+                    for (int i = 0; i < wagons.Count; i++)
+                    {
+                        if (passangers + wagons[i] <= maxPassangers)
+                        {
+                            wagons[i] += passangers;                         
+                            break;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", wagons));
+        }     
     }
 }
