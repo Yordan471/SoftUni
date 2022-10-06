6e62f28bdd9475449aa045604091b5221fde5265
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-06 20:35:58 +0300

    Adding Source code for Project - CardsGame

--- SoftUni Fundamentals Module/Lists - Exercises/CardsGame/Program.cs
+++ SoftUni Fundamentals Module/Lists - Exercises/CardsGame/Program.cs
@@ -10,33 +10,65 @@ namespace CardsGame
     {
         static void Main(string[] args)
         {
-            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
-            List<int> commands = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> firstPlayerDeck = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            List<int> secondPlayerDeck = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
 
-            for (int i = 0; i < numbers.Count; i++)
+            while (firstPlayerDeck.Count != 0 && secondPlayerDeck.Count != 0)
             {
-                if (numbers[i] == commands[0])
+
+                for (int i = 0; i < Math.Min(firstPlayerDeck.Count, secondPlayerDeck.Count); i++)
                 {
-                    int start = i - commands[1];
-                    if (start < 0)
+                    if (firstPlayerDeck[i] > secondPlayerDeck[i])
                     {
-                        start = 0;
+                        firstPlayerDeck.Add(firstPlayerDeck[i]);
+                        firstPlayerDeck.RemoveAt(i);
+                        firstPlayerDeck.Add(secondPlayerDeck[i]);
+                        secondPlayerDeck.RemoveAt(i);
+                    }
+                    else if (secondPlayerDeck[i] > firstPlayerDeck[i])
+                    {
+                        secondPlayerDeck.Add(secondPlayerDeck[i]);
+                        secondPlayerDeck.RemoveAt(i);
+                        secondPlayerDeck.Add(firstPlayerDeck[i]);
+                        firstPlayerDeck.RemoveAt(i);
+
+                    }
+                    else if (firstPlayerDeck[i] == secondPlayerDeck[i])
+                    {
+                        firstPlayerDeck.RemoveAt(i);
+                        secondPlayerDeck.RemoveAt(i);
                     }
 
-                    int finish = i + commands[1] + 1;
-                    if (finish > numbers.Count)
-                    {
-                        finish = numbers.Count;
-                    }
-
-                    for (int j = start; j < finish; j++)
-                    {
-                        numbers[j] = 0;
-                    }
+                    i--;
                 }
             }
 
-            Console.WriteLine(numbers.Sum());
+            int sum = 0;
+
+            if (firstPlayerDeck.Count > 0)
+            {
+                for (int i = 0; i < firstPlayerDeck.Count; i++)
+                {
+                    sum += firstPlayerDeck[i];
+                }
+
+                Console.WriteLine($"First player wins! Sum: {sum}");
+            }
+            else
+            {
+                for (int i = 0; i < secondPlayerDeck.Count; i++)
+                {
+                    sum += secondPlayerDeck[i];
+                }
+
+                Console.WriteLine($"Second player wins! Sum: {sum}");
+            }
         }
     }
 }
