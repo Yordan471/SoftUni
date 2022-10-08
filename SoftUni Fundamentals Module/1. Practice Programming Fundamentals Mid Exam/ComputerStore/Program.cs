f89d6932cc7e7e9f6a9cb1c7298c2c44a130a323
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-08 11:52:02 +0300

    Adding folder 1. Practice Mid Exam + Projects - ComputerStore, TheLift + Source Code; Adding Project - Messaging

---
+++ SoftUni Fundamentals Module/1. Practice Programming Fundamentals Mid Exam/ComputerStore/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerStore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            double priceWIthoutTaxes = 0;
+            double taxes = 0;
+            bool isSpecial = false;
+
+            while (true)
+            {
+                if (command == "special" || command == "regular")
+                {
+                    if (command == "special")
+                    {
+                        isSpecial = true;
+                    }
+
+                    break;
+                }
+
+                double priceForPart = double.Parse(command);
+
+                if (priceForPart <= 0)
+                {
+                    Console.WriteLine($"Invalid price!");
+                }
+                else
+                {
+                    priceWIthoutTaxes += priceForPart;
+                }
+
+                command = Console.ReadLine();          
+            }
+
+            taxes = priceWIthoutTaxes * 0.20;
+            double totalPrice = taxes + priceWIthoutTaxes;
+            
+            if (isSpecial)
+            {
+                //We have 10% discount of the total price 
+                //If the customer is special
+                totalPrice *= 0.90;
+            }
+
+            if (totalPrice == 0)
+            {
+                Console.WriteLine($"Invalid order!");
+            }
+            else
+            {
+                Console.WriteLine($"Congratulations you've just bought a new computer!");
+                Console.WriteLine($"Price without taxes: {priceWIthoutTaxes:f2}$");
+                Console.WriteLine($"Taxes: {taxes:f2}$");
+                Console.WriteLine("-----------");
+                Console.WriteLine($"Total price: {totalPrice:f2}$");
+            }
+        }
+    }
+}
---
+++ SoftUni Fundamentals Module/1. Practice Programming Fundamentals Mid Exam/TheLift/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheLift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleWaitingForTheLift = int.Parse(Console.ReadLine());
+            int[] peopleInWagons = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+
+            for (int i = 0; i <= peopleInWagons.Length - 1; i++)
+            {                      
+                if (peopleInWagons[i] < 4)
+                {
+                    int peopleWillGoInThisWagon = 4 - peopleInWagons[i];
+
+                    if (peopleWaitingForTheLift < peopleWillGoInThisWagon)
+                    {
+                        peopleInWagons[i] += peopleWaitingForTheLift;
+                        peopleWaitingForTheLift -= peopleWillGoInThisWagon;
+                        break;
+                    }
+                    else
+                    {
+                        peopleInWagons[i] += peopleWillGoInThisWagon;
+                        peopleWaitingForTheLift -= peopleWillGoInThisWagon;;
+                    }                      
+                }
+            }
+
+            int counterFullWagons = 0;
+
+            for (int i = 0; i <= peopleInWagons.Length - 1; i++)
+            {
+                if (peopleInWagons[i] == 4)
+                {
+                    counterFullWagons++;
+                }
+            }
+     
+            if (peopleWaitingForTheLift <= 0 && counterFullWagons != peopleInWagons.Length)
+            {
+                Console.WriteLine("The lift has empty spots!");
+                Console.WriteLine(string.Join(" ", peopleInWagons));
+            }
+            else if (peopleWaitingForTheLift > 0 && counterFullWagons == peopleInWagons.Length)
+            {
+                peopleWaitingForTheLift = Math.Abs(peopleWaitingForTheLift);
+
+                Console.WriteLine($"There isn't enough space! {peopleWaitingForTheLift} people in a queue!");
+                Console.WriteLine(string.Join(" ", peopleInWagons));
+            }
+            else
+            {
+                Console.WriteLine(String.Join(" ", peopleInWagons));
+            }
+        }    
+    }
+}
