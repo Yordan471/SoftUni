178443b5c99bbf3c72c53a45fad52b0d7179c25a
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-08 20:07:26 +0300

    Adding Source code to Project - ShootForTheWin

--- SoftUni Fundamentals Module/3. Programming Fundamentals - Practice Mid Exam/ShootForTheWin/Program.cs
+++ SoftUni Fundamentals Module/3. Programming Fundamentals - Practice Mid Exam/ShootForTheWin/Program.cs
@@ -10,6 +10,44 @@ namespace ShootForTheWin
     {
         static void Main(string[] args)
         {
+            int[] targets = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+            string command = string.Empty;
+
+            int counterForTargetsShot = 0;
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+                int index = int.Parse(command);
+
+                if (index < 0 || index >= targets.Length)
+                {
+                    continue;
+                }
+
+                for (int i = 0; i < targets.Length; i++)
+                {
+                    if (targets[index] != -1 && index != i && targets[i] != -1)
+                    {
+                        if (targets[index] >= targets[i])
+                        {
+                            targets[i] += targets[index];
+                        }
+                        else
+                        {
+                            targets[i] -= targets[index];
+                        }
+                    }                                        
+                }
+
+                targets[index] = -1;
+                counterForTargetsShot++;
+            }
+
+            Console.Write($"Shot targets {counterForTargetsShot} -> ");
+            Console.Write(String.Join(" ", targets));
         }
     }
 }
