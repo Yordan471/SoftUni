bbb84f8a8a31a4b0e732c80f751a9e36a8768572
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-08 18:45:42 +0300

    Adding Project - Counter-Strike + Source code

---
+++ SoftUni Fundamentals Module/3. Programming Fundamentals - Practice Mid Exam/Counter-Strike/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter_Strike
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int initialEnergy = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+
+            int counterWins = 0;
+            bool notEnoughEnergy = false;
+            bool end = false;
+
+            while (true)
+            {
+                if (command == "End of battle")
+                {
+                    end = true;
+                    break;
+                }
+
+                int distance = int.Parse(command);
+
+                if (initialEnergy < distance)
+                {
+                    notEnoughEnergy = true;
+                    break;
+                }
+
+                initialEnergy -= distance;
+                counterWins++;
+
+                if (counterWins % 3 == 0)
+                {
+                    initialEnergy += counterWins;
+                }
+
+                command = Console.ReadLine();   
+            }
+
+            if (end)
+            {
+                Console.WriteLine($"Won battles: {counterWins}. Energy left: {initialEnergy}");
+            }
+            else if (notEnoughEnergy)
+            {
+                Console.WriteLine($"Not enough energy! Game ends with {counterWins} won battles and {initialEnergy} energy");
+            }
+        }
+    }
+}
