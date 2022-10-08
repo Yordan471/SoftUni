fabc195c713d1fbf848a53dcc7b64dd57d05e150
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-08 16:52:29 +0300

    Adding Source code for Project - SoftUniReception

--- SoftUni Fundamentals Module/2. Programming Fundamentals - Practice Mid Exam/SoftUniRecepton/Program.cs
+++ SoftUni Fundamentals Module/2. Programming Fundamentals - Practice Mid Exam/SoftUniRecepton/Program.cs
@@ -10,6 +10,41 @@ namespace SoftUniRecepton
     {
         static void Main(string[] args)
         {
+            int firstEmployeeEfficiency = int.Parse(Console.ReadLine());
+            int secondEmployeeEfficiency = int.Parse(Console.ReadLine());
+            int thirdEmployeeEfficiency = int.Parse(Console.ReadLine());
+            int studentCount = int.Parse(Console.ReadLine());
+
+            int studentsGotAnswers = firstEmployeeEfficiency +
+                secondEmployeeEfficiency + thirdEmployeeEfficiency;
+
+            int hours = 1;
+
+            if (studentCount == 0)
+            {
+                hours = 0;
+            }
+
+            while (studentsGotAnswers < studentCount)
+            {
+                if (hours % 4 == 0)
+                {
+                    hours++;
+                    continue;
+                }
+
+                studentsGotAnswers += firstEmployeeEfficiency +
+                secondEmployeeEfficiency + thirdEmployeeEfficiency;
+
+                hours++;
+
+                if (studentsGotAnswers >= studentCount && hours % 4 == 0)
+                {
+                    hours++;
+                }
+            }
+
+            Console.WriteLine($"Time needed: {hours}h.");
         }
     }
 }
