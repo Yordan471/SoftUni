2689ff7914f59df133e280d4194295ff63792a08
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-08 17:59:32 +0300

    Adding Source code for Project - Numbers

--- SoftUni Fundamentals Module/2. Programming Fundamentals - Practice Mid Exam/Numbers/Program.cs
+++ SoftUni Fundamentals Module/2. Programming Fundamentals - Practice Mid Exam/Numbers/Program.cs
@@ -10,6 +10,50 @@ namespace Numbers
     {
         static void Main(string[] args)
         {
+            List<int> listOfIntegers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            int sum = 0;
+
+            for (int i = 0; i < listOfIntegers.Count; i++)
+            {
+                sum += listOfIntegers[i];
+            }
+
+            int counterForNumbersBiggerThanAvrgSum = 0;
+            List<int> reverseNumbers = new List<int>();
+            double avrgNumber = sum * 1.0 / listOfIntegers.Count;
+
+            for (int i = 0; i < listOfIntegers.Count; i++)
+            {
+                if (listOfIntegers[i] > avrgNumber)
+                {                                       
+                        reverseNumbers.Add(listOfIntegers[i]);                                      
+                }
+            }
+
+            if (reverseNumbers.Count > 0)
+            {
+                reverseNumbers.Sort();
+                reverseNumbers.Reverse();
+
+                for (int i = 0; i < reverseNumbers.Count; i++)
+                {
+                    if (counterForNumbersBiggerThanAvrgSum < 5)
+                    {
+                        Console.Write($"{reverseNumbers[i]} ");
+                    }
+
+                    counterForNumbersBiggerThanAvrgSum++;
+                }
+
+            }
+            else
+            {
+               Console.WriteLine("No");
+            }                             
         }
     }
 }
