19c33bea5384f436906878633b429cc25211ee75
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-09 10:57:48 +0300

    Adding Source code to Project - GuineaPig

--- SoftUni Fundamentals Module/4. Programming Fundamentals - Practice Mid Exam/GuineaPig/Program.cs
+++ SoftUni Fundamentals Module/4. Programming Fundamentals - Practice Mid Exam/GuineaPig/Program.cs
@@ -10,6 +10,46 @@ namespace GuineaPig
     {
         static void Main(string[] args)
         {
+            decimal quantityFood = decimal.Parse(Console.ReadLine());
+            decimal quantityHay = decimal.Parse(Console.ReadLine());
+            double quantityCover = double.Parse(Console.ReadLine());
+            double pigWeight = double.Parse(Console.ReadLine());
+
+            int counterDays = 0;
+            bool outOfProvisions = false;
+
+            while (counterDays != 30)
+            {
+                counterDays++;
+
+                quantityFood -= 0.300M;
+
+                if (counterDays % 2 == 0)
+                {
+                    quantityHay -= 0.05M * quantityFood;
+                }
+
+                if (counterDays % 3 == 0)
+                {
+                    quantityCover -= 0.33333333333 * pigWeight;
+                }
+
+                if (quantityFood <= 0 || quantityHay <= 0 ||
+                    quantityCover <= 0)
+                {
+                    outOfProvisions = true;
+                    break;
+                }
+            }
+
+            if (outOfProvisions)
+            {
+                Console.WriteLine("Merry must go to the pet store!");
+            }
+            else
+            {
+                Console.WriteLine($"Everything is fine! Puppy is happy! Food: {quantityFood:f2}, Hay: {quantityHay:f2}, Cover: {quantityCover:f2}.");
+            }
         }
     }
 }
