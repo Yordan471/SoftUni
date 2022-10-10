a825a909de022219fe4fee807842cb1cb1d40b9e
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-10 18:45:48 +0300

    Adding Source code to Project - Messaging

--- SoftUni Fundamentals Module/5.3 Lists - More Exercises/Messaging/Program.cs
+++ SoftUni Fundamentals Module/5.3 Lists - More Exercises/Messaging/Program.cs
@@ -10,7 +10,46 @@ namespace Messaging
     {
         static void Main(string[] args)
         {
+            List<int> numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+            string aString = Console.ReadLine();
 
+            List<char> aStringToChar = new List<char>(aString);
+            int sumDigits = 0;
+            string stringOutput = string.Empty;
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int saveNumber = numbers[i];
+
+                while (saveNumber != 0)
+                {
+                    sumDigits += saveNumber % 10;
+                    saveNumber /= 10;
+                }
+
+                if (sumDigits > aStringToChar.Count)
+                {
+                    while (sumDigits > aStringToChar.Count)
+                    {
+                        sumDigits -= aStringToChar.Count;
+                    }
+
+                    stringOutput += aStringToChar[sumDigits];
+                    aStringToChar.RemoveAt(sumDigits);
+                }
+                else
+                {
+                    stringOutput += aStringToChar[sumDigits];
+                    aStringToChar.RemoveAt(sumDigits);
+                }
+
+                sumDigits = 0;
+            }
+
+            Console.WriteLine(stringOutput); 
         }
     }
 }
