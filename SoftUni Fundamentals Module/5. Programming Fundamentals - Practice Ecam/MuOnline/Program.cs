67b2c6c89202ba6b1c8baa18b5122e8184eacc57
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-15 18:32:42 +0300

    Adding Source code for Project - MuOnline

--- SoftUni Fundamentals Module/5. Programming Fundamentals - Practice Ecam/MuOnline/Program.cs
+++ SoftUni Fundamentals Module/5. Programming Fundamentals - Practice Ecam/MuOnline/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Threading;
 
 namespace MuOnline
 {
@@ -6,7 +8,81 @@ namespace MuOnline
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] dungeonRooms = Console.ReadLine()
+                .Split('|')
+                .ToArray();
+
+            int counterRooms = 0;
+            int initialHealth = 100;
+            int bitcoins = 0;
+            bool died = false;
+            
+            while (counterRooms != dungeonRooms.Length)
+            {
+                if (died)
+                {
+                    break;
+                }
+
+                for (int i = 0; i < dungeonRooms.Length; i++)
+                {
+                    counterRooms++;
+
+                    string[] commandAndNumber = dungeonRooms[i]
+                        .Split()
+                        .ToArray();
+
+                    string command = commandAndNumber[0];
+                    int number = int.Parse(commandAndNumber[1]);
+
+                    if (command == "potion")
+                    {
+                        if (initialHealth + number > 100)
+                        {
+                            number = 100 - initialHealth;
+                            initialHealth = 100;
+                        }
+                        else
+                        {
+                            initialHealth += number;
+                        }
+
+                        Console.WriteLine($"You healed for {number} hp.");
+                        Console.WriteLine($"Current health: {initialHealth} hp.");
+                    }
+                    else if (command == "chest")
+                    {
+                        bitcoins += number;
+                        Console.WriteLine($"You found {number} bitcoins.");
+                    }
+                    else
+                    {
+                        initialHealth -= number;
+
+                        if (initialHealth <= 0)
+                        {
+                            Console.WriteLine($"You died! Killed by {command}.");
+                            died = true;
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"You've slayed {command}");
+                        }
+                    }
+                }
+            }
+
+            if (died)
+            {
+                Console.WriteLine($"Best room: {counterRooms}");
+            }
+            else
+            {
+                Console.WriteLine($"You've made it!");
+                Console.WriteLine($"Bitcoins: {bitcoins}");
+                Console.WriteLine($"Health: {initialHealth}");
+            }
         }
     }
 }
