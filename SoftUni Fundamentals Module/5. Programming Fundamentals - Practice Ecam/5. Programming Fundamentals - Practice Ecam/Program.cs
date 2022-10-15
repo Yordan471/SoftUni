fa9ca42ac680be8cc4768eaf21dc665a1de9c877
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-15 17:52:50 +0300

    Adding Source code for Project  BonusScoringSystem; Adding Project - MuOnline

--- SoftUni Fundamentals Module/5. Programming Fundamentals - Practice Ecam/5. Programming Fundamentals - Practice Ecam/Program.cs
+++ SoftUni Fundamentals Module/5. Programming Fundamentals - Practice Ecam/5. Programming Fundamentals - Practice Ecam/Program.cs
@@ -6,7 +6,35 @@ namespace _5._Programming_Fundamentals___Practice_Exam
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numberOfStudents = int.Parse(Console.ReadLine());
+            int numberOfLectures = int.Parse(Console.ReadLine());
+            int additionalBonus = int.Parse(Console.ReadLine());
+
+            double maxBonusPounts = -10000000;
+            double bonusPoints = 0;
+            double saveAttendances = 0;
+
+            if (numberOfStudents == 0)
+            {
+                maxBonusPounts = 0;
+            }
+
+            for (int i = 1; i < numberOfStudents; i++)
+            {
+                double attendances = int.Parse(Console.ReadLine());
+
+                bonusPoints = (attendances / (numberOfLectures) * (5 + additionalBonus) * 1.0);
+
+                if (bonusPoints >= maxBonusPounts)
+                {
+                    maxBonusPounts = bonusPoints;
+                    saveAttendances = attendances;
+                }
+            }
+
+            maxBonusPounts = Math.Ceiling(maxBonusPounts);
+            Console.WriteLine($"Max Bonus: {maxBonusPounts}.");
+            Console.WriteLine($"The student has attended {saveAttendances} lectures.");
         }
     }
 }
---
+++ SoftUni Fundamentals Module/5. Programming Fundamentals - Practice Ecam/MuOnline/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MuOnline
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
