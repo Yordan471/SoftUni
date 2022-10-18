0a5a998fea7e2194f0dc8dfbfb74714be7794f9a
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-18 20:21:15 +0300

    Adding Source code for Project TakeSkipRope from folder 5.3 Lists - More Exercises

--- SoftUni Fundamentals Module/5.3 Lists - More Exercises/TakeSkipRope/Program.cs
+++ SoftUni Fundamentals Module/5.3 Lists - More Exercises/TakeSkipRope/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +12,61 @@ namespace TakeSkipRope
     {
         static void Main(string[] args)
         {
-           
+            string decryptedMessage = Console.ReadLine();
+            List<char> lettersFromMessage = new List<char>();
+
+            List<int> digitsFromMessage = new List<int>();
+
+            for (int i = 0; i < decryptedMessage.Length; i++)
+            {
+                if (char.IsDigit(decryptedMessage[i]))
+                {
+                    digitsFromMessage.Add(int.Parse(decryptedMessage[i].ToString()));
+                }
+                else
+                {
+                    lettersFromMessage.Add(decryptedMessage[i]);
+                }
+            }
+
+            List<int> takeList = new List<int>();
+            List<int> skipList = new List<int>();
+            
+            for (int i = 0; i < digitsFromMessage.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    takeList.Add(digitsFromMessage[i]);
+                }
+                else
+                {
+                    skipList.Add(digitsFromMessage[i]);
+                }
+            }
+
+            string result = string.Empty;
+            int skipCount = 0;
+            int takeCount = 0;
+
+            for (int i = 0; i < takeList.Count; i++)
+            {
+                if (takeList[i] > 0)
+                {
+                    for (int j = skipCount + takeCount; j < takeList[i] + takeCount + skipCount; j++)
+                    {
+                        if (j == lettersFromMessage.Count)
+                        {
+                            break;
+                        }
+                        result += lettersFromMessage[j];
+                    }                  
+                }
+
+                skipCount += skipList[i];
+                takeCount += takeList[i];
+            }
+
+            Console.WriteLine(result);
         }
     }
 }
