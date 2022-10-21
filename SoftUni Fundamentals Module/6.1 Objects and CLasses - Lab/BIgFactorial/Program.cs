c48a45d5166a7df7d67a3030362537365866a5c3
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-21 20:03:57 +0300

    Adding Project - BigFactorial

---
+++ SoftUni Fundamentals Module/6.1 Objects and CLasses - Lab/BIgFactorial/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BIgFactorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
