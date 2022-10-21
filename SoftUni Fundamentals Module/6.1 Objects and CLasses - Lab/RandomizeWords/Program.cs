7fe0ee9fc515551bf31848414500c1a8664e24bf
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-21 18:47:45 +0300

    Adding folder 6.1 Objects and Classes - Lab; Adding Project - RandomizeWords + unfinished Source code

---
+++ SoftUni Fundamentals Module/6.1 Objects and CLasses - Lab/RandomizeWords/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace RandomizeWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+
+            string sequence = Console.ReadLine();
+            string[] arrayOfWords = sequence
+                .Split()
+                .ToArray();
+          
+            Random randomize = new Random();
+          
+            for (int i = 0; i < arrayOfWords.Length; i++)
+            {
+                int randomNum = randomize.Next(i, arrayOfWords.Length);
+                Console.WriteLine(arrayOfWords[randomNum]);
+            }
+        }
+    }
+}
