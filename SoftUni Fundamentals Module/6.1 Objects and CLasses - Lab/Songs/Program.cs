24a9d0daf6af3138a0782b8f1f2ecd737ac2a54c
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-22 16:35:00 +0300

    Adding Source code for Project - Songs

--- SoftUni Fundamentals Module/6.1 Objects and CLasses - Lab/Songs/Program.cs
+++ SoftUni Fundamentals Module/6.1 Objects and CLasses - Lab/Songs/Program.cs
@@ -1,12 +1,65 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 
 namespace Songs
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numberOfSongs = int.Parse(Console.ReadLine());
+
+            List<Songs> saveSongs = new List<Songs>();
+
+            for (int i = 0; i < numberOfSongs; i++)
+            {
+                string[] informationToList = Console.ReadLine()
+                    .Split('_')
+                    .ToArray();
+
+                Songs song = new Songs(informationToList[0],
+                    informationToList[1], informationToList[2]);
+
+                saveSongs.Add(song);
+            }
+
+            string listTypeOrAll = Console.ReadLine();
+
+            if (listTypeOrAll == "all")
+            {
+                foreach (Songs Song in saveSongs)
+                {
+                    Console.WriteLine(Song.Name);
+                }
+            }
+            else
+            {
+                foreach (Songs song in saveSongs)
+                {
+                    if (song.TypeList == listTypeOrAll)
+                    {
+                        Console.WriteLine(song.Name);
+                    }
+                }
+            }
         }
+    }
+
+    class Songs
+    {
+        public Songs(string typeList, string name, string time)
+        {
+            this.TypeList  = typeList;
+            this.Name = name;
+            this.Time = time;
+        }
+
+        public string TypeList { get; set; }    
+
+        public string Name { get; set; }
+
+        public string Time { get; set; }    
     }
 }
