a7fd61b04c28179018609e3096e858ffac9e14d6
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-22 19:18:12 +0300

    Adding Source code for Project - StoreBoxes

--- SoftUni Fundamentals Module/6.1 Objects and CLasses - Lab/StoreBoxes/Program.cs
+++ SoftUni Fundamentals Module/6.1 Objects and CLasses - Lab/StoreBoxes/Program.cs
@@ -1,12 +1,81 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace StoreBoxes
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string information = Console.ReadLine();
+
+            List<Box> boxes = new List<Box>();
+
+            while (true)
+            {
+                if (information == "end")
+                {
+                    break;
+                }
+
+                string[] informationToArray = information
+                    .Split()
+                    .ToArray();
+
+                string serialNumber = informationToArray[0];
+                string itemName = informationToArray[1];
+                int itemQuantity = int.Parse(informationToArray[2]);
+                decimal itemPrice = decimal.Parse(informationToArray[3]);
+
+                Box box = new Box()
+                {
+                    SerialNumber = serialNumber,
+                    Item = new Item()
+                    {
+                        Name = itemName,
+                        Price = itemPrice
+                    },
+                    ItemQuantity = itemQuantity,
+                };
+
+                information = Console.ReadLine();
+
+                boxes.Add(box);
+            }
+
+            List<Box> orderBoxes = boxes.OrderByDescending(box => box.PriceForABox).ToList();
+
+            foreach (Box box in orderBoxes)
+            {
+                Console.WriteLine($"{box.SerialNumber}");
+                Console.WriteLine($"-- {box.Item.Name} - {box.Item.Price}: {box.ItemQuantity}");
+                Console.WriteLine($"{box.PriceForABox:f2}");
+            }
+        }
+    }
+
+    class Item
+    {
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+
+    class Box
+    {
+        public string SerialNumber { get; set; }
+
+        public Item Item { get; set; }
+
+        public int ItemQuantity { get; set; }
+
+        public decimal PriceForABox 
+        {
+            get
+            {
+                return this.ItemQuantity * this.Item.Price;
+            }
         }
     }
 }
