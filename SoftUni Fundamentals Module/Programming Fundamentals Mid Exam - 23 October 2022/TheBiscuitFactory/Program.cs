3a1f6bdf623cce7811197c7ab75a56aa077f3bf8
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-23 09:40:17 +0300

    Adding Source code for Project - TheBiscuitFactory

--- SoftUni Fundamentals Module/Programming Fundamentals Mid Exam - 23 October 2022/TheBiscuitFactory/Program.cs
+++ SoftUni Fundamentals Module/Programming Fundamentals Mid Exam - 23 October 2022/TheBiscuitFactory/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 namespace TheBiscuitFactory
 {
@@ -6,7 +7,41 @@ namespace TheBiscuitFactory
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double numberOfBiscuitsPerWorker = int.Parse(Console.ReadLine());
+            double countOfWorkersInFactory = int.Parse(Console.ReadLine());
+            double numberOfBiscutsCompFactory = int.Parse(Console.ReadLine());
+            double saveNumberOfBiscuits = numberOfBiscuitsPerWorker;
+
+            double producedBiscutsPerDay = 0;
+            long producedBiscuitsForMonth = 0;
+
+            for (int i = 1; i <= 30; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    numberOfBiscuitsPerWorker *= 0.75;
+                }
+
+                producedBiscutsPerDay = Math.Floor(numberOfBiscuitsPerWorker * countOfWorkersInFactory);
+                producedBiscuitsForMonth += Convert.ToInt64(producedBiscutsPerDay);
+                numberOfBiscuitsPerWorker = saveNumberOfBiscuits;
+            }
+
+            Console.WriteLine($"You have produced {producedBiscuitsForMonth} biscuits for the past month.");
+
+            double productionDiff = Math.Abs(producedBiscuitsForMonth - numberOfBiscutsCompFactory);
+            double prcntgDiff = 100 * productionDiff / numberOfBiscutsCompFactory;
+            
+            if (producedBiscuitsForMonth > numberOfBiscutsCompFactory)
+            {
+                Console.WriteLine($"You produce {prcntgDiff:f2}% percent more biscuits.");
+            }
+            else if (producedBiscuitsForMonth < numberOfBiscutsCompFactory)
+            {
+                productionDiff = Math.Abs(productionDiff);
+                Console.WriteLine($"You produce {prcntgDiff:f2}% percent less biscuits.");
+            }
         }
+                    
     }
 }
