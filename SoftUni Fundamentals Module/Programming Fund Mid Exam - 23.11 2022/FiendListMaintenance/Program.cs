0ef1151e47e557bd3d489df3aa0a2336fa3a90bc
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-23 10:22:05 +0300

    Adding Source code for Project - FriendListMaintenance

--- SoftUni Fundamentals Module/Programming Fund Mid Exam - 23.11 2022/FiendListMaintenance/Program.cs
+++ SoftUni Fundamentals Module/Programming Fund Mid Exam - 23.11 2022/FiendListMaintenance/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace FiendListMaintenance
 {
@@ -6,7 +8,85 @@ namespace FiendListMaintenance
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] friendsList = Console.ReadLine()
+                .Split(", ")
+                .ToArray();
+
+            string command = Console.ReadLine();
+            int countBlacklistedNames = 0;
+            int countLostNames = 0;
+
+            while (true)
+            {
+                if (command == "Report")
+                {
+                    break;
+                }
+
+                string[] commandToArray = command
+                    .Split()
+                    .ToArray();
+
+                string operation = commandToArray[0];
+
+                if (operation == "Blacklist")
+                {
+                    string name = commandToArray[1];
+
+                    bool blackListed = false;
+
+                    for (int i = 0; i < friendsList.Length; i++)
+                    {
+                        if (friendsList[i] == name)
+                        {
+                            friendsList[i] = "Blacklisted";
+                            blackListed = true;
+                        }
+                    }
+
+                    if (blackListed)
+                    {
+                        Console.WriteLine($"{name} was blacklisted.");
+                        countBlacklistedNames++;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{name} was not found.");
+                    }
+                }
+                else if (operation == "Error")
+                {
+                    int index = int.Parse(commandToArray[1]);
+
+                    if (index >=0 && index < friendsList.Length)
+                    {
+                        if (friendsList[index] != "Blacklisted" &&
+                            friendsList[index] != "Lost")
+                        {
+                            Console.WriteLine($"{friendsList[index]} was lost due to an error.");
+                            friendsList[index] = "Lost";
+                            countLostNames++;
+                        }
+                    }
+                }
+                else if (operation == "Change")
+                {
+                    int index = int.Parse(commandToArray[1]);
+                    string newName = commandToArray[2];
+
+                    if (index >= 0 && index < friendsList.Length)
+                    {
+                        Console.WriteLine($"{friendsList[index]} changed his username to {newName}.");
+                        friendsList[index] = newName;
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Blacklisted names: {countBlacklistedNames}");
+            Console.WriteLine($"Lost names: {countLostNames}");
+            Console.WriteLine(string.Join(' ', friendsList));
         }
     }
 }
