eca91550c3af8d42b4a38f140827ddc0c54b8cb3
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-26 18:14:58 +0300

    Adding Source code for Project - AdvertisementMessage

--- SoftUni Fundamentals Module/6.2 Objects and Classes - Exercise/AdvertisementMessage/Program.cs
+++ SoftUni Fundamentals Module/6.2 Objects and Classes - Exercise/AdvertisementMessage/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.Tracing;
 
 namespace AdvertisementMessage
 {
@@ -6,7 +7,34 @@ namespace AdvertisementMessage
     {
         static void Main(string[] args)
         {
+            int numberOfMessage = int.Parse(Console.ReadLine());
+
+            string[] phrases = { "Excellent product.", "Such a great product.", 
+                "I always use that product.", "Best product of its category.", 
+                "Exceptional product.", "I can’t live without this product." };
+
+            string[] events = { "Now I feel good.", "I have succeeded with this product.", 
+                "Makes miracles. I am happy of the results!", 
+                "I cannot believe but now I feel awesome.", 
+                "Try it yourself, I am very satisfied.", "I feel great!" };
+
+            string[] authors = { "Diana", "Petya", "Stella", "Elena", "Katya", 
+                "Iva", "Annie", "Eva" };
+
+            string[] cities = { "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse" };
+
+
+            Random randomize = new Random();
             
+            for (int i = 1; i <= numberOfMessage; i++)
+            {
+                string randomPhrase = phrases[randomize.Next(0, 6)];
+                string randomEvent = events[randomize.Next(0, 6)];
+                string randomAuthor = authors[randomize.Next(0, 8)];
+                string randomCity = cities[randomize.Next(0, 5)];
+
+                Console.WriteLine($"{randomPhrase} {randomEvent} {randomAuthor} {randomCity}");
+            }
         }
     }
 }
