6d2a23187d00ba279f436859ed7108c3ecf7c915
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-27 11:13:01 +0300

    Adding Source code for Project - Students

--- SoftUni Fundamentals Module/6.2 Objects and Classes - Exercise/Students/Program.cs
+++ SoftUni Fundamentals Module/6.2 Objects and Classes - Exercise/Students/Program.cs
@@ -1,12 +1,57 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Students
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int countOfAllStudents = int.Parse(Console.ReadLine());
+
+            List<Students> students = new List<Students>();
+
+            for (int i = 0; i < countOfAllStudents; i++)
+            {
+                string[] information = Console.ReadLine().Split(' ');
+
+                Students  student = new Students(information[0], information[1],
+                  (information[2]));
+
+                students.Add(student);
+            }
+
+            List<Students> orderByGrade = students.OrderByDescending(student => student.Grade).ToList();
+
+            foreach (Students student in orderByGrade)
+            {
+                Console.WriteLine(student);
+            }
         }
+    }
+
+    class Students
+    {
+        public Students(string firstName, string lastName, string grade)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Grade = grade;
+        }
+        public Students()
+        {
+
+        }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Grade { get; set; }
+
+        public override string ToString() => $"{FirstName} {LastName} {Grade}";
+
+        public List<Students> students { get; set; }
     }
 }
