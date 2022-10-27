9fa7d4043c100d410cd219b26b375486c4caad78
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-27 10:41:42 +0300

    Adding Project - Articles2.0 + Source code

---
+++ SoftUni Fundamentals Module/6.2 Objects and Classes - Exercise/Articles2.0/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Articles2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfArticles = int.Parse(Console.ReadLine());
+
+            List<Articles> articlesInList = new List<Articles>();
+
+            for (int i = 0; i < numberOfArticles; i++)
+            {
+                string[] articles = Console.ReadLine().Split(", ");
+
+                Articles article = new Articles(articles[0], articles[1], articles[2]);
+
+                articlesInList.Add(article);
+            }
+
+            foreach (Articles article in articlesInList)
+            {
+                Console.WriteLine(article);
+            }
+        }
+    }
+
+    class Articles
+    {
+
+        public Articles(string title, string content, string author)
+        {
+            this.Title = title;
+            this.Content = content;
+            this.Author = author;
+        }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; } 
+
+        public string Author { get; set; }
+
+        public override string ToString() => $"{Title} - {Content}: {Author}";
+
+        public List<Articles> Article { get; set; } 
+    }
+}
