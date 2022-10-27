323166558720b9627cfc5d20ad6982d45fee43f6
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-27 11:13:44 +0300

    Adding Project - TeamworkProject

---
+++ SoftUni Fundamentals Module/6.2 Objects and Classes - Exercise/TeamworkProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeamworkProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
