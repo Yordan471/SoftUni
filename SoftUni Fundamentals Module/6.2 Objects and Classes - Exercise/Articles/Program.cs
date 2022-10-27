dcee6e48a63d4a76fbe2b5cd5446808499ffd5cc
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-27 10:23:01 +0300

    Adding Source code for Project - Articles

--- SoftUni Fundamentals Module/6.2 Objects and Classes - Exercise/Articles/Program.cs
+++ SoftUni Fundamentals Module/6.2 Objects and Classes - Exercise/Articles/Program.cs
@@ -1,12 +1,69 @@
 ﻿using System;
+using System.Linq;
 
 namespace Articles
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] readArticle = Console.ReadLine()
+                .Split(", ")
+                .ToArray();
+            int numberOfCommands = int.Parse(Console.ReadLine());
+
+            Articles article = new Articles(readArticle[0], readArticle[1], readArticle[2]);
+
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                string[] command = Console.ReadLine()
+                    .Split(": ")
+                    .ToArray();
+
+                string operation = command[0];
+                string argument = command[1];
+
+                if (operation == "Edit")
+                {
+                    article.EditNewContent(argument);            
+                }
+                else if (operation == "ChangeAuthor")
+                {
+                    article.ChangeAuthor(argument);
+                }
+                else if (operation == "Rename")
+                {
+                    article.RenameTitle(argument);
+                }             
+            }
+
+            Console.WriteLine(article);
         }
+    }
+
+    class Articles
+    {
+        public Articles(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Author { get; set; }
+
+
+        public void EditNewContent(string content) => Content = content;
+
+        public void ChangeAuthor(string author) => Author = author;
+
+        public void RenameTitle(string title) => Title = title;
+
+        public override string ToString() => $"{Title} - {Content}: {Author}";
+
     }
 }
