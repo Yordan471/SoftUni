97fb6b783b10645894cdc7ccf6d5911d1f18d0dd
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-29 11:31:39 +0300

    Project for creating a Rhomb

---
+++ SoftUni Fundamentals Module/RandomLittleProjects/RhombPattern/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace RhombPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int midPoint = number / 2 + 1;
+
+            for (int i = 1; i <= number; i++)
+            {
+                for (int j = 1; j <= number; j++)
+                {
+                    if (j == midPoint || j == number - midPoint + 1)
+                    {
+                        Console.Write("$");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+
+                if (i <= number / 2)
+                {
+                    midPoint--;
+                }
+                else
+                {
+                    midPoint++;
+                }
+
+                Console.WriteLine(" ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
