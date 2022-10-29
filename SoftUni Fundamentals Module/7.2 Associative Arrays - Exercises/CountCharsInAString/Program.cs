469c6d1292db90fa96ff7589552b1800c3ee4a65
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-29 18:27:11 +0300

    Adding Source code for Project - CountCharInString

--- SoftUni Fundamentals Module/7.2 Associative Arrays - Exercises/CountCharsInAString/Program.cs
+++ SoftUni Fundamentals Module/7.2 Associative Arrays - Exercises/CountCharsInAString/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CountCharsInAString
 {
@@ -6,7 +8,31 @@ namespace CountCharsInAString
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] text = Console.ReadLine()
+                .Split(' ')
+                .ToArray();
+
+            Dictionary<char, int> charsCount = new Dictionary<char, int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char[] textToChar = text[i].ToCharArray();
+
+                for (int j = 0; j < text[i].Length; j++)
+                {
+                    if (!charsCount.ContainsKey(textToChar[j]))
+                    {
+                        charsCount.Add(textToChar[j], 0);
+                    }
+
+                    charsCount[textToChar[j]]++;
+                }         
+            }
+
+            foreach (var charectar in charsCount)
+            {
+                Console.WriteLine($"{charectar.Key} -> {charectar.Value}");
+            }
         }
     }
 }
