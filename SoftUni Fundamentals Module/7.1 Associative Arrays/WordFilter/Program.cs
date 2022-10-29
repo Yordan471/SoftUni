2eeed96f1553267922d8c2146b68ae73c417bdb4
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-29 14:25:18 +0300

    Adding Project - WordFilter

---
+++ SoftUni Fundamentals Module/7.1 Associative Arrays/WordFilter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WordFilter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
