214a93a2b2d4745c2b0cd9d4b0cb1fdf95323776
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-29 12:47:12 +0300

    Adding Source code for Project - OddOccurrences

--- SoftUni Fundamentals Module/7.1 Associative Arrays/OddOccurrences/Program.cs
+++ SoftUni Fundamentals Module/7.1 Associative Arrays/OddOccurrences/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace OddOccurrences
 {
@@ -6,7 +8,55 @@ namespace OddOccurrences
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] sequenceOfElements = Console.ReadLine()
+                .Split(' ')
+                .ToArray();
+
+            sequenceOfElementsToLower(sequenceOfElements);
+
+            Dictionary<string, int> keyValuePairs = new Dictionary<string, int>();
+
+            AddingToDictionary(sequenceOfElements, keyValuePairs);
+
+            List<string> oddByOccurrences = new List<string>();
+
+            OddOccurrencesElements(keyValuePairs, oddByOccurrences);
+
+            Console.WriteLine(string.Join(" ", oddByOccurrences));
+        }
+
+        private static void OddOccurrencesElements(Dictionary<string, int> keyValuePairs, List<string> oddByOccurrences)
+        {
+            foreach (KeyValuePair<string, int> keyValue in keyValuePairs)
+            {
+                if (keyValue.Value % 2 != 0)
+                {
+                    oddByOccurrences.Add(keyValue.Key.ToLower());
+                }
+            }
+        }
+
+        private static void AddingToDictionary(string[] sequenceOfElements, Dictionary<string, int> keyValuePairs)
+        {
+            foreach (string element in sequenceOfElements)
+            {
+                if (keyValuePairs.ContainsKey(element))
+                {
+                    keyValuePairs[element]++;
+                }
+                else
+                {
+                    keyValuePairs.Add(element, 1);
+                }
+            }
+        }
+
+        private static void sequenceOfElementsToLower(string[] sequenceOfElements)
+        {
+            for (int i = 0; i < sequenceOfElements.Length; i++)
+            {
+                sequenceOfElements[i] = sequenceOfElements[i].ToLower();
+            }
         }
     }
 }
