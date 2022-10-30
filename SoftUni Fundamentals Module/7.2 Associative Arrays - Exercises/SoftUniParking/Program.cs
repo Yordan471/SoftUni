7063d03d9d976ce8e1e0dc7d1b325186798115ce
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-30 09:58:29 +0200

    Adding Source code for Project - SoftUniParking

--- SoftUni Fundamentals Module/7.2 Associative Arrays - Exercises/SoftUniParking/Program.cs
+++ SoftUni Fundamentals Module/7.2 Associative Arrays - Exercises/SoftUniParking/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace SoftUniParking
 {
@@ -6,7 +8,53 @@ namespace SoftUniParking
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numberOfCommands = int.Parse(Console.ReadLine());
+
+            Dictionary<string, string> registrationInfo = new Dictionary<string, string>();
+
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                string[] commandArgs = Console.ReadLine()
+                    .Split();
+
+                string operation = commandArgs[0];
+                
+                if (operation == "register")
+                {
+                    string username = commandArgs[1];
+                    string licensePlateNumber = commandArgs[2];
+
+                    if (!registrationInfo.ContainsKey(username))
+                    {
+                        registrationInfo.Add(username, licensePlateNumber);
+                        Console.WriteLine($"{username} registered {licensePlateNumber} successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {licensePlateNumber}");
+                    }
+                                     
+                }
+                else if (operation == "unregister")
+                {
+                    string username = commandArgs[1];
+
+                    if (registrationInfo.ContainsKey(username))
+                    {
+                        registrationInfo.Remove(username);
+                        Console.WriteLine($"{username} unregistered successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR: user {username} not found");
+                    }
+                }
+            }
+
+            foreach (KeyValuePair<string, string> username in registrationInfo)
+            {
+                Console.WriteLine($"{username.Key} => {username.Value}");
+            }
         }
     }
 }
