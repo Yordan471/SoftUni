73efa334e284cbb398a4d879c434defe87ce1f13
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-30 09:59:02 +0200

    Adding Project - Courses

---
+++ SoftUni Fundamentals Module/7.2 Associative Arrays - Exercises/Courses/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Courses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
