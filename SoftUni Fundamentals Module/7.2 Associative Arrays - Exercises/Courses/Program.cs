edf11dc67b43468bf274b6841cdc943c46a2f6ae
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-30 11:50:51 +0200

    Adding Source code for Project - Courses

--- SoftUni Fundamentals Module/7.2 Associative Arrays - Exercises/Courses/Program.cs
+++ SoftUni Fundamentals Module/7.2 Associative Arrays - Exercises/Courses/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Courses
 {
@@ -6,7 +7,38 @@ namespace Courses
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] courseStudentsArray = Console.ReadLine()
+                .Split(" : ");
+
+            Dictionary<string, List<string>> coursesAndStudents = new Dictionary<string, List<string>>();
+
+            while (courseStudentsArray[0] != "end")
+            {
+                string courseName = courseStudentsArray[0];
+                string studentName = courseStudentsArray[1];
+
+                if (!coursesAndStudents.ContainsKey(courseName))
+                {
+                    coursesAndStudents.Add(courseName, new List<string>() { studentName });
+                }
+                else
+                {
+                    coursesAndStudents[courseName].Add(studentName);
+                }
+
+                courseStudentsArray = Console.ReadLine()
+                .Split(" : ");
+            }
+               
+            foreach (KeyValuePair<string, List<string>> student in coursesAndStudents)
+            {
+                Console.WriteLine($"{student.Key}: {student.Value.Count}");
+
+                for (int i = 0; i < student.Value.Count; i++)
+                {
+                    Console.WriteLine($"-- {student.Value[i]}");
+                }
+            }
         }
     }
 }
