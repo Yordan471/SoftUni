3cc969e980eb64ee22a7772e2867c1ebbd3ea021
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-30 13:15:36 +0200

    Adding Source code for Project - StudentAcademy

--- SoftUni Fundamentals Module/7.2 Associative Arrays - Exercises/StudentAcademy/Program.cs
+++ SoftUni Fundamentals Module/7.2 Associative Arrays - Exercises/StudentAcademy/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace StudentAcademy
 {
@@ -6,7 +8,37 @@ namespace StudentAcademy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numberOfInputData = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<double>> studentsGrades = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < numberOfInputData; i++)
+            {
+                string studentName = Console.ReadLine();
+                double studentGrade = double.Parse(Console.ReadLine());
+
+                if (!studentsGrades.ContainsKey(studentName))
+                {
+                    studentsGrades.Add(studentName, new List<double>() { studentGrade });
+                }
+                else
+                {
+                    studentsGrades[studentName].Add(studentGrade);
+                }
+            }
+
+            Dictionary<string, double> avrgValues = studentsGrades
+                .ToDictionary(
+                student => student.Key,
+                student => student.Value.Average());
+
+            foreach (KeyValuePair<string, double> student in avrgValues)
+            {
+                if (student.Value >= 4.50)
+                {
+                    Console.WriteLine($"{student.Key} -> {student.Value:f2}");
+                }
+            }          
         }
     }
 }
