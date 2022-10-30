d85edbe1b0d78586b5902d77eff6b6cad132bb98
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-10-30 13:39:43 +0200

    Adding Source code for Project - CompanyUsers

--- SoftUni Fundamentals Module/7.2 Associative Arrays - Exercises/CompanyUsers/Program.cs
+++ SoftUni Fundamentals Module/7.2 Associative Arrays - Exercises/CompanyUsers/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CompanyUsers
 {
@@ -6,7 +8,41 @@ namespace CompanyUsers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] inputInformation = Console.ReadLine()
+                .Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, List<string>> companyEmployeeId = new Dictionary<string, List<string>>();
+
+            while (inputInformation[0] != "End")
+            {
+                string companyName = inputInformation[0];
+                string employeeId = inputInformation[1];
+
+                if (!companyEmployeeId.ContainsKey(companyName))
+                {
+                    companyEmployeeId.Add(companyName, new List<string>() { employeeId });
+                }
+                else
+                {
+                    if (!companyEmployeeId[companyName].Contains(employeeId))
+                    {
+                        companyEmployeeId[companyName].Add(employeeId);
+                    }               
+                }
+
+                inputInformation = Console.ReadLine()
+                    .Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            foreach (KeyValuePair<string, List<string>> employee in companyEmployeeId)
+            {
+                Console.WriteLine($"{employee.Key}");
+
+                for (int i = 0; i < employee.Value.Count; i++)
+                {
+                    Console.WriteLine($"-- {employee.Value[i]}");
+                }
+            }
         }
     }
 }
