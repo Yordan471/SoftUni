35cc8edc05b990669930cc2faeee32c88fef8054
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-02 21:14:56 +0200

    Adding Source code for Project - SnowwhiteDiffSolution

--- SoftUni Fundamentals Module/7.3 Associative Arrays - More Exercises/SnowwhiteDiffSolution/Program.cs
+++ SoftUni Fundamentals Module/7.3 Associative Arrays - More Exercises/SnowwhiteDiffSolution/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SnowwhiteDiffSolution
 {
@@ -6,7 +8,43 @@ namespace SnowwhiteDiffSolution
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, int> nameHatColorAndPhysics =
+                new Dictionary<string, int>();
+
+            string inputInfo = string.Empty;
+
+            while ((inputInfo = Console.ReadLine()) != "Once upon a time")
+            {
+                string[] inputInfoToArray = inputInfo
+                    .Split(" <:> ");
+
+                string name = inputInfoToArray[0];
+                string color = inputInfoToArray[1];
+                int physics = int.Parse(inputInfoToArray[2]);
+
+                string nameAndColor = name + "-" + color;
+
+                if (!nameHatColorAndPhysics.ContainsKey(nameAndColor))
+                {
+                    nameHatColorAndPhysics.Add(nameAndColor, physics);
+                }
+                else
+                {
+                    nameHatColorAndPhysics[nameAndColor] =
+                        Math.Max(nameHatColorAndPhysics[nameAndColor], physics);
+                }
+            }
+
+            foreach (var dwarf in nameHatColorAndPhysics
+                .OrderByDescending(x => x.Value)
+                .ThenByDescending(x => nameHatColorAndPhysics
+                .Where(y => y.Key.Split("-")[1] == x.Key.Split("-")[1])
+                .Count()))
+            {
+                Console.WriteLine($"({dwarf.Key.Split("-")[1]}) " +
+                    $"{dwarf.Key.Split("-")[0]} " +
+                    $"{dwarf.Value}");
+            }        
         }
     }
 }
