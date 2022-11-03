e8211eddd4fc07e82dad47a427eaadb5c3187651
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-03 18:57:25 +0200

    Adding Source code for Project - ReverseStrings

--- SoftUni Fundamentals Module/8.1 Text Processing - Lab/ReverseStrings/Program.cs
+++ SoftUni Fundamentals Module/8.1 Text Processing - Lab/ReverseStrings/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ReverseStrings
 {
@@ -6,7 +8,23 @@ namespace ReverseStrings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string word = string.Empty;
+            Dictionary<string, string> wordAndReverseWord = 
+                new Dictionary<string, string>();
+
+            while ((word = Console.ReadLine()) != "end")
+            {
+                string reverseString = string.Empty; 
+                
+                char[] chars = word.ToCharArray();
+                Array.Reverse(chars);
+
+                reverseString = new string(chars);
+                wordAndReverseWord.Add(word, reverseString);          
+            }
+
+            foreach (var wordd in wordAndReverseWord)
+            Console.WriteLine($"{wordd.Key} = {wordd.Value}");
         }
     }
 }
