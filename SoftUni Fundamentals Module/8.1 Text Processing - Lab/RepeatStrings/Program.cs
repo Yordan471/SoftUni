fc0399d71ec1aace3d363f0f13f5d309b7f49cef
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-03 19:08:08 +0200

    Adding Source code for Project - RepeatStrings

--- SoftUni Fundamentals Module/8.1 Text Processing - Lab/RepeatStrings/Program.cs
+++ SoftUni Fundamentals Module/8.1 Text Processing - Lab/RepeatStrings/Program.cs
@@ -6,7 +6,23 @@ namespace RepeatStrings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string words = Console.ReadLine();
+
+            string[] wordsToArray = words.Split();
+            string result = string.Empty;
+
+            for (int i = 0; i < wordsToArray.Length; i++)
+            {
+                for (int r = 0; r < wordsToArray[i].Length; r++)
+                {
+                    for (int j = 0; j < wordsToArray[i].Length; j++)
+                    {
+                        result += wordsToArray[i][j];
+                    }
+                }              
+            }
+
+            Console.WriteLine(result);
         }
     }
 }
