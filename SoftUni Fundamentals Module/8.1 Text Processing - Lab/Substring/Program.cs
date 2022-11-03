083f5d34e641ba48bb124ab14f975d5db75bb360
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-03 20:19:38 +0200

    Adding Source code for Project - Substring

---
+++ SoftUni Fundamentals Module/8.1 Text Processing - Lab/Substring/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string wordToRemove = Console.ReadLine();
+            string word = Console.ReadLine();
+            string result = string.Empty;
+            int index = word.IndexOf(wordToRemove);
+
+            while (word.Contains(wordToRemove))
+            {          
+                word = word.Remove(index, wordToRemove.Length);
+                index = word.IndexOf(wordToRemove);
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
