72eac716d3a609bfac4efea3ab947df41bb3a4e9
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-04 22:57:46 +0200

    Adding Source code for Project - TextFiltering

--- SoftUni Fundamentals Module/8.1 Text Processing - Lab/TextFiltering/Program.cs
+++ SoftUni Fundamentals Module/8.1 Text Processing - Lab/TextFiltering/Program.cs
@@ -6,7 +6,20 @@ namespace TextFiltering
     {
         static void Main(string[] args)
         {
-            
+            string[] banWords = Console.ReadLine()
+                .Split(", ");
+            string text = Console.ReadLine();
+
+            foreach (string word in banWords)
+            {
+                if (text.Contains(word))
+                {
+                    text = text.Replace(word, new string('*',
+                        word.Length));
+                }
+            }
+
+            Console.WriteLine(text);
         }
     }
 }
