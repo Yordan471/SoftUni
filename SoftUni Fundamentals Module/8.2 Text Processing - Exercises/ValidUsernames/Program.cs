205f7c12d38226663697d59040b0b64d258efb43
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-05 15:00:23 +0200

    Adding Source code for Project - ValidUsername

--- SoftUni Fundamentals Module/8.2 Text Processing - Exercises/ValidUsernames/Program.cs
+++ SoftUni Fundamentals Module/8.2 Text Processing - Exercises/ValidUsernames/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace ValidUsernames
 {
@@ -6,7 +9,47 @@ namespace ValidUsernames
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string readNames = Console.ReadLine();
+
+            string[] readNamesToArray = readNames
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            List<string> wordsToPrint = new List<string>();
+            bool isTrue = false;
+
+            foreach (string name in readNamesToArray)
+            {
+                char[] chars = name.ToCharArray();
+
+                if (chars.Length > 3 && chars.Length < 16)
+                {
+                    for (int i = 0; i < chars.Length; i++)
+                    {
+                        if ((chars[i] >= 48 && chars[i] <= 57) ||
+                            (chars[i] >= 65 && chars[i] <= 90) ||
+                            (chars[i] >= 97 && chars[i] <= 122) ||
+                            chars[i] == 45 || chars[i] == 95)
+                        {
+                            isTrue = true;
+                        }
+                        else
+                        {
+                            isTrue = false;
+                            break;
+                        }
+                    }
+
+                    if (isTrue)
+                    {
+                        wordsToPrint.Add(name);
+                    }
+                }            
+            }
+
+            foreach (string word in wordsToPrint)
+            {
+                Console.WriteLine(word);
+            }
         }
     }
 }
