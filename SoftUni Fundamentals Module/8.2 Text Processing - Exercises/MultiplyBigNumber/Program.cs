94a6a5b4626db368541d4fcd9d4f6717f46fcaeb
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-05 16:20:29 +0200

    Adding Project - MultiplyBigNumber

---
+++ SoftUni Fundamentals Module/8.2 Text Processing - Exercises/MultiplyBigNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultiplyBigNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
