ce756595ab3f3e148fb74669c7db1d9168422734
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-05 17:54:51 +0200

    Adding Project - ReplaceRepeatingChars

---
+++ SoftUni Fundamentals Module/8.2 Text Processing - Exercises/ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReplaceRepeatingChars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
