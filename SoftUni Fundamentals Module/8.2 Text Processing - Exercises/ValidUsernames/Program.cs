8e71171d1b6d9a59dd9e6226e22a6efb0449aa8e
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-05 12:05:26 +0200

    Adding folder  8.2 Text Processing - Exercises; Adding Project - ValidUsername

---
+++ SoftUni Fundamentals Module/8.2 Text Processing - Exercises/ValidUsernames/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ValidUsernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
