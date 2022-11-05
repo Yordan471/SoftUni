de9d2185ec316c621cf4c73ada78e9a579110ed7
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-05 18:49:23 +0200

    Adding Source code for Project - ReplaceRepeatingChars

--- SoftUni Fundamentals Module/8.2 Text Processing - Exercises/ReplaceRepeatingChars/Program.cs
+++ SoftUni Fundamentals Module/8.2 Text Processing - Exercises/ReplaceRepeatingChars/Program.cs
@@ -6,7 +6,44 @@ namespace ReplaceRepeatingChars
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string letters = Console.ReadLine();
+            char[] lettersToChars = letters.ToCharArray();
+            int count = 0;
+
+            for (int i = 0; i < letters.Length - 1; i++)
+            {
+                char letter = letters[i];
+                string sequenceToRemove = string.Empty;
+
+                for (int j = 0; j < letters.Length - 1; j++)
+                {        
+                    if (j + count == letters.Length - 1)
+                    {
+                        sequenceToRemove += letter;
+                        letters = letters.Replace(sequenceToRemove, letter.ToString());
+                        break;
+                    }
+
+                    if (letter == letters[j + count])
+                    {
+                        sequenceToRemove += letter;
+
+                        if (j == letters.Length - 2)
+                        {
+                            letters = letters.Replace(sequenceToRemove, letter.ToString());
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        count++;
+                        letters = letters.Replace(sequenceToRemove, letter.ToString());                      
+                        break;
+                    }    
+                }
+            }
+
+            Console.WriteLine(letters);
         }
     }
 }
