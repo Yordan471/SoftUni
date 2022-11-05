766b405d46304b33cd8923a86ba50e422166aaed
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-05 15:04:27 +0200

    Adding Project - CharacterMultiplier

---
+++ SoftUni Fundamentals Module/8.2 Text Processing - Exercises/CharacterMultiplier/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CharacterMultiplier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
