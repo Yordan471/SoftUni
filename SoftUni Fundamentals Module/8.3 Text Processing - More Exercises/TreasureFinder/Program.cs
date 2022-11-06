6393f47eb4f5423747bdaf62de5a1d6e2ad66b9b
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-06 10:40:41 +0200

    Adding Project - TreasureFinder

---
+++ SoftUni Fundamentals Module/8.3 Text Processing - More Exercises/TreasureFinder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TreasureFinder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
