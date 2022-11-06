7d37625e1e1b8e2ccd082a058e05d5cc6274cce6
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-06 12:05:22 +0200

    Adding Source code for Project - TreaureFinder

--- SoftUni Fundamentals Module/8.3 Text Processing - More Exercises/TreasureFinder/Program.cs
+++ SoftUni Fundamentals Module/8.3 Text Processing - More Exercises/TreasureFinder/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TreasureFinder
 {
@@ -6,7 +8,52 @@ namespace TreasureFinder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] key = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+
+            List<string> gatherDecryptedSeq = new List<string>();
+            List<string> arrangedSequences = new List<string>();
+            string decryptedSequence = string.Empty;
+            string command = string.Empty;
+            int counter = 0;
+
+            while ((command = Console.ReadLine()) != "find")
+            {
+                for (int i = 0; i < command.Length; i++)
+                {
+                    if (counter == key.Length)
+                    {
+                        counter = 0;
+                    }
+
+                    decryptedSequence += (char)(command[i] - key[counter]);
+                    counter++;
+                }
+
+                gatherDecryptedSeq.Add(decryptedSequence);
+                decryptedSequence = string.Empty;
+                counter = 0;
+            }
+
+            for (int i = 0; i < gatherDecryptedSeq.Count; i++)
+            {
+                int firstSymbolFirst = gatherDecryptedSeq[i].IndexOf('&');
+                int firstSymbolLast = gatherDecryptedSeq[i].LastIndexOf('&');
+                int typeLength = firstSymbolLast - firstSymbolFirst;
+
+                int secondSymbolStart = gatherDecryptedSeq[i].IndexOf('<');
+                int secondSymbolEnd = gatherDecryptedSeq[i].IndexOf('>');
+                int coordinatesLength = secondSymbolEnd - secondSymbolStart;
+
+                string type = gatherDecryptedSeq[i].Substring(firstSymbolFirst + 1, typeLength - 1);
+                string coordinates = gatherDecryptedSeq[i].Substring(secondSymbolStart + 1, coordinatesLength - 1);
+
+                arrangedSequences.Add($"Found {type} at {coordinates}");
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, arrangedSequences));
         }
     }
 }
