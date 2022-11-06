19f90d92e33b8842235df26115db2820675a07e7
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-06 10:39:57 +0200

    Adding Source code for Project - AsciiSumator

--- SoftUni Fundamentals Module/8.3 Text Processing - More Exercises/AsciiSumator/Program.cs
+++ SoftUni Fundamentals Module/8.3 Text Processing - More Exercises/AsciiSumator/Program.cs
@@ -6,7 +6,31 @@ namespace AsciiSumator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+            string sequence = Console.ReadLine();
+
+            int sum = 0;
+
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                if (firstChar > secondChar)
+                {
+                    if (sequence[i] > secondChar && sequence[i] < firstChar)
+                    {
+                        sum += char.Parse(sequence[i].ToString());
+                    }
+                }
+                else
+                {
+                    if (sequence[i] < secondChar && sequence[i] > firstChar)
+                    {
+                        sum += char.Parse(sequence[i].ToString());
+                    }
+                }            
+            }
+
+            Console.WriteLine(sum);
         }
     }
 }
