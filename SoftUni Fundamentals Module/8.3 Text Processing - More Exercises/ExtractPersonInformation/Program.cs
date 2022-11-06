92960ef6559d7a26f0c713cde73fa317f323cd83
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-06 10:26:55 +0200

    Adding Source code for Project - ExtractPeronalInformation

--- SoftUni Fundamentals Module/8.3 Text Processing - More Exercises/ExtractPersonInformation/Program.cs
+++ SoftUni Fundamentals Module/8.3 Text Processing - More Exercises/ExtractPersonInformation/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ExtractPersonInformation
 {
@@ -6,7 +8,32 @@ namespace ExtractPersonInformation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numberOfLines = int.Parse(Console.ReadLine());
+            List<string> gatherInfo = new List<string>();
+            char[] separatos = { '@', '|', '#', '*' };
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                string information = Console.ReadLine();
+
+                int firstSymbol = information.LastIndexOf(separatos[0]);
+                int secondSymbol = information.IndexOf(separatos[1]);
+                int lengthName = secondSymbol - firstSymbol;
+
+                int thirdSymbol = information.LastIndexOf(separatos[2]);
+                int fourthSymbol = information.IndexOf(separatos[3]);
+                int lengthAge = fourthSymbol - thirdSymbol;
+
+                string name = information.Substring(firstSymbol + 1, lengthName - 1);
+                string age = information.Substring(thirdSymbol + 1, lengthAge - 1);
+
+                gatherInfo.Add($"{name} is {age} years old.");
+            }
+
+            foreach (string information in gatherInfo)
+            {
+                Console.WriteLine(information);
+            }
         }
     }
 }
