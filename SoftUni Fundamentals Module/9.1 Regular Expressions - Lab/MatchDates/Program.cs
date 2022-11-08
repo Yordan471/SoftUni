9c55d95c2873b414b8c1954fd2ffeb554ae35a3a
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-08 20:57:26 +0200

    Adding Source code for Project - MatchDates

--- SoftUni Fundamentals Module/9.1 Regular Expressions - Lab/MatchDates/Program.cs
+++ SoftUni Fundamentals Module/9.1 Regular Expressions - Lab/MatchDates/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace MatchDates
 {
@@ -6,7 +7,22 @@ namespace MatchDates
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string pattern = @"\b(?<day>[0-9]{2})(?<separator>[-.\/])(?<month>[A-Z][a-z]{2})\k<separator>(?<year>[0-9]{4})\b";
+
+            string dates = Console.ReadLine();
+
+            MatchCollection matchDates = Regex.Matches(dates, pattern);
+
+            foreach (Match match in matchDates)
+            {
+                string day = match.Groups["day"].Value;
+                string month = match.Groups["month"].Value;
+                string year = match.Groups["year"].Value;
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+
+            
         }
     }
 }
