fd3bde3bdf81106545bbec8979af98eb305c711e
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-09 19:02:51 +0200

    Adding Source code for Project - Furniture

--- SoftUni Fundamentals Module/9.2 Regular Expressions - Exercises/Furniture/Program.cs
+++ SoftUni Fundamentals Module/9.2 Regular Expressions - Exercises/Furniture/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Furniture
 {
@@ -6,7 +10,31 @@ namespace Furniture
     {
         static void Main(string[] args)
         {
+            string command = string.Empty;
+            string pattern = @">>(?<furniture>[A-Za-z\s]+)<<(?<price>\d+(.\d+)?)!(?<quantity>[0-9]+)\b";
+
+            List<string> furnitures = new List<string>();
+            double sum = 0.0;
             
+            while ((command = Console.ReadLine()) != "Purchase")
+            {
+                Match match = Regex.Match(command, pattern, RegexOptions.IgnoreCase);
+
+                if (match.Success)
+                {
+                    sum += double.Parse(match.Groups["price"].Value) * int.Parse(match.Groups["quantity"].Value);
+                    furnitures.Add(match.Groups["furniture"].Value);
+                }
+            }
+
+            Console.WriteLine("Bought furniture:");
+
+            if (furnitures.Count > 0)
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, furnitures));
+            }
+                       
+            Console.WriteLine($"Total money spend: {sum:f2}");
         }
     }
 }
