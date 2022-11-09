9fdf16e99211fcc86cfddf3d30fcc8ffab6a8f02
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-09 20:31:33 +0200

    Adding Source code for Project - Race

--- SoftUni Fundamentals Module/9.2 Regular Expressions - Exercises/Race/Program.cs
+++ SoftUni Fundamentals Module/9.2 Regular Expressions - Exercises/Race/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Race
 {
@@ -6,7 +9,83 @@ namespace Race
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] names = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            string command = string.Empty;
+            string patter = @"[\w]";
+
+            Match match;
+            int sum = 0;
+            string name = string.Empty;
+            Dictionary<string, int> nameAndSum = new Dictionary<string, int>();
+
+            while ((command = Console.ReadLine()) != "end of race")
+            {
+                match = Regex.Match(command, patter);
+
+                if (match.Success)
+                {
+                    char[] chars = command.ToCharArray();
+
+                    for (int i = 0; i < chars.Length; i++)
+                    {
+                        if (char.IsDigit(chars[i]))
+                        {
+                            sum += int.Parse(chars[i].ToString());
+                        }
+                        else if (char.IsLetter(chars[i]))
+                        {
+                            name += chars[i];
+                        }
+                    }
+                }
+
+                if (!nameAndSum.ContainsKey(name))
+                {
+                    nameAndSum.Add(name, 0);
+                }
+
+                nameAndSum[name] += sum;
+                sum = 0;
+                name = string.Empty;
+            }
+
+            Dictionary<string, int> orderedDic = nameAndSum
+                .OrderByDescending(x => x.Value)                
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            int count = 0;
+
+            foreach (KeyValuePair<string, int> pair in orderedDic)
+            {
+
+                for (int i = 0; i < names.Length; i++)
+                {
+                    if (names[i] == pair.Key)
+                    {
+                        count++;
+                        if (count == 1)
+                        {
+                            Console.WriteLine($"{count}st place: {pair.Key}");
+                        }
+                        else if(count == 2)
+                        {
+                            Console.WriteLine($"{count}nd place: {pair.Key}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{count}rd place: {pair.Key}");
+                        }
+                        
+                    }
+                }   
+
+                if (count == 3)
+                {
+                    break;
+                }
+            }
         }
     }
 }
