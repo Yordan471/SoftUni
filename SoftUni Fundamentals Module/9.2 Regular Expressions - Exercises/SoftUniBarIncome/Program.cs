4c20a31e443582c9936cd16c0d2e2b127c6b0280
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-10 23:21:22 +0200

    Update Program.cs

--- SoftUni Fundamentals Module/9.2 Regular Expressions - Exercises/SoftUniBarIncome/Program.cs
+++ SoftUni Fundamentals Module/9.2 Regular Expressions - Exercises/SoftUniBarIncome/Program.cs
@@ -8,6 +8,7 @@ namespace SoftUniBarIncome
         {
             string pattern = @"(\w+)?%[A-Z][a-z]+\1?%\1?<\1?[A-Z][a-z]+\1?>([a-z]+)?\|\1?[0-9]+\1?\|\2?[0-9]+(\.[0-9]+)?\$";
 
+            string command = Console.ReadLine();
         }
     }
 }
