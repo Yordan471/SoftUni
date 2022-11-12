5b36d02bb3a01fc5fcce2f1e934036735bf4f7d1
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-12 16:58:00 +0200

    Adding Project - PlantDiscovery

---
+++ SoftUni Fundamentals Module/2. Programming Fundamentals - Practice Final Exam/PlantDiscovery/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlantDiscovery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
