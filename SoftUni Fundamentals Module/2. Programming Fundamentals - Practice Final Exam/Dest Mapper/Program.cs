d596e9cd2327ff0a71916445a03067ccf0884084
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-12 16:56:33 +0200

    Adding Source code for Project - Dest Mapper

--- SoftUni Fundamentals Module/2. Programming Fundamentals - Practice Final Exam/Dest Mapper/Program.cs
+++ SoftUni Fundamentals Module/2. Programming Fundamentals - Practice Final Exam/Dest Mapper/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace Dest_Mapper
 {
@@ -6,7 +9,38 @@ namespace Dest_Mapper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string pattern = @"([=\/]{1})(?<location>[A-Z][a-z]{2,})\1";
+
+            string inputInfo = Console.ReadLine();
+            List<string> matchList = new List<string>();
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(inputInfo);
+
+            int travelPoints = 0;
+
+            foreach (Match match in matches)
+            {
+                if (match.Success)
+                {
+                    matchList.Add(match.Groups["location"].Value);
+                    travelPoints += match.Groups["location"].Value.Length;
+                }           
+            }
+            
+            if (matchList.Count > 0)
+            {
+                Console.Write("Destinations: ");
+                Console.WriteLine(string.Join(", ", matchList));
+            }
+            else
+            {
+                Console.WriteLine("Destinations:");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Travel points: {travelPoints}");
         }
     }
 }
