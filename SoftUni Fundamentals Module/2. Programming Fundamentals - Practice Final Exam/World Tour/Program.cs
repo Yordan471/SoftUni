805e487b77879ff0ce4411be6844349419a4f7c6
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-12 16:20:05 +0200

    Adding Source code for Project - World Tour

--- SoftUni Fundamentals Module/2. Programming Fundamentals - Practice Final Exam/World Tour/Program.cs
+++ SoftUni Fundamentals Module/2. Programming Fundamentals - Practice Final Exam/World Tour/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace World_Tour
 {
@@ -6,7 +8,63 @@ namespace World_Tour
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string tourStops = Console.ReadLine();
+            string[] sep = { "::", "-", ":" };
+            string[] inputToArray = tourStops
+                .Split(sep, StringSplitOptions.RemoveEmptyEntries);
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "Travel")
+            {
+                string[] commandToArray = command
+                    .Split(':');
+
+                string operation = commandToArray[0];
+
+                if (operation == "Add Stop")
+                {
+                    int index = int.Parse(commandToArray[1]);
+                    string substring = commandToArray[2];
+
+                    if (index >= 0 && index <= tourStops.Length)
+                    {
+                        tourStops = tourStops.Insert(index, substring);
+                        Console.WriteLine(tourStops);
+                    }         
+                }
+                else if (operation == "Remove Stop")
+                {
+                    int startIndex = int.Parse(commandToArray[1]);
+                    int endIndex = int.Parse(commandToArray[2]);
+
+                    if ((startIndex >= 0 && startIndex <= tourStops.Length) &&
+                        (endIndex >= 0 && endIndex <= tourStops.Length))
+                    {
+                        int length = endIndex - startIndex;
+                        tourStops = tourStops.Remove(startIndex, length + 1);
+                        Console.WriteLine(tourStops);
+                    }
+                }
+                else
+                {
+                    string oldString = commandToArray[1];
+                    string newString = commandToArray[2];
+                    string[] separator = { "::", "-", ":" };
+                    List<string> tourToList = tourStops
+                        .Split(separator, StringSplitOptions.RemoveEmptyEntries)
+                        .ToList();
+
+                    if (inputToArray.Contains(oldString))
+                    {
+                        tourStops = tourStops.Replace(oldString, newString);
+                        Console.WriteLine(tourStops);
+                    }
+    
+                }
+            }
+
+            Console.WriteLine($"Ready for world tour! Planned stops: {tourStops}");
         }
     }
 }
