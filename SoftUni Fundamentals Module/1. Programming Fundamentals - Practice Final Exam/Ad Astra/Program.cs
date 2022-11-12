4700416f03e50df6fada4a948dfd6028e963c5b4
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-12 13:16:36 +0200

    Adding Source code for Project - Ad Astra

--- SoftUni Fundamentals Module/1. Programming Fundamentals - Practice Final Exam/Ad Astra/Program.cs
+++ SoftUni Fundamentals Module/1. Programming Fundamentals - Practice Final Exam/Ad Astra/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
 
 namespace Ad_Astra
 {
@@ -6,7 +8,36 @@ namespace Ad_Astra
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string foodInformation = Console.ReadLine();
+
+            string pattern = @"([#|])(?<itemName>[A-Z][a-z]+( ([A-Z][a-z]+))?)\1(?<expirationDate>[0-9]{2}\/[0-9]{2}\/[0-9]{2})\1(?<calories>\d+)\1";
+            int sumCalories = 0;
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(foodInformation);
+
+            foreach (Match match in matches)
+            {
+                if (match.Success)
+                {
+                    sumCalories += int.Parse(match.Groups["calories"].Value);
+                }
+            }
+
+            int daysWithFood = sumCalories / 2000;
+
+            Console.WriteLine($"You have food to last you for: {daysWithFood} days!");
+
+            foreach (Match match in matches)
+            {
+                if (match.Success)
+                {
+                    string itemName = match.Groups["itemName"].Value;
+                    string expirationDate = match.Groups["expirationDate"].Value;
+                    string calories = match.Groups["calories"].Value;
+                    Console.WriteLine($"Item: {itemName}, Best before: {expirationDate}, Nutrition: {calories}");
+                }               
+            }
         }
     }
 }
