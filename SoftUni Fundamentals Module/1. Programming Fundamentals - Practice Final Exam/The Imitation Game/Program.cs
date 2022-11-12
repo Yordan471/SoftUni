55b4cabbebabab873b68988d248ac561f9c16ea1
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-12 12:44:12 +0200

    Adding Source code for Project - The Imitation Game

--- SoftUni Fundamentals Module/1. Programming Fundamentals - Practice Final Exam/The Imitation Game/Program.cs
+++ SoftUni Fundamentals Module/1. Programming Fundamentals - Practice Final Exam/The Imitation Game/Program.cs
@@ -6,7 +6,42 @@ namespace The_Imitation_Game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string encryptedMessage = Console.ReadLine();
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "Decode")
+            {
+                string[] commandToArray = command
+                    .Split('|');
+
+                string operation = commandToArray[0];
+
+                if (operation == "Move")
+                {
+                    int numberOfLetters = int.Parse(commandToArray[1]);
+
+                    string moveLetters = encryptedMessage.Substring(0, numberOfLetters);
+                    encryptedMessage = encryptedMessage.Remove(0, numberOfLetters);
+                    encryptedMessage += moveLetters;
+                }
+                else if (operation == "Insert")
+                {
+                    int index = int.Parse(commandToArray[1]);
+                    string value = commandToArray[2];
+
+                    encryptedMessage = encryptedMessage.Insert(index, value);
+                }
+                else if (operation == "ChangeAll")
+                {
+                    string substring = commandToArray[1];
+                    string replacement = commandToArray[2];
+
+                    encryptedMessage = encryptedMessage.Replace(substring, replacement);
+                }
+            }
+
+            Console.WriteLine($"The decrypted message is: {encryptedMessage}");
         }
     }
 }
