5eb3d7a7b906b31bc9b3d24ab44852d11f7ab144
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-13 17:11:43 +0200

    Adding Source code for Project - Activation Keys

--- SoftUni Fundamentals Module/5. Programming Fundamentals - Practice FE/Activation Keys/Program.cs
+++ SoftUni Fundamentals Module/5. Programming Fundamentals - Practice FE/Activation Keys/Program.cs
@@ -6,7 +6,61 @@ namespace Activation_Keys
     {
         static void Main(string[] args)
         {
-            
+            string rawActivationKey = Console.ReadLine();
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "Generate")
+            {
+                string[] commandToArray = command
+                    .Split(">>>");
+
+                string operation = commandToArray[0];
+
+                if (operation == "Contains")
+                {
+                    string substring = commandToArray[1];
+
+                    if (rawActivationKey.Contains(substring))
+                    {
+                        Console.WriteLine($"{rawActivationKey} contains {substring}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Substring not found!");
+                    }
+                }
+                else if (operation == "Flip")
+                {
+                    string upperOrLower = commandToArray[1];
+                    int startIndex = int.Parse(commandToArray[2]);
+                    int endIndex = int.Parse(commandToArray[3]);   
+                    
+                    string substring = rawActivationKey.Substring(startIndex, endIndex - startIndex);
+
+                    if (upperOrLower == "Upper")
+                    {
+                        rawActivationKey = rawActivationKey.Replace(substring, substring.ToUpper());
+                    }
+                    else if (upperOrLower == "Lower")
+                    {
+                        rawActivationKey = rawActivationKey.Replace(substring, substring.ToLower());
+                    }
+
+                    Console.WriteLine(rawActivationKey);
+                }
+                else if (operation == "Slice")
+                {
+                    int startIndex = int.Parse(commandToArray[1]);
+                    int endIndex = int.Parse(commandToArray[2]);
+
+                    rawActivationKey = rawActivationKey.Remove(startIndex, endIndex - startIndex);
+
+                    Console.WriteLine(rawActivationKey);
+                }
+            }
+
+            Console.WriteLine($"Your activation key is: {rawActivationKey}");
         }
     }
 }
