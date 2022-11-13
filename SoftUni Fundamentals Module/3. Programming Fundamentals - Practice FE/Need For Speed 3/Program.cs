2383aa96c13565b4de618ea727d0548565989d22
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-13 10:23:14 +0200

    Adding Project - Need For Speed 3

---
+++ SoftUni Fundamentals Module/3. Programming Fundamentals - Practice FE/Need For Speed 3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Need_For_Speed_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
