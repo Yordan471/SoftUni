f7edd651cd88d63a20de31cfceecd0436105be7a
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-13 09:38:30 +0200

    Adding Source code for Project - Secret Chat; Adding Project - Mirror Words

---
+++ SoftUni Fundamentals Module/3. Programming Fundamentals - Practice FE/3. Programming Fundamentals - Practice FE/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+
+namespace Secret_Chat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string encryptedMessage = Console.ReadLine();
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "Reveal")
+            {
+                string[] commandToArray = command
+                    .Split(":|:");
+
+                string operation = commandToArray[0];
+
+                if (operation == "InsertSpace")
+                {
+                    int index = int.Parse(commandToArray[1]);
+
+                    encryptedMessage = encryptedMessage.Insert(index, " ");
+
+                    Console.WriteLine(encryptedMessage);
+                }
+                else if (operation == "Reverse")
+                {
+                    string substring = commandToArray[1];
+                    
+                    if (encryptedMessage.Contains(substring))
+                    {
+                        int index = encryptedMessage.IndexOf(substring);
+
+                        encryptedMessage = encryptedMessage.Replace(substring, string.Empty);
+                        
+                        char[] chars = substring.ToCharArray();
+                        Array.Reverse(chars);
+
+                        string reversed = string.Empty;
+
+                        foreach (char ch in chars)
+                        {
+                            reversed+= ch;  
+                        }
+
+                        encryptedMessage += reversed;
+                    }
+                    else
+                    {
+                        Console.WriteLine("error");
+                    }
+
+                    Console.WriteLine(encryptedMessage);
+                }
+                else if (operation == "ChangeAll")
+                {
+                    string substring = commandToArray[1];
+                    string replacement = commandToArray[2];
+
+                    if (encryptedMessage.Contains(substring))
+                    {
+                        int index = encryptedMessage.IndexOf(substring);
+
+                        while (encryptedMessage.Contains(substring))
+                        {
+                            encryptedMessage = encryptedMessage.Replace(substring, replacement);
+                        }
+                    }
+
+                    Console.WriteLine(encryptedMessage);
+                }
+            }
+
+            Console.WriteLine($"You have a new message: {encryptedMessage}");
+        }
+    }
+}
---
+++ SoftUni Fundamentals Module/3. Programming Fundamentals - Practice FE/Mirror Words/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mirror_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
