29239d711f151dcbc6804d22a867305bd9264e71
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-13 12:48:36 +0200

    Adding Source code for Project - Password Reset

--- SoftUni Fundamentals Module/4. Programming Fundamentals - Practice FE/Password Reset/Program.cs
+++ SoftUni Fundamentals Module/4. Programming Fundamentals - Practice FE/Password Reset/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Password_Reset
 {
@@ -6,7 +7,56 @@ namespace Password_Reset
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string inputString = Console.ReadLine();
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "Done")
+            {
+                string[] commandToArray = command
+                    .Split(' ');
+
+                string operation = commandToArray[0];
+
+                if (operation == "TakeOdd")
+                {
+                    StringBuilder rawPassword = new StringBuilder();
+
+                    for (int i = 1; i < inputString.Length; i += 2)
+                    {
+                        rawPassword.Append(inputString[i]);
+                    }
+
+                    inputString = rawPassword.ToString();
+
+                    Console.WriteLine(inputString);
+                }
+                else if (operation == "Cut")
+                {
+                    int index = int.Parse(commandToArray[1]);
+                    int length = int.Parse(commandToArray[2]);
+
+                    inputString = inputString.Remove(index, length);
+                    Console.WriteLine(inputString);
+                }
+                else if (operation == "Substitute")
+                {
+                    string substring = commandToArray[1];
+                    string substitute = commandToArray[2];
+
+                    if (inputString.Contains(substring))
+                    {
+                        inputString = inputString.Replace(substring, substitute);
+                        Console.WriteLine(inputString);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nothing to replace");
+                    }
+                }                   
+            }
+
+            Console.WriteLine($"Your password is: {inputString}");
         }
     }
 }
