005a9a28a3f4e4134ad2a300de8dc2bd5732eede
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-13 13:27:41 +0200

    Adding Source code for Project - Fancy Barrcode

--- SoftUni Fundamentals Module/4. Programming Fundamentals - Practice FE/Fancy Barcodes/Program.cs
+++ SoftUni Fundamentals Module/4. Programming Fundamentals - Practice FE/Fancy Barcodes/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Fancy_Barcodes
 {
@@ -6,7 +8,52 @@ namespace Fancy_Barcodes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number = int.Parse(Console.ReadLine());
+
+            string pattern = @"@#+(?<product>[A-Za-z0-9]+[A-Z])@#+";
+
+            Regex regex= new Regex(pattern);
+            List<string> productGroup = new List<string>();
+
+            for (int i = 0; i < number; i++)
+            {
+                string barrcode = Console.ReadLine();
+
+                Match match= regex.Match(barrcode);
+
+                if (match.Success)
+                {
+                    char[] productToChars = match.Groups["product"]
+                        .Value
+                        .ToCharArray();
+
+                    string group = string.Empty;
+                    bool isDigit = false;
+
+                    foreach (char c in productToChars)
+                    {
+                        if (char.IsDigit(c))
+                        {
+                            isDigit= true;
+                            group += c;
+                        }
+                    }
+
+                    if (!isDigit)
+                    {
+                        group = "00";
+                    }
+
+                    productGroup.Add($"Product group: {group}");
+                }
+                else
+                {
+                    productGroup.Add("Invalid barcode");
+                }
+            }
+
+            
+            Console.WriteLine(string.Join(Environment.NewLine, productGroup));
         }
     }
 }
