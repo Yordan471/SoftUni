85b04975fd575ed6caa4d0383016fb267ca733e4
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-16 18:09:23 +0200

    Update Program.cs

--- SoftUni Fundamentals Module/Regular Expressions - More Exercises/Winning Ticket/Program.cs
+++ SoftUni Fundamentals Module/Regular Expressions - More Exercises/Winning Ticket/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Winning_Ticket
 {
@@ -6,7 +8,25 @@ namespace Winning_Ticket
     {
         static void Main(string[] args)
         {
-            
+            string pattern = @"(?<ticket>[!-+\--~]+)([, ]+)?";
+
+            string tickets = Console.ReadLine();
+
+            MatchCollection matches = Regex.Matches(tickets, pattern);
+            List<string> result = new List<string>();
+
+            foreach (Match match in matches)
+            {
+                if (match.Success)
+                {
+                    char[] chars = match.Groups["ticket"].Value.ToCharArray();
+
+                    for (int i = 0; i < chars.Length; i++)
+                    {
+                        if (c)
+                    }
+                }
+            }
         }
     }
 }
