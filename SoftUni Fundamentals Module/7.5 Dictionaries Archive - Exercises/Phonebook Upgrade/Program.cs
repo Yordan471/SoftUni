a95b9014ce3bc716ea05afab8d834f498a215ca8
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-19 19:59:41 +0200

    Adding Source code for Project - Phonebook Upgrade

--- SoftUni Fundamentals Module/7.5 Dictionaries Archive - Exercises/Phonebook Upgrade/Program.cs
+++ SoftUni Fundamentals Module/7.5 Dictionaries Archive - Exercises/Phonebook Upgrade/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Phonebook_Upgrade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = string.Empty;
+
+            Dictionary<string, string> nameAndPhone = new Dictionary<string, string>();
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] commandToArray = command
+                    .Split(' ');
+
+                string operation = commandToArray[0];
+
+                if (operation == "A")
+                {
+                    string name = commandToArray[1];
+                    string number = commandToArray[2];
+
+                    if (!nameAndPhone.ContainsKey(name))
+                    {
+                        nameAndPhone[name] = number;
+                    }
+
+                    nameAndPhone[name] = number;
+                }
+                else if (operation == "S")
+                {
+                    string name = commandToArray[1];
+
+                    if (nameAndPhone.ContainsKey(name))
+                    {
+                        Console.WriteLine($"{name} -> {nameAndPhone[name]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {name} does not exist.");
+                    }
+                }
+                else if (operation == "ListAll")
+                {
+                    foreach (var kvp in nameAndPhone.OrderBy(x => x.Key))
+                    {
+                        Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+                    }
+                }
+            }
+        }
+    }
+}
