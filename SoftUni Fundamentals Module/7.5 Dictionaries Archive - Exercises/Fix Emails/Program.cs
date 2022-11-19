c364197e103d47679d3b3b8bf33dd6777353dc04
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-19 20:54:02 +0200

    Adding Source code for Project - Fix Emails

--- SoftUni Fundamentals Module/7.5 Dictionaries Archive - Exercises/Fix Emails/Program.cs
+++ SoftUni Fundamentals Module/7.5 Dictionaries Archive - Exercises/Fix Emails/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Fix_Emails
 {
@@ -6,7 +7,35 @@ namespace Fix_Emails
     {
         static void Main(string[] args)
         {
-            
+            string command = string.Empty;
+
+            Dictionary<string, string> nameAndEmail = new Dictionary<string, string>();
+
+            while ((command = Console.ReadLine()) != "stop")
+            {
+                string name = command;
+                string email = Console.ReadLine();
+
+                if (!nameAndEmail.ContainsKey(name))
+                {
+                    nameAndEmail[name] = email;
+                }
+                else
+                {
+                    nameAndEmail[name] = email;
+                }
+                
+            }
+
+            foreach (var kvp in nameAndEmail)
+            {
+                if (kvp.Value.Contains(".uk") || kvp.Value.Contains(".us"))
+                {
+                    continue;
+                }
+
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
         }
     }
 }
