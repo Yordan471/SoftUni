a5e30a190eb834164e6d9a97b7f4429878ae0c1a
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-19 18:13:50 +0200

    Adding Source Code for Project - Short Words Sorted

--- SoftUni Fundamentals Module/7.4 Dictionaries Archive - Lab/Short Words Sorted/Program.cs
+++ SoftUni Fundamentals Module/7.4 Dictionaries Archive - Lab/Short Words Sorted/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Short_Words_Sorted
 {
@@ -6,7 +7,17 @@ namespace Short_Words_Sorted
     {
         static void Main(string[] args)
         {
-            
+            string[] split = new string[]{ ".", ",", ":", ";", "()", "[]", "\"", "\\/", "!", "?", " "};
+
+            string[] words = Console.ReadLine()
+                .ToLower()
+                .Split(split, StringSplitOptions.RemoveEmptyEntries)
+                .Distinct()
+                .Where(x => x.Length < 5)
+                .OrderBy(x => x)
+                .ToArray();
+
+            Console.WriteLine(string.Join(", ", words));               
         }
     }
 }
