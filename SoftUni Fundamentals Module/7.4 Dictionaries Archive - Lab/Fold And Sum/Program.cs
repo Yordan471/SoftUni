1eb448c0e61628c7bbe3d4b46058793c0628bc0b
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-19 18:14:43 +0200

    Adding Project - Fold And Sum

---
+++ SoftUni Fundamentals Module/7.4 Dictionaries Archive - Lab/Fold And Sum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fold_And_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
