d1b83ac5f3e6bf6d6480ae4ca7a0f75d6b07672a
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-19 18:45:51 +0200

    Adding Source code for Project - Fold And Sum

--- SoftUni Fundamentals Module/7.4 Dictionaries Archive - Lab/Fold And Sum/Program.cs
+++ SoftUni Fundamentals Module/7.4 Dictionaries Archive - Lab/Fold And Sum/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Fold_And_Sum
 {
@@ -6,7 +8,38 @@ namespace Fold_And_Sum
     {
         static void Main(string[] args)
         {
+            List<int> numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(x => int.Parse(x))
+                .ToList();
+
+            int quarterLength = numbers.Count / 4;
             
+            List<int> foldedNumbers = numbers
+                .Skip(quarterLength * 3)
+                .Reverse()
+                .ToList();
+
+            foldedNumbers = new List<int>(
+                numbers
+                .Take(quarterLength)
+                .Reverse()
+                .ToList()
+                .Concat(foldedNumbers));
+
+            numbers = numbers
+                .Skip(quarterLength)
+                .Take(quarterLength * 2)
+                .ToList();
+
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < quarterLength * 2; i++)
+            {
+                result.Add(numbers[i] + foldedNumbers[i]);
+            }
+
+            Console.WriteLine(string.Join(" ", result));
         }
     }
 }
