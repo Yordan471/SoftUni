378d0f747ab243a5e0fcd6fdf77fb87b4bfb96fb
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-19 17:01:07 +0200

    Adding Source code for Project - Min,Max...

--- SoftUni Fundamentals Module/7.4 Dictionaries Archive - Lab/Min,Max,Sum,Avrg/Program.cs
+++ SoftUni Fundamentals Module/7.4 Dictionaries Archive - Lab/Min,Max,Sum,Avrg/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 
 namespace Min_Max_Sum_Avrg
 {
@@ -6,7 +9,20 @@ namespace Min_Max_Sum_Avrg
     {
         static void Main(string[] args)
         {
-            
+            int number = int.Parse(Console.ReadLine());
+            List<int> ints = new List<int>();
+
+            for (int i = 0; i < number; i++)
+            {
+                int numbers = int.Parse(Console.ReadLine());
+
+                ints.Add(numbers);
+            }
+
+            Console.WriteLine($"Sum = {ints.Sum()}");
+            Console.WriteLine($"Min = {ints.Min()}");
+            Console.WriteLine($"Max = {ints.Max()}");
+            Console.WriteLine($"Average = {ints.Average()}");
         }
     }
 }
