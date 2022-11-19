6f80f28808936e7ab24590628b2bea5a5f5e871e
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-19 10:09:00 +0200

    Adding Source code for Project - Post Office

--- SoftUni Fundamentals Module/Regular Expressions - More Exercises/Post Office/Program.cs
+++ SoftUni Fundamentals Module/Regular Expressions - More Exercises/Post Office/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Post_Office
 {
@@ -6,7 +7,39 @@ namespace Post_Office
     {
         static void Main(string[] args)
         {
-            
+            string patternFirstWord = @"([#$*&])(?<firstWord>[A-Z]+)\1";
+
+            string[] inputInfo = Console.ReadLine()
+                .Split('|');
+
+            string firstWord = inputInfo[0];
+            string secondWord = inputInfo[1];
+            string thirdWord = inputInfo[2];
+
+            Regex regexFirstWord = new Regex(patternFirstWord);
+            Match matchFrstWord = regexFirstWord.Match(firstWord);
+
+            string capitalLetters = matchFrstWord.Groups["firstWord"].Value;
+
+            for (int i = 0; i < capitalLetters.Length; i++)
+            {
+                char letter = capitalLetters[i];
+                int letterToASCI = letter;
+
+                string patternSecondWord = $@"{letterToASCI}:(?<length>[0-9][0-9])";
+                Regex regexSecondWord = new Regex(patternSecondWord);
+                Match matchSecondWord = regexSecondWord.Match(secondWord);
+
+                int wordLength = int.Parse(matchSecondWord.Groups["length"].Value);
+
+                string patternThirdWord = $@"(?<=\s|^){letter}[^\s]{wordLength}(?=\s|$)";
+                Regex regexThirdWord = new Regex(patternThirdWord);
+                Match matchThirdWord = regexThirdWord.Match(thirdWord);
+
+                string word = matchThirdWord.ToString();
+
+                Console.WriteLine(word);
+            }
         }
     }
 }
