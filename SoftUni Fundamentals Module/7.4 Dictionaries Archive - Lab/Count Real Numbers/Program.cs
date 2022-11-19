96040f90ed174b6bb01bf5f80a4939607447ee5d
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-19 16:50:16 +0200

    Adding Project - Min,Max,Sum,Avrg

--- SoftUni Fundamentals Module/7.4 Dictionaries Archive - Lab/Count Real Numbers/Program.cs
+++ SoftUni Fundamentals Module/7.4 Dictionaries Archive - Lab/Count Real Numbers/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Count_Real_Numbers
 {
@@ -6,7 +8,27 @@ namespace Count_Real_Numbers
     {
         static void Main(string[] args)
         {
-            
+            List<double> numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(x => double.Parse(x))
+                .ToList();
+
+            Dictionary <double, double> dict = new Dictionary<double, double>();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (!dict.ContainsKey(numbers[i]))
+                {
+                    dict[numbers[i]] = 0;
+                }
+
+                dict[numbers[i]] += 1;
+            }
+
+            foreach (var d in dict.OrderBy(c => c.Key))
+            {
+                Console.WriteLine($"{d.Key} -> {d.Value}");
+            }
         }
     }
 }
---
+++ SoftUni Fundamentals Module/7.4 Dictionaries Archive - Lab/Min,Max,Sum,Avrg/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Min_Max_Sum_Avrg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
