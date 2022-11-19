d795ee338fe20718e7785316462909bc3244b786
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-19 17:49:32 +0200

    Adding Source code for Project - Largest 3 Numbers

--- SoftUni Fundamentals Module/7.4 Dictionaries Archive - Lab/Largest 3 Numbers/Program.cs
+++ SoftUni Fundamentals Module/7.4 Dictionaries Archive - Lab/Largest 3 Numbers/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
 
 namespace Largest_3_Numbers
 {
@@ -6,7 +9,15 @@ namespace Largest_3_Numbers
     {
         static void Main(string[] args)
         {
-            
+            List<double> numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(x => double.Parse(x))
+                .OrderByDescending(x => x)
+                .Take(3)
+                .ToList();
+
+            Console.Write(string.Join(' ', numbers));
+
         }
     }
 }
