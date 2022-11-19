2bbdfcefda24b9110816d98c56a3d7bc8526dc89
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-19 20:23:51 +0200

    Adding Source code for Project - A Minor Task

--- SoftUni Fundamentals Module/7.5 Dictionaries Archive - Exercises/A Minor Task/Program.cs
+++ SoftUni Fundamentals Module/7.5 Dictionaries Archive - Exercises/A Minor Task/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace A_Minor_Task
 {
@@ -6,7 +7,27 @@ namespace A_Minor_Task
     {
         static void Main(string[] args)
         {
-            
+            string command = string.Empty;
+            Dictionary <string, int> resourceAndQuantity = new Dictionary<string, int> ();
+
+            while ((command = Console.ReadLine()) != "stop")
+            {
+                string resource = command;
+                int quantity = int.Parse(Console.ReadLine());
+
+
+                if (!resourceAndQuantity.ContainsKey(resource))
+                {
+                     resourceAndQuantity.Add(resource, 0);
+                }
+
+                resourceAndQuantity[resource] += quantity;                                   
+            }
+
+            foreach (var kvp in resourceAndQuantity)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }        
         }
     }
 }
