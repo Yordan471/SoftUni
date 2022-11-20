a35e5f77b58a10f1e15837f01887debb3bce4af9
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-20 14:25:09 +0200

    Adding Source code for Project - User Logs

--- SoftUni Fundamentals Module/7.5 Dictionaries Archive - Exercises/User Logs/Program.cs
+++ SoftUni Fundamentals Module/7.5 Dictionaries Archive - Exercises/User Logs/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace User_Logs
 {
@@ -6,7 +9,46 @@ namespace User_Logs
     {
         static void Main(string[] args)
         {
-            
+            char[] separators= { '=', ' ' };
+
+            Dictionary <string, List<string>> nameAndIP = new Dictionary<string, List<string>>();
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] commandToArray = command
+                    .Split(separators);
+
+                string IP = commandToArray[1];
+                string name = commandToArray[5];
+
+                if (!nameAndIP.ContainsKey(name))
+                {
+                    nameAndIP.Add(name, new List<string>());
+                }
+
+                nameAndIP[name].Add(IP);
+            }
+
+            foreach (var nameIP in nameAndIP.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{nameIP.Key}:");
+                int counter = 0;
+
+                foreach (var IP in nameIP.Value.Distinct())
+                {
+                    int count = nameIP.Value.Count(x => x.Equals(IP));
+                    counter++;
+
+                    if (counter == nameIP.Value.Distinct().Count())
+                    {
+                        Console.WriteLine($"{IP} => {count}.");
+                        break;
+                    }
+
+                    Console.Write($"{IP} => {count}, ");
+                }
+            }
         }
     }
 }
