6d762dba460aa5b6db14c11228a40606805c7ac5
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-20 11:38:49 +0200

    Adding Source code for Project - Hands of Cards

--- SoftUni Fundamentals Module/7.5 Dictionaries Archive - Exercises/Hands of Cards/Program.cs
+++ SoftUni Fundamentals Module/7.5 Dictionaries Archive - Exercises/Hands of Cards/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Hands_of_Cards
 {
@@ -6,7 +8,126 @@ namespace Hands_of_Cards
     {
         static void Main(string[] args)
         {
-            
+            string command = string.Empty;
+
+            Dictionary<string, string> nameAndValue = new Dictionary<string, string>();
+
+            string cards = string.Empty;
+
+            while ((command = Console.ReadLine()) != "JOKER")
+            {
+                string[] commandToArray = command
+                    .Split(": ");
+
+                string name = commandToArray[0];
+                cards = commandToArray[1];                
+               
+                if (!nameAndValue.ContainsKey(name))
+                {
+                    nameAndValue.Add(name, cards);
+                }
+                else
+                {
+
+                    nameAndValue[name] += $", {cards}";
+                }                           
+            }
+
+            List<string> cardsToList = new List<string>();
+            Dictionary <string, int> nameAndPoints = new Dictionary<string, int>(); 
+                
+            foreach (var name in nameAndValue)
+            {
+                cardsToList = name.Value
+                    .Split(", ")
+                    .ToList();
+
+                for (int i = 0; i < cardsToList.Count; i++)
+                {
+                    string card = cardsToList[i];
+                    bool removed = false;
+
+                    for (int j = i + 1; j < cardsToList.Count; j++)
+                    {
+                        if (card == cardsToList[j])
+                        {
+                            cardsToList.Remove(cardsToList[j]);
+                            j--;
+                            removed = true;
+                        }
+                    }
+
+                    if (removed)
+                    {
+                        i--;
+                    }
+                }
+
+                int value = 0;
+
+                for (int i = 0; i < cardsToList.Count; i++)
+                {
+                    int firstDigit = 0;
+                    int secondDigit = 0;
+
+                 
+
+                    foreach (char ch in cardsToList[i])
+                    {                      
+                        if (char.IsDigit(ch))
+                        {
+                            firstDigit = int.Parse(ch.ToString());
+                        }
+                        else
+                        {
+                            switch (ch)
+                            {
+                                case 'A':
+                                    firstDigit = 14;
+                                    break;
+                                case 'K':
+                                    firstDigit = 13;
+                                    break;
+                                case 'Q':
+                                    firstDigit = 12;
+                                    break;
+                                case 'J':
+                                    firstDigit = 11;
+                                    break;
+                                case 'S':
+                                    secondDigit = 4;
+                                    break;
+                                case 'H':
+                                    secondDigit = 3;
+                                    break;
+                                case 'D':
+                                    secondDigit = 2;
+                                    break;
+                                case 'C':
+                                    secondDigit = 1;
+                                    break;
+                            }
+
+                            if (cardsToList[i].Count() == 3)
+                            {
+                                firstDigit = 10;
+                            }
+                        }
+                    }
+
+                    value += firstDigit * secondDigit;
+                }
+
+                if (!nameAndPoints.ContainsKey(name.Key))
+                {
+                    nameAndPoints.Add(name.Key, value);
+                }
+            }
+          
+            foreach (var namePoints in nameAndPoints)
+            {
+                Console.WriteLine($"{namePoints.Key}: {namePoints.Value}");
+            }
         }
     }
 }
