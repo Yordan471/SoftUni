53efb07dcc8b3ccf25c9df29976f0c0594e8ab0d
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-20 17:46:57 +0200

    Adding Source code for Project - Logs Aggregator

--- SoftUni Fundamentals Module/7.5 Dictionaries Archive - Exercises/Logs Aggregator/Program.cs
+++ SoftUni Fundamentals Module/7.5 Dictionaries Archive - Exercises/Logs Aggregator/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Logs_Aggregator
 {
@@ -6,7 +8,57 @@ namespace Logs_Aggregator
     {
         static void Main(string[] args)
         {
-            
+            int numberOfLogs = int.Parse(Console.ReadLine());
+
+            Dictionary <string, int> nameAndDuration = new Dictionary<string, int>();
+            Dictionary <string, List<string>> nameAndIPs = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < numberOfLogs; i++)
+            {
+                string[] accessLogs = Console.ReadLine()
+                    .Split(' ');
+
+                string iP = accessLogs[0];
+                string name = accessLogs[1];
+                int duration = int.Parse(accessLogs[2]);
+
+                if (!nameAndIPs.ContainsKey(name))
+                {
+                    nameAndIPs.Add(name, new List<string>());
+                    nameAndIPs[name].Add(iP);
+
+                    nameAndDuration.Add(name, 0);
+                }
+                else if (!nameAndIPs[name].Contains(iP))
+                {
+                    nameAndIPs[name].Add(iP);
+                }
+
+                nameAndDuration[name] += duration;               
+            }
+
+            foreach (var nameIPs in nameAndIPs.OrderBy(x => x.Key))
+            {
+                Console.Write($"{nameIPs.Key}: ");
+
+                int sumDuration = 0;
+
+                foreach (var nameDuration in nameAndDuration)
+                {
+                    if (nameIPs.Key == nameDuration.Key)
+                    {
+                        sumDuration = nameDuration.Value;
+                    }                   
+                }
+
+                Console.Write($"{sumDuration} ");
+                
+                List<string> logs = new List<string>();
+
+                logs = nameIPs.Value.OrderBy(x => x).ToList();
+
+                Console.WriteLine($"[{string.Join(", ", logs)}]");          
+            }
         }
     }
 }
