f812b4ad8e40a0a3b860517903801e8d9bc70876
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-21 21:48:34 +0200

    Update Program.cs

--- SoftUni Fundamentals Module/8.5 Text Processing Archive/Convert from base-10 to base-N/Program.cs
+++ SoftUni Fundamentals Module/8.5 Text Processing Archive/Convert from base-10 to base-N/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 namespace Convert_from_base_10_to_base_N
 {
@@ -6,7 +7,22 @@ namespace Convert_from_base_10_to_base_N
     {
         static void Main(string[] args)
         {
-            
+            string[] inputInfo = Console.ReadLine()
+                .Split();
+
+            int numberBase = int.Parse(inputInfo[0]);
+            BigInteger numberBaseTen = BigInteger.Parse(inputInfo[1]);
+            string numberToString = string.Empty;
+
+            while (numberBaseTen > 0)
+            {
+                BigInteger remainder = numberBaseTen % numberBase;
+                numberToString = remainder + numberToString;
+                numberBaseTen /= numberBase;
+            }
+
+            Console.WriteLine(numberToString);
+
         }
     }
 }
