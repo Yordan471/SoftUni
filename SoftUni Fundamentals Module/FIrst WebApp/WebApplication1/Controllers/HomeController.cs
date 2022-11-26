5ef8df5551101113a1ba7222e607671f3eaba32f
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-11-26 11:20:13 +0200

    Adding code for Project - Calculator(WebApplication1)

---
+++ SoftUni Fundamentals Module/FIrst WebApp/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Calculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calculator(double firstNumber, double secondNumber, string operation)
+        {
+            string result = string.Empty;
+
+            switch (operation)
+            {
+                case "+":
+                    result = $"{firstNumber + secondNumber}";
+                    break;
+                case "-":
+                    result = $"{firstNumber - secondNumber}";
+                    break;
+                case "*":
+                    result = $"{firstNumber - secondNumber}";
+                    break;
+                case "/":
+                    result = $"{firstNumber - secondNumber}";
+                    break;
+                default:
+                    result = "Invalid operator";
+                    break;
+            }
+
+            ViewBag.Result = result;
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
