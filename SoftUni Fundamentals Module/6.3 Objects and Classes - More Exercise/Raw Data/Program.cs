1ac8f53e598ad75dfbb21aa0daee7b8476c01447
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-13 20:53:18 +0200

    Adding Source code for Project - Raw Data

--- SoftUni Fundamentals Module/6.3 Objects and Classes - More Exercise/Raw Data/Program.cs
+++ SoftUni Fundamentals Module/6.3 Objects and Classes - More Exercise/Raw Data/Program.cs
@@ -6,11 +6,92 @@ using System.Threading.Tasks;
 
 namespace Raw_Data
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            int numbersOfCars = int.Parse(Console.ReadLine());
 
+            List<Car> cars = new List<Car>();
+
+            for (int i = 0; i < numbersOfCars; i++)
+            {
+                string[] carsInfo = Console.ReadLine()
+                    .Split(' ');
+
+                string model = carsInfo[0];
+                string engineSpeed = carsInfo[1];
+                int enginePower = int.Parse(carsInfo[2]);
+                int cargoWeight = int.Parse(carsInfo[3]);
+                string cargoType = carsInfo[4];
+
+                Car car = new Car(model, engineSpeed, enginePower, cargoWeight, cargoType);
+
+                cars.Add(car);
+            }
+
+            string command = Console.ReadLine();
+
+            if (command == "fragile")
+            {
+                foreach(Car car in cars)
+                {
+                    if (car.Cargo.CargoWeight < 1000 && car.Cargo.CargoType == "fragile")
+                    {
+                        Console.WriteLine(car.Model);
+                    }
+                }
+            }
+            else if (command == "flamable")
+            {
+                foreach(Car car in cars)
+                {
+                    if (car.Engine.EnginePower > 250 && car.Cargo.CargoType == "flamable")
+                    {
+                        Console.WriteLine(car.Model);
+                    }
+                }
+            }
         }
+    }
+
+    public class Car
+    {
+        public Car(string model, string engineSpeed, int enginePower,
+            int cargoWeight, string cargoType)
+        {
+            Model = model;
+            Engine = new Engine()
+            {
+                EngineSpeed = engineSpeed,
+                EnginePower = enginePower,
+            };
+
+            Cargo = new Cargo()
+            {
+                CargoWeight = cargoWeight,
+                CargoType = cargoType
+            };
+        }
+
+        public string Model { get; set; }
+
+        public Engine Engine { get; set; }
+
+        public Cargo Cargo { get; set; }
+    }
+
+    public class Engine
+    {
+        public string EngineSpeed { get; set; }
+
+        public int EnginePower { get; set; }
+    }
+
+    public class Cargo
+    {
+        public int CargoWeight { get; set; }
+
+        public string CargoType { get; set; }
     }
 }
