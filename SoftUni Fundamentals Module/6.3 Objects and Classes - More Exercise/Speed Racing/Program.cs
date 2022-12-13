278d32fca0aee28c18bd33bda8d84012f34b5df9
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-13 20:13:08 +0200

    Update Program.cs

--- SoftUni Fundamentals Module/6.3 Objects and Classes - More Exercise/Speed Racing/Program.cs
+++ SoftUni Fundamentals Module/6.3 Objects and Classes - More Exercise/Speed Racing/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,12 +11,89 @@ namespace Speed_Racing
     {
         static void Main(string[] args)
         {
+            int numberOfCars = int.Parse(Console.ReadLine());
 
+            List<Car> cars = new List<Car>();
+
+            AddingCarsToAList(numberOfCars, cars);
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] commandsInfo = command
+                    .Split();
+
+                string model = commandsInfo[1];
+                double distance = double.Parse(commandsInfo[2]);
+
+                Car car = cars.First(c => c.Model == model);
+
+                Car.CHechIfThereIsEnoughFuel(distance, car);
+            }
+
+            PrintingResult(cars);
+        }
+
+        private static void PrintingResult(List<Car> cars)
+        {
+            foreach (Car car in cars)
+            {
+                Console.WriteLine($"{car.Model} {car.FuelAmount:f2} {car.TraveledDistance}");
+            }
+        }
+
+        private static void AddingCarsToAList(int numberOfCars, List<Car> cars)
+        {
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                string[] carInfo = Console.ReadLine()
+                    .Split();
+
+                string model = carInfo[0];
+                double fuelAmount = double.Parse(carInfo[1]);
+                double fuelConsumption = double.Parse(carInfo[2]);
+                double traveledDistance = 0;
+
+                Car car = new Car(model, fuelAmount, fuelConsumption, traveledDistance);
+
+                cars.Add(car);
+            }
         }
     }
 
     public class Car
     {
 
+        public Car(string model, double fuelAmount, double fuelConsumption, double traveledDistance)
+        {
+            Model = model;
+            FuelAmount = fuelAmount;
+            FuelConsumption = fuelConsumption;
+            TraveledDistance = traveledDistance;
+        }
+
+        public string Model { get; set; }
+
+        public double FuelAmount { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public double TraveledDistance { get; set; }
+
+        public static void CHechIfThereIsEnoughFuel(double distance, Car car)
+        {
+            double fuelNeeded = car.FuelConsumption * distance;
+
+            if (fuelNeeded > car.FuelAmount)
+            {
+                Console.WriteLine($"Insufficient fuel for the drive");
+            }
+            else
+            {
+                car.FuelAmount -= fuelNeeded;
+                car.TraveledDistance += distance;
+            }
+        }
     }
 }
