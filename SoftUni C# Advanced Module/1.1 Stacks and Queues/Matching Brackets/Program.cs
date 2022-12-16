07ae8fb5f222241dae1e6c353429cf49c403ae02
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-16 20:17:18 +0200

    Adding Source code for Project - Matching Brackets

--- SoftUni C# Advanced Module/1.1 Stacks and Queues/Matching Brackets/Program.cs
+++ SoftUni C# Advanced Module/1.1 Stacks and Queues/Matching Brackets/Program.cs
@@ -10,7 +10,26 @@ namespace Matching_Brackets
     {
         static void Main(string[] args)
         {
+            string input = Console.ReadLine();
 
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (input[i] == ')')
+                {
+                    int startIndex = stack.Pop();
+                    int endIndex = i;
+
+                    string substring = input.Substring(startIndex, endIndex - startIndex + 1);
+
+                    Console.WriteLine(substring);
+                }
+            }
         }
     }
 }
