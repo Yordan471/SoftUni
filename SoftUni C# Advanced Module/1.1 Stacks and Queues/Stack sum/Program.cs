41c93ad406987179d941822a0bf2ec491feddf71
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-16 18:34:20 +0200

    Adding Source code for Project - Stack sum

--- SoftUni C# Advanced Module/1.1 Stacks and Queues/Stack sum/Program.cs
+++ SoftUni C# Advanced Module/1.1 Stacks and Queues/Stack sum/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,46 @@ namespace Stack_sum
     {
         static void Main(string[] args)
         {
+            int[] arrayOfInts = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
 
+            Stack<int> ints= new Stack<int>(arrayOfInts);
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine().ToLower()) != "end")
+            {
+                string[] commandToArray = command
+                    .Split();
+
+                string operation = commandToArray[0];
+
+                if (operation == "add")
+                {
+                    int firstNum = int.Parse(commandToArray[1]);
+                    int secondNum = int.Parse(commandToArray[2]);
+
+                    ints.Push(firstNum);
+                    ints.Push(secondNum);
+                }
+                else if (operation == "remove")
+                {
+                    int count = int.Parse(commandToArray[1]);
+                   
+                    for (int i = 0; i < count; i++)
+                    {
+                        if (count <= ints.Count)
+                        {
+                            ints.Pop();
+                        }
+                    }
+                }
+            }
+
+            int sum = ints.Sum();
+            Console.WriteLine(sum);
         }
     }
 }
