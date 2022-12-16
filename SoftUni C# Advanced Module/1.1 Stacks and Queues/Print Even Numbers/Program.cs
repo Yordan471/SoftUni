2e4da679b988cb7a4cf94747f2ab743b655f5742
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-16 20:18:07 +0200

    Adding Project - Print Even Numbers

---
+++ SoftUni C# Advanced Module/1.1 Stacks and Queues/Print Even Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
