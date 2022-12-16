e6a4434393b09b1c054d71a542eb9e3703342476
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-16 19:33:52 +0200

    Adding Source code for Project - Simple Calculator

--- SoftUni C# Advanced Module/1.1 Stacks and Queues/Simple Calculator/Program.cs
+++ SoftUni C# Advanced Module/1.1 Stacks and Queues/Simple Calculator/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,32 @@ namespace Simple_Calculator
     {
         static void Main(string[] args)
         {
+            string[] input = Console.ReadLine()
+                .Split(' ');
 
+            Array.Reverse(input);
+
+            Stack<string> stack = new Stack<string>(input);
+
+            int result = int.Parse(stack.Pop());
+
+            for (int i = 0; i < stack.Count; i++)
+            {
+                if (stack.Peek() == "-")
+                {
+                    stack.Pop();
+                    result -= int.Parse(stack.Pop());
+                    i--;
+                }
+                else
+                {
+                    stack.Pop();
+                    result += int.Parse(stack.Pop());
+                    i--;
+                }
+            }
+
+            Console.WriteLine(result);
         }
     }
 }
