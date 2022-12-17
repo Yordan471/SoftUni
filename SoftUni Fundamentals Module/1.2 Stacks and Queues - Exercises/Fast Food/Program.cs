9581c70fdd9487cdcb17b21cf5f7cf49e753fcd7
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-17 15:45:20 +0200

    Adding Project - Fast Food

---
+++ SoftUni Fundamentals Module/1.2 Stacks and Queues - Exercises/Fast Food/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fast_Food
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
