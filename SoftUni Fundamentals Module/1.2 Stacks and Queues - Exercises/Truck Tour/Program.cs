0d5571eef90b9ac0b527807159adc5b353c17463
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-17 23:50:34 +0200

    Update Program.cs

--- SoftUni Fundamentals Module/1.2 Stacks and Queues - Exercises/Truck Tour/Program.cs
+++ SoftUni Fundamentals Module/1.2 Stacks and Queues - Exercises/Truck Tour/Program.cs
@@ -10,7 +10,38 @@ namespace Truck_Tour
     {
         static void Main(string[] args)
         {
+            int numberOfPetrolPumps = int.Parse(Console.ReadLine());
 
+            Queue<int> lPetrol = new Queue<int>();
+            int lastStop = 0;
+
+            for (int i = 0; i < numberOfPetrolPumps; i++)
+            {
+                int[] integers = Console.ReadLine()
+                    .Split(' ')
+                    .Select(int.Parse)
+                    .ToArray();
+
+                int amountOfPetrol = integers[0];
+                int distance = integers[1];               
+
+                if (amountOfPetrol >= distance)
+                {
+                    if (amountOfPetrol > lastStop)
+                    {
+                        lPetrol.Enqueue(amountOfPetrol);
+                    }
+                    else
+                    {
+                        
+                    }
+                    
+                    
+                }
+
+
+
+            }
         }
     }
 }
