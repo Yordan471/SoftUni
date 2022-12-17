6af523a85209b435ef750fce49e033a8f37884fb
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-17 13:18:14 +0200

    Adding Source code for Project - Print Even Numbers

--- SoftUni C# Advanced Module/1.1 Stacks and Queues/Print Even Numbers/Program.cs
+++ SoftUni C# Advanced Module/1.1 Stacks and Queues/Print Even Numbers/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,22 @@ namespace Print_Even_Numbers
     {
         static void Main(string[] args)
         {
+            int[] integers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
 
+            Queue<int> evenNumbers = new Queue<int>();
+
+            for (int i = 0; i < integers.Length; i++)
+            {
+                if (integers[i] % 2 == 0)
+                {
+                    evenNumbers.Enqueue(integers[i]);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", evenNumbers));
         }
     }
 }
