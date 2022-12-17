83f61b29c46498e29146bc6065e9b8e42ffd34b4
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-17 14:48:30 +0200

    Adding Source code for Project - Basic Operations

--- SoftUni Fundamentals Module/1.2 Stacks and Queues - Exercises/Basic Operations/Program.cs
+++ SoftUni Fundamentals Module/1.2 Stacks and Queues - Exercises/Basic Operations/Program.cs
@@ -10,7 +10,64 @@ namespace Basic_Operations
     {
         static void Main(string[] args)
         {
+            int[] nsx = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
 
+            Stack<int> integers = new Stack<int>();
+
+            PushNumbersInStack(nsx, numbers, integers);
+
+            PopNumbersFromStack(nsx, integers);
+
+            FindIfQueueContainsNumber(nsx, integers);
+        }
+
+        private static void FindIfQueueContainsNumber(int[] nsx, Stack<int> integers)
+        {
+            if (integers.Count > 0)
+            {
+                if (!(integers.Contains(nsx[2])))
+                {
+                    Console.WriteLine(integers.Min());
+                }
+                else if (integers.Contains(nsx[2]))
+                {
+                    Console.WriteLine(true);
+                }
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+
+        private static void PopNumbersFromStack(int[] nsx, Stack<int> integers)
+        {
+            if (nsx[1] >= integers.Count)
+            {
+                integers.Clear();
+            }
+            else
+            {
+                for (int i = 0; i < nsx[1]; i++)
+                {
+                    integers.Pop();
+                }
+            }
+        }
+
+        private static void PushNumbersInStack(int[] nsx, int[] numbers, Stack<int> integers)
+        {
+            for (int i = 0; i < nsx[0]; i++)
+            {
+                integers.Push(numbers[i]);
+            }
         }
     }
 }
