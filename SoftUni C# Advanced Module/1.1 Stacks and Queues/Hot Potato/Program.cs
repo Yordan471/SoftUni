caf6e9328ffdfcfe7f04dd1c1e19e3b7ca74ec8b
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-17 13:41:50 +0200

    Adding Source code for Project - Hot Potato

--- SoftUni C# Advanced Module/1.1 Stacks and Queues/Hot Potato/Program.cs
+++ SoftUni C# Advanced Module/1.1 Stacks and Queues/Hot Potato/Program.cs
@@ -10,7 +10,30 @@ namespace Hot_Potato
     {
         static void Main(string[] args)
         {
+            string[] kids = Console.ReadLine()
+                .Split(' ');
 
+            int numberOfRotations = int.Parse(Console.ReadLine());
+
+            int tossBall = 1;
+            Queue<string> winner = new Queue<string>(kids);
+
+            while (winner.Count > 1)
+            {
+                string currentKid = winner.Dequeue();
+
+                if (tossBall == numberOfRotations)
+                {
+                    Console.WriteLine($"Removed {currentKid}");
+                    tossBall = 1;
+                    continue;
+                }
+
+                tossBall++;
+                winner.Enqueue(currentKid);
+            }
+
+            Console.WriteLine($"Last is {winner.Peek()}");
         }
     }
 }
