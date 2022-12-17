28d46d9f866a1b39178821e2a2a26341fbcc324f
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-17 15:44:44 +0200

    Adding Source code for Project - Max Min Element

--- SoftUni Fundamentals Module/1.2 Stacks and Queues - Exercises/Max Min Element/Program.cs
+++ SoftUni Fundamentals Module/1.2 Stacks and Queues - Exercises/Max Min Element/Program.cs
@@ -10,7 +10,41 @@ namespace Max_Min_Element
     {
         static void Main(string[] args)
         {
+            int numOfOperations = int.Parse(Console.ReadLine());
 
+            Stack<int> numbers = new Stack<int>();
+
+            for (int i = 0; i < numOfOperations; i++)
+            {
+                string[] operations = Console.ReadLine()
+                    .Split(' ');
+
+                string operation = operations[0];
+
+                if (operation == "1")
+                {
+                    int number = int.Parse(operations[1]);
+
+                    numbers.Push(number);
+                }
+                else if (operation == "2" && numbers.Count > 0)
+                {
+                        numbers.Pop();                    
+                }
+                else if (operation == "3" && numbers.Count > 0)
+                {
+                        Console.WriteLine($"{numbers.Max()}");                    
+                }
+                else if (operation == "4" && numbers.Count > 0)
+                {                  
+                        Console.WriteLine($"{numbers.Min()}");                                   
+                }
+            }
+
+            if (numbers.Count > 0)
+            {
+                Console.WriteLine(string.Join(", ", numbers));
+            }            
         }
     }
 }
