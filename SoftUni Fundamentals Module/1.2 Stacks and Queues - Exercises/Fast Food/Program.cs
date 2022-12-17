4e7069d4ddb6079a8f94891ecc7151219debef60
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-17 16:19:34 +0200

    Adding Source code for Project - Fast Food

--- SoftUni Fundamentals Module/1.2 Stacks and Queues - Exercises/Fast Food/Program.cs
+++ SoftUni Fundamentals Module/1.2 Stacks and Queues - Exercises/Fast Food/Program.cs
@@ -10,7 +10,30 @@ namespace Fast_Food
     {
         static void Main(string[] args)
         {
+            int quantityOfTheFood = int.Parse(Console.ReadLine());
+            int[] quantityOfOrders = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
 
+            Queue<int> orders = new Queue<int>(quantityOfOrders);
+
+            Console.WriteLine(orders.Max());
+
+            while (orders.Count != 0 && orders.Peek() <= quantityOfTheFood)
+            {
+                quantityOfTheFood -= orders.Dequeue();
+            }
+
+            if (orders.Count > 0)
+            {
+                Console.Write($"Orders left: ");
+                Console.WriteLine(string.Join(", ", orders));
+            }
+            else
+            {
+                Console.WriteLine("Orders complete");
+            }
         }
     }
 }
