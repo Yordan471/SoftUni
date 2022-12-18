91210a93b23a53af2e497a4d5fcc6fdb33914630
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-18 18:19:44 +0200

    Adding Source code for Project - Key Revolver

--- SoftUni Fundamentals Module/1.2 Stacks and Queues - Exercises/Key Revolver/Program.cs
+++ SoftUni Fundamentals Module/1.2 Stacks and Queues - Exercises/Key Revolver/Program.cs
@@ -10,7 +10,75 @@ namespace Key_Revolver
     {
         static void Main(string[] args)
         {
+            int priceForBullet = int.Parse(Console.ReadLine());
+            int sizeOfGunBarrel = int.Parse(Console.ReadLine());
+            int[] bullets = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
 
+            int[] locks = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int intelValue = int.Parse(Console.ReadLine());
+
+            Queue<int> queueLocks = new Queue<int>(locks);
+            Stack<int> stackBullets = new Stack<int>(bullets);
+            int countBullets = 0;
+            int barrelCount = 0;
+
+            while (queueLocks.Count !=0)
+            {
+                for (int i = 0; i < sizeOfGunBarrel; i++)
+                {
+                    if (stackBullets.Count != 0 && queueLocks.Count != 0)
+                    {
+                        barrelCount++;
+                        countBullets++;
+
+                        if (stackBullets.Peek() <= queueLocks.Peek())
+                        {
+                            Console.WriteLine("Bang!");
+
+                            stackBullets.Pop();
+                            queueLocks.Dequeue();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ping!");
+
+                            stackBullets.Pop();
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (stackBullets.Count != 0 && barrelCount == sizeOfGunBarrel)
+                {
+                    Console.WriteLine("Reloading!");
+                }
+                else
+                {
+                    break;
+                }
+
+                barrelCount = 0;
+            }
+
+            if (stackBullets.Count >= 0 && queueLocks.Count == 0)
+            {
+                Console.WriteLine($"Couldn't get through. Locks left: {queueLocks.Count}");
+            }
+            else
+            {
+                int moneyEarned = intelValue - (countBullets * priceForBullet);
+                Console.WriteLine($"{stackBullets.Count} bullets left. Earned ${moneyEarned}");
+            }
         }
     }
 }
