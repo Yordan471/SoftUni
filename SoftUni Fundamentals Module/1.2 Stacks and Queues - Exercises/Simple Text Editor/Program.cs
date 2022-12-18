048115a30bc7f9b794dea7c9d0633217689d9558
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-18 16:07:16 +0200

    Adding Source code for Project  - Simple Text Editor

--- SoftUni Fundamentals Module/1.2 Stacks and Queues - Exercises/Simple Text Editor/Program.cs
+++ SoftUni Fundamentals Module/1.2 Stacks and Queues - Exercises/Simple Text Editor/Program.cs
@@ -10,7 +10,48 @@ namespace Simple_Text_Editor
     {
         static void Main(string[] args)
         {
+            int numberOfCommands = int.Parse(Console.ReadLine());
 
+            string text = string.Empty;
+            Stack<string> stack = new Stack<string>();
+
+            stack.Push(string.Empty);
+            
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                string[] command = Console.ReadLine()
+                    .Split(' ');
+                
+                string operation = command[0];
+
+                if (operation == "1")
+                {
+                    string someString = command[1];
+
+                    text += someString;
+                    stack.Push(text);
+                }
+                else if (operation == "2")
+                {
+                    int count = int.Parse(command[1]);
+
+                    text = text.Remove(text.Length - 1 - count + 1);
+                    stack.Push(text);
+                }
+                else if (operation == "3")
+                {
+                    int index = int.Parse(command[1]);
+
+                    Console.WriteLine($"{text[index - 1]}");
+                }
+                else if (operation == "4")
+                {
+                    stack.Pop();
+
+                    string currText = stack.Peek();
+                    text = currText;
+                }
+            }
         }
     }
 }
