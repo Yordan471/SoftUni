a56205b4b96c9cc7c35a59ee83f7a83ccd064093
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-19 17:56:26 +0200

    Adding folder - 2.1 Multidimensional Arrays - Lab; Adding Project - Sum Matrix Elements

---
+++ SoftUni Fundamentals Module/2.1 Multidimensional Arrays - Lab/Sum Matrix Elements/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_Matrix_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
