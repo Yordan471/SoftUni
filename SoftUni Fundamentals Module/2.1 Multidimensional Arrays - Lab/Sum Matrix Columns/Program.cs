0417fbe311be477745bc72c2591b5264b9c90419
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-19 22:53:10 +0200

    Update Program.cs

--- SoftUni Fundamentals Module/2.1 Multidimensional Arrays - Lab/Sum Matrix Columns/Program.cs
+++ SoftUni Fundamentals Module/2.1 Multidimensional Arrays - Lab/Sum Matrix Columns/Program.cs
@@ -10,7 +10,40 @@ namespace Sum_Matrix_Columns
     {
         static void Main(string[] args)
         {
+            int[] matrix = Console.ReadLine()
+               .Split(new string[] { ", " }, StringSplitOptions.None)
+               .Select(int.Parse)
+               .ToArray();
 
+            int[,] doubleMatrix = new int[matrix[0], matrix[1]];
+
+            for (int row = 0; row < doubleMatrix.GetLength(0); row++)
+            {
+                int[] colElements = Console.ReadLine()
+                    .Split(new string[] { " " }, StringSplitOptions.None)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < doubleMatrix.GetLength(1); col++)
+                {
+                    doubleMatrix[row, col] = colElements[col];
+                }
+            }
+
+            long[] sum = new long[matrix[1]];
+
+            for (int row = 0; row < doubleMatrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < doubleMatrix.GetLength(1); col++)
+                {
+                    sum[col] += doubleMatrix[row, col];
+                }
+            }
+
+            foreach (int number in sum)
+            {
+                Console.WriteLine(number);
+            }
         }
     }
 }
