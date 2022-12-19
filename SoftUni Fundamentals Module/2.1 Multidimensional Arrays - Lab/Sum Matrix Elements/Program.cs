628ef1003cd452c1b3d7fb29b9ca7e9f745f8df7
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-19 18:50:26 +0200

    Adding Source code for Project - Sum Matrix Elements

--- SoftUni Fundamentals Module/2.1 Multidimensional Arrays - Lab/Sum Matrix Elements/Program.cs
+++ SoftUni Fundamentals Module/2.1 Multidimensional Arrays - Lab/Sum Matrix Elements/Program.cs
@@ -10,7 +10,39 @@ namespace Sum_Matrix_Elements
     {
         static void Main(string[] args)
         {
+            int[] matrix = Console.ReadLine()
+                .Split(new string[] {", "}, StringSplitOptions.None)
+                .Select(int.Parse)
+                .ToArray();
 
+            int[,] doubleMatrix = new int[matrix[0], matrix[1]];
+
+            for (int row = 0; row < doubleMatrix.GetLength(0); row++)
+            {
+                int[] colElements = Console.ReadLine()
+                    .Split(new string[] {", "}, StringSplitOptions.None)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < doubleMatrix.GetLength(1); col++)
+                {
+                    doubleMatrix[row, col] = colElements[col];
+                }
+            }
+
+            long sum = 0;
+
+            for (int row = 0; row < doubleMatrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < doubleMatrix.GetLength(1); col++)
+                {
+                    sum += doubleMatrix[row, col];
+                }
+            }
+
+            Console.WriteLine(doubleMatrix.GetLength(0));
+            Console.WriteLine(doubleMatrix.GetLength(1));
+            Console.WriteLine(sum);
         }
     }
 }
