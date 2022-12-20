11c4c4bb860e4544dff06ad755f843d3c8d22879
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-20 18:43:33 +0200

    Adding Project - Jagged-Array Modification

---
+++ SoftUni Fundamentals Module/2.1 Multidimensional Arrays - Lab/Jagged-Array Modification/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jagged_Array_Modification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
