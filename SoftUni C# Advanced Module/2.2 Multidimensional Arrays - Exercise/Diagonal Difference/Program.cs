c5066f88ccbb255f484910f71bda83ee6605a965
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-24 16:36:40 +0200

    Adding Source code for Project - Diagonal Difference

--- SoftUni C# Advanced Module/2.2 Multidimensional Arrays - Exercise/Diagonal Difference/Program.cs
+++ SoftUni C# Advanced Module/2.2 Multidimensional Arrays - Exercise/Diagonal Difference/Program.cs
@@ -10,6 +10,40 @@ namespace Diagonal_Difference
     {
         static void Main(string[] args)
         {
+            int sizeOfSquareMatrix = int.Parse(Console.ReadLine());
+
+            int[,] squareMatrix = new int[sizeOfSquareMatrix, sizeOfSquareMatrix];
+
+            for (int row = 0; row < sizeOfSquareMatrix; row++)
+            {
+                int[] rowArray = Console.ReadLine()
+                    .Split(' ')
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < sizeOfSquareMatrix; col++)
+                {
+                    squareMatrix[row, col] = rowArray[col];
+                }
+            }
+
+            int firstSum = 0;
+
+            for (int row = 0; row < sizeOfSquareMatrix; row++)
+            {
+                firstSum += squareMatrix[row, row];
+            }
+
+            int secondSum = 0;
+            int count = 0;
+
+            for (int row = 0; row < sizeOfSquareMatrix; row++)
+            {
+                count++;
+                secondSum += squareMatrix[row, sizeOfSquareMatrix - count];
+            }
+
+            Console.WriteLine($"{Math.Abs(firstSum - secondSum)}");
         }
     }
 }
