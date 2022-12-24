b9fec0d04e0bf8ab3661a9b734053e52c531bb6c
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-24 18:58:34 +0200

    Adding Source code for Project - Maximal Sum

--- SoftUni C# Advanced Module/2.2 Multidimensional Arrays - Exercise/Maximal Sum/Program.cs
+++ SoftUni C# Advanced Module/2.2 Multidimensional Arrays - Exercise/Maximal Sum/Program.cs
@@ -10,7 +10,60 @@ namespace Maximal_Sum
     {
         static void Main(string[] args)
         {
+            int[] sizeOfRetangleMatrix = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
 
+            int rows = sizeOfRetangleMatrix[0];
+            int cols = sizeOfRetangleMatrix[1];
+
+            int[,] rectangleMatrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] rowArray = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    rectangleMatrix[row, col] = rowArray[col];
+                }
+            }
+
+            int sum = 0;
+            int saveSum = 0;
+            string saveSquare = string.Empty;
+
+            for (int row = 0; row < rows - 2; row++)
+            {
+                for (int col = 0; col < cols - 2; col++)
+                {
+                    
+                    sum = rectangleMatrix[row, col] +
+                        rectangleMatrix[row, col + 1] +
+                        rectangleMatrix[row, col + 2] +
+                        rectangleMatrix[row + 1, col] +
+                        rectangleMatrix[row + 1, col + 1] +
+                        rectangleMatrix[row + 1, col + 2] +
+                        rectangleMatrix[row + 2, col] +
+                        rectangleMatrix[row + 2, col + 1] +
+                        rectangleMatrix[row + 2, col + 2];    
+                    
+                    if (saveSum < sum)
+                    {
+                        saveSum = sum;
+                        saveSquare = $"{rectangleMatrix[row, col]} {rectangleMatrix[row, col + 1]} {rectangleMatrix[row, col + 2]}" +
+                            $" \n {rectangleMatrix[row + 1, col]} {rectangleMatrix[row + 1, col + 1]} {rectangleMatrix[row + 1, col + 2]}" +
+                            $" \n {rectangleMatrix[row + 2, col]} {rectangleMatrix[row + 2, col + 1]} {rectangleMatrix[row + 2, col + 2]}";
+                    }                   
+                }
+            }
+
+            Console.WriteLine(saveSum);
+            Console.WriteLine(saveSquare);
         }
     }
 }
