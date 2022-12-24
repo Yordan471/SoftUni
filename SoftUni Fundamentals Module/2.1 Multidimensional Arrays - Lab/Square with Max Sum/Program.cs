3f1122e8344f57d856204bc48f0af9303e0761cc
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-24 13:35:11 +0200

    Adding Project - Square with Max Sum

---
+++ SoftUni Fundamentals Module/2.1 Multidimensional Arrays - Lab/Square with Max Sum/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square_with_Max_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
