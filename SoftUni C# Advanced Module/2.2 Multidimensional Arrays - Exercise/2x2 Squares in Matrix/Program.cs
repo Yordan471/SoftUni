45a6257e5b5ff0c92e92b2b592365f8f7d8f9f30
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-24 16:59:47 +0200

    Adding Source code for Project - 2x2 Squares in Matrix

--- SoftUni C# Advanced Module/2.2 Multidimensional Arrays - Exercise/2x2 Squares in Matrix/Program.cs
+++ SoftUni C# Advanced Module/2.2 Multidimensional Arrays - Exercise/2x2 Squares in Matrix/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,44 @@ namespace _2x2_Squares_in_Matrix
     {
         static void Main(string[] args)
         {
+            int[] rowsAndCols = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
 
+            int rows = rowsAndCols[0];
+            int cols = rowsAndCols[1];
+
+            string[,] rectangleMatrix = new string[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                string[] rowArray = Console.ReadLine()
+                    .Split(' ')
+                    .ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    rectangleMatrix[row, col] = rowArray[col];
+                }
+            }
+
+            int countMatches = 0;
+
+            for (int row = 0; row < rows - 1; row++)
+            {
+                for (int col = 0; col < cols - 1; col++)
+                {
+                    if (rectangleMatrix[row, col] == rectangleMatrix[row, col + 1] &&
+                        rectangleMatrix[row, col] == rectangleMatrix[row + 1, col] &&
+                        rectangleMatrix[row, col] == rectangleMatrix[row + 1, col +1])
+                    {
+                        countMatches++;
+                    }
+                }
+            }
+
+            Console.WriteLine(countMatches);
         }
     }
 }
