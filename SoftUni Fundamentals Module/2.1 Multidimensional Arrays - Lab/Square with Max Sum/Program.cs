7197ab5ee83f98254dfc381e21d3a452cffd5032
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-24 15:52:40 +0200

    Adding Source code for Project - Square with Max Sum

--- SoftUni Fundamentals Module/2.1 Multidimensional Arrays - Lab/Square with Max Sum/Program.cs
+++ SoftUni Fundamentals Module/2.1 Multidimensional Arrays - Lab/Square with Max Sum/Program.cs
@@ -10,7 +10,53 @@ namespace Square_with_Max_Sum
     {
         static void Main(string[] args)
         {
+            int[] matrixSize = Console.ReadLine()
+                .Split(new string[] {", "}, StringSplitOptions.None)
+                .Select(int.Parse)
+                .ToArray();
 
+            int rows = matrixSize[0];
+            int cols = matrixSize[1];
+
+            int[,] doubleMatrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] rowArray = Console.ReadLine()
+                    .Split(new string[] {", "}, StringSplitOptions.None)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    doubleMatrix[row, col] = rowArray[col];
+                }
+            }
+
+            int saveBiggestSum = 0;
+            int sumNumbers = 0;
+            string saveSquare = string.Empty;
+
+            for (int row = 0; row < rows - 1; row++)
+            {
+                for (int col = 0; col < cols - 1; col++)
+                {
+                    sumNumbers = doubleMatrix[row, col] +
+                        doubleMatrix[row, col + 1] +
+                        doubleMatrix[row + 1, col] +
+                        doubleMatrix[row + 1, col + 1];
+
+                    if (saveBiggestSum < sumNumbers)
+                    {
+                        saveBiggestSum = sumNumbers;
+                        saveSquare = $"{doubleMatrix[row, col]} {doubleMatrix[row, col + 1]}\n" +
+                            $"{doubleMatrix[row + 1, col]} {doubleMatrix[row + 1, col + 1]}";
+                    }
+                }
+            }
+
+            Console.WriteLine(saveSquare);
+            Console.WriteLine(saveBiggestSum);
         }
     }
 }
