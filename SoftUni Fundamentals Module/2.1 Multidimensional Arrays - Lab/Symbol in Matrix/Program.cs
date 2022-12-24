e3df11c63041cd8ec78e4614634719338eb06ef2
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-24 13:32:57 +0200

    Adding Source code for Project - Symbol in Matrix

--- SoftUni Fundamentals Module/2.1 Multidimensional Arrays - Lab/Symbol in Matrix/Program.cs
+++ SoftUni Fundamentals Module/2.1 Multidimensional Arrays - Lab/Symbol in Matrix/Program.cs
@@ -10,7 +10,49 @@ namespace Symbol_in_Matrix
     {
         static void Main(string[] args)
         {
+            int sizeOfMatrix = int.Parse(Console.ReadLine());
 
+            char[,] squareMatrix = new char[sizeOfMatrix, sizeOfMatrix];
+
+            for (int row = 0; row < sizeOfMatrix; row++)
+            {
+                string characters = Console.ReadLine();
+
+                char[] rowArray = characters.ToCharArray();
+
+                for (int col = 0; col < sizeOfMatrix; col++)
+                {
+                    squareMatrix[row, col] = rowArray[col];
+                }
+            }
+
+            char symbol = char.Parse(Console.ReadLine());
+            bool foundSymbol = false;
+            bool notFoundSymbol = true;
+
+            for (int row = 0; row < sizeOfMatrix; row++)
+            {
+                for (int col = 0; col < sizeOfMatrix; col++)
+                {
+                    if (symbol == squareMatrix[row, col])
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        notFoundSymbol = false;
+                        foundSymbol = true;
+                        break;
+                    }
+                }
+
+                if (foundSymbol)
+                {
+                    break;
+                }
+            }
+
+            if (notFoundSymbol)
+            {
+                Console.WriteLine($"{symbol} does not occur in the matrix");
+            }
         }
     }
 }
