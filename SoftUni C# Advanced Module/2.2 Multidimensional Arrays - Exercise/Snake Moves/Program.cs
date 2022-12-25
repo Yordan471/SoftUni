381dc516e467e28785b47a9af138141fcb666c63
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-25 14:07:40 +0200

    Update Program.cs

--- SoftUni C# Advanced Module/2.2 Multidimensional Arrays - Exercise/Snake Moves/Program.cs
+++ SoftUni C# Advanced Module/2.2 Multidimensional Arrays - Exercise/Snake Moves/Program.cs
@@ -10,7 +10,43 @@ namespace Snake_Moves
     {
         static void Main(string[] args)
         {
+            int[] matrixSize = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
 
+            int rows = matrixSize[0];
+            int cols = matrixSize[1];
+
+            string snake = Console.ReadLine();
+
+            char[,] matrix = new char[rows, cols];
+
+            int currIndex = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                if (row % 2 == 0)
+                {
+                    for (int col = 0; col < cols; col++)
+                    {
+                        if (currIndex == snake.Length)
+                        {
+                            currIndex = 0;
+                        }
+
+                        matrix[row, cols] = snake[currIndex];
+
+                        currIndex++;
+                    }
+                }
+                else
+                {
+
+                }
+            }
+
+            
         }
     }
 }
