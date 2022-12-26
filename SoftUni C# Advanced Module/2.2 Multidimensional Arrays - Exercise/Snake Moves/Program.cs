f50577c412f15dc0a246662eee935de0698d1ec1
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-26 09:48:35 +0200

    Adding Source code for Project - Snake Moves

--- SoftUni C# Advanced Module/2.2 Multidimensional Arrays - Exercise/Snake Moves/Program.cs
+++ SoftUni C# Advanced Module/2.2 Multidimensional Arrays - Exercise/Snake Moves/Program.cs
@@ -24,7 +24,7 @@ namespace Snake_Moves
 
             int currIndex = 0;
 
-            for (int row = 0; row < rows; row++)
+            for (int row = 0; row < matrix.GetLength(0); row++)
             {
                 if (row % 2 == 0)
                 {
@@ -35,18 +35,36 @@ namespace Snake_Moves
                             currIndex = 0;
                         }
 
-                        matrix[row, cols] = snake[currIndex];
+                        matrix[row, col] = snake[currIndex];
 
                         currIndex++;
                     }
                 }
                 else
                 {
+                    for (int col = matrix.GetLength(1) - 1; col >= 0; col--)
+                    {
+                        if (currIndex == snake.Length)
+                        {
+                            currIndex = 0;
+                        }
 
+                        matrix[row, col] = snake[currIndex];
+
+                        currIndex++;
+                    }
                 }
             }
 
-            
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write($"{matrix[row, col]}");
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
