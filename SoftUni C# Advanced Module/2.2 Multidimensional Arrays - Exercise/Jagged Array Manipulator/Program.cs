30bd8979310f955bdcb09e49ecd2c80a4e83806b
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-27 11:02:08 +0200

    Adding Source code for Project - Jagged Array Manipulator

--- SoftUni C# Advanced Module/2.2 Multidimensional Arrays - Exercise/Jagged Array Manipulator/Program.cs
+++ SoftUni C# Advanced Module/2.2 Multidimensional Arrays - Exercise/Jagged Array Manipulator/Program.cs
@@ -16,18 +16,86 @@ namespace Jagged_Array_Manipulator
 
             for (int row = 0; row < numberOfRows; row++)
             {
-                int[] rowArray = Console.ReadLine()
+                jagged[row] = Console.ReadLine()
                     .Split(' ')
                     .Select(int.Parse)
                     .ToArray();
+            }
 
-                for (int col = 0; col < rowArray.Length; col++)
+            for (int row = 0; row < jagged.Length - 1; row++)
+            {
+                if (jagged[row].Length == jagged[row + 1].Length)
                 {
-                    jagged[row][col] = rowArray[col];
+                    for (int col = 0; col < jagged[row].Length; col++)
+                    {
+                        jagged[row][col] *= 2;
+                        jagged[row + 1][col] *= 2;
+                    }
+                }
+                else
+                {
+                    if (jagged[row].Length != jagged[row + 1].Length)
+                    {
+                        for (int col = 0; col < jagged[row].Length; col++)
+                        {
+                            jagged[row][col] /= 2;
+                        }
+
+                        for (int col1 = 0; col1 < jagged[row + 1].Length; col1++)
+                        {
+                            jagged[row + 1][col1] /= 2;
+                        }
+                    }
                 }
             }
 
-            
+            string command = string.Empty;
+            bool end = false;
+
+            while (true)
+            {
+                string[] commandToArray = Console.ReadLine()
+                    .Split()
+                    .ToArray();
+
+                string operation = commandToArray[0];
+
+                if (operation == "End")
+                {                   
+                    end = true;
+                    break;
+                }
+
+                int rowIndex = int.Parse(commandToArray[1]);
+                int columnIndex = int.Parse(commandToArray[2]);
+                int value = int.Parse(commandToArray[3]);
+
+                if (rowIndex >= 0 && rowIndex <= jagged.GetLength(0) &&
+                    columnIndex >= 0 && columnIndex <= jagged[rowIndex].Length)
+                {
+                    if (operation == "Add")
+                    {
+                        jagged[rowIndex][columnIndex] += value;
+                    }
+                    else if (operation == "Subtract")
+                    {
+                        jagged[rowIndex][columnIndex] -= value;
+                    }
+                }
+            }
+
+            if (end)
+            {
+                for (int row = 0; row < jagged.GetLength(0); row++)
+                {
+                    for (int col = 0; col < jagged[row].Length; col++)
+                    {
+                        Console.Write($"{jagged[row][col]} ");
+                    }
+
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
