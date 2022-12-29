1973d13578db107f1efab9ccef1edf15f5040fc9
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-29 18:07:08 +0200

    Adding Source code for Project - Average Student Grades

--- SoftUni C# Advanced Module/3.1 Sets and Dictionaries Advanced/Average Student Grades/Program.cs
+++ SoftUni C# Advanced Module/3.1 Sets and Dictionaries Advanced/Average Student Grades/Program.cs
@@ -10,7 +10,37 @@ namespace Average_Student_Grades
     {
         static void Main(string[] args)
         {
+            int numberOfStudents = int.Parse(Console.ReadLine());
 
+            Dictionary<string, List<decimal>> studentAndGrades = new Dictionary<string, List<decimal>>();
+
+            for (int i = 0; i < numberOfStudents; i++)
+            {
+                string[] studentInfo = Console.ReadLine()
+                    .Split(' ');
+
+                string studentName = studentInfo[0];
+                decimal grade = decimal.Parse(studentInfo[1]);
+
+                if (!(studentAndGrades.ContainsKey(studentName)))
+                {
+                    studentAndGrades[studentName] = new List<decimal>();
+                }
+
+                studentAndGrades[studentName].Add(grade);
+            }
+
+            foreach (KeyValuePair<string, List<decimal>> student in studentAndGrades)
+            {
+                Console.Write($"{student.Key} -> ");
+
+                foreach (decimal grade in student.Value)
+                {
+                    Console.Write($"{grade:f2} ");
+                }
+
+                Console.WriteLine($"(avg: {student.Value.Average():f2})");
+            }
         }
     }
 }
