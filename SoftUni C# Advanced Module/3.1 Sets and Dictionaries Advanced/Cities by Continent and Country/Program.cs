6199b46f1b2073912552f9d3f6da4fd1011444a0
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-30 13:25:53 +0200

    Adding Source code for Project - Cities by Continent and Country

--- SoftUni C# Advanced Module/3.1 Sets and Dictionaries Advanced/Cities by Continent and Country/Program.cs
+++ SoftUni C# Advanced Module/3.1 Sets and Dictionaries Advanced/Cities by Continent and Country/Program.cs
@@ -10,7 +10,53 @@ namespace Cities_by_Continent_and_Country
     {
         static void Main(string[] args)
         {
+            int numberOfInputInfo = int.Parse(Console.ReadLine());
 
+            Dictionary<string, Dictionary<string, List<string>>> continentAndCountryCities = 
+                new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < numberOfInputInfo; i++)
+            {
+                string[] continentInfo = Console.ReadLine()
+                    .Split(' ');
+
+                string continent = continentInfo[0];
+                string country = continentInfo[1];
+                string city = continentInfo[2];
+
+                if (!(continentAndCountryCities.ContainsKey(continent)))
+                {
+                    Dictionary<string, List<string>> countryAndCities = new Dictionary<string, List<string>>();
+
+                    countryAndCities[country] = new List<string>() { city };
+                    continentAndCountryCities[continent] = countryAndCities;
+                }
+                else
+                {
+                    if (!(continentAndCountryCities[continent].ContainsKey(country)))
+                    {
+                        continentAndCountryCities[continent][country] = new List<string>() { city };
+                    }
+                    else
+                    {
+                        continentAndCountryCities[continent][country].Add(city);
+                    }
+                }
+            }
+
+            foreach (var continent in continentAndCountryCities)
+            {
+                Console.WriteLine($"{continent.Key}:");
+                
+                foreach (KeyValuePair<string, List<string>> countryCities in continent.Value)
+                {
+                    List<string> cities = new List<string>();
+                    cities = countryCities.Value;
+
+                    Console.Write($"   {countryCities.Key} -> ");
+                    Console.WriteLine(string.Join(", ", cities));
+                }
+            }
         }
     }
 }
