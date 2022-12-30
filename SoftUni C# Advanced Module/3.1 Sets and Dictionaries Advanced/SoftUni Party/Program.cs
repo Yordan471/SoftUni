f353f2a54bf3bca02c154de40b2570b95ce31cf6
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-30 15:53:04 +0200

    Adding Source code for Project - SoftUni Party

--- SoftUni C# Advanced Module/3.1 Sets and Dictionaries Advanced/SoftUni Party/Program.cs
+++ SoftUni C# Advanced Module/3.1 Sets and Dictionaries Advanced/SoftUni Party/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,54 @@ namespace SoftUni_Party
     {
         static void Main(string[] args)
         {
+            HashSet<string> regular = new HashSet<string>();
+            HashSet<string> vip = new HashSet<string>();
 
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "PARTY")
+            {
+                string guestOrParty = command;
+
+                if (guestOrParty.Length == 8)
+                {
+                    if (char.IsDigit(guestOrParty[0]))
+                    {
+                        vip.Add(guestOrParty);
+                    }
+                    else
+                    {
+                        regular.Add(guestOrParty);
+                    }
+                }
+            }
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string resNumber = command;
+
+                if (vip.Contains(resNumber))
+                {
+                    vip.Remove(resNumber);
+                }
+                else if (regular.Contains(resNumber))
+                {
+                    regular.Remove(resNumber);
+                }
+            }
+
+            int countGuestsLeft = vip.Count + regular.Count;
+            Console.WriteLine(countGuestsLeft);
+            if (vip.Count > 0)
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, vip));
+            }
+
+            if (regular.Count > 0)
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, regular));
+            }
+            
         }
     }
 }
