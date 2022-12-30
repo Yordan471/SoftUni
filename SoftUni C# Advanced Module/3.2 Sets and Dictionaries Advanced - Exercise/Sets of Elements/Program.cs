3883afeb195dac0b41c757f4bc6236ec2781ef0f
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-30 23:52:17 +0200

    Update Program.cs

--- SoftUni C# Advanced Module/3.2 Sets and Dictionaries Advanced - Exercise/Sets of Elements/Program.cs
+++ SoftUni C# Advanced Module/3.2 Sets and Dictionaries Advanced - Exercise/Sets of Elements/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,18 @@ namespace Sets_of_Elements
     {
         static void Main(string[] args)
         {
+            int[] setsLength = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
 
+            HashSet<int> set = new HashSet<int>();
+            HashSet<int> saveNumbers = new HashSet<int>();
+
+            int firstSetLength = setsLength[0];
+            int secondSetLength = setsLength[1];
+
+            
         }
     }
 }
