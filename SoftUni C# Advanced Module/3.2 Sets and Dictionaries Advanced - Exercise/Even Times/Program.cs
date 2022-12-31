a628f862b842b6dae315dc1aac4bf16c4e6c8e5f
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-31 09:02:18 +0200

    Adding Input Info + for loop

--- SoftUni C# Advanced Module/3.2 Sets and Dictionaries Advanced - Exercise/Even Times/Program.cs
+++ SoftUni C# Advanced Module/3.2 Sets and Dictionaries Advanced - Exercise/Even Times/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,14 @@ namespace Even_Times
     {
         static void Main(string[] args)
         {
+            int numberOfIntegers = int.Parse(Console.ReadLine());
+
+            HashSet<int> numbers = new HashSet<int>();
+
+            for (int i = 0; i < numberOfIntegers; i++)
+            {
+
+            }
         }
     }
 }
