71950bbdcd07e2bfa594d3a7e2341d105b28d5fd
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-01-01 01:11:35 +0200

    Update Program.cs

--- SoftUni C# Advanced Module/3.2 Sets and Dictionaries Advanced - Exercise/The V-Logger/Program.cs
+++ SoftUni C# Advanced Module/3.2 Sets and Dictionaries Advanced - Exercise/The V-Logger/Program.cs
@@ -10,6 +10,41 @@ namespace The_V_Logger
     {
         static void Main(string[] args)
         {
+            string[] commands = Console.ReadLine()
+                .Split(' ');
+
+            Dictionary<string, Dictionary<string, HashSet<string>>> vloggerAndFollowedFollowers = 
+                new Dictionary<string, Dictionary<string, HashSet<string>>>();
+
+            while (commands[0] != "Statistics")
+            {
+                string vloggerName = commands[0];
+                string operation = commands[1];
+                string followedVLogger = commands[2];
+
+                if (operation == "joined")
+                {
+                    if (!(vloggerAndFollowedFollowers.ContainsKey(vloggerName)))
+                    {
+                        vloggerAndFollowedFollowers[vloggerName] = new Dictionary<string, HashSet<string>>();
+                    }
+                }
+                else if (operation == "followed" && 
+                    vloggerAndFollowedFollowers.ContainsKey(followedVLogger) &&
+                    vloggerAndFollowedFollowers.ContainsKey(vloggerName))
+                {
+                    if (!(vloggerAndFollowedFollowers[followedVLogger].ContainsKey(followedVLogger)))
+                    {
+                        vloggerAndFollowedFollowers[followedVLogger].Add(followedVLogger, new HashSet<string>());
+                    }
+
+                    if (vloggerName != followedVLogger)
+                    {
+                        vloggerAndFollowedFollowers[followedVLogger][followedVLogger].Add(vloggerName);
+                    }                   
+                }
+            }
+
 
         }
     }
