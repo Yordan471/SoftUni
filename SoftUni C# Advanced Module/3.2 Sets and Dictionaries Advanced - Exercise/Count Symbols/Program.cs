dc38823fb1c9ba9142e77df185b4f636867be286
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-31 11:23:26 +0200

    Adding Source code for Project - Count Symbols

--- SoftUni C# Advanced Module/3.2 Sets and Dictionaries Advanced - Exercise/Count Symbols/Program.cs
+++ SoftUni C# Advanced Module/3.2 Sets and Dictionaries Advanced - Exercise/Count Symbols/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,24 @@ namespace Count_Symbols
     {
         static void Main(string[] args)
         {
+            string inputInfo = Console.ReadLine();
 
+            Dictionary<char, int> countChars = new Dictionary<char, int>();
+
+            for (int i = 0; i < inputInfo.Length; i++)
+            {
+                if (!(countChars.ContainsKey(inputInfo[i])))
+                {
+                    countChars[inputInfo[i]] = 0;
+                }
+
+                countChars[inputInfo[i]]++;
+            }
+
+            foreach (var ch in countChars.Keys.OrderBy(c => c))
+            {
+                Console.WriteLine($"{ch}: {countChars[ch]} time/s");
+            }
         }
     }
 }
