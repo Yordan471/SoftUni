707b133205cb5788786181b85142b1e748246cee
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-31 08:59:48 +0200

    Adding Source code for Project - Periodic Table

--- SoftUni C# Advanced Module/3.2 Sets and Dictionaries Advanced - Exercise/Periodic Table/Program.cs
+++ SoftUni C# Advanced Module/3.2 Sets and Dictionaries Advanced - Exercise/Periodic Table/Program.cs
@@ -10,7 +10,22 @@ namespace Periodic_Table
     {
         static void Main(string[] args)
         {
+            int countOfInputLines = int.Parse(Console.ReadLine());
 
+            HashSet<string> chemicalElements = new HashSet<string>();
+
+            for (int i = 0; i < countOfInputLines; i++)
+            {
+                string[] elements = Console.ReadLine()
+                    .Split(' ');
+
+                for (int j = 0; j < elements.Length; j++)
+                {
+                    chemicalElements.Add(elements[j]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", chemicalElements.OrderBy(el => el)));
         }
     }
 }
