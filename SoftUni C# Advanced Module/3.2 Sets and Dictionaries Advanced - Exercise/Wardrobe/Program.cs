49356cc6d85d85ad8873ccca698e347b600f7beb
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2022-12-31 14:14:01 +0200

    Adding Source code for Project - Wardrobe

--- SoftUni C# Advanced Module/3.2 Sets and Dictionaries Advanced - Exercise/Wardrobe/Program.cs
+++ SoftUni C# Advanced Module/3.2 Sets and Dictionaries Advanced - Exercise/Wardrobe/Program.cs
@@ -12,11 +12,62 @@ namespace Wardrobe
         {
             int clothesToWear = int.Parse(Console.ReadLine());
 
-            Dictionary<string, Dictionary<string, int>> colorAndClothesNumbers = new Dictionary<string, Dictionary<string, int>>(); 
+            Dictionary<string, List<string>> colorAndClothesNumbers = new Dictionary<string, List<string>>(); 
 
             for (int i = 0; i < clothesToWear; i++)
             {
+                string[] clothesInfo = Console.ReadLine()
+                    .Split(new string[] { " -> " }, StringSplitOptions.None);
 
+                string color = clothesInfo[0];
+                string[] clothes = clothesInfo[1]
+                    .Split(',');
+
+                if (!(colorAndClothesNumbers.ContainsKey(color)))
+                {
+                    colorAndClothesNumbers[color] = new List<string>();
+                }
+
+                foreach (string cloth in clothes)
+                {
+                    colorAndClothesNumbers[color].Add(cloth);
+                }
+            }
+
+            string[] colorCloth = Console.ReadLine()
+                .Split(' ');
+
+            string searchColor = colorCloth[0];
+            string searchCloth = colorCloth[1];
+
+            foreach (KeyValuePair<string, List<string>> color in colorAndClothesNumbers)
+            {
+                Console.WriteLine($"{color.Key} clothes:");
+
+                List<string> clothesList = new List<string>();
+                clothesList = color.Value.ToList();
+
+                foreach (string clothes in clothesList.Distinct())
+                {
+                    int countClothes = 0;
+
+                    for (int i = 0; i < color.Value.Count; i++)
+                    {
+                        if (clothes == color.Value[i])
+                        {
+                            countClothes++;
+                        }
+                    }
+
+                    if (color.Key == searchColor && searchCloth == clothes)
+                    {
+                        Console.WriteLine($"* {clothes} - {countClothes} (found!)");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"* {clothes} - {countClothes}");
+                    }                   
+                }
             }
         }
     }
