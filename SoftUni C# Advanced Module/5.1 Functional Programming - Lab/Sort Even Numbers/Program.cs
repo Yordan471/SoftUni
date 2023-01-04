ce1db38854a339e59c01f4396c91b19037b1d5e9
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-01-04 20:31:35 +0200

    Adding folder - Functional Programming - Lab; Adding Project - Sort Even Numbers

---
+++ SoftUni C# Advanced Module/5.1 Functional Programming - Lab/Sort Even Numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
