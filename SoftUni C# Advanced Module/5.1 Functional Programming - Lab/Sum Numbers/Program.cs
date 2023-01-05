a66eb08ae82106cc13e554e07b4025a7d6b9c073
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-01-05 18:59:45 +0200

    Adding Source code for Project - Sum Numbers

--- SoftUni C# Advanced Module/5.1 Functional Programming - Lab/Sum Numbers/Program.cs
+++ SoftUni C# Advanced Module/5.1 Functional Programming - Lab/Sum Numbers/Program.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace Sum_Numbers
 {
+    public delegate int Aggregate(int a, int b);
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            int[] lineOfIntegers = Console.ReadLine()
+                .Split(new string[] {", "}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(num => int.Parse(num))
+                .ToArray();
+
+            Console.WriteLine(lineOfIntegers.Length);
+            Console.WriteLine(lineOfIntegers.Sum());
 
         }
     }
