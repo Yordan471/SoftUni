d418c8637a02fa9d3703eafb42a394439afc57fb
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-01-05 18:21:05 +0200

    Adding Source code for Project - Sort Even Numbers

--- SoftUni C# Advanced Module/5.1 Functional Programming - Lab/Sort Even Numbers/Program.cs
+++ SoftUni C# Advanced Module/5.1 Functional Programming - Lab/Sort Even Numbers/Program.cs
@@ -10,7 +10,14 @@ namespace Sort_Even_Numbers
     {
         static void Main(string[] args)
         {
+            int[] numbers = (Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .Where(x => x % 2 == 0)
+                .OrderBy(x => x)
+                .ToArray());
 
+            Console.WriteLine(string.Join(", ", numbers));
         }
     }
 }
