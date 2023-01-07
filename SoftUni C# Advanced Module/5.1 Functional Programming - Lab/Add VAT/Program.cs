f75bf21dd3e2c32ceb6665b3dba663433fa43a72
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-01-07 09:22:30 +0200

    Adding Project - Add VAT

---
+++ SoftUni C# Advanced Module/5.1 Functional Programming - Lab/Add VAT/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Add_VAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
