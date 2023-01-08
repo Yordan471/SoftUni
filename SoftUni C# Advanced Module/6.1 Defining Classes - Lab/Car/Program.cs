a624e7ca6a08dc25bc8c9a817486b6bdb26899e8
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-01-08 15:29:34 +0200

    changing Namespace to CarManufacturer and Class Program to Car

--- SoftUni C# Advanced Module/6.1 Defining Classes - Lab/Car/Program.cs
+++ SoftUni C# Advanced Module/6.1 Defining Classes - Lab/Car/Program.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Car
+namespace CarManufacturer
 {
-    internal class Program
+    internal class Car
     {
         static void Main(string[] args)
         {
