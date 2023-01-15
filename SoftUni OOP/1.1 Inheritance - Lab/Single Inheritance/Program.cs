0284f767cadefae5d33e1178cc3033583ce3ea5b
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-01-15 19:44:04 +0200

    Adding folder OOP; Adding folder 1.1 Inheritance; Adding Project - single Inheitance

---
+++ SoftUni OOP/1.1 Inheritance - Lab/Single Inheritance/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
