795553c0a459ea6309367d8e38a00482a829fb79
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-01-15 13:29:48 +0200

    Adding Source code for Project - Cups And Bottles

--- SoftUni C# Advanced Module/1.2 Stacks and Queues - Exercises/Cups And Bottles/Program.cs
+++ SoftUni C# Advanced Module/1.2 Stacks and Queues - Exercises/Cups And Bottles/Program.cs
@@ -10,7 +10,60 @@ namespace Cups_And_Bottles
     {
         static void Main(string[] args)
         {
+            int[] cupsInfo = Console.ReadLine()
+                .Split()
+                .Select(x => int.Parse(x))
+                .ToArray();
 
+            int[] bottlesInfo = Console.ReadLine()
+                .Split()
+                .Select(x => int.Parse(x))
+                .ToArray();
+                
+            Queue<int> cups = new Queue<int>(cupsInfo);
+            Stack<int> bottles = new Stack<int>(bottlesInfo);
+            int wastedLittersOfWater = 0;
+
+            while (cups.Count > 0 && bottles.Count > 0) 
+            {
+                if (cups.Peek() <= bottles.Peek())
+                {
+                    wastedLittersOfWater += bottles.Pop() - cups.Dequeue();
+                }
+                else
+                {
+                    int cupCapacity = cups.Peek();
+
+                    while (true)
+                    {
+                        if (cupCapacity - bottles.Peek() > 0)
+                        {
+                            cupCapacity -= bottles.Peek();
+                            bottles.Pop();
+                        }
+                        else
+                        {
+                            wastedLittersOfWater += bottles.Pop() - cupCapacity;
+                            cups.Dequeue();
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (cups.Count == 0)
+            {
+                Console.Write($"Bottles: ");
+                Console.WriteLine(string.Join(" ", bottles));
+                Console.WriteLine($"Wasted litters of water: {wastedLittersOfWater}");
+            }
+
+            if (bottles.Count == 0)
+            {
+                Console.Write($"Cups: ");
+                Console.WriteLine(string.Join(" ", cups));
+                Console.WriteLine($"Wasted litters of water: {wastedLittersOfWater}");
+            }
         }
     }
 }
