d0e24f33a80c05dc32c88205889113a019f2dc70
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-01-19 19:01:15 +0200

    Adding diff solution

--- SoftUni C# Advanced Module/3.2 Sets and Dictionaries Advanced - Exercise/Even Times/Program.cs
+++ SoftUni C# Advanced Module/3.2 Sets and Dictionaries Advanced - Exercise/Even Times/Program.cs
@@ -13,31 +13,50 @@ namespace Even_Times
         {
             int numberOfIntegers = int.Parse(Console.ReadLine());
 
-            HashSet<int> numbers = new HashSet<int>();
-            HashSet<int> saveNumbers = new HashSet<int>();
+            Dictionary<int, int> numbersAndCounts = new 
+                Dictionary<int, int>();
 
             for (int i = 0; i < numberOfIntegers; i++)
             {
                 int number = int.Parse(Console.ReadLine());
 
-                if (!(numbers.Contains(number)))
+                if (!numbersAndCounts.ContainsKey(number))
                 {
-                    numbers.Add(number);
+                    numbersAndCounts.Add(number, 0 );
                 }
-                else
-                {                    
-                    if (saveNumbers.Contains(number))
-                    {
-                        saveNumbers.Remove(number);
-                    }
-                    else
-                    {
-                        saveNumbers.Add(number);
-                    }                   
-                }               
-            }
 
-            Console.WriteLine(string.Join("", saveNumbers));
+                numbersAndCounts[number]++;
+            }
+            // Single method returns if there is only one element as count
+            // else fals
+            Console.WriteLine(numbersAndCounts.Single(x => x.Value % 2 == 0).Key);
+            
+
+          //  HashSet<int> numbers = new HashSet<int>();
+          //  HashSet<int> saveNumbers = new HashSet<int>();
+          //
+          //  for (int i = 0; i < numberOfIntegers; i++)
+          //  {
+          //      int number = int.Parse(Console.ReadLine());
+          //
+          //      if (!(numbers.Contains(number)))
+          //      {
+          //          numbers.Add(number);
+          //      }
+          //      else
+          //      {                    
+          //          if (saveNumbers.Contains(number))
+          //          {
+          //              saveNumbers.Remove(number);
+          //          }
+          //          else
+          //          {
+          //              saveNumbers.Add(number);
+          //          }                   
+          //      }               
+          //  }
+          //
+          //  Console.WriteLine(string.Join("", saveNumbers));
         }
     }
 }
