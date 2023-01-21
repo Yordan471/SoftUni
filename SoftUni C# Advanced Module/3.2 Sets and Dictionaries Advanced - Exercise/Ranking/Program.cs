f0c2f40dea5db33f3f0bbfe470f2fc631bc96341
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-01-21 15:13:09 +0200

    Adding input information and filing Dictionaries;

--- SoftUni C# Advanced Module/3.2 Sets and Dictionaries Advanced - Exercise/Ranking/Program.cs
+++ SoftUni C# Advanced Module/3.2 Sets and Dictionaries Advanced - Exercise/Ranking/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,56 @@ namespace Ranking
     {
         static void Main(string[] args)
         {
+            Dictionary<Dictionary<string, string>, Dictionary<string, int>> passwordContestAndUserPoints = new
+                Dictionary<Dictionary<string, string>, Dictionary<string, int>>();
 
+            string command = string.Empty;
+
+            while((command = Console.ReadLine()) != "end of contests")
+            {
+                string[] contestInfo = command
+                    .Split(':');
+
+                string contestName = contestInfo[0];
+                string password = contestInfo[1];
+
+                foreach (var contestPassword in passwordContestAndUserPoints.Keys)
+                {
+                    if(!contestPassword.ContainsKey(contestName))
+                    {
+                        contestPassword.Add(contestName, password);
+                    }
+                }
+            }
+
+            string secondCommand = string.Empty;
+
+            while ((secondCommand = Console.ReadLine()) != "end of submissions")
+            {
+                string[] userInfo = secondCommand
+                    .Split(new string[] {"=>"}, StringSplitOptions.RemoveEmptyEntries);
+
+                string contest = userInfo[0];
+                string password = userInfo[1];
+                string username = userInfo[2];
+                int points = int.Parse(userInfo[3]);
+
+                foreach (var contestPassword in passwordContestAndUserPoints)
+                {                  
+                    if (contestPassword.Key.ContainsKey(contest))
+                    {
+                        if (contestPassword.Key[contest] == password)
+                        {
+                            if (!contestPassword.Value.ContainsKey(username))
+                            {
+                                contestPassword.Value.Add(username, 0);
+                            }
+
+                            contestPassword.Value[username] += points;                           
+                        }
+                    }                 
+                }
+            }
         }
     }
 }
