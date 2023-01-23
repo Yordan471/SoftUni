95ea20d1ae5a769bc515edcf5ece8eb1902b2d90
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-01-23 18:26:15 +0200

    Adding initial code for Project - Even Lines

--- SoftUni C# Advanced Module/4.2 Streams, Files and Directories - Exercises/EvenLines/EvenLines.cs
+++ SoftUni C# Advanced Module/4.2 Streams, Files and Directories - Exercises/EvenLines/EvenLines.cs
@@ -1,6 +1,8 @@
 ﻿namespace EvenLines
 {
     using System;
+    using System.IO;
+
     public class EvenLines
     {
         static void Main()
@@ -12,7 +14,19 @@
 
         public static string ProcessLines(string inputFilePath)
         {
-            throw new NotImplementedException();
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                string line = string.Empty;
+
+                int counter = 0;
+
+                while (true)
+                {
+                    line = reader.ReadLine();
+
+                    counter++;
+                }
+            }
         }
     }
 }
