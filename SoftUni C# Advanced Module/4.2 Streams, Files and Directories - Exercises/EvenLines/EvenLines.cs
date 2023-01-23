2acfc0c44b2c812a21955afc9b1093cdc6ffea42
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-01-23 18:40:15 +0200

    Adding finished code for Project - Even Lines

--- SoftUni C# Advanced Module/4.2 Streams, Files and Directories - Exercises/EvenLines/EvenLines.cs
+++ SoftUni C# Advanced Module/4.2 Streams, Files and Directories - Exercises/EvenLines/EvenLines.cs
@@ -2,6 +2,8 @@
 {
     using System;
     using System.IO;
+    using System.Linq;
+    using System.Text;
 
     public class EvenLines
     {
@@ -14,19 +16,54 @@
 
         public static string ProcessLines(string inputFilePath)
         {
+            StringBuilder sb  = new StringBuilder();
+
             using (StreamReader reader = new StreamReader(inputFilePath))
             {
                 string line = string.Empty;
 
                 int counter = 0;
 
-                while (true)
+                while (line != null)
                 {
                     line = reader.ReadLine();
+
+                    if (counter % 2 == 0)
+                    {
+                        string replacedSymbols = ReplacedSymbols(line);
+                        string reversedWords = ReversedWords(replacedSymbols);
+                        sb.Append(reversedWords);
+                    }
 
                     counter++;
                 }
             }
+
+            return sb.ToString();
+        }
+
+        private static string ReversedWords(string replacedSymbols)
+        {
+            string[] reversedWords = replacedSymbols
+                .Split(' ')
+                .Reverse()
+                .ToArray();
+
+            return string.Join(" ", reversedWords);
+        }
+
+        private static string ReplacedSymbols(string line)
+        {
+            StringBuilder sb = new StringBuilder(line);
+
+            string[] symbolsToReplace = { "-", ", ", ". ", "! ", "? " };
+
+            foreach (var symbol in symbolsToReplace)
+            {
+                sb.Replace(symbol, "@");
+            }
+
+            return sb.ToString();
         }
     }
 }
