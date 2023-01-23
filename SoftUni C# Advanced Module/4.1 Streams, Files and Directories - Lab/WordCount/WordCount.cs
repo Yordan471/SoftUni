baef44324a8d504acd75d1cbefd010f24a6d2cf1
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-01-23 07:13:13 +0200

    Update WordCount.cs

--- SoftUni C# Advanced Module/4.1 Streams, Files and Directories - Lab/WordCount/WordCount.cs
+++ SoftUni C# Advanced Module/4.1 Streams, Files and Directories - Lab/WordCount/WordCount.cs
@@ -8,15 +8,59 @@
     {
         static void Main()
         {
+            Dictionary<string, int> wordsAndCount = new
+                Dictionary<string, int>();
+
             string wordPath = @"..\..\..\Files\words.txt";
             string textPath = @"..\..\..\Files\text.txt";
             string outputPath = @"..\..\..\Files\output.txt";
 
-            CalculateWordCounts(wordPath, textPath, outputPath);
+            CalculateWordCounts(wordPath, textPath, outputPath, wordsAndCount);
         }
 
-        public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
+        public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath, 
+            Dictionary<string, int> wordsAndCount)
         {
+            using (StreamReader readerWords = new StreamReader(wordsFilePath))
+            {
+                string word = string.Empty;
+
+                while ((word = readerWords.ReadLine()) != null)
+                {
+                    string[] words = word.Split(' ');
+
+                    foreach (string wrd in words)
+                    {
+                        wordsAndCount.Add(wrd, 0);
+                    }                 
+                }
+            }
+
+            using (StreamReader readerText = new StreamReader(textFilePath))
+            {
+                string line = string.Empty;
+
+                while ((line = readerText.ReadLine()) != null)
+                {
+                    foreach (var word in wordsAndCount)
+                    {
+                       
+                        if (line.Contains(word.Key))
+                        {
+                            wordsAndCount[word.Key] += 1;
+                        }
+                    }
+                        
+                }
+            }
+
+            using (StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                foreach (var word in wordsAndCount)
+                {
+                    writer.WriteLine($"{word.Key} - {word.Value}");
+                }
+            }
         }
     }
 }
