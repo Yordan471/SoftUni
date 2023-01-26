2417d210075963a866942c9b552d01250d0d0d32
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-01-26 19:25:45 +0200

    adding Project - Reverse and Exclide

---
+++ SoftUni C# Advanced Module/5.2 Functional Programming - Exercises/Reverse and Exclude/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_and_Exclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
