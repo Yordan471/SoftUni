b1ede9fa08d401660f54ffe8b6bf1de38a4f8462
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-01-27 15:15:27 +0200

    adding Source code for Project - Reverse and Exclude

--- SoftUni C# Advanced Module/5.2 Functional Programming - Exercises/Reverse and Exclude/Program.cs
+++ SoftUni C# Advanced Module/5.2 Functional Programming - Exercises/Reverse and Exclude/Program.cs
@@ -10,7 +10,44 @@ namespace Reverse_and_Exclude
     {
         static void Main(string[] args)
         {
-            
+            List<int> numbers = Console.ReadLine()
+                .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(number => int.Parse(number))
+                .ToList();
+
+            Func<List<int>, List<int>> reverse = numberss =>
+            {
+                List<int> result = new List<int>();
+
+                for (int i = numbers.Count - 1; i >= 0; i--)
+                {
+                    result.Add(numbers[i]);
+                }
+
+                return result;
+            };
+
+            Func<List<int>, Predicate<int>, List<int>> excludeDivisible = (numberss, match) =>
+            {
+                List<int> result = new List<int>();
+
+                foreach (int num in numbers)
+                {
+                    if (!match(num))
+                    {
+                        result.Add(num);
+                    }
+                }
+
+                return result;
+            };
+
+            int divisible = int.Parse(Console.ReadLine());
+
+            numbers = excludeDivisible(numbers, n => n % divisible == 0);
+            numbers = reverse(numbers);
+
+            Console.WriteLine(string.Join(" ", numbers));
         }
 
        
