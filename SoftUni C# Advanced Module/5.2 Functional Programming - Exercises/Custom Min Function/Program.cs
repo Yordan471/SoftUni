d39bd6cf9a1aa3c2a60bdcbab356cb69bed25ca3
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-01-27 13:00:48 +0200

    Adding Source code for Project - Custom Min Function

--- SoftUni C# Advanced Module/5.2 Functional Programming - Exercises/Custom Min Function/Program.cs
+++ SoftUni C# Advanced Module/5.2 Functional Programming - Exercises/Custom Min Function/Program.cs
@@ -10,7 +10,27 @@ namespace Custom_Min_Function
     {
         static void Main(string[] args)
         {
+            int[] integers = Console.ReadLine()
+                .Split(new string[] {" "}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(number => int.Parse(number))
+                .ToArray();
 
+            Func<int[], int> minNumber = numbers =>
+            {
+                int smallestNumber = 10000;
+
+                foreach (int number in numbers)
+                {
+                    if (number < smallestNumber)
+                    {
+                        smallestNumber = number;
+                    }
+                }
+
+                return smallestNumber;
+            };
+
+            Console.WriteLine(minNumber(integers));
         }
     }
 }
