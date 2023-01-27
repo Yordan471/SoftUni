bd3c75d3f972cc81ad1102b8773c1bce7075de71
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-01-27 14:47:31 +0200

    Adding Source code for Project - Applied Arithmetics

--- SoftUni C# Advanced Module/5.2 Functional Programming - Exercises/Applied Arithmetics/Program.cs
+++ SoftUni C# Advanced Module/5.2 Functional Programming - Exercises/Applied Arithmetics/Program.cs
@@ -10,7 +10,50 @@ namespace Applied_Arithmetics
     {
         static void Main(string[] args)
         {
+            Func<List<int>, string, List<int>> operations =
+               (numberss, operation) =>
+               {
+                   List<int> result = new List<int>();
 
+                   foreach (int number in numberss)
+                   {
+                       switch (operation)
+                       {
+                           case "add":
+                               result.Add(number + 1);
+                               break;
+                           case "multiply":
+                               result.Add(number * 2);
+                               break;
+                           case "subtract":
+                               result.Add(number - 1);
+                               break;
+                       }
+                   }
+
+                   return result;
+               };
+
+            List<int> numbers = Console.ReadLine()
+                .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(number => int.Parse(number))
+                .ToList();
+
+            Action<List<int>> print = numberss => Console.WriteLine(string.Join(" ", numbers)); ;
+              
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                if (command == "print")
+                {
+                    print(numbers);
+                }
+                else
+                {
+                    numbers = operations(numbers, command);
+                }
+            }         
         }
     }
 }
