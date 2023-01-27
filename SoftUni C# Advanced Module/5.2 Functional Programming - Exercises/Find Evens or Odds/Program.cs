a1fade044cb4baaf29dc6b5a1aa48597718d139e
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-01-27 13:39:21 +0200

    Adding Source code for Project - Find Evens or Odds

--- SoftUni C# Advanced Module/5.2 Functional Programming - Exercises/Find Evens or Odds/Program.cs
+++ SoftUni C# Advanced Module/5.2 Functional Programming - Exercises/Find Evens or Odds/Program.cs
@@ -10,7 +10,51 @@ namespace Find_Evens_or_Odds
     {
         static void Main(string[] args)
         {
+            
 
+            int[] ranges = Console.ReadLine()
+                .Split(new string[] {" "}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(range => int.Parse(range))
+                .ToArray();
+
+            int lowRange = ranges[0];
+            int highRange = ranges[1];
+
+            string oddOrEven = Console.ReadLine();
+            List<int> numbers = new List<int>();
+
+            Func<int, int, List<int>> numbersInRange = (firstRange, secondRange) =>
+            {
+                List<int> numsInRange = new List<int>();
+
+                for (int range = lowRange; range < highRange; range++)
+                {
+                    numsInRange.Add(range);
+                }
+
+                return numsInRange;
+            };
+
+            numbers = numbersInRange(lowRange, highRange);
+
+            Predicate<int> match = null;
+
+            if (oddOrEven == "odd")
+            {
+                match = number => number % 2 == 1;
+            }
+            else if (oddOrEven == "even")
+            {
+                match = number => number % 2 == 0;
+            }
+
+            foreach (int num in numbers)
+            {
+                if (match(num))
+                {
+                    Console.Write($"{num} ");
+                }
+            }
         }
     }
 }
