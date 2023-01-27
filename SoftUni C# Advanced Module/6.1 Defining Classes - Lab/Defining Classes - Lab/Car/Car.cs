48e0a2b841440791ac5e9144ab5eb7392f97c8c6
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-01-27 21:00:03 +0200

    Adding Project - Car and Car Extension

---
+++ SoftUni C# Advanced Module/6.1 Defining Classes - Lab/Defining Classes - Lab/Car/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+    }
+}
---
+++ SoftUni C# Advanced Module/6.1 Defining Classes - Lab/Defining Classes - Lab/Car/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Car car = new Car();
+
+            car.Make = "VW";
+            car.Model = "MK3";
+            car.Year= 1992;
+
+
+        }
+    }
+}
+
