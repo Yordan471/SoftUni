a4d1149428d776beae23ed8bcaa4bdb54e7576fe
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-01-28 12:27:45 +0200

    Adding Class Engine

---
+++ SoftUni C# Advanced Module/6.1 Defining Classes - Lab/Defining Classes - Lab/Car Engine and Tires/Engine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Engine_and_Tires
+{
+    public class Engine
+    {
+
+    }
+}
