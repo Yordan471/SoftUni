b8a413fbc59786b591ff91d2c6d939a829c0b952
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-01-28 18:45:01 +0200

    Adding Source code for Project - The Party Reservation Filter

--- SoftUni C# Advanced Module/5.2 Functional Programming - Exercises/Party Reservation Filter Module/Program.cs
+++ SoftUni C# Advanced Module/5.2 Functional Programming - Exercises/Party Reservation Filter Module/Program.cs
@@ -1,1 +1,53 @@
-﻿
+﻿using System.Security.Cryptography.X509Certificates;
+
+List<string> people = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .ToList();
+
+Dictionary<string, Predicate<string>> filters = new();
+
+string command = string.Empty;
+
+while ((command = Console.ReadLine()) != "Print")
+{
+    string[] operations = command
+        .Split(";", StringSplitOptions.RemoveEmptyEntries);
+
+    string operation = operations[0];
+    string filter = operations[1];
+    string value = operations[2];
+
+    if (operation == "Add filter")
+    {
+        filters.Add(filter + value, GetPredicate(filter, value));
+    }
+    else
+    {
+        filters.Remove(filter + value);
+    }
+}
+
+foreach (var filter in filters)
+{
+    people.RemoveAll(filter.Value);
+}
+
+ Console.WriteLine(string.Join(" ", people));
+
+
+static Predicate<string> GetPredicate(string filter, string value)
+{
+    switch (filter)
+    {
+        case "Starts with":
+            return x => x.StartsWith(value);
+        case "Ends with":
+            return x => x.EndsWith(value);
+        case "Contains":
+            return x => x.Contains(value);
+        case "Length":
+            return x => x.Length == int.Parse(value);
+        default:
+            return default;
+    }
+}
