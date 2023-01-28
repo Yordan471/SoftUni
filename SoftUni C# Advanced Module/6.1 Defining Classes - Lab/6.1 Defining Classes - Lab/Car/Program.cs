87697b5c049a5ce051e58debcade62c920b32794
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-01-28 14:07:20 +0200

    Update Program.cs

--- SoftUni C# Advanced Module/6.1 Defining Classes - Lab/6.1 Defining Classes - Lab/Car/Program.cs
+++ SoftUni C# Advanced Module/6.1 Defining Classes - Lab/6.1 Defining Classes - Lab/Car/Program.cs
@@ -2,23 +2,21 @@
 {
     public class StartUp
     {
+         static void Main()
+         {
+             Tire[] tires = new Tire[4]
+             {
+             new Tire(1, 2.5),
+             new Tire(2, 3),
+             new Tire(3, 3.5),
+             new Tire(4, 4),
+             };
 
-            static void Main()
-            {
-                Tire[] tires = new Tire[4]
-                {
-                new Tire(1, 2.5),
-                new Tire(2, 3),
-                new Tire(3, 3.5),
-                new Tire(4, 4),
-                };
+             Engine engine = new(560, 6300);
 
-                Engine engine = new(560, 6300);
+             Car car = new("Lamborghini", "Urus", 2010, 250, 9, engine, tires);
 
-                Car car = new("Lamborghini", "Urus", 2010, 250, 9, engine, tires);
-
-                Console.WriteLine($"Car {car.Make}, Model {car.Model}, Year {car.Year}");
-            }
-        
+             Console.WriteLine($"Car {car.Make}, Model {car.Model}, Year {car.Year}");
+         }      
     }
 }
