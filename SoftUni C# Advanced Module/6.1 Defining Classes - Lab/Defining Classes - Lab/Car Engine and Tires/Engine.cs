5ce5703ef2233520dacd0af1b90965ccc1e6c010
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-01-28 12:30:02 +0200

    filling Class engine

--- SoftUni C# Advanced Module/6.1 Defining Classes - Lab/Defining Classes - Lab/Car Engine and Tires/Engine.cs
+++ SoftUni C# Advanced Module/6.1 Defining Classes - Lab/Defining Classes - Lab/Car Engine and Tires/Engine.cs
@@ -8,6 +8,17 @@ namespace Car_Engine_and_Tires
 {
     public class Engine
     {
+        private int horsePower;
+        private double cubicCapacity;
 
+        public Engine(int horsePower, double cubicCapacity)
+        {
+            HorsePower = horsePower;
+            CubicCapacity = cubicCapacity;
+        }
+
+        public int HorsePower { get; set; }
+
+        public double CubicCapacity { get; set; }
     }
 }
