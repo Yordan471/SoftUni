9883ebd19fa4a2d42fc08688dde8607fd7c8bee5
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-01-28 15:53:07 +0200

    adding Source code for Project - Predicate For Names

--- SoftUni C# Advanced Module/5.2 Functional Programming - Exercises/Predicate for Names/Program.cs
+++ SoftUni C# Advanced Module/5.2 Functional Programming - Exercises/Predicate for Names/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿int namesLength = int.Parse(Console.ReadLine());
+
+string[] names = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+Predicate<string> match = name => name.Length <= namesLength;
+Func<string[], Predicate<string>, List<string>> asd = (names, match) =>
+{
+    List<string> currNames = new List<string>();
+
+    foreach (string name in names)
+    {
+        if (match(name))
+        {
+            currNames.Add(name);
+        }
+    }
+
+    return currNames;
+};
+
+List<string> filteredNames = new();
+
+filteredNames = asd(names, match);
+
+Console.WriteLine(string.Join(Environment.NewLine, filteredNames));
+
+
+    
