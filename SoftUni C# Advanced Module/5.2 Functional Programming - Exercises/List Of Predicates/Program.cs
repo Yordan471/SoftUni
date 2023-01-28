290c3afa40ba159503140358f13b1d5514c032d0
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-01-28 17:06:15 +0200

    Adding Source code for Project - List of Predicates

--- SoftUni C# Advanced Module/5.2 Functional Programming - Exercises/List Of Predicates/Program.cs
+++ SoftUni C# Advanced Module/5.2 Functional Programming - Exercises/List Of Predicates/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿using System;
+
+int endOfRange = int.Parse(Console.ReadLine());
+
+List<Predicate<int>> predicates = new();
+
+HashSet<int> dividers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(num => int.Parse(num))
+    .ToHashSet();
+
+int[] numbers  = Enumerable.Range(1, endOfRange).ToArray();
+
+foreach (int divider in dividers)
+{
+    predicates.Add(num => num % divider == 0);
+}
+
+foreach (int number in numbers)
+{
+    bool isDivisible = true;
+
+    foreach (var match in predicates)
+    {
+        if (!match(number))
+        {
+            isDivisible = false;
+        }
+    }
+
+    if (isDivisible)
+    {
+        Console.WriteLine($"{number} ");
+    }
+}
+
