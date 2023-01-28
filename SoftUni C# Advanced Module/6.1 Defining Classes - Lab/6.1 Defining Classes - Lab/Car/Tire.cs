10244ac43657e8cdfb32a1870cca5cb244d6e705
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-01-28 14:06:55 +0200

    Adding Source code to 6.1

---
+++ SoftUni C# Advanced Module/6.1 Defining Classes - Lab/6.1 Defining Classes - Lab/Car/Tire.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Tire
+    {
+        private int year;
+        private double pressure;
+
+        public Tire(int year, double pressure)
+        {
+            this.Year = year;
+            this.Pressure = pressure;
+        }
+
+        public int Year { get; set; }
+
+        public double Pressure { get; set; }
+    }
+}
