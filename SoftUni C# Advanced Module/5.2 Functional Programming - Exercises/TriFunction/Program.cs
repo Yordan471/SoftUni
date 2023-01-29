1bba0ad357c1e25c10565f60d3c2472dfa4ae699
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-01-29 11:59:21 +0200

    Adding Source code for Project - TrrFunction

--- SoftUni C# Advanced Module/5.2 Functional Programming - Exercises/TriFunction/Program.cs
+++ SoftUni C# Advanced Module/5.2 Functional Programming - Exercises/TriFunction/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using System;
+
+Func<string, int, bool> checkEqualOrLargerNameSum = (name, sum) =>
+{
+   // name.Sum(ch => ch) >= sum
+    int charsSum = name.Sum(ch => ch);
+
+    return charsSum >= sum;
+};
+
+Func<string[], int, Func<string, int, bool>, string> getFirstName =
+   // (names, sum, match) => names.FirstOrDefault(name => match(name, sum));
+   (names, sum, match) =>
+   {
+       foreach (var name in names)
+       {
+           if (match(name, sum))
+           {
+               return name;
+           }
+       }
+
+       return null;
+   };
+int sum = int.Parse(Console.ReadLine());
+
+string[] names = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+Console.WriteLine(getFirstName(names, sum, checkEqualOrLargerNameSum));
