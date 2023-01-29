c5fcfeb2e8af698a4e807440b4ab9f98f97c719e
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-01-29 13:00:38 +0200

    filling Class Family

--- SoftUni C# Advanced Module/6.1 Defining Classes - Lab/6.2 Defining Classes - Exercise/Oldest Family Member/Family.cs
+++ SoftUni C# Advanced Module/6.1 Defining Classes - Lab/6.2 Defining Classes - Exercise/Oldest Family Member/Family.cs
@@ -1,13 +1,33 @@
-﻿using System;
+﻿using DefiningClasses;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Oldest_Family_Member
+namespace DefiningClasses
 {
-    internal class Family
+    public class Family
     {
+        List<Person> people = null;
 
+        public Family()
+        {
+            People = new List<Person>();
+        }
+
+        public List<Person> People { get; set; }
+
+        void AddMember(Person person)
+        {
+            People.Add(person);
+        }
+
+        public Person GetOldestMember(List<Person> People)
+        {
+            return People
+                .OrderByDescending(p => p.Age)
+                .FirstOrDefault();
+        }
     }
 }
--- SoftUni C# Advanced Module/6.1 Defining Classes - Lab/6.2 Defining Classes - Exercise/Oldest Family Member/Program.cs
+++ SoftUni C# Advanced Module/6.1 Defining Classes - Lab/6.2 Defining Classes - Exercise/Oldest Family Member/Program.cs
@@ -4,7 +4,19 @@
     {
         public static void Main()
         {
+            int numberOfPeople = int.Parse(Console.ReadLine());
 
+            for (int i = 0; i < numberOfPeople; i++)
+            {
+                string[] personInfo = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string name = personInfo[0];
+                int age = int.Parse(personInfo[1]);
+
+                Person person = new Person(name, age);
+                
+            }
         }
     }
 }
