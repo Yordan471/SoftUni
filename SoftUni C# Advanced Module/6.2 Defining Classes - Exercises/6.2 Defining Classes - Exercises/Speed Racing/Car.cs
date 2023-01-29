97249338d85fea817e924548538a71b1f39cec89
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-01-29 15:00:53 +0200

    Adding Source code for Project - Speed Racing

---
+++ SoftUni C# Advanced Module/6.2 Defining Classes - Exercises/6.2 Defining Classes - Exercises/Speed Racing/Car.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Car
+    {
+        //private List<Car> cars;
+
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKilometer, double travelledDistance)
+        {
+            this.Model = model;
+            this.FuelAmount = fuelAmount;
+            this.FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+            this.TravelledDistance = travelledDistance;
+        }
+
+        public Car()
+        {
+            this.Cars = new List<Car>();
+        }
+        public string Model { get; set; }
+
+        public double FuelAmount { get; set; }
+
+        public double FuelConsumptionPerKilometer { get; set; }
+
+        public double TravelledDistance { get; set; }
+
+        public List<Car> Cars { get; set; }
+
+        public void GetVehicles(Car vehicle)
+        {
+            Cars.Add(vehicle);
+        }
+
+        public void Drive(int distance)
+        {
+            double neededFuel = distance * this.FuelConsumptionPerKilometer;
+
+            if (neededFuel <= this.FuelAmount)
+            {
+                FuelAmount -= neededFuel;
+                TravelledDistance += distance;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive") ;
+            }
+        }
+    }
+}
--- SoftUni C# Advanced Module/6.2 Defining Classes - Exercises/6.2 Defining Classes - Exercises/Speed Racing/Program.cs
+++ SoftUni C# Advanced Module/6.2 Defining Classes - Exercises/6.2 Defining Classes - Exercises/Speed Racing/Program.cs
@@ -1,1 +1,58 @@
 ﻿
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int numberOfCars = int.Parse(Console.ReadLine());
+
+            Car cars = new Car();
+
+            for (int car = 0; car < numberOfCars; car++)
+            {
+                string[] carInfo = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string model = carInfo[0];
+                double fuelAmount = double.Parse(carInfo[1]);
+                double fuelConsumptionPerKilometer = double.Parse(carInfo[2]);
+                double travelledDistance = 0.0;
+
+                Car vehicle = new(model, fuelAmount, fuelConsumptionPerKilometer, travelledDistance);
+
+                cars.GetVehicles(vehicle);
+            }
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] commandInfo = command
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string model = commandInfo[1];
+                int amountOfKilometers = int.Parse(commandInfo[2]);
+
+                foreach (Car car in cars.Cars)
+                {
+                    if (car.Model == model)
+                    {
+                        car.Drive(amountOfKilometers);
+                    }
+                }
+            }
+
+            PrintCars(cars);
+        }
+
+        private static void PrintCars(Car cars)
+        {
+            foreach (Car car in cars.Cars)
+            {
+                Console.WriteLine($"{car.Model} {car.FuelAmount:F2} {car.TravelledDistance}");
+            }
+        }
+    }
+}
