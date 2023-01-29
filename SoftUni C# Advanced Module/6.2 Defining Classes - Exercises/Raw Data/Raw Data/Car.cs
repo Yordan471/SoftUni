92ab663d4ceb58dff58454e5d34893bf44132686
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-01-29 21:17:27 +0200

    Adding Source code for Project - Raw Data

---
+++ SoftUni C# Advanced Module/6.2 Defining Classes - Exercises/Raw Data/Raw Data/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    internal class Car
+    {
+       //public Car()
+       //{
+       //    Cars = new();
+       //}
+
+        public Car(
+           string model, int engineSpeed, int enginePower, int cargoWeight,
+                   string cargoType, double firstTirePressure, int firstTireAge, double secondTirePressure,
+                  int secondTireAge, double thirdTirePressure, int thirdTireAge, double fourthTirePressure,
+                   int fourthTireAge)
+        {
+            Model = model;
+
+            Engine = new Engine { Speed = engineSpeed, Power = enginePower };
+
+            Cargo = new Cargo { Weight = cargoWeight, Type = cargoType };
+
+            Tires = new Tires[4];
+            Tires[0] = new Tires { Pressure = firstTirePressure, Age = firstTireAge };
+            Tires[1] = new Tires { Pressure = secondTirePressure, Age = secondTireAge };
+            Tires[2] = new Tires { Pressure = thirdTirePressure, Age = thirdTireAge };
+            Tires[3] = new Tires { Pressure = fourthTirePressure, Age = fourthTireAge };
+        }
+
+        public string Model { get; set; }
+
+        public Engine Engine { get; set; }
+
+        public Cargo Cargo { get; set; }
+
+        public Tires[] Tires { get; set; }
+
+        //public List<Car> Cars { get; set; }
+    }
+}
---
+++ SoftUni C# Advanced Module/6.2 Defining Classes - Exercises/Raw Data/Raw Data/StartUp.cs
@@ -0,0 +1,81 @@
+﻿
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int numberOfCars = int.Parse(Console.ReadLine());
+
+            List<Car> cars = new();
+           // Car cars = new();
+           // cars.Cars = new();
+
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                string[] carInfo = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string model = carInfo[0];
+                int engineSpeed = int.Parse(carInfo[1]);
+                int enginePower = int.Parse(carInfo[2]);
+                int cargoWeight = int.Parse(carInfo[3]);
+                string cargoType = carInfo[4];
+                double firstTirePressure = double.Parse(carInfo[5]);
+                int firstTireAge = int.Parse(carInfo[6]);
+                double secondTirePressure = double.Parse(carInfo[7]);
+                int secondTireAge = int.Parse(carInfo[8]);
+                double thirdTirePressure = double.Parse(carInfo[9]);
+                int thirdTireAge = int.Parse(carInfo[10]);
+                double fourthTirePressure = double.Parse(carInfo[11]);
+                int fourthTireAge = int.Parse(carInfo[12]);
+
+                Car car = new(model, engineSpeed, enginePower, cargoWeight,
+                    cargoType, firstTirePressure, firstTireAge, secondTirePressure,
+                    secondTireAge, thirdTirePressure, thirdTireAge, fourthTirePressure,
+                    fourthTireAge);
+
+                cars.Add(car);
+            }
+
+            string command = Console.ReadLine();
+
+            if (command == "fragile")
+            {
+                List<Car> fragileCars = new();
+                fragileCars = cars.Where(c => c.Cargo.Type == "fragile").ToList();
+
+                foreach (var car in fragileCars)
+                {
+                    bool isBelowOne = false;
+
+                    for (int i = 0; i < 4; i++)
+                    {
+                        if (car.Tires[i].Pressure < 1)
+                        {
+                            isBelowOne = true;
+                            break;
+                        }
+                    }
+
+                    if (isBelowOne)
+                    {
+                        Console.WriteLine(car.Model);
+                    }                  
+                }
+            }
+            else if (command == "flammable")
+            {
+                List<Car> fragileCars = new();
+                fragileCars = cars
+                    .Where(c => c.Cargo.Type == "flammable" && c.Engine.Power > 250)
+                    .ToList();
+
+                foreach (var car in fragileCars)
+                {
+                    Console.WriteLine(car.Model);
+                }
+            }
+        }
+    }
+}
