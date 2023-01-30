934bbc6d297199802922e36c23806d01032d0e29
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-01-30 07:48:08 +0200

    filling classes with Information

--- SoftUni C# Advanced Module/6.2 Defining Classes - Exercises/CarSalesman/CarSalesman/Car.cs
+++ SoftUni C# Advanced Module/6.2 Defining Classes - Exercises/CarSalesman/CarSalesman/Car.cs
@@ -8,6 +8,12 @@ namespace CarSalesman
 {
     public class Car
     {
+        public string Model { get; set; }
 
+        public Engine Engine { get; set; }
+
+        public int Weight { get; set; }
+
+        public string Color { get; set; }
     }
 }
--- SoftUni C# Advanced Module/6.2 Defining Classes - Exercises/CarSalesman/CarSalesman/Engine.cs
+++ SoftUni C# Advanced Module/6.2 Defining Classes - Exercises/CarSalesman/CarSalesman/Engine.cs
@@ -8,6 +8,12 @@ namespace CarSalesman
 {
     public class Engine
     {
+        public string Model { get; set; }
 
+        public int Power { get; set; }
+
+        public int Displacement { get; set; }
+
+        public string Efficiency { get; set; }
     }
 }
