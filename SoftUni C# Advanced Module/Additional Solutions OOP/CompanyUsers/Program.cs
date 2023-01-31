e6a0f7ae03ba4d0bb0d1f0cf921c66ff405bb506
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-01-31 16:31:27 +0200

    Adding Source code for Project - CompanyUsers

---
+++ SoftUni C# Advanced Module/Additional Solutions OOP/CompanyUsers/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+
+namespace CompanyUsers
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string command = string.Empty;
+
+            List<Company> companies = new List<Company>();
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] companyInfo = command
+                    .Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+
+                string companyName = companyInfo[0];
+                string id = companyInfo[1];
+
+                if (!companies.Any(c => c.CompanyName.Equals(companyName)))
+                {
+                    Company company = new(companyName);
+                    company.employeeIDs.Add(id);
+                    companies.Add(company);
+                    continue;
+                }
+
+                companies
+                    .Find(c => c.CompanyName
+                    .Equals(companyName))
+                    .employeeIDs.Add(id);
+            }
+
+            foreach (Company company in companies)
+            {
+                Console.WriteLine(company);             
+            }
+        }
+    }
+
+    public class Company
+    {
+        public Company()
+        {
+            this.CompanyName = string.Empty;
+            this.employeeIDs = new();
+        }
+
+        public Company(string CompanyName) : this()
+        {
+            this.CompanyName = CompanyName;
+        }
+
+        public string CompanyName { get; set; }
+
+        public HashSet<string> employeeIDs { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new();
+
+            sb.AppendLine(this.CompanyName);
+
+            foreach(string employee in employeeIDs)
+            {
+                sb.AppendLine($"-- {employee}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
