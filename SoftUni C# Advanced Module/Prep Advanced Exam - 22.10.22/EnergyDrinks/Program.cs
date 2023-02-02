afe480a6208d27d15fa76b7d7ff1cb9654182701
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-02 10:33:25 +0200

    Adding Source code for Project - EnergyDrink

--- SoftUni C# Advanced Module/Prep Advanced Exam - 22.10.22/EnergyDrinks/Program.cs
+++ SoftUni C# Advanced Module/Prep Advanced Exam - 22.10.22/EnergyDrinks/Program.cs
@@ -1,4 +1,6 @@
-﻿int[] miligramsOfCaffein = Console.ReadLine()
+﻿using System.ComponentModel.Design;
+
+int[] miligramsOfCaffein = Console.ReadLine()
     .Split(", ", StringSplitOptions.RemoveEmptyEntries)
     .Select(m => int.Parse(m))
     .ToArray();
@@ -12,7 +14,47 @@ Stack<int> caffeine = new(miligramsOfCaffein);
 Queue<int> eDrinks = new(energyDrinks);
 
 int maxCaffeine = 0;
+bool noCaffeine = false;
+bool noDrinks = false;
 
+while (caffeine.Count != 0 && eDrinks.Count != 0)
+ {
+    if (!(caffeine.Peek() * eDrinks.Peek() + maxCaffeine > 300))
+    {
+        maxCaffeine += caffeine.Pop() * eDrinks.Dequeue();
+    }
+    else
+    {
+        caffeine.Pop();
+        int saveDrink = eDrinks.Dequeue();
 
+        int[] shuffleDrinks = new int[eDrinks.Count + 1];
+        shuffleDrinks[eDrinks.Count] = saveDrink;
+        int countIndex = 0;
+
+        while (eDrinks.Count != 0)
+        {
+            shuffleDrinks[countIndex++] = eDrinks.Dequeue();
+        }
+
+        eDrinks = new(shuffleDrinks);
+
+        if (!(maxCaffeine - 30 < 0))
+        {
+            maxCaffeine -= 30;
+        }        
+    }
+}
+
+if (caffeine.Count == 0 && eDrinks.Count !=0)
+{
+    Console.WriteLine($"Drinks left: {string.Join(", ", eDrinks)}");
+}
+else
+{
+    Console.WriteLine($"At least Stamat wasn't exceeding the maximum caffeine.");
+}
+
+Console.WriteLine($"Stamat is going to sleep with {maxCaffeine} mg caffeine.");
 
 
