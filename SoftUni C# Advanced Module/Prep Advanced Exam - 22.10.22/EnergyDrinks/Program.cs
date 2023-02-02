48951c56ac386e376e208e661b128ae60344c80b
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-02 09:33:07 +0200

    read input

--- SoftUni C# Advanced Module/Prep Advanced Exam - 22.10.22/EnergyDrinks/Program.cs
+++ SoftUni C# Advanced Module/Prep Advanced Exam - 22.10.22/EnergyDrinks/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿int[] miligramsOfCaffein = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(m => int.Parse(m))
+    .ToArray();
+
+int[] energyDrinks = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(m => int.Parse(m))
+    .ToArray();
+
+Stack<int> caffeine = new(miligramsOfCaffein);
+Queue<int> eDrinks = new(energyDrinks);
+
+int maxCaffeine = 0;
+
+
+
+
