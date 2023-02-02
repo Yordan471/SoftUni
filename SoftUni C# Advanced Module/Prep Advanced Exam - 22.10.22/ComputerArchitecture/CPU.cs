a3a4777d0b03e1fd9e4f35c0cb1916c5edda9412
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-02 14:08:33 +0200

    filling class CPU

--- SoftUni C# Advanced Module/Prep Advanced Exam - 22.10.22/ComputerArchitecture/CPU.cs
+++ SoftUni C# Advanced Module/Prep Advanced Exam - 22.10.22/ComputerArchitecture/CPU.cs
@@ -1,6 +1,38 @@
-﻿namespace ComputerArchitecture
+﻿using System.Text;
+
+namespace ComputerArchitecture
 {
     public class CPU
     {
+        public CPU()
+        {
+            this.Brand = string.Empty;
+            this.Cores = 0;
+            this.Frequency = 0;
+        }
+
+        public CPU(string brand, int cores, double frequency) : this()
+        {
+            Brand = brand;
+            Cores = cores;
+            Frequency = frequency;
+        }
+
+        public string Brand { get; set; }
+
+        public int Cores { get; set; }
+
+        public double Frequency { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new();
+
+            sb.AppendLine($"{this.Brand} CPU:");
+            sb.AppendLine($"Cores: {this.Cores}");
+            sb.AppendLine($"Frequency: {this.Frequency}:f1");
+
+            return sb.ToString();
+        }
     }
 }
