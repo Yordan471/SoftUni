4a94de21280bd434eec19405b1c7715a189b90dd
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-02 19:34:36 +0200

    asd

--- SoftUni C# Advanced Module/Prep Advanced Exam - 18.8.22/BaristaContest/Program.cs
+++ SoftUni C# Advanced Module/Prep Advanced Exam - 18.8.22/BaristaContest/Program.cs
@@ -1,103 +1,94 @@
-﻿int[] coffeeQuantities = Console.ReadLine()
-    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
-    .Select(c => int.Parse(c))
-    .ToArray();
-
-int[] milkQuantities = Console.ReadLine()
-    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
-    .Select(c => int.Parse(c))
-    .ToArray();
-
-Queue<int> coffeeQ = new(coffeeQuantities);
-Stack<int> milkQ = new(milkQuantities);
-
-Dictionary<string, Dictionary<int, int>> coffeeAndPriceQuantity = new Dictionary<string, Dictionary<int, int>>
+﻿namespace BaristaContest
 {
-    { "Cortado",
-    new Dictionary<int, int> {{ 50, 0 }}}
-    ,
-    { "Espreso",
-        new Dictionary<int, int> {{75, 0}}
-    },
-    { "Capuccino",
-        new Dictionary<int, int> {{100, 0}}
-    },
-    { "Americano",
-        new Dictionary<int, int>{{ 150, 0 }}
-    },
-    {"Latte", 
-        new Dictionary < int, int > {{ 200, 0 }} 
-    }
-};
-
-    
-    
-    
-    
-
-while(coffeeQ.Count != 0 && milkQ.Count != 0)
-{
-    int mixedAmount = 0;
-    mixedAmount = coffeeQ.Peek() + milkQ.Peek();
-
-    if (coffeeAndPriceQuantity.Values.Any(c => c.ContainsKey(mixedAmount)))
+    public class Program
     {
-        foreach (var coffe in coffeeAndPriceQuantity.Values)
+        public static void Main()
         {
-            if (coffe.ContainsKey(mixedAmount))
+            int[] coffeeQuantities = Console.ReadLine()
+                 .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                 .Select(c => int.Parse(c))
+                 .ToArray();
+
+            int[] milkQuantities = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(c => int.Parse(c))
+                .ToArray();
+
+            Queue<int> coffeeQ = new(coffeeQuantities);
+            Stack<int> milkQ = new(milkQuantities);
+
+            Dictionary<string, Dictionary<int, int>> coffeeAndPriceQuantity = new Dictionary<string, Dictionary<int, int>>
+             {
+                 { "Cortado",
+                 new Dictionary<int, int> {{ 50, 0 }}}
+                 ,
+                 { "Espreso",
+                     new Dictionary<int, int> {{75, 0}}
+                 },
+                 { "Capuccino",
+                     new Dictionary<int, int> {{100, 0}}
+                 },
+                 { "Americano",
+                     new Dictionary<int, int>{{ 150, 0 }}
+                 },
+                 {"Latte",
+                     new Dictionary < int, int > {{ 200, 0 }}
+                 }
+             };
+
+            while (coffeeQ.Count != 0 && milkQ.Count != 0)
             {
-                coffe[mixedAmount]++;
+                int mixedAmount = 0;
+                mixedAmount = coffeeQ.Peek() + milkQ.Peek();
+
+                if (coffeeAndPriceQuantity.Values.Any(c => c.ContainsKey(mixedAmount)))
+                {
+                    foreach (var coffe in coffeeAndPriceQuantity.Values)
+                    {
+                        if (coffe.ContainsKey(mixedAmount))
+                        {
+                            coffe[mixedAmount]++;
+                            coffeeQ.Dequeue();
+                            milkQ.Pop();
+                            continue;
+                        }
+                    }
+                }
+
                 coffeeQ.Dequeue();
-                milkQ.Pop();
-                continue;
+                int currMilk = milkQ.Pop() - 5;
+                milkQ.Push(currMilk);
+            }
+
+            if (coffeeQ.Count == 0 && milkQ.Count == 0)
+            {
+                Console.WriteLine("Nina is going to win! She used all the coffee and milk!");
+            }
+            else
+            {
+                Console.WriteLine("Nina needs to exercise more! She didn't use all the coffee and milk!");
+            }
+
+            if (coffeeQ.Count > 0)
+            {
+                Console.WriteLine($"Coffee left: {string.Join(", ", coffeeQ)}");
+            }
+            else
+            {
+                Console.WriteLine("Coffee left: none");
+            }
+
+            if (milkQ.Count > 0)
+            {
+                Console.WriteLine($"Coffee left: {string.Join(", ", milkQ)}");
+            }
+            else
+            {
+                Console.WriteLine("Milk left: none");
             }
         }
     }
-
-    coffeeQ.Dequeue();
-    int currMilk = milkQ.Pop() - 5;
-    milkQ.Push(currMilk);
-}
-
-if (coffeeQ.Count == 0 && milkQ.Count == 0)
-{
-    Console.WriteLine("Nina is going to win! She used all the coffee and milk!");
-}
-else
-{
-    Console.WriteLine("Nina needs to exercise more! She didn't use all the coffee and milk!");
-}
-
-if (coffeeQ.Count > 0)
-{
-    Console.WriteLine($"Coffee left: {string.Join(", ", coffeeQ)}");
-}
-else
-{
-    Console.WriteLine("Coffee left: none");
-}
-
-if (milkQ.Count > 0)
-{
-    Console.WriteLine($"Coffee left: {string.Join(", ", milkQ)}");
-}
-else
-{
-    Console.WriteLine("Milk left: none");
 }
 
 
-foreach (var coffee in coffeeAndPriceQuantity
-    .OrderBy(c => c.Value.Values)
-    .ThenBy(c => c.Key))
-{
-    Dictionary<int, int> values = coffee.Value;
-    
-    foreach (var kvp in values)
-    {
-        if (kvp.Value > 0)
-        {
-            Console.WriteLine($"{coffee.Key}: {kvp.Value}");
-        }      
-    }
-}
+
