a227edaab2573cd353d3bb3e3f281b2ab399d17b
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-04 19:16:48 +0200

    filling class Renovator

--- SoftUni C# Advanced Module/Prep Advanced  Exam - 25.6.22/Renovators/Renovator.cs
+++ SoftUni C# Advanced Module/Prep Advanced  Exam - 25.6.22/Renovators/Renovator.cs
@@ -8,6 +8,31 @@ namespace Renovators
 {
     public class Renovator
     {
+        private string Name;
+        private string Type;
+        private double Rate;
+        private int Days;
+        private bool Hired;
+
+        public Renovator(string name, string type, double rate, int days)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.Rate = rate;
+            this.Days = days;
+            Hired = false;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"-Renovator: {Name}");
+            sb.AppendLine($"--Specialty: {Type}");
+            sb.AppendLine($"--Rate per day: {Rate} BGN");
+
+            return sb.ToString().Trim();
+        }
 
     }
 }
