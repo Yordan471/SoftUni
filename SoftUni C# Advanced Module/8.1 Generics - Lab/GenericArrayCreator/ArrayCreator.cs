3ef0e3e5c3cbdb0b51dd99f73ef574fcded96615
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-04 10:04:32 +0200

    filling class ArrayCreator

--- SoftUni C# Advanced Module/8.1 Generics - Lab/GenericArrayCreator/ArrayCreator.cs
+++ SoftUni C# Advanced Module/8.1 Generics - Lab/GenericArrayCreator/ArrayCreator.cs
@@ -6,8 +6,20 @@ using System.Threading.Tasks;
 
 namespace GenericArrayCreator
 {
-    public class ArrayCreator
+    public class ArrayCreator<T>
     {
+        private T[] array;
 
+        static T[] Creater(int length, T Item)
+        {
+            T[] array = new T[length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Item;
+            }
+
+            return array;
+        }        
     }
 }
