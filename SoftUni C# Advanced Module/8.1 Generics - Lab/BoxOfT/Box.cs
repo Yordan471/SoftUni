7195c811aeca349021857206e7a0bc85d4cb85ec
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-04 09:39:47 +0200

    minor changes

--- SoftUni C# Advanced Module/8.1 Generics - Lab/BoxOfT/Box.cs
+++ SoftUni C# Advanced Module/8.1 Generics - Lab/BoxOfT/Box.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace BoxOfT
 {
-    internal class Box
+    public class Box<T>
     {
+        
+        private List<T> list;
+
+        private int count;
+
+        public Box()
+        {
+           // count = list.Count;
+            list = new List<T> { };
+        }
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+        public void Add(T element)
+        {
+           list.Add(element);
+        }
+
+        public T Remove()
+        {
+            T removedElement = list[list.Count - 1];
+            list.Remove(removedElement);
+
+            return removedElement;
+        }
     }
 }
