b7f5991b96be24d973a2aabee5b0b028d5acec39
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-04 12:33:49 +0200

    reading Input info

--- SoftUni C# Advanced Module/Prep Advanced  Exam - 25.6.22/WallDestroyer/Program.cs
+++ SoftUni C# Advanced Module/Prep Advanced  Exam - 25.6.22/WallDestroyer/Program.cs
@@ -10,6 +10,29 @@ namespace WallDestroyer
     {
         static void Main(string[] args)
         {
+            int wallSize = int.Parse(Console.ReadLine());
+
+            string[,] wall = new string[wallSize, wallSize];
+            int saveRow = 0;
+            int saveCol = 0;    
+
+            for (int row = 0; row < wall.GetLength(0); row++)
+            {
+                string rowInfo = Console.ReadLine();
+
+                for (int col = 0; col < wall.GetLength(1); col++)
+                {
+                    wall[row, col] = rowInfo[col].ToString();
+
+                    if (wall[row, col] == "V")
+                    {
+                        saveRow = row;
+                        saveCol = col;
+                        wall[row, col] = "*";
+                    }
+                }
+            }
+
 
         }
     }
