70045b0b0dfe51aa09f9c0f2193027f6cf560602
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-05 18:34:29 +0200

    Adding Project Zoo

---
+++ SoftUni C# Advanced Module/Prep Advanced Exam - 13.4.22/Zoo/Zoo/Animal.cs
@@ -0,0 +1,31 @@
+﻿namespace Zoo
+{
+    public class Animal
+    {
+        private string species;
+        private string diet;
+        private double weight;
+        private double length;
+
+        public Animal (string species, string diet, double weight, double length)
+        {
+            this.Species = species;
+            this.Diet = diet;
+            this.Weight = weight;
+            this.Length = length;
+        }
+
+        public string Species { get; private set; }
+
+        public string Diet { get; private set; }
+
+        public double Weight { get; private set; }
+
+        public double Length { get; private set; }
+
+        public override string ToString()
+        {
+            return $"The {this.Species} is a {this.Diet} and weighs {this.Weight} kg.";
+        }
+    }
+}
---
+++ SoftUni C# Advanced Module/Prep Advanced Exam - 13.4.22/Zoo/Zoo/Zoo.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace Zoo
+{
+    public class Zoo
+    {
+        private string name;
+        private int capacity;
+        private List<Animal> animals;
+
+        public Zoo(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            animals = new List<Animal>();
+        }
+
+        public string  Name { get; private set; }
+
+        public int Capacity { get; private set; }
+
+        public IReadOnlyCollection<Animal> Animals => this.animals;
+
+        public string AddAnimal(Animal animal)
+        {
+            if (string.IsNullOrWhiteSpace(animal.Species))
+            {
+                return "Invalid animal species.";
+            }
+            else if (animal.Diet != "herbivore" && animal.Diet != "carnivore")
+            {
+                return "Invalid animal diet.";
+            }
+            else if (Animals.Count == this.Capacity)
+            {
+                return "The zoo is full.";
+            }
+            else
+            {
+                animals.Add(animal);
+                return $"Successfully added {animal.Species} to the zoo.";
+            }
+        }
+
+        public int RemoveAnimals(string species)
+        {
+            int countRemovedAnimals = 0;
+
+            if (Animals.Any(a => a.Species == species))
+            {
+                while (Animals.FirstOrDefault(a => a.Species == species) != null)
+                {
+                    Animal removeAnimal = Animals.FirstOrDefault(a => a.Species == species);
+                    animals.Remove(removeAnimal);
+                    countRemovedAnimals++;
+                }
+            }
+
+            return countRemovedAnimals;
+        }
+
+        public List<Animal> GetAnimalsByDiet(string diet)
+        {
+            List<Animal> animalsWithSpecificDiet = new List<Animal>();
+
+            foreach (Animal animal in Animals)
+            {
+                if (animal.Diet == diet)
+                {
+                    animalsWithSpecificDiet.Add(animal);
+                }
+            }
+
+            return animalsWithSpecificDiet;
+        }
+
+        public Animal GetAnimalByWeight(double weight)
+        {
+            return Animals.FirstOrDefault(a => a.Weight == weight);
+        }
+
+        public string GetAnimalCountByLength(double minimumLength, double maximumLength)
+        {
+            int count = 0;
+
+            foreach (Animal animal in Animals)
+            {
+                if (animal.Length >= minimumLength && animal.Length <= maximumLength)
+                {
+                    count++;
+                }
+            }
+
+            return $"There are {count} animals with a length between {minimumLength} and {maximumLength} meters.";
+        }
+    }
+}
