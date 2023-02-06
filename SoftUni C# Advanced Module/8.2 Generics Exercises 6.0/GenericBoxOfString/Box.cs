9e2ef498a2d71cd8c58741531a42b7d0aef5a204
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-06 18:35:36 +0200

    adding Source code for Project- BoxOfString

---
+++ SoftUni C# Advanced Module/8.2 Generics Exercises 6.0/GenericBoxOfString/Box.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericBoxOfString
+{
+    public class Box<T>
+    {
+        private List<T> items { get; set; }
+
+        public Box()
+        {
+            items = new List<T>();
+        }
+
+        public void AddItem(T item)
+        {
+            items.Add(item);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (T item in items)
+            {
+                sb.AppendLine($"{typeof(T)}: {item}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ SoftUni C# Advanced Module/8.2 Generics Exercises 6.0/GenericBoxOfString/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using GenericBoxOfString;
+
+Box<string> box = new Box<string>();
+
+int count = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < count; i++)
+{
+    string item = Console.ReadLine();
+    box.AddItem(item);
+}
+
+Console.WriteLine(box.ToString());
