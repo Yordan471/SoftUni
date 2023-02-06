3e48995f350a56f33d1c4e468d7cb60f4bb75b07
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-06 18:45:52 +0200

    adding project - Generic Swap Method String

--- SoftUni C# Advanced Module/8.2 Generics Exercises 6.0/GenericBoxOfInteger/Program.cs
+++ SoftUni C# Advanced Module/8.2 Generics Exercises 6.0/GenericBoxOfInteger/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using GenericBoxOfInteger;
+
+Box<int> box = new Box<int>();
+
+int count = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < count; i++)
+{
+    int item = int.Parse(Console.ReadLine());
+    box.AddItem(item);
+}
+
+Console.WriteLine(box.ToString());
