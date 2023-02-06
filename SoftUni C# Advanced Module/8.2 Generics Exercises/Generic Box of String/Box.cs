58169a015368234719ec8ddecb6060bfe937f530
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-06 18:16:02 +0200

    Add Project BoxOfString

---
+++ SoftUni C# Advanced Module/8.2 Generics Exercises/Generic Box of String/Box.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Box_of_String
+{
+    public class Box
+    {
+
+    }
+}
---
+++ SoftUni C# Advanced Module/8.2 Generics Exercises/Generic Box of String/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Box_of_String
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
