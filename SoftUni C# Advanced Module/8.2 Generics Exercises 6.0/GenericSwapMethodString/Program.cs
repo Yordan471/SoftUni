28dfe689d8dfd428e4f59ffddce1fa84483c9d60
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-06 18:55:04 +0200

    add project - Swap Method Integer

--- SoftUni C# Advanced Module/8.2 Generics Exercises 6.0/GenericSwapMethodString/Program.cs
+++ SoftUni C# Advanced Module/8.2 Generics Exercises 6.0/GenericSwapMethodString/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using GenericSwapMethodStrings;
+
+Box<string> box = new Box<string>();
+
+int count = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < count; i++)
+{
+    string item = Console.ReadLine();
+    box.AddItem(item);
+}
+
+int[] indices = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(x => int.Parse(x))
+    .ToArray();
+
+box.Swap(indices[0], indices[1]);
+
+Console.WriteLine(box.ToString());
