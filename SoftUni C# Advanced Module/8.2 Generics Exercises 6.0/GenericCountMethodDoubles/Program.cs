e5c2fd17701f551a46af61c06d644aa2b155f227
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-06 20:14:39 +0200

    adding source code

--- SoftUni C# Advanced Module/8.2 Generics Exercises 6.0/GenericCountMethodDoubles/Program.cs
+++ SoftUni C# Advanced Module/8.2 Generics Exercises 6.0/GenericCountMethodDoubles/Program.cs
@@ -1,1 +1,17 @@
 ﻿
+
+using GenericCountMethodDouble;
+
+Box<double> box = new Box<double>();
+
+int count = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < count; i++)
+{
+    double item = double.Parse(Console.ReadLine());
+    box.AddItem(item);
+}
+
+double compareItem = double.Parse(Console.ReadLine());
+
+Console.WriteLine(box.Count(compareItem));
