2d0e134aaed0f63868f772828690fd1d434ceb9f
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-06 18:33:02 +0200

    adding anew

--- SoftUni C# Advanced Module/8.2 Generics Exercises/Generic Box of String/Box.cs
+++ SoftUni C# Advanced Module/8.2 Generics Exercises/Generic Box of String/Box.cs
@@ -6,8 +6,30 @@ using System.Threading.Tasks;
 
 namespace Generic_Box_of_String
 {
-    public class Box
+    public class Box<T>
     {
+        private List<T> items { get; set; }
 
+        public Box()
+        {
+            items = new List<T>();
+        }
+
+        public void AddItem(T item)
+        {
+            items.Add(item);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (T item in items)
+            {
+                sb.AppendLine($"{typeof(T)}: {item}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
     }
 }
--- SoftUni C# Advanced Module/8.2 Generics Exercises/Generic Box of String/Program.cs
+++ SoftUni C# Advanced Module/8.2 Generics Exercises/Generic Box of String/Program.cs
@@ -10,7 +10,17 @@ namespace Generic_Box_of_String
     {
         static void Main(string[] args)
         {
+            Box<string> box = new Box<string>();
 
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string item = Console.ReadLine();
+                box.AddItem(item);
+            }
+
+            Console.WriteLine(box.ToString());
         }
     }
 }
