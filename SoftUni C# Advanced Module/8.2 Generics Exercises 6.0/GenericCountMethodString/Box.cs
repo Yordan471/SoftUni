8844a95edf050c8e7b64dee09593214d068b14e2
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-06 20:12:25 +0200

    add project -Generic Count Method Doubles

---
+++ SoftUni C# Advanced Module/8.2 Generics Exercises 6.0/GenericCountMethodString/Box.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCountMethodString
+{
+    public class Box<T> where T : IComparable<T>
+    {
+        private List<T> items { get; set; }
+
+        public Box()
+        {
+            items = new List<T>();
+        }
+
+        public void AddItem(T item)
+        {
+            items.Add(item);
+        }
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            // tupple
+            (items[secondIndex], items[firstIndex]) =
+                (items[firstIndex], items[secondIndex]);
+        }
+
+        public int Count(T itemToCompare)
+        {
+            int count = 0;
+
+            foreach (T item in items)
+            {
+                if (item.CompareTo(itemToCompare) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (T item in items)
+            {
+                sb.AppendLine($"{typeof(T)}: {item}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
--- SoftUni C# Advanced Module/8.2 Generics Exercises 6.0/GenericCountMethodString/Program.cs
+++ SoftUni C# Advanced Module/8.2 Generics Exercises 6.0/GenericCountMethodString/Program.cs
@@ -1,1 +1,16 @@
 ﻿
+using GenericCountMethodString;
+
+Box<string> box = new Box<string>();
+
+int count = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < count; i++)
+{
+    string item = Console.ReadLine();
+    box.AddItem(item);
+}
+
+string compareItem = Console.ReadLine();
+
+Console.WriteLine(box.Count(compareItem));
