a2c199b387ef6396b8afd459b1a20f8f301c97af
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-06 18:58:38 +0200

    add project - Generic Count Method String

---
+++ SoftUni C# Advanced Module/8.2 Generics Exercises 6.0/SwapMethodInteger/Box.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapMethodStringsIntegers
+{
+    public class Box<T>
+    {
+        private List<T> items { get; set; }
+
+        public Box()
+        {
+            items = new List<T>();
+        }
+
+        public void AddItem(T item)
+        {
+            items.Add(item);
+        }
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            // tupple
+            (items[secondIndex], items[firstIndex]) =
+                (items[firstIndex], items[secondIndex]);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (T item in items)
+            {
+                sb.AppendLine($"{typeof(T)}: {item}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
--- SoftUni C# Advanced Module/8.2 Generics Exercises 6.0/SwapMethodInteger/Program.cs
+++ SoftUni C# Advanced Module/8.2 Generics Exercises 6.0/SwapMethodInteger/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SwapMethodStringsIntegers;
+
+Box<int> box = new Box<int>();
+
+int count = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < count; i++)
+{
+    int item = int.Parse(Console.ReadLine());
+    box.AddItem(item);
+}
+
+int[] indices = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(x => int.Parse(x))
+    .ToArray();
+
+box.Swap(indices[0], indices[1]);
+
+Console.WriteLine(box.ToString());
