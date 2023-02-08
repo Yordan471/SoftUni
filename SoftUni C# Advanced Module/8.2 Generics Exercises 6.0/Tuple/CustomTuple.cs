c63d8dfc5eafd95206cbe389a34abaf769011165
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-08 21:41:37 +0200

    ovveride method

--- SoftUni C# Advanced Module/8.2 Generics Exercises 6.0/Tuple/CustomTuple.cs
+++ SoftUni C# Advanced Module/8.2 Generics Exercises 6.0/Tuple/CustomTuple.cs
@@ -6,8 +6,18 @@ using System.Threading.Tasks;
 
 namespace Tuple
 {
-    public class CustomTuple
+    public class CustomTuple<T1, T2>
     {
+        public CustomTuple(T1 item1, T2 item2)
+        {
+            Item1 = item1;
+            Item2 = item2;
+        }
 
+        public T1 Item1 { get; private set; }
+
+        public T2 Item2 { get; private set; }
+
+        public override string ToString() => $"{Item1} -> {Item2}";
     }
 }
