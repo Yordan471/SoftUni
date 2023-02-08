3d1244939865b728a05d8b3f83fa33cebd637a8b
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-08 20:18:56 +0200

    adding some changes in class names and namespace

--- SoftUni C# Advanced Module/9.1Iterators and Comparators - Lab/Library/Library.cs
+++ SoftUni C# Advanced Module/9.1Iterators and Comparators - Lab/Library/Library.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Libraries
+namespace IteratorsAndComparators
 {
     public class Library : IEnumerable<Book>
     {
--- SoftUni C# Advanced Module/9.1Iterators and Comparators - Lab/Library/StartUp.cs
+++ SoftUni C# Advanced Module/9.1Iterators and Comparators - Lab/Library/StartUp.cs
@@ -1,4 +1,4 @@
-﻿using Libraries;
+﻿
 
 namespace IteratorsAndComparators
 {
