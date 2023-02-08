fbe86f349cfa5e6e54d874d3cf223f87dc69b7ed
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-08 20:31:52 +0200

    filling class Book

--- SoftUni C# Advanced Module/9.1Iterators and Comparators - Lab/IteratorsAndComparators/Book.cs
+++ SoftUni C# Advanced Module/9.1Iterators and Comparators - Lab/IteratorsAndComparators/Book.cs
@@ -8,6 +8,18 @@ namespace IteratorsAndComparators
 {
     public class Book
     {
+        public Book(string title, int year, params string[] authors)
+        {
+            Title = title;
+            Year = year;
+            Authors = authors.ToList();
+        }
 
+
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+        public List<string> Authors { get; set; }
     }
 }
