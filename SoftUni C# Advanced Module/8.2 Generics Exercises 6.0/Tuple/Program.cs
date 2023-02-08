27b3ed8ca99d5182f701382f074e796b7dd1978b
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-08 21:54:35 +0200

    adding source code for project - Tuple

--- SoftUni C# Advanced Module/8.2 Generics Exercises 6.0/Tuple/Program.cs
+++ SoftUni C# Advanced Module/8.2 Generics Exercises 6.0/Tuple/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using Tuple;
+
+string[] personInfo = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+string[] nameAndAmountOfBeer = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+string[] numbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+CustomTuple<string, string> nameAndAdress = new($"{personInfo[0]} {personInfo[1]}", personInfo[2]);
+
+CustomTuple<string, int> nameAndBeer = new(nameAndAmountOfBeer[0], int.Parse(nameAndAmountOfBeer[1]));
+
+CustomTuple<int, double> nums = new(int.Parse(numbers[0]), double.Parse(numbers[1]));
+
+Console.WriteLine(nameAndAdress);
+Console.WriteLine(nameAndBeer);
+Console.WriteLine(nums);
