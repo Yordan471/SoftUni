f1dfae74e7d71aea2cae63718066e3c6b8d1bc1e
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-09 14:13:18 +0200

    Add Timer for Inserting numbers in a List and in LinkedList - see the difference

--- SoftUni C# Advanced Module/7.1 Implementing Linked List/CustomLinkedListPerformance/Program.cs
+++ SoftUni C# Advanced Module/7.1 Implementing Linked List/CustomLinkedListPerformance/Program.cs
@@ -1,1 +1,44 @@
-﻿
+﻿using LinkedList;
+using System.Diagnostics;
+
+int Insertions = 100000;
+Stopwatch watch = Stopwatch.StartNew();
+Console.WriteLine("Insert numbers in List:");
+watch.Start();
+// insert numbers in List<int>
+InsertFirst(Insertions);
+
+watch.Stop();
+Console.WriteLine($"{watch.ElapsedMilliseconds} milliseconds");
+Console.WriteLine($"{watch.ElapsedMilliseconds / 100 * 1.00:f2} seconds");
+Console.WriteLine();
+
+Console.WriteLine("Add numbers in CustomLinkedList:");
+//Stopwatch watchLinkedList = Stopwatch.StartNew();
+watch = Stopwatch.StartNew();
+// insert numbers in List<int>
+InsertLinkedList(Insertions);
+
+watch.Stop();
+Console.WriteLine($"{watch.ElapsedMilliseconds} milliseconds");
+Console.WriteLine($"{watch.ElapsedMilliseconds / 100 * 1.00:f2} seconds");
+
+void InsertFirst(int insertions)
+{
+    List<int> list = new();
+
+    for (int i = 0; i < insertions; i++)
+    {
+        list.Insert(0, i);
+    }
+}
+
+void InsertLinkedList(int insertions)
+{
+    CustomLinkedList linkedList = new();
+
+    for (int i = 0; i < insertions; i++)
+    {
+        linkedList.AddFirst(i);
+    }
+}
