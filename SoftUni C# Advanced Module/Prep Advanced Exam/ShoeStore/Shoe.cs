f690ae3231bd449482ec7197cc4e436efa26f875
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-11 12:15:32 +0200

    filling class Shoe

--- SoftUni C# Advanced Module/Prep Advanced Exam/ShoeStore/Shoe.cs
+++ SoftUni C# Advanced Module/Prep Advanced Exam/ShoeStore/Shoe.cs
@@ -8,6 +8,26 @@ namespace ShoeStore
 {
     public class Shoe
     {
+        public Shoe(string brand, string type, double size, string material)
+        {
+            this.Brand = brand;
+            this.Type = type;
+            this.Size = size;
+            this.Material = material;
+        }
 
+        public string Brand { get; set; }
+
+        public string Type { get; set; }
+
+        public double Size { get; set; }
+
+        public string Material { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"Size {this.Size} {this.Material} {this.Brand} {this.Type} shoe.";
+        }
     }
 }
