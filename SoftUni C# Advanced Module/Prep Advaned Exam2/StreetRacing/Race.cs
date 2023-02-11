1e9317b446d4576c1fd5c040ce4e3a07be5e0634
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-11 17:42:43 +0200

    filling class Race

--- SoftUni C# Advanced Module/Prep Advaned Exam2/StreetRacing/Race.cs
+++ SoftUni C# Advanced Module/Prep Advaned Exam2/StreetRacing/Race.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,93 @@ namespace StreetRacing
 {
     public class Race
     {
+        public Race(string name, string type, int laps, int capacity, int maxHorsePower)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.Laps = laps;
+            this.Capacity = capacity;
+            this.MaxHorsePower = maxHorsePower;
+            this.Participants = new List<Car>();
+        }
 
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public int Laps { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int MaxHorsePower { get; set; }
+
+        public List<Car> Participants { get; set; }
+
+        public int Count
+        {
+            get
+            {
+                return Participants.Count;
+            }
+        }
+
+        public void Add(Car car)
+        {
+            if (this.Count != this.Capacity &&
+                car.HorsePower >= MaxHorsePower &&
+                Participants.Any(c => c.LicensePlate != car.LicensePlate))
+            {
+                Participants.Add(car);
+            }
+        }
+
+        public bool Remove(string licensePlate)
+        {
+            if (Participants.Any(c => c.LicensePlate == licensePlate))
+            {
+                Car removeCar = Participants.FirstOrDefault(c => c.LicensePlate == licensePlate);
+                Participants.Remove(removeCar);
+                return true;
+            }
+
+            return false;
+        }
+
+        public Car FindParticipant(string licensePlate)
+        {
+            if (Participants.Any(c => c.LicensePlate == licensePlate))
+            {
+                Car returnCar = Participants.FirstOrDefault(c => c.LicensePlate == licensePlate);
+                return returnCar;
+            }
+
+            return null;
+        }
+
+        public Car GetMostPowerfulCar()
+        {
+            if (Participants.Count == 0)
+            {
+                return null;
+            }
+
+            Car mostPowerfulCar = Participants.OrderByDescending(c => c.HorsePower).FirstOrDefault();
+
+            return mostPowerfulCar;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Race: {this.Name} - {this.Type} (Laps: {this.Laps}");
+            
+            foreach (Car car in Participants)
+            {
+                sb.AppendLine(car.ToString());
+            }
+
+            return sb.ToString().Trim();
+        }
     }
 }
