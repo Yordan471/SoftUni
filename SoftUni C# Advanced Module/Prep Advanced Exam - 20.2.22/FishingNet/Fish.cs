74eec718fb70d8293968968dbab124aadbe20dc4
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-12 18:06:39 +0200

    filling class Fish

--- SoftUni C# Advanced Module/Prep Advanced Exam - 20.2.22/FishingNet/Fish.cs
+++ SoftUni C# Advanced Module/Prep Advanced Exam - 20.2.22/FishingNet/Fish.cs
@@ -8,6 +8,20 @@ namespace FishingNet
 {
     public class Fish
     {
+        private string FishType;
+        private double Length;
+        private double Weight;
 
+        public Fish(string fishType, double length, double weight)
+        {
+            this.FishType = fishType;
+            this.Length = length;
+            this.Weight = weight;
+        }
+
+        public override string ToString()
+        {
+            return $"There is a {this.FishType}, {this.Length} cm. long, and {this.Weight} gr. in weight.";
+        }
     }
 }
