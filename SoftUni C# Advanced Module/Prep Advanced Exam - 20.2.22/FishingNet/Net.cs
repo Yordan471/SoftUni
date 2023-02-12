d2d2a488ed642492772e715358f690d6f426f158
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-12 21:44:19 +0200

    filling class Net

--- SoftUni C# Advanced Module/Prep Advanced Exam - 20.2.22/FishingNet/Net.cs
+++ SoftUni C# Advanced Module/Prep Advanced Exam - 20.2.22/FishingNet/Net.cs
@@ -8,6 +8,95 @@ namespace FishingNet
 {
     public class Net
     {
+        private readonly ICollection<Fish> fish;
+        public Net(string material, int capacity)
+        {
+            this.Material = material;
+            this.Capacity = capacity;
+            this.fish = new List<Fish>();
+        }
+        public string  Material { get; set; }
 
+        public int Capacity { get; set; }
+
+        public IReadOnlyCollection<Fish> Fish => (IReadOnlyCollection<Fish>)this.fish;
+
+        public int Count
+        {
+            get
+            {
+                return Fish.Count;
+            }
+        }
+
+        public string AddFish(Fish fish)
+        {
+            if (string.IsNullOrEmpty(fish.FishType) &&
+                fish.Weight <= 0 && fish.Length <= 0)
+            {
+                return "Invalid fish.";
+            }
+            if (Count == this.Capacity)
+            {
+                return "Fishing net is full.";
+            }
+
+            this.fish.Add(fish);
+            return $"Successfully added {fish.FishType} to the fishing net.";
+        }
+
+        public bool ReleaseFish(double weight)
+        {
+            if (this.fish.Any(f => f.Weight == weight))
+            {
+                Fish removeFish = this.fish.FirstOrDefault(f => f.Weight == weight);
+                this.fish.Remove(removeFish);
+                return true;
+            }
+
+            return false;
+        }
+
+        public Fish GetFish(string fishType)
+        {
+            if (this.fish.Any(f => f.FishType == fishType))
+            {
+                return this.fish.FirstOrDefault(f => f.FishType.Equals(fishType));
+            }
+
+            return null;
+        }
+
+        public Fish GetBiggestFish()
+        {
+            Fish longestFish = null;
+
+            if (this.fish.Count > 0)
+            {
+                foreach (var f in this.fish.OrderByDescending(f => f.Length))
+                {
+                    longestFish = f;
+                }
+            }
+
+            return longestFish;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Into the {this.Material}:");
+
+            if (this.fish.Count > 0)
+            {
+                foreach (var f in fish.OrderByDescending(f => f.Length))
+                {
+                    sb.AppendLine($"{f}");
+                }
+            }
+
+            return sb.ToString().Trim();
+        }
     }
 }
