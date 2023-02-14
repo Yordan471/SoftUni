af3a8d3c646f6d966ae2f8538fc432dbc381641f
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-14 18:43:14 +0200

    adding source code for Project - Blacksmith

--- SoftUni C# Advanced Module/Prep Advanced Exam - 16.12.21/Blacksmith/Program.cs
+++ SoftUni C# Advanced Module/Prep Advanced Exam - 16.12.21/Blacksmith/Program.cs
@@ -10,7 +10,86 @@ namespace Blacksmith
     {
         static void Main(string[] args)
         {
+            int[] steelInput = Console.ReadLine()
+                .Split()
+                .Select(s => int.Parse(s))
+                .ToArray();
 
+            int[] carbonInput = Console.ReadLine()
+               .Split()
+               .Select(s => int.Parse(s))
+               .ToArray();
+
+            Queue<int> steel = new Queue<int>(steelInput);
+            Stack<int> carbon = new Stack<int>(carbonInput);
+
+            Dictionary<int, string> valueAndSwords = new Dictionary<int, string>()
+            {
+                { 70, "Gladius" },
+                { 80, "Shamshir" },
+                { 90, "Katana" },
+                { 110, "Sabre" },
+                { 150, "Broadsword" }
+            };
+
+            Dictionary<string, int> swordsAndAmout = new Dictionary<string, int>();
+
+            while (steel.Count != 0 && carbon.Count != 0)
+            {
+                int forgeSwordValue = steel.Peek() + carbon.Peek();
+
+                if (valueAndSwords.ContainsKey(forgeSwordValue))
+                {
+                    string sword = valueAndSwords[forgeSwordValue];
+
+                    if (!swordsAndAmout.ContainsKey(sword))
+                    {
+                        swordsAndAmout.Add(sword, 0);
+                    }
+
+                    swordsAndAmout[sword]++;
+                    carbon.Pop();
+                }
+                else
+                {
+                    int saveCarbon = carbon.Pop() + 5;
+                    carbon.Push(saveCarbon);
+                }
+
+                steel.Dequeue();
+            }
+
+            if (swordsAndAmout.Any())
+            {
+                Console.WriteLine($"You have forged {swordsAndAmout.Values.Sum()} swords.");
+            }
+            else
+            {
+                Console.WriteLine("You did not have enough resources to forge a sword.");
+            }
+
+            if (steel.Any())
+            {
+                Console.WriteLine($"Steel left: {string.Join(", ", steel)}");
+            }
+            else
+            {
+                Console.WriteLine("Steel left: none");
+            }
+
+            if (carbon.Count > 0)
+            {
+                Console.WriteLine($"Carbon left: {string.Join(", ", carbon)}");
+            }
+            else
+            {
+                Console.WriteLine("Carbon left: none");
+            }
+
+            foreach (var sword in swordsAndAmout.OrderBy(s => s.Key))
+            {
+                Console.WriteLine($"{sword.Key}: {sword.Value}");
+            }
         }
     }
 }
