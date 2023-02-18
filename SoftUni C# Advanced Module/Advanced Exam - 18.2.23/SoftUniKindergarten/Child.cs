f60df2ecc56f3b4c00127a418ac6b48cc2de2e8f
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-18 11:19:05 +0200

    adding source code for Project - Kindergarten

---
+++ SoftUni C# Advanced Module/Advanced Exam - 18.2.23/SoftUniKindergarten/Child.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniKindergarten
+{
+    public class Child
+    {
+        public Child(string firstName, string lastName, int age, string parentName, string contactNumber)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.ParentName = parentName;
+            this.ContactNumber = contactNumber;
+        }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public string ParentName { get; set; }
+
+        public string ContactNumber { get; set; }
+
+        public override string ToString()
+        {
+            return $"Child: {this.FirstName} {this.LastName}, Age: {this.Age}, Contact info: {this.ParentName} - {this.ContactNumber}".ToString();
+        }
+    }
+}
---
+++ SoftUni C# Advanced Module/Advanced Exam - 18.2.23/SoftUniKindergarten/Kindergarten.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniKindergarten
+{
+    public class Kindergarten
+    {
+        public Kindergarten(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            Registry = new();
+        }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public List<Child> Registry { get; set; }
+
+        public int ChildrenCount
+        {
+            get
+            {
+                return Registry.Count;
+            }
+        }
+
+        public bool AddChild(Child child)
+        {
+            if (ChildrenCount >= this.Capacity)
+            {
+                return false;
+            }
+
+            Registry.Add(child);
+            return true;
+        }
+
+        public bool RemoveChild(string childFullName)
+        {
+            if (Registry.Any(c => c.FirstName + " " + c.LastName == childFullName))
+            {
+                Child removeChild = Registry.First(c => c.FirstName + " " + c.LastName == childFullName);
+                Registry.Remove(removeChild);
+                return true;
+            }
+
+            return false;
+        }
+
+        public Child GetChild(string childFullName)
+        {
+            if (Registry.Any(c => c.FirstName + " " + c.LastName == childFullName))
+            {
+                Child getChild = Registry.First(c => c.FirstName + " " + c.LastName == childFullName);
+                return getChild;
+            }
+
+            return null;
+        }
+
+        public string RegistryReport()
+        {
+            StringBuilder sb = new();
+
+            sb.AppendLine($"Registered children in {this.Name}:");
+
+            if (this.ChildrenCount > 0)
+            {
+                foreach (var child in Registry
+                    .OrderByDescending(c => c.Age)
+                    .ThenBy(c => c.LastName)
+                    .ThenBy(c => c.FirstName))
+                {
+                    sb.AppendLine(child.ToString());
+                }
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
