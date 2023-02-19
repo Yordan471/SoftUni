260e3037229ccd7e3b58b6aa39112c27a8ce4e5a
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-19 13:32:53 +0200

    filling class Player

--- SoftUni C# Advanced Module/Prep Advanced Exam - 22.2.20/Guild/Player.cs
+++ SoftUni C# Advanced Module/Prep Advanced Exam - 22.2.20/Guild/Player.cs
@@ -8,6 +8,31 @@ namespace Guild
 {
     public class Player
     {
+        public Player(string name, string classs)
+        {
+            this.Name = name;
+            this.Class = classs;
+            this.Rank = "Trial";
+            this.Description = "n/a";
+        }
 
+        public string Name { get; set; }
+
+        public string Class { get; set; }
+
+        public string Rank { get; set; }
+
+        public string Description { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Player {this.Name}: {this.Class}");
+            sb.AppendLine($"Rank: {this.Rank}");
+            sb.AppendLine($"Description: {this.Description}");
+
+            return sb.ToString().Trim();
+        }
     }
 }
