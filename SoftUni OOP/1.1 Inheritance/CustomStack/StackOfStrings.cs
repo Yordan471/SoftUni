6bdac0be3adb992fa4320f0d380b6b08cf8bf9bd
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-22 19:06:51 +0200

    finishing code for Project - CustomStack

--- SoftUni OOP/1.1 Inheritance/CustomStack/StackOfStrings.cs
+++ SoftUni OOP/1.1 Inheritance/CustomStack/StackOfStrings.cs
@@ -8,6 +8,24 @@ namespace CustomStack
 {
     public class StackOfStrings : Stack<string>
     {
+        public bool IsEmpty()
+        {
+            if (this.Count == 0)
+            {
+                return true;
+            }
 
+            return false;
+        }
+            
+        public Stack<string> AddRange(IEnumerable<string> collection)
+        {
+            foreach (var item in collection)
+            {
+                this.Push(item);
+            }
+
+            return this;
+        }
     }
 }
---
+++ SoftUni OOP/1.1 Inheritance/CustomStack/StartUp.cs
@@ -0,0 +1,25 @@
+﻿namespace CustomStack
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            StackOfStrings stack = new();
+
+            string[] array = new string[4]
+            {
+                "pesho",
+                "gosho",
+                "sasho",
+                "demit"
+            };
+
+            stack.AddRange(array);
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
