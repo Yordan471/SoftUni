8fc83363bcc6628bc02e58dc2ad952cd3ac3d764
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-25 13:13:51 +0200

    adding class Vehicle and filling it

---
+++ SoftUni OOP/1.2 Inheritance - Eercises/NeedForSpeed/Vehicle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        private const double DefaultFuelConsumption = 1.25;
+       //public double DefaultFuelConsumption  { get; set; }
+
+        public Vehicle(int horsePower, double fuel)
+        {
+            HorsePower = horsePower;
+            Fuel = fuel;
+        }
+
+        public virtual double FuelConsumption => DefaultFuelConsumption;
+
+        public double Fuel { get; set; }
+
+        public int HorsePower { get; set; }
+
+        public virtual void Drive(double kilometers)
+        {
+            Fuel -= kilometers * FuelConsumption;
+        }
+    }
+}
