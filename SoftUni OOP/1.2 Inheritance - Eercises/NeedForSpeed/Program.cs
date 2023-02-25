773db1128ee7672b7dded5ab6a1def69c84983e1
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-25 13:18:45 +0200

    adding tests in Class Program

--- SoftUni OOP/1.2 Inheritance - Eercises/NeedForSpeed/Program.cs
+++ SoftUni OOP/1.2 Inheritance - Eercises/NeedForSpeed/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using NeedForSpeed;
+
+RaceMotorcycle motor = new(200, 1200);
+SportCar sportCar = new(200, 1200);
+Car car = new(200, 1200);
+
+motor.Drive(100);
+Console.WriteLine(motor.Fuel);
+
+sportCar.Drive(100);
+Console.WriteLine(sportCar.Fuel);
+
+car.Drive(100);
+Console.WriteLine(car.Fuel);
