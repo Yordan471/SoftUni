7b00fe7415a54d7f18583407ef9a05efe48f4f8d
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-26 09:01:38 +0200

    filling class Person

--- SoftUni OOP/2.1 PersonsInfo - Lab/PersonsInfo/Person.cs
+++ SoftUni OOP/2.1 PersonsInfo - Lab/PersonsInfo/Person.cs
@@ -8,6 +8,31 @@ namespace PersonsInfo
 {
     public class Person
     {
+        private string firstName;
+        private string lastName;
+        private int age;
 
+        public string FirstName 
+        {   
+            get => firstName;
+            set => firstName = value;
+        }
+
+        public string LastName
+        {
+            get => lastName;
+            set => lastName = value;
+        }
+
+        public int Age
+        {
+            get => age;
+            set => age = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} is {this.Age} years old.";
+        }
     }
 }
