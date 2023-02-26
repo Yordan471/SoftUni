fe5d5162371ca6ab294088f1f8d9f412f23c5e07
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-02-26 18:00:20 +0200

    Adding solution for task - Class Box Data

--- SoftUni OOP/2.2 Encapsulation - Exercises/ClassBoxData/Program.cs
+++ SoftUni OOP/2.2 Encapsulation - Exercises/ClassBoxData/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using ClassBoxData;
+
+double lenght = double.Parse(Console.ReadLine());
+double width =  double.Parse(Console.ReadLine());
+double height = double.Parse(Console.ReadLine());
+
+Box box = new();
+
+try
+{
+   box = new(lenght, width, height);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    return;
+}
+
+Console.WriteLine($"Surface Area - {box.SurfaceArea():f2}");
+Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():f2}");
+Console.WriteLine($"Volume - {box.Volume():f2}");
