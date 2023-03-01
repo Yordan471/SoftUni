1250cc81d6489911ed72ca4645a901f587c41bee
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-01 21:12:39 +0200

    filling interface ICar

---
+++ SoftUni OOP/3.1 Interfaces and Abstraction - Lab/Cars/ICar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public interface ICar
+    {
+        public string Model { get; }
+
+        public string Color { get; }
+
+        public void Start();
+
+        public void Stop();
+    }
+}
