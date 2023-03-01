3205629afe27579234d723f794eeb97b2b1796d2
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-01 21:21:52 +0200

    filling class Tesla

--- SoftUni OOP/3.1 Interfaces and Abstraction - Lab/Cars/Tesla.cs
+++ SoftUni OOP/3.1 Interfaces and Abstraction - Lab/Cars/Tesla.cs
@@ -6,8 +6,50 @@ using System.Threading.Tasks;
 
 namespace Cars
 {
-    public class Tesla
+    public class Tesla : IElectricalCar
     {
+        private int battery;
+        private string model;
+        private string color;
 
+        public Tesla(string model, string color, int battery)
+        {
+            this.Model = model;
+            this.Color = color;
+            this.Battery = battery;
+        }
+
+        public int Battery
+        {
+            get => battery;
+            set => this.battery = value;
+        }
+
+        public string Model
+        {
+            get => model;
+            set => model = value;
+        }
+
+        public string Color
+        {
+            get => color;
+            set => color = value;
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Engine start");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Breaaak!");
+        }
+
+        public override string ToString()
+        {
+            return $"{this.color} Tesla {this.model} witch {this.battery} Batteries";
+        }
     }
 }
