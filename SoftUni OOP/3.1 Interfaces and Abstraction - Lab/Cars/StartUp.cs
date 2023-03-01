24fde05e10237bf180e9e268eb2efe0009781fb9
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-01 21:24:45 +0200

    adding tests in the main class

--- SoftUni OOP/3.1 Interfaces and Abstraction - Lab/Cars/StartUp.cs
+++ SoftUni OOP/3.1 Interfaces and Abstraction - Lab/Cars/StartUp.cs
@@ -4,8 +4,16 @@
     {
         public static void Main()
         {
+            Tesla tesla = new("A1", "Red", 2);
+            Seat seat = new("A2", "Yellow");
 
+            Console.WriteLine(tesla);
+            tesla.Start();
+            tesla.Stop();
+
+            Console.WriteLine(seat);
+            seat.Start();
+            seat.Stop();
         }
     }
-
 }
