84e3bea53245067e31a76d81078cc4d41063712c
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-04 09:55:53 +0200

    adding source cod for Task - PersonInfo

--- SoftUni OOP/3.2 Interfaces and Abstraction - Exercises/PersonInfo/Program.cs
+++ SoftUni OOP/3.2 Interfaces and Abstraction - Exercises/PersonInfo/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using PersonInfo;
+
+string name = Console.ReadLine();
+int age = int.Parse(Console.ReadLine());
+
+IPerson person = new Citizen(name , age);
+
+Console.WriteLine(person.Name);
+Console.WriteLine(person.Age);
