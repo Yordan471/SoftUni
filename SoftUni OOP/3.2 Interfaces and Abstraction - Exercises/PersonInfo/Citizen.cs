d3f46930a6a5cdb99b4370d2d37c342ad7b29f3c
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-04 10:03:36 +0200

    adding more complexity to Task - PersonInfo

--- SoftUni OOP/3.2 Interfaces and Abstraction - Exercises/PersonInfo/Citizen.cs
+++ SoftUni OOP/3.2 Interfaces and Abstraction - Exercises/PersonInfo/Citizen.cs
@@ -6,16 +6,22 @@ using System.Threading.Tasks;
 
 namespace PersonInfo
 {
-    public class Citizen : IPerson
+    public class Citizen : IPerson, IIdentifiable, IBirthable
     {
-        public Citizen(string name, int age)
+        public Citizen(string name, int age, string iD, string birthdate)
         {
             this.Name = name;
             this.Age = age;
+            this.ID = iD;
+            this.Birthdate = birthdate;
         }
 
 
         public string Name { get;  set; }
         public int Age { get; set;   }
+
+        public string ID { get; set; }
+
+        public string Birthdate { get; set; }
     }
 }
--- SoftUni OOP/3.2 Interfaces and Abstraction - Exercises/PersonInfo/Program.cs
+++ SoftUni OOP/3.2 Interfaces and Abstraction - Exercises/PersonInfo/Program.cs
@@ -2,8 +2,15 @@
 
 string name = Console.ReadLine();
 int age = int.Parse(Console.ReadLine());
+string id = Console.ReadLine();
+string birthdate = Console.ReadLine();
 
-IPerson person = new Citizen(name , age);
+Citizen citizen = new(name, age, id, birthdate);
+IPerson person = new Citizen(name , age, id, birthdate);
+IIdentifiable identifiable = new Citizen(name, age, id, birthdate);
+IBirthable birthable = new Citizen(name, age, id, birthdate);
 
 Console.WriteLine(person.Name);
 Console.WriteLine(person.Age);
+Console.WriteLine(identifiable.ID);
+Console.WriteLine(birthable.Birthdate);
