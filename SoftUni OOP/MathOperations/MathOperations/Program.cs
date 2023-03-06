868fc73671d4ed2aa5a336b889fa2f2e71e8cfa3
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-06 16:34:40 +0200

    Adding class Engine

--- SoftUni OOP/MathOperations/MathOperations/Program.cs
+++ SoftUni OOP/MathOperations/MathOperations/Program.cs
@@ -4,7 +4,13 @@
     {
         public static void Main()
         {
+            MathOperations operation = new();
 
+            Console.WriteLine(operation.Add(1, 2));
+
+            Console.WriteLine(operation.Add(2.2, 2.3, 2.4));
+
+            Console.WriteLine(operation.Add(2.5M, 2.4M, 2.989492M));
         }
     }
 }
