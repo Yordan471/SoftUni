c435f591d3dce9444a86ba20a17d4f926b99b240
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-06 16:30:11 +0200

    filling Class - MathOperations

--- SoftUni OOP/MathOperations/MathOperations/MathOperations.cs
+++ SoftUni OOP/MathOperations/MathOperations/MathOperations.cs
@@ -8,6 +8,19 @@ namespace MathOperations
 {
     public class MathOperations
     {
+        public int Add(int firstNum, int secondNum)
+        {
+            return firstNum + secondNum;
+        }
 
+        public double Add(double firstNum, double secondNum, double thirdNum)
+        {
+            return firstNum + secondNum * thirdNum;
+        }
+
+        public decimal Add(decimal firstNum, decimal secondNum, decimal thirdNum)
+        {
+            return firstNum + secondNum + thirdNum;
+        }
     }
 }
