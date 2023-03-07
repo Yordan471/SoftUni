4900989a448fa10cbec0f0282371cbb4c4d145ca
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-07 07:46:54 +0200

    adding Task Operations and finishing it; Adding Task Animals

---
+++ SoftUni OOP/MathOperations/Operations/Program.cs
@@ -0,0 +1,19 @@
+﻿using Operations;
+
+namespace Operations
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            MathOperations operation = new();
+
+            Console.WriteLine(operation.Add(1, 2));
+
+            Console.WriteLine(operation.Add(2.2, 2.3, 2.4));
+
+            Console.WriteLine(operation.Add(2.5M, 2.4M, 2.989492M));
+        }
+    }
+}
+
