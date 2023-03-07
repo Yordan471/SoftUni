ab166e4c14cfc9b58f0d30f861ef5906b415d045
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-07 10:36:10 +0200

    filling Class Animal

--- SoftUni OOP/MathOperations/Animals/Animal.cs
+++ SoftUni OOP/MathOperations/Animals/Animal.cs
@@ -8,6 +8,26 @@ namespace Animals
 {
     public class Animal
     {
+        private string name;
+        private string favouriteFood;
 
+
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public string FavouriteFood
+        {
+            get => favouriteFood;
+            set => favouriteFood = value;
+        }
+
+        public virtual string ExmpainSelf()
+        {
+            return "";
+        }
     }
 }
