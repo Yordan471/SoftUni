a3e8e155b0d1f35e02e7f4ddadc048c24cb81cff
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-07 13:41:14 +0200

    adding class Truck

---
+++ SoftUni OOP/4.2 - Polymorphism - Exercises/Vehicles/Truck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles
+{
+    public class Truck : Vehicle
+    {
+        public Truck(double fuelQuantity, double fuelConsuption) : base(fuelQuantity, fuelConsuption)
+        {
+        }
+
+        public override string Drive(int distance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Refuel(int amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
