3e058122327921899b9666570fdcb323d945216a
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-08 17:10:45 +0200

    adding Bird classes - Owl and Hen

---
+++ SoftUni OOP/4.2 - Polymorphism - Exercises/WildFarm/Models/Classes/Hen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WildFarm.Models.Abstract_Classes;
+using WildFarm.Models.Interfaces;
+
+namespace WildFarm.Models.Classes
+{
+    public class Hen : Animal, IBird
+    {
+        public Hen(string name, double weight, int foodEaten) 
+            : base(name, weight, foodEaten)
+        {
+        }
+
+        public double WingSize { get; private set; }
+    }
+}
