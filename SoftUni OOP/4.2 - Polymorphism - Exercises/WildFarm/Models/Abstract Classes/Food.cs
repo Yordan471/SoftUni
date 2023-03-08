bd8a75678bbecd8cb7a8145159e3df06c2503078
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-08 17:00:27 +0200

    adding abstract Class Food

---
+++ SoftUni OOP/4.2 - Polymorphism - Exercises/WildFarm/Models/Abstract Classes/Food.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WildFarm.Models.Interfaces;
+
+namespace WildFarm.Models.Abstract_Classes
+{
+    public abstract class Food : IFood
+    {
+        public Food(int quantity)
+        {
+            Quantity = quantity;
+        }
+
+        public int Quantity
+        {
+            get; private set;
+        }
+    }
+}
