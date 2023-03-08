235900c98d1551a93e02e454a9f4e6ca52e1bc45
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-08 17:06:02 +0200

    adding abstract class Mammal

---
+++ SoftUni OOP/4.2 - Polymorphism - Exercises/WildFarm/Models/Abstract Classes/Mammal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WildFarm.Models.Abstract_Classes
+{
+    internal class Mammal
+    {
+    }
+}
