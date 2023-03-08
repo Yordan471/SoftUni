a9eaef186710c6fd87c613d50f900b59a6feca34
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-08 14:13:44 +0200

    adding classes - Rogue and Warrior

---
+++ SoftUni OOP/4.2 - Polymorphism - Exercises/Raiding/Models/Warrior.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raiding.Models
+{
+    public class Warrior : BaseHero
+    {
+        public Warrior(string name, int power) : base(name, power)
+        {
+        }
+
+        public override string CastAbility()
+        {
+            return $"{this.GetType().Name} - {this.Name} Hit for {this.Power} damage";
+        }
+    }
+}
