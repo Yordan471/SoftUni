ca2fd11a3075342c713348b154a0ad94251736db
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-08 17:01:55 +0200

    adding abstract Class - Animal

---
+++ SoftUni OOP/4.2 - Polymorphism - Exercises/WildFarm/Models/Abstract Classes/Animal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WildFarm.Models.Interfaces;
+
+namespace WildFarm.Models.Abstract_Classes
+{
+    public abstract class Animal : IAnimal
+    {
+        public Animal(string name, double weight, int foodEaten)
+        {
+            Name = name;
+            Weight = weight;
+            FoodEaten = foodEaten;
+        }
+
+        public string Name { get; private set; }
+
+        public double Weight { get; private set; }
+
+        public int FoodEaten { get; private set; }
+    }
+}
