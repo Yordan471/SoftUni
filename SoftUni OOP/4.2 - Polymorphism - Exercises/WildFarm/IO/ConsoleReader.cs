50ff7e6f56b8c737e09912c296106a8497f0d1b4
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-10 17:40:30 +0200

    adding IO

---
+++ SoftUni OOP/4.2 - Polymorphism - Exercises/WildFarm/IO/ConsoleReader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WildFarm.IO.Interfaces;
+
+namespace WildFarm.IO
+{
+    public class ConsoleReader : IReader
+    {
+        public void ReadLine(string str)
+        {
+            Console.ReadLine();
+        }
+    }
+}
