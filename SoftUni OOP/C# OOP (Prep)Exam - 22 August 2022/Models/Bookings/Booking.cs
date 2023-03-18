43ed5b27774a9042c3e72938192c9f358c5ca9a6
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-18 15:30:59 +0200

    Adding class Booking (implementing interface IBooking) and adjusting its members

---
+++ SoftUni OOP/C# OOP (Prep)Exam - 22 August 2022/Models/Bookings/Booking.cs
@@ -0,0 +1,87 @@
+﻿using BookingApp.Models.Bookings.Contracts;
+using BookingApp.Models.Rooms.Contracts;
+using BookingApp.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Models.Bookings
+{
+    public abstract class Booking : IBooking
+    {
+        IRoom room;
+        private int residenceDuration;
+        private int adultCount;
+        private int childrenCount;
+        private int bookingNumber;
+
+        public Booking(IRoom room, int residenceDuration, int adultCount, int childrenCount, int bookingNumber)
+        {
+            this.room = room;
+            this.ResidenceDuration = residenceDuration;
+            this.AdultsCount = adultCount;
+            this.ChildrenCount = childrenCount;
+            this.bookingNumber = bookingNumber;
+        }
+
+        public IRoom Room => room;        
+
+        public int ResidenceDuration
+        {
+            get => residenceDuration;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.DurationZeroOrLess);
+                }
+
+                residenceDuration = value;
+            }
+        }
+
+        public int AdultsCount
+        {
+            get => adultCount;
+            set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException(ExceptionMessages.AdultsZeroOrLess);
+                }
+
+                adultCount = value;
+            }
+        }
+
+        public int ChildrenCount
+        {
+            get => childrenCount;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.ChildrenNegative);
+                }
+
+                childrenCount = value;
+            }
+        }
+
+        public int BookingNumber => bookingNumber;
+
+        public string BookingSummary()
+        {
+            StringBuilder sb = new();
+
+            sb.AppendLine($"Booking number: {BookingNumber}");
+            sb.AppendLine($"Room type: {Room.BedCapacity}");
+            sb.AppendLine($"Adults: {AdultsCount} Children: {ChildrenCount}");
+            sb.AppendLine($"Total amount paid: {Math.Round(ResidenceDuration * Room.PricePerNight):F2}");
+
+            return sb.ToString().Trim();
+        }
+    }
+}
