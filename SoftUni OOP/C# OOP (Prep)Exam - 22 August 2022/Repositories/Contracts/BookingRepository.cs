48f202a95740957805207a85a785a022495f0d90
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-18 16:11:18 +0200

    adding Class BookingRepository witch implements interface IRepository<T>(IBooking) and adjust its Members

---
+++ SoftUni OOP/C# OOP (Prep)Exam - 22 August 2022/Repositories/Contracts/BookingRepository.cs
@@ -0,0 +1,29 @@
+﻿using BookingApp.Models.Bookings.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Repositories.Contracts
+{
+    public class BookingRepository : IRepository<IBooking>
+    {
+        private readonly ICollection<IBooking> bookings;
+
+        public void AddNew(IBooking model)
+        {
+            bookings.Add(model);
+        }
+
+        public IReadOnlyCollection<IBooking> All()
+        {
+            return (IReadOnlyCollection<IBooking>)bookings;
+        }
+
+        public IBooking Select(string criteria)
+        {
+            return bookings.FirstOrDefault(b => b.GetType().Name ==  criteria);
+        }
+    }
+}
