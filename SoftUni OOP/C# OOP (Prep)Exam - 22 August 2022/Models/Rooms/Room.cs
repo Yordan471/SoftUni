25250979f7205d4ee6f430df0634c68759334642
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-18 15:06:17 +0200

    Adding class Room and Filling it with Members (implementing interface IRoom)

---
+++ SoftUni OOP/C# OOP (Prep)Exam - 22 August 2022/Models/Rooms/Room.cs
@@ -0,0 +1,49 @@
+﻿using BookingApp.Models.Rooms.Contracts;
+using BookingApp.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Models.Rooms
+{
+    abstract class Room : IRoom
+    {
+        private int bedCapacity;
+        private double pricePerNight;
+
+        public Room(int bedCapacity, double pricePerNight = 0)
+        {
+            this.BedCapacity = bedCapacity;
+        }
+
+        public int BedCapacity
+        {
+            get => bedCapacity;
+            set
+            {
+                bedCapacity = value;
+            }
+        }
+
+        public double PricePerNight
+        { 
+            get => pricePerNight;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.PricePerNightNegative);
+                }
+
+                pricePerNight = value;
+            }
+        }
+
+        public void SetPrice(double price)
+        {
+            pricePerNight = price;
+        }
+    }
+}
