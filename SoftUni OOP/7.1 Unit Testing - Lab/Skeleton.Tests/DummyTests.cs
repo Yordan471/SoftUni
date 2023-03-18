a51e615998c902d660ac24c9e16e340880fa8f90
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-18 12:15:12 +0200

    adding TestDummyConstructor with test code

--- SoftUni OOP/7.1 Unit Testing - Lab/Skeleton.Tests/DummyTests.cs
+++ SoftUni OOP/7.1 Unit Testing - Lab/Skeleton.Tests/DummyTests.cs
@@ -5,10 +5,24 @@ namespace Skeleton.Tests
     [TestFixture]
     public class DummyTests
     {
-        [Test]
-        public void Test1()
-        {
+        Dummy dummy;
+        int dummyHealth;
+        int dummyExp;
 
+        [SetUp]
+        public void SetUp()
+        {
+            dummyHealth = 100;
+            dummyExp = 10;
+            dummy = new(dummyHealth, dummyExp);
         }
+
+        [Test]
+        public void TestDummyConstructor()
+        {
+            Assert.That(dummy.Health, Is.EqualTo(dummyHealth), "Dummy Health points are different then the one set from the constructor");
+        }
+
+
     }
 }
