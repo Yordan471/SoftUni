36ae6822f359d84cd55852cca2c060ed650fff77
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-19 13:53:38 +0200

    adding Class WeaponRepository, implementing interface IRepository

--- SoftUni OOP/C# OOP (Prep)Exam - 14 August 2022/Models/Planets/Planet.cs
+++ SoftUni OOP/C# OOP (Prep)Exam - 14 August 2022/Models/Planets/Planet.cs
@@ -1,10 +1,52 @@
-﻿using System;
+﻿using PlanetWars.Models.MilitaryUnits.Contracts;
+using PlanetWars.Models.Planets.Contracts;
+using PlanetWars.Models.Weapons.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace PlanetWars.Models.Planets
 {
-    internal class Planet
+    public class Planet : IPlanet
     {
+        public string Name => throw new NotImplementedException();
+
+        public double Budget => throw new NotImplementedException();
+
+        public double MilitaryPower => throw new NotImplementedException();
+
+        public IReadOnlyCollection<IMilitaryUnit> Army => throw new NotImplementedException();
+
+        public IReadOnlyCollection<IWeapon> Weapons => throw new NotImplementedException();
+
+        public void AddUnit(IMilitaryUnit unit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddWeapon(IWeapon weapon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string PlanetInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Profit(double amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Spend(double amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TrainArmy()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ SoftUni OOP/C# OOP (Prep)Exam - 14 August 2022/Repositories/WeaponRepository.cs
@@ -0,0 +1,48 @@
+﻿using PlanetWars.Models.Weapons.Contracts;
+using PlanetWars.Repositories.Contracts;
+using PlanetWars.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlanetWars.Repositories
+{
+    public class WeaponRepository : IRepository<IWeapon>
+    {
+        private readonly List<IWeapon> weapons;
+
+        public WeaponRepository()
+        {
+            weapons = new List<IWeapon>();
+        }
+
+        public IReadOnlyCollection<IWeapon> Models => this.weapons;
+
+        public void AddItem(IWeapon model)
+        {
+            this.weapons.Add(model);
+        }
+
+        public IWeapon FindByName(string name)
+        {
+            if (!weapons.Any(w => w.GetType().Name == name)
+            {
+                return null;
+            }
+
+            return this.weapons.FirstOrDefault(w => w.GetType().Name == name);
+        }
+
+        public bool RemoveItem(string name)
+        {
+            if (!weapons.Any(w => w.GetType().Name == name)
+            {
+                return false;
+            }
+
+            weapons.Remove(FindByName(name));
+            return true;
+        }
+    }
+}
