759d4a00d4876362cb88002d95f169408ec35d53
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-19 13:19:09 +0200

    filling class Weapon and Adjusting its Members information

--- SoftUni OOP/C# OOP (Prep)Exam - 14 August 2022/Models/Weapons/Weapon.cs
+++ SoftUni OOP/C# OOP (Prep)Exam - 14 August 2022/Models/Weapons/Weapon.cs
@@ -1,10 +1,38 @@
-﻿using System;
+﻿using PlanetWars.Models.Weapons.Contracts;
+using PlanetWars.Utilities.Messages;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace PlanetWars.Models.Weapons
 {
-    internal class Weapon
+    public abstract class Weapon : IWeapon
     {
+        private int destructionLevel;
+        private double price;
+
+        public double Price 
+        { 
+            get => price; 
+            private set => price = value; 
+        }
+
+        public int DestructionLevel
+        {
+            get => destructionLevel;
+            private set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.TooLowDestructionLevel));
+                }
+                else if (value > 10)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.TooHighDestructionLevel));
+                }
+
+                destructionLevel = value;
+            }
+        }
     }
 }
