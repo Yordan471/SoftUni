cd4442a6886b865312a06854b7b45915a3f297cd
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-19 14:04:16 +0200

    Adding class PlanetRepository , implementing Interface IRepository

---
+++ SoftUni OOP/C# OOP (Prep)Exam - 14 August 2022/Repositories/PlanetRepository.cs
@@ -0,0 +1,51 @@
+﻿using PlanetWars.Models.Planets.Contracts;
+using PlanetWars.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlanetWars.Repositories
+{
+    public class PlanetRepository : IRepository<IPlanet>
+    {
+        private readonly List<IPlanet> planets;
+
+        public PlanetRepository()
+        {
+            this.planets = new List<IPlanet>();
+        }
+
+        public IReadOnlyCollection<IPlanet> Models => this.planets;
+
+        public void AddItem(IPlanet model)
+        {
+            this.planets.Add(model);
+        }
+
+        public IPlanet FindByName(string name)
+        {
+            IPlanet planet = planets.FirstOrDefault(p => p.Name == name);
+
+            if (planet == null)
+            {
+                return null;
+            }
+
+            return planet;
+        }
+
+        public bool RemoveItem(string name)
+        {
+            IPlanet planet = FindByName(name);
+
+            if (planet == null)
+            {
+                return false;
+            }
+
+            planets.Remove(planet);
+            return true;
+        }
+    }
+}
