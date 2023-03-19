6ab8b8aa528e1bd11da5b750e1d6d073c359f09a
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-19 13:26:52 +0200

    adding Class SpaceMissiles, inheriting Class Weapon

---
+++ SoftUni OOP/C# OOP (Prep)Exam - 14 August 2022/Models/Weapons/SpaceMissiles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetWars.Models.Weapons
+{
+    public class SpaceMissiles : Weapon
+    {
+        const double SpaceMissilesPrice = 8.75;
+        public SpaceMissiles(int destructionLevel) : base(destructionLevel, SpaceMissilesPrice)
+        {
+        }
+    }
+}
