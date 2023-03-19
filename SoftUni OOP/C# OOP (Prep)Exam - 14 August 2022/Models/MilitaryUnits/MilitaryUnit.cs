f73be06d1cccdd57648f6cd086938cd86dc77fd2
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-19 13:33:51 +0200

    adding class - StormTroopers, inheriting CLass MilitaryUnit

---
+++ SoftUni OOP/C# OOP (Prep)Exam - 14 August 2022/Models/MilitaryUnits/MilitaryUnit.cs
@@ -0,0 +1,33 @@
+﻿using PlanetWars.Models.MilitaryUnits.Contracts;
+using PlanetWars.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetWars.Models.MilitaryUnits
+{
+    public abstract class MilitaryUnit : IMilitaryUnit
+    {
+        private double cost;
+        private int enduranceLevel = 1;
+
+        public MilitaryUnit(double cost)
+        {
+            this.cost = cost;
+        }
+
+        public double Cost => cost;
+        
+        public int EnduranceLevel => this.enduranceLevel;
+
+        public void IncreaseEndurance()
+        {
+            if (this.enduranceLevel + 1 > 20)
+            {
+                throw new ArgumentException(string.Format(ExceptionMessages.EnduranceLevelExceeded));
+            }
+
+            this.enduranceLevel++;
+        }
+    }
+}
