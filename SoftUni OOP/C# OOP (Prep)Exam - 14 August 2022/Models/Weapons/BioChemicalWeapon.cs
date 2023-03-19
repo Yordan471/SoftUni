24450a3b08ced2585f86449e3c7cc1243f31bad8
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-19 13:25:46 +0200

    adding Class - NuclearWeapon inheriting Class Weapon

--- SoftUni OOP/C# OOP (Prep)Exam - 14 August 2022/Models/Weapons/BioChemicalWeapon.cs
+++ SoftUni OOP/C# OOP (Prep)Exam - 14 August 2022/Models/Weapons/BioChemicalWeapon.cs
@@ -6,9 +6,9 @@ namespace PlanetWars.Models.Weapons
 {
     public class BioChemicalWeapon : Weapon
     {
-        private const double Price = 3.2;
+        private const double BioChemicalWeaponPrice = 3.2;
 
-        public BioChemicalWeapon(int destructionLevel) : base(destructionLevel, Price)
+        public BioChemicalWeapon(int destructionLevel) : base(destructionLevel, BioChemicalWeaponPrice)
         {
         }
     }
