affb049cc16beff6facb622828775a393fb4b140
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-19 13:35:19 +0200

    adding class - SpaceForces, inheriting MilitaryUnit

---
+++ SoftUni OOP/C# OOP (Prep)Exam - 14 August 2022/Models/MilitaryUnits/SpaceForces.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetWars.Models.MilitaryUnits
+{
+    public class SpaceForces : MilitaryUnit
+    {
+        private const double SpaceForcesPrice = 11;
+
+        public SpaceForces() : base(SpaceForcesPrice)
+        {
+        }
+    }
+}
