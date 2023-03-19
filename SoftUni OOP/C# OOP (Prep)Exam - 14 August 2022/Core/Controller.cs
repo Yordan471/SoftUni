5988cb069bb9576dd833c108bba18c2b7e7c32b5
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-19 17:04:31 +0200

    Adding class Controller Implementing Interface IController

---
+++ SoftUni OOP/C# OOP (Prep)Exam - 14 August 2022/Core/Controller.cs
@@ -0,0 +1,40 @@
+﻿using PlanetWars.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetWars.Core
+{
+    public class Controller : IController
+    {
+        public string AddUnit(string unitTypeName, string planetName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string AddWeapon(string planetName, string weaponTypeName, int destructionLevel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CreatePlanet(string name, double budget)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ForcesReport()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SpaceCombat(string planetOne, string planetTwo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SpecializeForces(string planetName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
