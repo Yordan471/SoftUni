f8ebf4cd93837473a9bb98abaa7d837977106bd0
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-25 19:14:53 +0200

    Filling Class Pilot

---
+++ SoftUni OOP/C# OOP (Prep)Exam - 09 April 2022/Formula1/Models/Models/Pilot.cs
@@ -0,0 +1,64 @@
+﻿using Formula1.Models.Contracts;
+using Formula1.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formula1.Models.Models
+{
+    public class Pilot : IPilot
+    {
+        private const int LessThen5Symbols = 5;
+
+        private string fullName;
+
+        public Pilot(string fullName)
+        {
+            FullName = fullName;
+        }
+
+        public string FullName
+        {
+            get => fullName;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < LessThen5Symbols)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidPilot, value);
+                }
+
+                fullName = value;
+            }
+        }
+
+        public IFormulaOneCar Car { get; private set; }
+
+        public int NumberOfWins { get; private set; }
+
+        public bool CanRace { get; private set; }
+
+
+        public void AddCar(IFormulaOneCar car)
+        {
+            if (car == null)
+            {
+                throw new NullReferenceException(ExceptionMessages.InvalidCarForPilot);
+            }
+
+            Car = car;
+            CanRace = true;
+        }
+
+        public void WinRace()
+        {
+            this.NumberOfWins++;
+        }
+
+        public override string ToString()
+        {
+            return $"Pilot {this.FullName} has {this.NumberOfWins} wins.";
+        }
+    }
+}
