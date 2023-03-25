1ab871db84e4306528da6adea06a5e28c6d53f13
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-25 12:56:39 +0200

    adding Class Controller, implementing Interface IController

---
+++ SoftUni OOP/C# OOP (Prep)Exam - 18 April 2022/Skeleton/Heroes/Core/Controller.cs
@@ -0,0 +1,35 @@
+﻿using Heroes.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heroes.Core
+{
+    public class Controller : IController
+    {
+        public string AddWeaponToHero(string weaponName, string heroName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CreateHero(string type, string name, int health, int armour)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CreateWeapon(string type, string name, int durability)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string HeroReport()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string StartBattle()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
