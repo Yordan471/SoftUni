43cf11776f6315d818ff93eedd855c41a47298f7
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-25 13:09:00 +0200

    Adding Functionality to method CreateHero

--- SoftUni OOP/C# OOP (Prep)Exam - 18 April 2022/Skeleton/Heroes/Core/Controller.cs
+++ SoftUni OOP/C# OOP (Prep)Exam - 18 April 2022/Skeleton/Heroes/Core/Controller.cs
@@ -1,4 +1,8 @@
 ﻿using Heroes.Core.Contracts;
+using Heroes.Models.Contracts;
+using Heroes.Models.Heroes;
+using Heroes.Repositories;
+using Heroes.Repositories.Contracts;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,6 +11,15 @@ namespace Heroes.Core
 {
     public class Controller : IController
     {
+        private IRepository<IHero> heroes;
+        private IRepository<IWeapon> weapons;
+
+        public Controller()
+        {
+            heroes = new HeroRepository();
+            weapons = new WeaponRepository();
+        }
+
         public string AddWeaponToHero(string weaponName, string heroName)
         {
             throw new NotImplementedException();
@@ -14,7 +27,38 @@ namespace Heroes.Core
 
         public string CreateHero(string type, string name, int health, int armour)
         {
-            throw new NotImplementedException();
+            if (heroes.FindByName(name) != null)
+            {
+                throw new InvalidOperationException("Invalid hero type.");
+            }
+
+            IHero hero = null;
+            bool isKnight = false;
+
+            if (type == nameof(Knight))
+            {
+                hero = new Knight(name, health, armour);
+                isKnight = true;
+            }
+            else if (type == nameof(Barbarian))
+            {
+                hero = new Barbarian(name, health, armour);
+            }
+            else
+            {
+                throw new InvalidOperationException("Invalid hero type.");
+            }
+
+            heroes.Add(hero);
+
+            if (isKnight)
+            {
+                return $"Successfully added Sir {name} to the collection.";
+            }
+            else
+            {
+                return $"Successfully added Barbarian {name} to the collection.";
+            }
         }
 
         public string CreateWeapon(string type, string name, int durability)
