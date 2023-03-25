14e84084f23ddeabc75d21aca1af5f702d2e12de
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-25 13:43:55 +0200

    clearing StartUp

--- SoftUni OOP/C# OOP (Prep)Exam - 18 April 2022/Skeleton/Heroes/StartUp.cs
+++ SoftUni OOP/C# OOP (Prep)Exam - 18 April 2022/Skeleton/Heroes/StartUp.cs
@@ -11,12 +11,6 @@ namespace Heroes
     {
         public static void Main()
         {
-            IHero hero = new Barbarian("Pesho", 100, 50);
-            IWeapon Mace = null;
-
-            hero.AddWeapon(Mace);
-            Console.WriteLine(hero.Weapon);
-
             IEngine engine = new Engine();
             engine.Run();
         }
