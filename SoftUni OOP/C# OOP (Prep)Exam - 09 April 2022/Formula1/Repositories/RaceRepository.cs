4cde980a162be4958da8d778c9f7d36225aeb0ad
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-25 20:50:46 +0200

    adding Class RaceRepository

---
+++ SoftUni OOP/C# OOP (Prep)Exam - 09 April 2022/Formula1/Repositories/RaceRepository.cs
@@ -0,0 +1,54 @@
+﻿using Formula1.Models.Contracts;
+using Formula1.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Formula1.Repositories
+{
+    public class RaceRepository : IRepository<IRace>
+    {
+        private readonly ICollection<IRace> races;
+
+        public RaceRepository()
+        {
+            races = new List<IRace>();
+        }
+
+        public IReadOnlyCollection<IRace> Models => (IReadOnlyCollection<IRace>)races;
+
+        public void Add(IRace model)
+        {
+            races.Add(model);
+        }
+
+        public IRace FindByName(string name)
+        {
+            IRace race = races.FirstOrDefault(r => r.RaceName == name);
+
+            if (race == null)
+            {
+                return null;
+            }
+
+            return race;
+        }
+
+        public bool Remove(IRace model)
+        {
+            IRace removeRace = races.FirstOrDefault(r => r.Equals(model));
+
+            if (removeRace != null)
+            {
+                races.Remove(removeRace);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
