1e4c0a49ef29114606fc768c1950fe65afbfabdd
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-25 09:26:42 +0200

    adding class Weapon implementing Interface IWeapon

---
+++ SoftUni OOP/C# OOP (Prep)Exam - 18 April 2022/Skeleton/Heroes/Models/Weapons/Weapon.cs
@@ -0,0 +1,19 @@
+﻿using Heroes.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heroes.Models.Weapons
+{
+    public class Weapon : IWeapon
+    {
+        public string Name => throw new NotImplementedException();
+
+        public int Durability => throw new NotImplementedException();
+
+        public int DoDamage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
