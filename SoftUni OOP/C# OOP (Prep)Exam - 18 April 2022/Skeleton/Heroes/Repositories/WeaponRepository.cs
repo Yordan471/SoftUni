14b140b808c39113cd75e135e84df7b860caf3c2
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-25 11:44:07 +0200

    Adding Class WeaponRepository, Implementing Interface IRepository<IWeapon>

---
+++ SoftUni OOP/C# OOP (Prep)Exam - 18 April 2022/Skeleton/Heroes/Repositories/WeaponRepository.cs
@@ -0,0 +1,46 @@
+﻿using Heroes.Models.Contracts;
+using Heroes.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Heroes.Repositories
+{
+    public class WeaponRepository : IRepository<IWeapon>
+    {
+        private ICollection<IWeapon> weapons;
+
+        public IReadOnlyCollection<IWeapon> Models => (IReadOnlyCollection<IWeapon>)weapons;
+
+        public void Add(IWeapon model)
+        {
+            weapons.Add(model);
+        }
+
+        public IWeapon FindByName(string name)
+        {
+            IWeapon findWeapon = weapons.FirstOrDefault(w => w.Name == name);
+
+            if (findWeapon != null)
+            {
+                return findWeapon;
+            }
+
+            return null;
+        }
+
+        public bool Remove(IWeapon model)
+        {
+            IWeapon weaponToRemove = weapons.FirstOrDefault(w => w.Equals(model));
+
+            if (weaponToRemove != null)
+            {
+                weapons.Remove(weaponToRemove);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
