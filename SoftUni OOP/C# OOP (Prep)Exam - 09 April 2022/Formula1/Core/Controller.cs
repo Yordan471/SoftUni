852b9334082ab2c17b22b38c177e591813233dbb
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-26 09:37:28 +0300

    adding private fields

--- SoftUni OOP/C# OOP (Prep)Exam - 09 April 2022/Formula1/Core/Controller.cs
+++ SoftUni OOP/C# OOP (Prep)Exam - 09 April 2022/Formula1/Core/Controller.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Formula1.Core.Contracts;
+using Formula1.Models.Contracts;
+using Formula1.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,57 @@ using System.Threading.Tasks;
 
 namespace Formula1.Core
 {
-    internal class Controller
+    public class Controller : IController
     {
+        private PilotRepository pilotRepository;
+        private RaceRepository raceRepository;
+        private FormulaOneCarRepository formulaOneCarRepository;
+
+        public Controller()
+        {
+            pilotRepository = new PilotRepository();
+            raceRepository = new RaceRepository();
+            formulaOneCarRepository = new FormulaOneCarRepository();
+        }
+
+        public string AddCarToPilot(string pilotName, string carModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string AddPilotToRace(string raceName, string pilotFullName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CreateCar(string type, string model, int horsepower, double engineDisplacement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CreatePilot(string fullName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CreateRace(string raceName, int numberOfLaps)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string PilotReport()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string RaceReport()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string StartRace(string raceName)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
