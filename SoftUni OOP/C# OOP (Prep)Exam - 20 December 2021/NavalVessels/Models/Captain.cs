7221832b240cafbbcf03870a70b2a427c8a16d8b
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-26 16:27:14 +0300

    Adding Class - Captain, Implementing Interface - ICaptain

---
+++ SoftUni OOP/C# OOP (Prep)Exam - 20 December 2021/NavalVessels/Models/Captain.cs
@@ -0,0 +1,33 @@
+﻿using NavalVessels.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavalVessels.Models
+{
+    public class Captain : ICaptain
+    {
+        public string FullName => throw new NotImplementedException();
+
+        public int CombatExperience => throw new NotImplementedException();
+
+        public ICollection<IVessel> Vessels => throw new NotImplementedException();
+
+        public void AddVessel(IVessel vessel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void IncreaseCombatExperience()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Report()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
