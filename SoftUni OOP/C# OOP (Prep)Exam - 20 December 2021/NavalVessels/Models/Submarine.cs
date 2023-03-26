9c3dda50c28006b4f759577c0a6fad0802882a30
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-26 16:26:28 +0300

    Adding Class - Submarine, Inheriting Class - Vessel, and Implementing Interface - ISubmarine

---
+++ SoftUni OOP/C# OOP (Prep)Exam - 20 December 2021/NavalVessels/Models/Submarine.cs
@@ -0,0 +1,59 @@
+﻿using NavalVessels.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavalVessels.Models
+{
+    public class Submarine : Vessel, ISubmarine
+    {
+        private const double InitialSubmarineArmorThickness = 200;
+
+        public Submarine(string name, double mainWeaponCalibar, double speed) 
+            : base(name, mainWeaponCalibar, speed, InitialSubmarineArmorThickness)
+        {
+        }
+
+        public bool SubmergeMode { get; private set; }
+
+        public void ToggleSubmergeMode()
+        {
+            if (SubmergeMode == false)
+            {
+                this.MainWeaponCaliber += 40;
+                this.Speed -= 4;
+            }
+            else if (SubmergeMode == true)
+            {
+                this.MainWeaponCaliber -= 40;
+                this.Speed += 4;
+            }
+        }
+
+        public override void RepairVessel()
+        {
+            if (this.ArmorThickness < InitialSubmarineArmorThickness)
+            {
+                this.ArmorThickness = InitialSubmarineArmorThickness;
+            }
+        }
+
+        public override string ToString()
+        {
+            string SubmergeMode = "Submerge mode: ";
+
+            if (this.SubmergeMode == false)
+            {
+                SubmergeMode += "OFF";
+            }
+            else if (this.SubmergeMode == true)
+            {
+                SubmergeMode += "ON";
+            }
+
+            return base.ToString() + SubmergeMode;
+        }
+    }
+}
