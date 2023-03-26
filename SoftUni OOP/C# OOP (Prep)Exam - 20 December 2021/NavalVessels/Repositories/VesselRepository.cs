177e3a65f4ad3f6588e9149f0a87c3675617b10d
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-26 16:41:24 +0300

    Writing Functionality for Class VesselRepository

--- SoftUni OOP/C# OOP (Prep)Exam - 20 December 2021/NavalVessels/Repositories/VesselRepository.cs
+++ SoftUni OOP/C# OOP (Prep)Exam - 20 December 2021/NavalVessels/Repositories/VesselRepository.cs
@@ -5,26 +5,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace NavalVessels.Repositories
 {
     public class VesselRepository : IRepository<IVessel>
     {
-        public IReadOnlyCollection<IVessel> Models => throw new NotImplementedException();
+        private ICollection<IVessel> vessels;
+
+        public VesselRepository()
+        {
+            vessels = new List<IVessel>();
+        }
+
+        public IReadOnlyCollection<IVessel> Models { get => (IReadOnlyCollection<IVessel>)vessels;}
 
         public void Add(IVessel model)
         {
-            throw new NotImplementedException();
+            vessels.Add(model);
         }
 
         public IVessel FindByName(string name)
         {
-            throw new NotImplementedException();
+            IVessel vessel = vessels.FirstOrDefault(v => v.Name == name);
+
+            return vessel;
         }
 
         public bool Remove(IVessel model)
         {
-            throw new NotImplementedException();
+            return vessels.Remove(model);
         }
     }
 }
