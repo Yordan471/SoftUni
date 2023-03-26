c95cf65c3dcbc3e58e6113558dd43535a27e6e3d
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-26 16:38:08 +0300

    Adding Class VesselRepository, Implementing Interface - IRepository<IVessel>

---
+++ SoftUni OOP/C# OOP (Prep)Exam - 20 December 2021/NavalVessels/Repositories/VesselRepository.cs
@@ -0,0 +1,30 @@
+﻿using NavalVessels.Models.Contracts;
+using NavalVessels.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavalVessels.Repositories
+{
+    public class VesselRepository : IRepository<IVessel>
+    {
+        public IReadOnlyCollection<IVessel> Models => throw new NotImplementedException();
+
+        public void Add(IVessel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IVessel FindByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(IVessel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
