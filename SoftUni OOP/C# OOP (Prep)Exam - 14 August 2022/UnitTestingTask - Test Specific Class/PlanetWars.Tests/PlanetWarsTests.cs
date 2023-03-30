1fa64aa9425dad76d254b5e2a4a586c322cdbb5b
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-03-30 21:09:47 +0300

    Testing Planet Costructor

--- SoftUni OOP/C# OOP (Prep)Exam - 14 August 2022/UnitTestingTask - Test Specific Class/PlanetWars.Tests/PlanetWarsTests.cs
+++ SoftUni OOP/C# OOP (Prep)Exam - 14 August 2022/UnitTestingTask - Test Specific Class/PlanetWars.Tests/PlanetWarsTests.cs
@@ -8,7 +8,29 @@ namespace PlanetWars.Tests
         [TestFixture]
         public class PlanetWarsTests
         {
+            Planet planet;
+
+            [SetUp]
             
+            public void SetUp()
+            {
+                planet = new Planet("Mercury", 505.40);
+            }
+
+            [Test]
+
+            public void Test_PlanetWarsConstructor_SetsPropertiesCorrectly()
+            {
+                Assert.That("Mercury", Is.EqualTo(planet.Name));
+                Assert.That(505.40, Is.EqualTo(planet.Budget));
+            }
+        }
+
+        [TestFixture]
+
+        public class WeaponTests
+        {
+
         }
     }
 }
