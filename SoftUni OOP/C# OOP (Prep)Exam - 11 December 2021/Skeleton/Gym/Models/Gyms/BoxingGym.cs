f1c3f499c1704b6c76d3bd5cec3d96b634377d4e
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-01 14:54:29 +0300

    adding BoxingGym inheriting Class Gym

---
+++ SoftUni OOP/C# OOP (Prep)Exam - 11 December 2021/Skeleton/Gym/Models/Gyms/BoxingGym.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gym.Models.Gyms
+{
+    public class BoxingGym : Gym
+    {
+        private const int BoxingGymCapacity = 15;
+
+        public BoxingGym(string name) : base(name, BoxingGymCapacity)
+        {
+        }
+    }
+}
