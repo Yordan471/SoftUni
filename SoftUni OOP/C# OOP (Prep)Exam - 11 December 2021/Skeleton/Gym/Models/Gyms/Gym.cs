3973668cf8ace48646631417bc9d168787b7b2d1
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-01 13:59:27 +0300

    Adding Class Gym, Implementing Interface IGym

---
+++ SoftUni OOP/C# OOP (Prep)Exam - 11 December 2021/Skeleton/Gym/Models/Gyms/Gym.cs
@@ -0,0 +1,47 @@
+﻿using Gym.Models.Athletes.Contracts;
+using Gym.Models.Equipment.Contracts;
+using Gym.Models.Gyms.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gym.Models.Gyms
+{
+    public abstract class Gym : IGym
+    {
+        public string Name => throw new NotImplementedException();
+
+        public int Capacity => throw new NotImplementedException();
+
+        public double EquipmentWeight => throw new NotImplementedException();
+
+        public ICollection<IEquipment> Equipment => throw new NotImplementedException();
+
+        public ICollection<IAthlete> Athletes => throw new NotImplementedException();
+
+        public void AddAthlete(IAthlete athlete)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddEquipment(IEquipment equipment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Exercise()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GymInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveAthlete(IAthlete athlete)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
