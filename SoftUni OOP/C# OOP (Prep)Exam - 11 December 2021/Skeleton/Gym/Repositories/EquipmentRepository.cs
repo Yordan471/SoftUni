4652cd383e736645f17f483599d493e13fde1477
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-01 14:59:38 +0300

    adding Class EquipmentRepository, implementing Interface IRepository<IEquipment>

---
+++ SoftUni OOP/C# OOP (Prep)Exam - 11 December 2021/Skeleton/Gym/Repositories/EquipmentRepository.cs
@@ -0,0 +1,38 @@
+﻿using Gym.Models.Equipment.Contracts;
+using Gym.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gym.Repositories
+{
+    public class EquipmentRepository : IRepository<IEquipment>
+    {
+        private readonly ICollection<IEquipment> equipment;
+
+        public EquipmentRepository()
+        {
+            equipment = new List<IEquipment>();
+        }
+
+        public IReadOnlyCollection<IEquipment> Models => equipment as IReadOnlyCollection<IEquipment>;
+
+        public void Add(IEquipment model)
+        {
+            equipment.Add(model);
+        }
+
+        public IEquipment FindByType(string type)
+        {
+            IEquipment findEquipment = equipment
+               .FirstOrDefault(e => e.GetType().Name == type);
+            return findEquipment;
+        }
+
+        public bool Remove(IEquipment model)
+        {
+            return equipment.Remove(model);
+        }
+    }
+}
