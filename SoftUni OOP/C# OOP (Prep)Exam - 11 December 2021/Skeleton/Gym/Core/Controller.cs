17ab2147ac8da9732d49da2f6dd38a9d70470704
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-01 15:08:53 +0300

    Adding Class Controller, implementing Interface IController

---
+++ SoftUni OOP/C# OOP (Prep)Exam - 11 December 2021/Skeleton/Gym/Core/Controller.cs
@@ -0,0 +1,45 @@
+﻿using Gym.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gym.Core
+{
+    public class Controller : IController
+    {
+        public string AddAthlete(string gymName, string athleteType, string athleteName, string motivation, int numberOfMedals)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string AddEquipment(string equipmentType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string AddGym(string gymType, string gymName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string EquipmentWeight(string gymName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string InsertEquipment(string gymName, string equipmentType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Report()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string TrainAthletes(string gymName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
