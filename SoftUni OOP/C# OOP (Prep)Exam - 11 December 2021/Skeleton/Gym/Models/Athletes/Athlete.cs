c4bc8f5132f3e1d4c444d48e3af692e1bae23435
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-01 12:11:39 +0300

    Adding class Athlete implementing interface IAthlete

---
+++ SoftUni OOP/C# OOP (Prep)Exam - 11 December 2021/Skeleton/Gym/Models/Athletes/Athlete.cs
@@ -0,0 +1,39 @@
+﻿using Gym.Models.Athletes.Contracts;
+using Gym.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gym.Models.Athletes
+{
+    public abstract class Athlete : IAthlete
+    {
+        private string fullName;
+
+        public string FullName
+        {
+            get => fullName;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidAthleteName);
+                }
+
+                fullName = value;
+            }
+        }
+
+
+        public string Motivation => throw new NotImplementedException();
+
+        public int Stamina => throw new NotImplementedException();
+
+        public int NumberOfMedals => throw new NotImplementedException();
+
+        public void Exercise()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
