1977bd97b8f3ceb46e5cb9a187951a01a105fe15
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-01 13:54:09 +0300

    Adding Class BoxingGloves, inheriting Class Equipment

---
+++ SoftUni OOP/C# OOP (Prep)Exam - 11 December 2021/Skeleton/Gym/Models/Equipment/BoxingGloves.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gym.Models.Equipment
+{
+    public class BoxingGloves : Equipment
+    {
+        private const double BoxingGlovesWeight = 10000;
+        private const decimal BoxingGlovesPRICE = 80;
+
+        public BoxingGloves() : base(BoxingGlovesWeight, BoxingGlovesPRICE)
+        {
+        }
+    }
+}
