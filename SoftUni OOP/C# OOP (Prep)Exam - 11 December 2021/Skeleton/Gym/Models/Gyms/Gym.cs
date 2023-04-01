2dc54116407313533e455ef0eef04943bfec485f
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-01 14:47:46 +0300

    Adding functionality to Class Gym

--- SoftUni OOP/C# OOP (Prep)Exam - 11 December 2021/Skeleton/Gym/Models/Gyms/Gym.cs
+++ SoftUni OOP/C# OOP (Prep)Exam - 11 December 2021/Skeleton/Gym/Models/Gyms/Gym.cs
@@ -1,47 +1,101 @@
 ﻿using Gym.Models.Athletes.Contracts;
 using Gym.Models.Equipment.Contracts;
 using Gym.Models.Gyms.Contracts;
+using Gym.Utilities.Messages;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace Gym.Models.Gyms
 {
     public abstract class Gym : IGym
     {
-        public string Name => throw new NotImplementedException();
+        private string name;
+        private int capacity;
+        private readonly List<IEquipment> equipments;
+        private readonly List<IAthlete> athletes;
 
-        public int Capacity => throw new NotImplementedException();
+        public Gym(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+        }
 
-        public double EquipmentWeight => throw new NotImplementedException();
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidGymName);
+                }
+                
+                name = value;
+            }
+        }
 
-        public ICollection<IEquipment> Equipment => throw new NotImplementedException();
+        public int Capacity { get => capacity; private set => capacity = value; }
 
-        public ICollection<IAthlete> Athletes => throw new NotImplementedException();
+        public double EquipmentWeight => equipments.Sum(e => e.Weight);
+
+        public ICollection<IEquipment> Equipment => equipments;
+
+        public ICollection<IAthlete> Athletes => athletes;
 
         public void AddAthlete(IAthlete athlete)
         {
-            throw new NotImplementedException();
+            if (this.Capacity <= athletes.Count)
+            {
+                throw new InvalidOperationException(ExceptionMessages.NotEnoughSize);
+            }
+
+            athletes.Add(athlete);
         }
 
         public void AddEquipment(IEquipment equipment)
         {
-            throw new NotImplementedException();
+            equipments.Add(equipment);
         }
 
         public void Exercise()
         {
-            throw new NotImplementedException();
+            athletes.ForEach(a => a.Exercise());
         }
 
         public string GymInfo()
         {
-            throw new NotImplementedException();
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{this.Name} is a {this.GetType().Name}:");
+            Queue<string> athletesNames = new Queue<string>();
+
+            if (athletes.Count > 0)
+            {
+                foreach (var athlete in athletes)
+                {
+                    athletesNames.Enqueue(athlete.FullName);
+                }
+
+                sb.AppendLine($"{string.Join(", ", athletesNames)}");
+            }
+            else
+            {
+                sb.AppendLine("No athletes");
+            }
+
+            sb.AppendLine($"Equipment total count: {equipments.Count}");
+            sb.AppendLine($"Equipment total weight: {EquipmentWeight} grams");
+
+            return sb.ToString().Trim();
         }
 
         public bool RemoveAthlete(IAthlete athlete)
         {
-            throw new NotImplementedException();
+            return athletes.Remove(athlete);
         }
     }
 }
