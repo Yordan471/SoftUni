af7ebbd4487abccd5b2eb5e6e7928c4215b73aea
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-01 17:54:31 +0300

    adding skeleton for Third Task in Prep Exam - C# OOP (Prep)Exam - 11 December 2021

---
+++ SoftUni OOP/C# OOP (Prep)Exam - 11 December 2021/Skeleton/UnitTestingTask Test Gym Class/UnitTests-Skeleton/Gyms.Tests/GymsTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace Gyms.Tests
+{
+    public class GymsTests
+    {
+        Gym gym;
+
+        [SetUp]
+        public void SetUp()
+        {
+            gym = new Gym("Berkovica", 200);
+        }
+
+        [Test]
+        public void Test_GymConstructor_SettingPropertiesCorrectly()
+        {
+            string expectedGymName = "Berkovica";
+            int expectedGymSize = 200;
+            
+            Assert.That(expectedGymName, Is.EqualTo(gym.Name));
+            Assert.That(expectedGymSize, Is.EqualTo(gym.Capacity));
+        }
+    }
+}
