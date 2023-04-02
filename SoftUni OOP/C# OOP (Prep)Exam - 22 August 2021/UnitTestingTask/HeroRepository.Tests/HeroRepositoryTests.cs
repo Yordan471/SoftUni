05518c2409f321ed60fcb562dddfe8dabeb5df2c
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-02 12:57:20 +0300

    Adding SetUp Attribute and First Test for HeroRep Constructor (test if data collection initializes correctly)

--- SoftUni OOP/C# OOP (Prep)Exam - 22 August 2021/UnitTestingTask/HeroRepository.Tests/HeroRepositoryTests.cs
+++ SoftUni OOP/C# OOP (Prep)Exam - 22 August 2021/UnitTestingTask/HeroRepository.Tests/HeroRepositoryTests.cs
@@ -3,5 +3,19 @@ using NUnit.Framework;
 
 public class HeroRepositoryTests
 {
-    
+    HeroRepository heroRep;
+    Hero hero;
+
+    [SetUp]
+    public void SetUp()
+    {
+        heroRep = new HeroRepository();
+        hero = new Hero("Peshaka", 20);
+    }
+
+    [Test]
+    public void Test_HeroRepositoryConstructor_SetsPropertiesCorrectly()
+    {
+        Assert.NotNull(heroRep.Heroes);
+    }
 }
