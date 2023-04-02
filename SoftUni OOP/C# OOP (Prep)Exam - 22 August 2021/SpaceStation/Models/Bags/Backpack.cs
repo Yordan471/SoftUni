2e90912adab604408da1cb0df74a3653cbd2a6a3
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-02 09:47:14 +0300

    adding Class Backpack implementing interface IBag

---
+++ SoftUni OOP/C# OOP (Prep)Exam - 22 August 2021/SpaceStation/Models/Bags/Backpack.cs
@@ -0,0 +1,19 @@
+﻿using SpaceStation.Models.Bags.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceStation.Models.Bags
+{
+    public class Backpack : IBag
+    {
+        private readonly ICollection<string> bags;
+
+        public Backpack()
+        {
+            bags = new List<string>();
+        }
+
+        public ICollection<string> Items => bags;
+    }
+}
