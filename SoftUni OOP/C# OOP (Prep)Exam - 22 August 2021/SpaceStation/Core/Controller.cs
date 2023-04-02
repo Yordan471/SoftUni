e79ffd086395b781f1bc1b2db24d85872b5c11e7
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-02 11:44:53 +0300

    adding functionality for method AddAstronauth

--- SoftUni OOP/C# OOP (Prep)Exam - 22 August 2021/SpaceStation/Core/Controller.cs
+++ SoftUni OOP/C# OOP (Prep)Exam - 22 August 2021/SpaceStation/Core/Controller.cs
@@ -1,10 +1,69 @@
-﻿using System;
+﻿using SpaceStation.Core.Contracts;
+using SpaceStation.Models.Astronauts;
+using SpaceStation.Models.Astronauts.Contracts;
+using SpaceStation.Repositories;
+using SpaceStation.Utilities.Messages;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SpaceStation.Core
 {
-    internal class Controller
+    public class Controller : IController
     {
+        private AstronautRepository astronauts;
+        private PlanetRepository planets;
+
+        public Controller()
+        {
+            astronauts = new AstronautRepository();
+            planets = new PlanetRepository();
+        }
+
+        public string AddAstronaut(string type, string astronautName)
+        {
+            IAstronaut astronaut = null;
+
+            if (type == nameof(Biologist))
+            {
+                astronaut = new Biologist(astronautName);
+            }
+            else if (type == nameof(Geodesist))
+            {
+                astronaut = new Geodesist(astronautName);
+            }
+            else if (type == nameof(Meteorologist))
+            {
+                astronaut = new Meteorologist(astronautName);
+            }
+            else
+            {
+                throw new InvalidOperationException(ExceptionMessages.InvalidAstronautType);
+            }
+
+            astronauts.Add(astronaut);
+
+            return string.Format(OutputMessages.AstronautAdded, type, astronautName);
+        }
+
+        public string AddPlanet(string planetName, params string[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ExplorePlanet(string planetName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Report()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string RetireAstronaut(string astronautName)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
