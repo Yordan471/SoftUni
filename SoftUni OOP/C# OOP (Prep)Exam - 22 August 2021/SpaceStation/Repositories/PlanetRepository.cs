6cf07a94e557258c6c4046ce1a691ba52be3340e
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-02 10:15:48 +0300

    adding Class PlanetRepository implementing Interface IRepository<IPlanet>

---
+++ SoftUni OOP/C# OOP (Prep)Exam - 22 August 2021/SpaceStation/Repositories/PlanetRepository.cs
@@ -0,0 +1,41 @@
+﻿using SpaceStation.Models.Astronauts.Contracts;
+using SpaceStation.Models.Planets.Contracts;
+using SpaceStation.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceStation.Repositories
+{
+    public class PlanetRepository : IRepository<IPlanet>
+    {
+        private readonly ICollection<IPlanet> models;
+
+        public PlanetRepository()
+        {
+            models = new List<IPlanet>();
+        }
+
+        public IReadOnlyCollection<IPlanet> Models => models as IReadOnlyCollection<IPlanet>;
+
+        public void Add(IPlanet model)
+        {
+            models.Add(model);
+        }
+
+        public IPlanet FindByName(string name)
+        {
+            IPlanet astronaut = Models.FirstOrDefault(a => a.Name == name);
+
+            return astronaut;
+        }
+
+        public bool Remove(IPlanet model)
+        {
+            IPlanet astronaut = Models.FirstOrDefault(a => a.Equals(model));
+
+            return models.Remove(model);
+        }
+    }
+}
