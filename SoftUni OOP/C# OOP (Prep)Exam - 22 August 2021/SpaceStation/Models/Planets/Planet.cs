75ca5698d1bc24bbd5ba29052a7c70b6e661abd8
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-02 09:50:28 +0300

    adding Class Planet implementing interface IPlanet

---
+++ SoftUni OOP/C# OOP (Prep)Exam - 22 August 2021/SpaceStation/Models/Planets/Planet.cs
@@ -0,0 +1,37 @@
+﻿using SpaceStation.Models.Planets.Contracts;
+using SpaceStation.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceStation.Models.Planets
+{
+    public class Planet : IPlanet
+    {
+        private readonly ICollection<string> items;
+        private string name;
+
+        public Planet(string name)
+        {
+            Name = name;
+
+            items = new List<string>();
+        }
+
+        public ICollection<string> Items => items;
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(ExceptionMessages.InvalidPlanetName);
+                }
+
+                name = value;
+            }
+        }
+    }
+}
