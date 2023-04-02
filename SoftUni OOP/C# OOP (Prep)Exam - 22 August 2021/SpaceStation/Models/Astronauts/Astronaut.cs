da02932311dba4d85089bd286f258927a55362e5
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-02 09:36:17 +0300

    Adding Class Astronaut, implementing Interface IAstronaut

---
+++ SoftUni OOP/C# OOP (Prep)Exam - 22 August 2021/SpaceStation/Models/Astronauts/Astronaut.cs
@@ -0,0 +1,59 @@
+﻿using SpaceStation.Models.Astronauts.Contracts;
+using SpaceStation.Models.Bags.Contracts;
+using SpaceStation.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceStation.Models.Astronauts
+{
+    public abstract class Astronaut : IAstronaut
+    {
+        private const double DecreaseOxygenByTen = 10;
+
+        private string name;
+        private double oxygen;
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(ExceptionMessages.InvalidAstronautName);
+                }
+
+                name = value;
+            }
+        }
+
+        public double Oxygen
+        {
+            get => oxygen;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidOxygen);
+                }
+
+                oxygen = value;
+            }
+        }
+
+        public bool CanBreath { get; protected set; }
+
+        public IBag Bag { get; protected set; }
+
+        public virtual void Breath()
+        {
+            Oxygen -= DecreaseOxygenByTen;
+
+            if (Oxygen < 0)
+            {
+                Oxygen = 0;
+            }
+        }
+    }
+}
