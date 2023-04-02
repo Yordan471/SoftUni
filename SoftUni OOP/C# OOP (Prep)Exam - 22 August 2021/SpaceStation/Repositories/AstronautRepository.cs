f707ab6cdae4f0d157ec963926ef94c7a290f90b
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-02 10:13:24 +0300

    Adding Class AstronautRepository Implementinf interface IRepository<IAstronaut>

---
+++ SoftUni OOP/C# OOP (Prep)Exam - 22 August 2021/SpaceStation/Repositories/AstronautRepository.cs
@@ -0,0 +1,42 @@
+﻿using SpaceStation.Models.Astronauts.Contracts;
+using SpaceStation.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Xml.Linq;
+
+namespace SpaceStation.Repositories
+{
+    public class AstronautRepository : IRepository<IAstronaut>
+    {
+        private readonly ICollection<IAstronaut> models;
+
+        public AstronautRepository()
+        {
+            models = new List<IAstronaut>();
+        }
+
+        public IReadOnlyCollection<IAstronaut> Models => models as IReadOnlyCollection<IAstronaut>;
+
+        public void Add(IAstronaut model)
+        {
+            models.Add(model);
+        }
+
+        public IAstronaut FindByName(string name)
+        {
+            IAstronaut astronaut = Models.FirstOrDefault(a => a.Name == name);
+
+            return astronaut;
+        }
+
+        public bool Remove(IAstronaut model)
+        {
+            IAstronaut astronaut = Models.FirstOrDefault(a => a.Equals(model));
+
+            return models.Remove(model);
+        }
+    }
+}
