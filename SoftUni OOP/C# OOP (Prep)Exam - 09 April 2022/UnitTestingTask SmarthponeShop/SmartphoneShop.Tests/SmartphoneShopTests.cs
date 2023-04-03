e9011f1e8f62d345a6af065a56d00a50be63386a
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-03 19:57:38 +0300

    Adding SetUp Attribute and First Test for constructor setting properties correctly

--- SoftUni OOP/C# OOP (Prep)Exam - 09 April 2022/UnitTestingTask SmarthponeShop/SmartphoneShop.Tests/SmartphoneShopTests.cs
+++ SoftUni OOP/C# OOP (Prep)Exam - 09 April 2022/UnitTestingTask SmarthponeShop/SmartphoneShop.Tests/SmartphoneShopTests.cs
@@ -5,6 +5,23 @@ namespace SmartphoneShop.Tests
     [TestFixture]
     public class SmartphoneShopTests
     {
-       
+        Shop shop;
+        Smartphone phone;
+
+        [SetUp]
+        public void SetUp()
+        {
+            shop = new(10);
+            phone = new("Nokia", 100);
+        }
+
+        [Test]
+        public void Test_ConstructorSettingProperties_Correctly()
+        {
+            int expectedCapacity = 10;
+            int actualCapacity = shop.Capacity;
+
+            Assert.That(expectedCapacity, Is.EqualTo(actualCapacity));
+        }
     }
 }
