50c341a52a83189f6b5346153c8cf9513acbf35c
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-03 19:48:17 +0300

    Adding Task to Unit Testing Exercises

--- SoftUni OOP/7.2 Unit Testing - Exercises/FightingArena.Tests/ArenaTests.cs
+++ SoftUni OOP/7.2 Unit Testing - Exercises/FightingArena.Tests/ArenaTests.cs
@@ -1,10 +1,111 @@
 ﻿namespace FightingArena.Tests
 {
     using NUnit.Framework;
+    using System;
 
     [TestFixture]
     public class ArenaTests
     {
+        private Arena arena;
+        private Warrior firstWarrior;
+        private Warrior secondWarrior;
+        [SetUp]
+        public void Setup()
+        {
+            firstWarrior = new Warrior("viking", 50, 80);
+            secondWarrior = new Warrior("secondViking", 60, 90);
+            arena = new Arena();
+            arena.Enroll(firstWarrior);
+            arena.Enroll(secondWarrior);
+        }
 
+        [Test]
+        public void ValidateIfArenaIncreaseCountAfterEnrollCommand()
+        {
+            // Arrange
+            // Act
+
+            // Assert
+            Assert.That(() => arena.Count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Test_WarriorsNotNull_AfterInstantiation()
+        {
+            Assert.NotNull(arena.Warriors);
+        }
+
+        [Test]
+        public void ValidateIfArenaContainsEnrolledWarrior()
+        {
+            // Arrange
+            // Act
+
+            // Assert
+            Warrior warriorTest = new Warrior("Peshaka", 60, 100);
+            arena.Enroll(warriorTest);
+            Assert.That(3, Is.EqualTo(arena.Warriors.Count));
+            Assert.That(() => arena.Warriors, Has.Member(firstWarrior));
+
+        }
+
+        [Test]
+        public void ValidateIfEnrollCommandThrowExceptionWhenWarriorIsAlreadyEnrolled()
+        {
+            // Arrange
+            // Act
+
+            // Assert
+            Assert.That(() => arena.Enroll(firstWarrior), Throws.InvalidOperationException.With.Message.EqualTo("Warrior is already enrolled for the fights!"));
+        }
+
+        [Test]
+        public void ValidateIfFightCommandThrowExceptionWhenDefenderWarriorIsMissing()
+        {
+            // Arrange
+            // Act
+            var defender = new Warrior("anotherOrk", 50, 60);
+
+            // Assert
+            Assert.That(() => arena.Fight(firstWarrior.Name, defender.Name), Throws.InvalidOperationException.With.Message.EqualTo($"There is no fighter with name {defender.Name} enrolled for the fights!"));
+        }
+
+        [Test]
+        public void ValidateIfFightCommandThrowExceptionWhenAttackerWarriorIsMissing()
+        {
+            // Arrange
+            // Act
+            var attacker = new Warrior("ork", 60, 80);
+
+            // Assert
+            Assert.That(() => arena.Fight(attacker.Name, firstWarrior.Name), Throws.InvalidOperationException.With.Message.EqualTo($"There is no fighter with name {attacker.Name} enrolled for the fights!"));
+        }
+
+        [Test]
+        public void ValidateIfAttackCommandWorkCorrectlyWhenBothOfWarriorsExistInTheArena()
+        {
+            // Arrange
+            // Act
+            arena.Fight(firstWarrior.Name, secondWarrior.Name);
+
+            int expectedFirstWarHp = 20;
+            int expectedSecondWarHp = 40;
+            // Assert
+            Assert.That(expectedFirstWarHp, Is.EqualTo(firstWarrior.HP));
+            Assert.That(expectedSecondWarHp, Is.EqualTo(secondWarrior.HP));
+            //Assert.That(() => firstWarrior.Attack(secondWarrior), !Throws.InvalidOperationException.With.Message.EqualTo($"There is no fighter with name {secondWarrior.Name} enrolled for the fights!"));
+        }
+
+        [Test]
+        public void Test_FightMethod_WhenBothWarriorsAreNull_ReturnDefenderNameAsMissing()
+        {
+            var defender = new Warrior("anotherOrk", 50, 60);
+            var attacker = new Warrior("ork", 60, 80);
+            
+
+            Assert.Throws<InvalidOperationException>(() =>
+            arena.Fight(attacker.Name, defender.Name), $"There is no fighter with name {defender.Name} enrolled for the fights!"
+            );
+        }
     }
 }
--- SoftUni OOP/7.2 Unit Testing - Exercises/FightingArena.Tests/WarriorTests.cs
+++ SoftUni OOP/7.2 Unit Testing - Exercises/FightingArena.Tests/WarriorTests.cs
@@ -1,10 +1,197 @@
 namespace FightingArena.Tests
 {
     using NUnit.Framework;
+    using System;
 
     [TestFixture]
     public class WarriorTests
     {
+        private Warrior warrior;
+        private Warrior defenceWarrior;
+        [SetUp]
+        public void Setup()
+        {
+            warrior = new Warrior("viking", 50, 80);
+            defenceWarrior = new Warrior("defenceViking", 50, 80);
+        }
 
+        [Test]
+        public void TestNameValidatorWithNullName()
+        {
+            string name = null;
+            int damage = 50;
+            int HP = 100;
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Warrior warrior = new Warrior(name, damage, HP);
+            });
+        }
+
+        [Test]
+        public void TestNameValidatorWithEmptyName()
+        {
+            string name = string.Empty;
+            int damage = 50;
+            int HP = 100;
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Warrior warrior = new Warrior(name, damage, HP);
+            });
+        }
+
+        [Test]
+        public void TestNameValidatorWithWhitespaceName()
+        {
+            string name = "         ";
+            int damage = 50;
+            int HP = 100;
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Warrior warrior = new Warrior(name, damage, HP);
+            });
+        }
+
+        [Test]
+        public void TestDamageValidatorWithZeroDamage()
+        {
+            string name = "Kesho";
+            int damage = 0;
+            int HP = 100;
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Warrior warrior = new Warrior(name, damage, HP);
+            });
+        }
+
+        [Test]
+        public void ValidateTheConstructor()
+        {
+            // Arrange
+            // Act
+
+            // Assert
+            Assert.That(() => warrior.Name, Is.EqualTo("viking"));
+            Assert.That(() => warrior.Damage, Is.EqualTo(50));
+            Assert.That(() => warrior.HP, Is.EqualTo(80));
+        }
+
+        [Test]
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(null)]
+        public void ValidateIfThrowExceptionWhenNameIsEmpty(string name)
+        {
+            // Arrange
+            // Act
+
+            //Assert
+            Assert.That(() => new Warrior(name, 50, 80), Throws.ArgumentException.With.Message.EqualTo("Name should not be empty or whitespace!"));
+            string anotherName = string.Empty;
+            Assert.That(() => new Warrior(anotherName, 50, 80), Throws.ArgumentException.With.Message.EqualTo("Name should not be empty or whitespace!"));
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(-1)]
+        public void ValidateIfThrowExceptionWhenDamageIsNegativeNumberOrZero(int damage)
+        {
+            // Arrange
+            // Act
+
+            // Assert
+            Assert.That(() => new Warrior("viking", damage, 80), Throws.ArgumentException.With.Message.EqualTo("Damage value should be positive!"));
+        }
+
+        [Test]
+        public void ValidateIfThrowExceptionWhenHPIsNegativeNumber()
+        {
+            // Arrange
+            // Act
+
+            // Assert
+            Assert.That(() => new Warrior("viking", 50, -1), Throws.ArgumentException.With.Message.EqualTo("HP should not be negative!"));
+        }
+
+        [Test]
+        [TestCase(30)]
+        [TestCase(29)]
+        public void ValidateIfThrowExceptionWhenWarriorAttackWithLessHPThanMinimum(int HP)
+        {
+            // Arrange
+            // Act
+            var warrior = new Warrior("viking", 50, HP);
+            var defenceWarrior = new Warrior("anotherViking", 40, 50);
+
+            // Assert
+            Assert.That(() => warrior.Attack(defenceWarrior), Throws.InvalidOperationException.With.Message.EqualTo("Your HP is too low in order to attack other warriors!"));
+        }
+
+        [Test]
+        [TestCase(30)]
+        [TestCase(29)]
+        public void ValidateIfThrowExceptionWhenDefenceWarriorHPIsLessThanMinimum(int HP)
+        {
+            // Arrange
+            // Act
+            var defenceWarrior = new Warrior("defenceViking", 40, HP);
+
+            // Assert
+            Assert.That(() => warrior.Attack(defenceWarrior), Throws.InvalidOperationException.With.Message.EqualTo("Enemy HP must be greater than 30 in order to attack him!"));
+        }
+
+        [Test]
+        public void ValidateIfThrowExceptionWhenWarriorTryToAttackAnotherTooStrongWarriorWithHigherDamage()
+        {
+            // Arrange
+            // Act
+            var defenceWarrior = new Warrior("defenceViking", 200, 80);
+
+            // Assert
+            Assert.That(() => warrior.Attack(defenceWarrior), Throws.InvalidOperationException.With.Message.EqualTo("You are trying to attack too strong enemy"));
+        }
+
+        [Test]
+        public void ValidateIfAttackWorkCorrectly()
+        {
+            // Arrange
+            var warrior = new Warrior("viking", 50, 110);
+
+            // Act
+            warrior.Attack(defenceWarrior);
+
+            // Assert
+            Assert.That(() => warrior.HP, Is.EqualTo(60));
+        }
+
+        [Test]
+        public void ValidateIfSetToZeroDefenceWarriorHPWhenAttackWarriorDamageIsBiggerThanDefenceWarriorHP()
+        {
+            // Arrange
+            var warrior = new Warrior("viking", 100, 110);
+
+            // Act
+            warrior.Attack(defenceWarrior);
+
+            // Assert
+            Assert.That(() => defenceWarrior.HP, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void ValidateIfDefenceWarriorHPReduceByAttackerDamageWhenDefenceHPIsBigger()
+        {
+            // Arrange
+            var warrior = new Warrior("viking", 100, 110);
+            var defenceWarrior = new Warrior("defenceViking", 50, 210);
+
+            // Act
+            warrior.Attack(defenceWarrior);
+
+            // Assert
+            Assert.That(() => defenceWarrior.HP, Is.EqualTo(110));
+        }
     }
 }
