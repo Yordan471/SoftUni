7eb1cab1d54b042d8addaa638917df1ee5f06ccd
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-04 20:17:12 +0300

    Adding functionality for Class Car, implementing interface ICar

--- SoftUni OOP/C# OOP (Prep)Exam - 15 August 2021/CarRacing/Models/Cars/Car.cs
+++ SoftUni OOP/C# OOP (Prep)Exam - 15 August 2021/CarRacing/Models/Cars/Car.cs
@@ -1,10 +1,107 @@
-﻿using System;
+﻿using CarRacing.Models.Cars.Contracts;
+using CarRacing.Utilities.Messages;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace CarRacing.Models.Cars
 {
-    internal class Car
+    public class Car : ICar
     {
+        private const int VINExactLenght = 17;
+
+        private string make;
+        private string model;
+        private string vin;
+        private int horsePower;
+        private double fuelAvailable;
+        private double fuelConsumtpionPerRace;
+
+        public string Make
+        {
+            get => make;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidCarMake);
+                }
+
+                make = value;
+            }
+        }
+
+        public string Model
+        {
+            get => model;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidCarModel);
+                }
+
+                model = value;
+            }
+        }
+
+        public string VIN
+        {
+            get => vin;
+            private set
+            {
+                if (value.Length != VINExactLenght)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidCarVIN);
+                }
+
+                vin = value;
+            }
+        }
+
+        public int HorsePower
+        {
+            get => horsePower;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidCarHorsePower);
+                }
+
+                horsePower = value;
+            }
+        }
+
+        public double FuelAvailable
+        {
+            get => fuelAvailable;
+            private set => fuelAvailable = value;
+        }
+
+        public double FuelConsumptionPerRace
+        {
+            get => fuelConsumtpionPerRace;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidCarFuelConsumption);
+                }
+
+                fuelConsumtpionPerRace = value;
+            }
+        }
+
+        public virtual void Drive()
+        {
+            fuelAvailable -= fuelConsumtpionPerRace;
+
+            if (fuelAvailable < 0)
+            {
+                fuelAvailable = 0;
+            }
+        }
     }
 }
