f2337950f18eab86060d124b58a37dacd9fdb009
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-06 17:57:44 +0300

    Adding Class StreetRacer inheriting Racer Class

---
+++ SoftUni OOP/C# OOP (Prep)Exam - 15 August 2021/CarRacing/Models/Racers/StreetRacer.cs
@@ -0,0 +1,23 @@
+﻿using CarRacing.Models.Cars.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRacing.Models.Racers
+{
+    public class StreetRacer : Racer
+    {
+        private const int StreetRacerDrivingExperience = 10;
+        private const string StreetRacerRacingBehaviour = "aggressive";
+
+        public StreetRacer(string username, ICar car) : base(username, StreetRacerRacingBehaviour, StreetRacerDrivingExperience, car)
+        {
+        }
+
+        public override void Race()
+        {
+            base.Race();
+            this.DrivingExperience += 5;
+        }
+    }
+}
