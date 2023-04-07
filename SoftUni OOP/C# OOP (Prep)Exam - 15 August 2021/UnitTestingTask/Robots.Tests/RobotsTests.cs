357aa7253717f3e53cce3f5a9519211c380838cb
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-07 21:08:04 +0300

    Adding   Test_CapacityThrows_ArgumentException_WhenValueIsBellowZero()

--- SoftUni OOP/C# OOP (Prep)Exam - 15 August 2021/UnitTestingTask/Robots.Tests/RobotsTests.cs
+++ SoftUni OOP/C# OOP (Prep)Exam - 15 August 2021/UnitTestingTask/Robots.Tests/RobotsTests.cs
@@ -20,5 +20,13 @@
 
             Assert.That(expectedCapacity, Is.EqualTo(robotManager.Capacity));
         }
+
+        [Test]
+        public void Test_CapacityThrows_ArgumentException_WhenValueIsBellowZero()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            robotManager = new RobotManager(-1), "Invalid capacity!"
+            );
+        }
     }
 }
