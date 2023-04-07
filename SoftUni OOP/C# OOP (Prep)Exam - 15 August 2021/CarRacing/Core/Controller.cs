0c0422e33b9b1a7dd14286b46697b0ddc7b90d5c
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-07 18:28:00 +0300

    Adding Functionality for Method AddCar

--- SoftUni OOP/C# OOP (Prep)Exam - 15 August 2021/CarRacing/Core/Controller.cs
+++ SoftUni OOP/C# OOP (Prep)Exam - 15 August 2021/CarRacing/Core/Controller.cs
@@ -1,10 +1,62 @@
-﻿using System;
+﻿using CarRacing.Core.Contracts;
+using CarRacing.Models.Cars;
+using CarRacing.Models.Cars.Contracts;
+using CarRacing.Models.Maps.Contracts;
+using CarRacing.Repositories;
+using CarRacing.Utilities.Messages;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CarRacing.Core
 {
-    internal class Controller
+    public class Controller : IController
     {
+        private CarRepository cars;
+        private RacerRepository racers;
+        private IMap map;
+
+        private Controller()
+        {
+            cars = new CarRepository();
+            racers = new RacerRepository();
+        }
+
+        public string AddCar(string type, string make, string model, string VIN, int horsePower)
+        {
+            ICar car = null;
+
+            if (type == nameof(SuperCar))
+            {
+                car = new SuperCar(make, model, VIN, horsePower);
+            }
+            else if (type == nameof(TunedCar))
+            {
+                car = new TunedCar(make, model, VIN, horsePower);
+            }
+            else
+            {
+                throw new ArgumentException(ExceptionMessages.InvalidCarType);
+            }
+
+            cars.Add(car);
+
+            return string.Format(OutputMessages.SuccessfullyAddedCar, make, model, VIN);
+        }
+
+        public string AddRacer(string type, string username, string carVIN)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string BeginRace(string racerOneUsername, string racerTwoUsername)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Report()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
