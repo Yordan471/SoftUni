3a95b87a186af051a235d6b95e0ea2a76ee987c5
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-09 16:32:06 +0300

    Adding Interfaces in folder IO, IWriter and IReader

--- SoftUni OOP/7.2 Reflection and Attributes - Exercise/CommandPattern/Core/CommandInterpreter.cs
+++ SoftUni OOP/7.2 Reflection and Attributes - Exercise/CommandPattern/Core/CommandInterpreter.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using CommandPattern.Core.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CommandPattern.Core
 {
-    internal class CommandInterpreter
+    public class CommandInterpreter : ICommandInterpreter
     {
+        public string Read(string args)
+        {
+            string[] splitArgs = args.Split(' ');
+            string typeName = splitArgs[0];
+
+            string[] invokeArgs = splitArgs
+                .Skip(1)
+                .ToArray();  
+
+            Assembly assembly = Assembly.GetEntryAssembly();
+
+            Type intendedCmdType = assembly
+                .GetTypes()
+                .FirstOrDefault(t => t.Name == $"{typeName}Command");
+
+            MethodInfo method = intendedCmdType
+                .GetMethods(BindingFlags.Instance | BindingFlags.Public)
+                .FirstOrDefault(m => m.Name == "Execute");
+
+            object cmdInstance = Activator.CreateInstance(intendedCmdType);
+            string result = (string)method.Invoke(cmdInstance, new object[] { splitArgs });
+
+            return result;
+        }
     }
 }
---
+++ SoftUni OOP/7.2 Reflection and Attributes - Exercise/CommandPattern/IO/Contracts/IWriter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandPattern.IO.Contracts
+{
+    public interface IWriter
+    {
+        void Write(object value);   
+
+        void WriteLine(object value);
+    }
+}
