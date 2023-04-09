acc9e01eb3c8fc2ec63633a8bfc1bb91bbf4678e
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-09 17:04:36 +0300

    adding class MyRequiredAttribute

--- SoftUni OOP/7.2 Reflection and Attributes - Exercise/ValidationAttributes/Utilities/Attributes/MyRangeAttribute.cs
+++ SoftUni OOP/7.2 Reflection and Attributes - Exercise/ValidationAttributes/Utilities/Attributes/MyRangeAttribute.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace ValidationAttributes.Utilities.Attributes
 {
-    internal class MyRangeAttribute
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class MyRangeAttribute : MyValidationAttribute
     {
+        private int minValue = 0;
+        private int maxValue = 0;
+
+        public MyRangeAttribute(int minValue, int maxValue)
+        {
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+        }
+
+        public override bool IsValid(object value) 
+            => (int)value >= minValue && (int)value <= maxValue;
+        
     }
 }
---
+++ SoftUni OOP/7.2 Reflection and Attributes - Exercise/ValidationAttributes/Utilities/Attributes/MyRequiredAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidationAttributes.Utilities.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public class MyRequiredAttribute : MyValidationAttribute
+    {
+        public override bool IsValid(object value)
+           => value != null;
+    }
+}
