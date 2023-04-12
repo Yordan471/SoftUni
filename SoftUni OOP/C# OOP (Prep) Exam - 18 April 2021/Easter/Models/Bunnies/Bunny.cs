fe1be8a0d7a6f8f59d1b05d860fe96595bb21332
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-12 18:41:31 +0300

    adding Functionality for Class Bunny

--- SoftUni OOP/C# OOP (Prep) Exam - 18 April 2021/Easter/Models/Bunnies/Bunny.cs
+++ SoftUni OOP/C# OOP (Prep) Exam - 18 April 2021/Easter/Models/Bunnies/Bunny.cs
@@ -1,10 +1,62 @@
-﻿using System;
+﻿using Easter.Models.Bunnies.Contracts;
+using Easter.Models.Dyes.Contracts;
+using Easter.Utilities.Messages;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Easter.Models.Bunnies
 {
-    internal class Bunny
+    public abstract class Bunny : IBunny
     {
+        private string name;
+        private int energy;
+        private ICollection<IDye> dyes;
+
+        public Bunny(string name, int energy)
+        {
+            Name = name;
+            Energy = energy;
+        }
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidEggName);
+                }
+
+                name = value;
+            }
+        }
+
+        public int Energy
+        {
+            get => energy;
+            private set
+            {
+                if (value < 0)
+                {
+                    energy = 0;
+                }
+
+                energy = value;
+            }
+        }
+
+        public ICollection<IDye> Dyes
+        {
+            get => dyes; private set => dyes = value;
+        }
+
+        public void AddDye(IDye dye)
+        {
+            dyes.Add(dye);
+        }
+
+        public abstract void Work();        
     }
 }
