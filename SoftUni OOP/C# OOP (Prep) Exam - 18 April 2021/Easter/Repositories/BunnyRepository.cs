96fdc8ad2687b0cdda5e8642a76795ea06815a72
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-12 20:28:53 +0300

    Adding Functionality for BunnyRepository

--- SoftUni OOP/C# OOP (Prep) Exam - 18 April 2021/Easter/Repositories/BunnyRepository.cs
+++ SoftUni OOP/C# OOP (Prep) Exam - 18 April 2021/Easter/Repositories/BunnyRepository.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using Easter.Models.Bunnies.Contracts;
+using Easter.Repositories.Contracts;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Easter.Repositories
 {
-    internal class BunnyRepository
+    public class BunnyRepository : IRepository<IBunny>
     {
+        private ICollection<IBunny> bunnies;
+
+        public IReadOnlyCollection<IBunny> Models => bunnies as IReadOnlyCollection<IBunny>;
+
+        public void Add(IBunny model)
+        {
+            bunnies.Add(model);
+        }
+
+        public IBunny FindByName(string name)
+        {
+            return bunnies.FirstOrDefault(b => b.Name == name);
+        }
+
+        public bool Remove(IBunny model)
+        {
+            return bunnies.Remove(model);
+        }
     }
 }
