224f67b69d50ee5d8e69bedecfef50e703ae834c
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-14 11:56:29 +0300

    Adding More Tests

--- SoftUni OOP/C# OOP (Prep) Exam - 18 April 2021/UnitTestingTask/Presents.Tests/PresentsTests.cs
+++ SoftUni OOP/C# OOP (Prep) Exam - 18 April 2021/UnitTestingTask/Presents.Tests/PresentsTests.cs
@@ -1,6 +1,9 @@
 ﻿namespace Presents.Tests
 {
     using NUnit.Framework;
+    using System;
+    using System.Collections;
+    using System.Globalization;
 
     [TestFixture]
     public class PresentsTests
@@ -31,6 +34,66 @@
         public void Test_BagConstructorInitializes_PrivateFieldPresents()
         {
             Assert.NotNull(bag.GetPresents());
+        }
+
+        [Test]
+        public void Test_CreateMethodThrows_ArgumentNullException_WhenPresentIsNull()
+        {
+            Present nullPresent = null;
+
+            Assert.Throws<ArgumentNullException>(() =>
+            bag.Create(nullPresent), "Present is null");
+        }
+
+        [Test]
+        public void Test_CreateMethodThrows_InvalidOperationException_WhenPresentAlreadyExists()
+        {
+            bag.Create(present);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            bag.Create(present), "This present already exists!");
+        }
+
+        [Test]
+        public void Test_CreateMethod_AddsPresentToCollection()
+        {
+            bag.Create(present);
+            Present newPresent = new Present("Geraka", 3.4);
+            bag.Create(newPresent);
+            Present ExpectedPresent = newPresent;
+
+            Assert.Contains(ExpectedPresent, bag.GetPresents() as ICollection);
+        }
+
+        [Test]
+        public void Test_CreateMethod_ReturnsStringWhenSuccessfulyAdded()
+        {           
+            string actualString = bag.Create(present);
+            string expectedString = $"Successfully added present {present.Name}.";
+
+            Assert.AreEqual(expectedString, actualString);
+        }
+
+        [Test]
+        public void Test_RemoveMethod_RemovesGivenPresent()
+        {
+            bag.Create(present);
+
+            bag.Remove(present);
+
+            Assert.IsEmpty(bag.GetPresents());
+        }
+
+        [Test]
+        public void Test_GetPresentWithLeastMagicMethod_WorksCorrectly()
+        {
+            bag.Create(present);
+            Present newPresent = new Present("Geraka", 3.4);
+            bag.Create(newPresent);
+
+            Present actualPresent = bag.GetPresentWithLeastMagic();
+
+            Assert.Equals(present, actualPresent);
         }
     }
 }
