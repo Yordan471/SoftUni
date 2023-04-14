82d7b576a54f42e4d8f7633f94524b50595db113
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-14 08:07:50 +0300

    Adding Functionality for method AddBunny

--- SoftUni OOP/C# OOP (Prep) Exam - 18 April 2021/Easter/IO/Controller.cs
+++ SoftUni OOP/C# OOP (Prep) Exam - 18 April 2021/Easter/IO/Controller.cs
@@ -1,10 +1,65 @@
-﻿using System;
+﻿using Easter.Core.Contracts;
+using Easter.Models.Bunnies;
+using Easter.Models.Bunnies.Contracts;
+using Easter.Repositories;
+using Easter.Utilities.Messages;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Easter.IO
 {
-    internal class Controller
+    public class Controller : IController
     {
+        private BunnyRepository bunnies;
+        private EggRepository eggs;
+
+        public Controller()
+        {
+            bunnies = new BunnyRepository();
+            eggs = new EggRepository();
+        }
+
+        public string AddBunny(string bunnyType, string bunnyName)
+        {
+            IBunny bunny = null;
+
+            if (bunnyType == nameof(HappyBunny))
+            {
+                bunny = new HappyBunny(bunnyName);
+            }
+            else if (bunnyType == nameof(SleepyBunny))
+            {
+                bunny = new SleepyBunny(bunnyName);
+            }
+            else
+            {
+                throw new InvalidOperationException(ExceptionMessages.InvalidBunnyType);
+            }
+
+            bunnies.Add(bunny);
+
+            return string.Format(OutputMessages.BunnyAdded, bunnyType, bunnyName);
+        }
+
+        public string AddDyeToBunny(string bunnyName, int power)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string AddEgg(string eggName, int energyRequired)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ColorEgg(string eggName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Report()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
