91cacfad922dfe967e2acb569b649a0e5391f0e3
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-14 08:47:17 +0300

    Adding Functionality foe Method Report

--- SoftUni OOP/C# OOP (Prep) Exam - 18 April 2021/Easter/Core/Controller.cs
+++ SoftUni OOP/C# OOP (Prep) Exam - 18 April 2021/Easter/Core/Controller.cs
@@ -5,10 +5,13 @@ using Easter.Models.Dyes;
 using Easter.Models.Dyes.Contracts;
 using Easter.Models.Eggs;
 using Easter.Models.Eggs.Contracts;
+using Easter.Models.Workshops;
+using Easter.Models.Workshops.Contracts;
 using Easter.Repositories;
 using Easter.Utilities.Messages;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Easter.Core
@@ -72,12 +75,56 @@ namespace Easter.Core
 
         public string ColorEgg(string eggName)
         {
-            throw new NotImplementedException();
+            ICollection<IBunny> suitableBunnies = bunnies.Models.Where(b => b.Energy >= 50).ToList();
+
+            if (suitableBunnies.Count == 0)
+            {
+                throw new InvalidOperationException(ExceptionMessages.BunniesNotReady);
+            }
+
+            IEgg egg = eggs.FindByName(eggName);
+            IWorkshop workshop = new Workshop();
+
+            foreach (var bunny in suitableBunnies.Where(b => b.Energy != 0))
+            {
+                workshop.Color(egg, bunny);
+
+                if (bunny.Energy == 0)
+                {
+                    bunnies.Remove(bunny);
+                }  
+                
+                if (egg.IsDone())
+                {
+                    break;
+                }
+            }
+
+            if (egg.IsDone())
+            {
+                return string.Format(OutputMessages.EggIsDone, eggName);
+            }
+            else
+            {
+                return string.Format(OutputMessages.EggIsNotDone, eggName);
+            }
         }
 
         public string Report()
         {
-            throw new NotImplementedException();
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{eggs.Models.Count(e => e.IsDone())} eggs are done!");
+            sb.AppendLine($"Bunnies info:");
+
+            foreach (var bunny in bunnies.Models)
+            {
+                sb.AppendLine($"Name: {bunny.Name}");
+                sb.AppendLine($"Energy: {bunny.Energy}");
+                sb.AppendLine($"Dyes: {bunny.Dyes.Count} not finished");
+            }
+
+            return sb.ToString().TrimEnd();
         }
     }
 }
