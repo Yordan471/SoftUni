5830c20b7f35c40d0094b8f7182212f41624b8ce
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-15 08:17:55 +0300

    Adding Skeleton for Prep Exam - C# OOP (Prep)Exam - 10 April 2021

---
+++ SoftUni OOP/C# OOP (Prep)Exam - 10 April 2021/AquaShop/Models/Decorations/Decoration.cs
@@ -0,0 +1,14 @@
+﻿using AquaShop.Models.Decorations.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AquaShop.Models.Decorations
+{
+    public abstract class Decoration : IDecoration
+    {
+        public int Comfort => throw new NotImplementedException();
+
+        public decimal Price => throw new NotImplementedException();
+    }
+}
