8505d4e821052e87dda00f6ae2338f92d5b28735
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-15 09:18:57 +0300

    Adding Functionality for DecorationRepository

---
+++ SoftUni OOP/C# OOP (Prep)Exam - 10 April 2021/AquaShop/Repositories/DecorationRepository.cs
@@ -0,0 +1,36 @@
+﻿using AquaShop.Models.Decorations.Contracts;
+using AquaShop.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AquaShop.Repositories
+{
+    public class DecorationRepository : IRepository<IDecoration>
+    {
+        private ICollection<IDecoration> decorations;
+
+        public DecorationRepository()
+        {
+            decorations = new List<IDecoration>();
+        }
+
+        public IReadOnlyCollection<IDecoration> Models => decorations as IReadOnlyCollection<IDecoration>;
+
+        public void Add(IDecoration model)
+        {
+            decorations.Add(model);
+        }
+
+        public IDecoration FindByType(string type)
+        {
+            return decorations.FirstOrDefault(d => d.GetType().Name == type);
+        }
+
+        public bool Remove(IDecoration model)
+        {
+            return decorations.Remove(model);
+        }
+    }
+}
