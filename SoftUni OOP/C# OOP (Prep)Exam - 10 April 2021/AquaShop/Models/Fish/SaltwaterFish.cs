acf0b79d81013f2083d9ae302e8cc8a5c24725cb
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-15 08:38:19 +0300

    Adding Functionality for method SaltwaterFishInitialSize

---
+++ SoftUni OOP/C# OOP (Prep)Exam - 10 April 2021/AquaShop/Models/Fish/SaltwaterFish.cs
@@ -0,0 +1,32 @@
+﻿using AquaShop.Models.Fish.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AquaShop.Models.Fish
+{
+    public class SaltwaterFish : Fish
+    {
+        private const int SaltwaterFishInitialSize = 5;
+        private const int EatIncreasesInitialSizeByTwo = 2;
+
+        public SaltwaterFish(string name, string species, decimal price) : base(name, species, price)
+        {
+        }
+
+        private void SetFishSize()
+        {
+            this.Size = SaltwaterFishInitialSize;
+        }
+
+        public override void Eat()
+        {
+            if (this.Size == 0)
+            {
+                this.SetFishSize();
+            }
+
+            this.Size += EatIncreasesInitialSizeByTwo;
+        }
+    }
+}
