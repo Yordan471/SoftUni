fee8934f1eaba4928eb690d018f6abae7b1accd3
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-16 14:45:37 +0300

    Adding Finished members for Class Character

--- SoftUni OOP/C# OOP (Prep) Exam - 19 December 2020-/Entities/Characters/Character.cs
+++ SoftUni OOP/C# OOP (Prep) Exam - 19 December 2020-/Entities/Characters/Character.cs
@@ -1,12 +1,19 @@
 ﻿using System;
-
+using System.Collections.Concurrent;
 using WarCroft.Constants;
+using WarCroft.Entities.Items;
 
 namespace WarCroft.Entities.Characters.Contracts
 {
     public abstract class Character
     {
 		private string name;
+		private double baseHealth;
+		private double health;
+		private double baseArmor;
+		private double armor;
+		private double abilityPoints;
+		private Bag bag;
 
         public string Name
 		{
@@ -20,6 +27,73 @@ namespace WarCroft.Entities.Characters.Contracts
 
 				name = value;
 			}
+		}
+
+		public double BaseHealth
+		{
+			get => baseHealth;
+			private set
+			{
+				baseHealth = value;	
+			}
+        }
+
+		public double Health
+		{
+			get => health;
+			private set
+			{
+				if (value > BaseHealth)
+				{
+					health = BaseHealth;
+				}
+				else if (value < 0)
+			    {
+					health = 0;
+				}
+
+				health = value;
+			}
+        }
+
+		public double BaseArmor
+		{
+			get => baseArmor;
+			private set
+			{
+				
+
+				baseArmor = value;
+			}
+        }
+
+		public double Armor
+		{
+			get => armor;
+			private set
+			{
+                if (value < 0)
+                {
+                    armor = 0;
+                }
+
+				armor = value;
+            }			
+        }
+
+		public double AbilityPoints
+		{
+			get => abilityPoints;
+			private set
+			{
+                abilityPoints = value;
+            }
+        }
+
+		public Bag Bag
+		{
+			get => bag;
+			private set => bag = value;
 		}
 
         public bool IsAlive { get; set; } = true;
