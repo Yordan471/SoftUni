542ff101d819614e0fa1ef4a8de9bef85629b411
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-16 17:02:42 +0300

    Adding Derived Class Priest, Inheriting Base Class Character and implementing Interface IHealer

---
+++ SoftUni OOP/C# OOP (Prep) Exam - 19 December 2020-/Entities/Characters/Priest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WarCroft.Entities.Characters.Contracts;
+using WarCroft.Entities.Inventory;
+
+namespace WarCroft.Entities.Characters
+{
+    public class Priest : Character, IHealer
+    {
+        public Priest(string name, double health, double armor, double abilityPoints, Bag bag) : base(name, health, armor, abilityPoints, bag)
+        {
+        }
+
+        public void Heal(Character character)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
