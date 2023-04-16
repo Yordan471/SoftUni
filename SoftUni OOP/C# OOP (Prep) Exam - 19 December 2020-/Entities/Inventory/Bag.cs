35404cbf51af68481c4adb1a2111afb1abd07f05
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-16 15:04:42 +0300

    Adding Class Bag, implementing interface IBag and Adding Functionality to its methods

---
+++ SoftUni OOP/C# OOP (Prep) Exam - 19 December 2020-/Entities/Inventory/Bag.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WarCroft.Constants;
+using WarCroft.Entities.Items;
+
+namespace WarCroft.Entities.Inventory
+{
+    public abstract class Bag : IBag
+    {
+        private int capacity;
+        private readonly ICollection<Item> items;
+
+        public Bag(int capacity)
+        {
+            Capacity = capacity;
+        }
+
+        public int Capacity { get => capacity; set => capacity = value; }
+
+        public int Load => CalculateLoad();
+
+        public IReadOnlyCollection<Item> Items => items as IReadOnlyCollection<Item>;
+
+        private int CalculateLoad()
+        {
+            return items.Sum(i => i.Weight);
+        }
+
+        public void AddItem(Item item)
+        {
+            if (this.Load + item.Weight > this.Capacity)
+            {
+                throw new InvalidOperationException(ExceptionMessages.ExceedMaximumBagCapacity);
+            }
+
+            items.Add(item);
+        }
+
+        public Item GetItem(string name)
+        {
+            if (!this.Items.Any())
+            {
+                throw new InvalidOperationException(ExceptionMessages.EmptyBag);
+            }
+
+            Item item = Items.FirstOrDefault(i => i.GetType().Name == name);
+
+            if (item == null)
+            {
+                throw new ArgumentException(string.Format(ExceptionMessages.ItemNotFoundInBag, name));
+            }
+
+            items.Remove(item);
+            return item;
+        }
+    }
+}
