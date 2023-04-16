514db7cd9a032a3cb10920397c4553a5b67b7390
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-16 14:52:27 +0300

    Adding Class FirePotion, inheriting Class Item

---
+++ SoftUni OOP/C# OOP (Prep) Exam - 19 December 2020-/Entities/Items/FirePotion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WarCroft.Entities.Characters.Contracts;
+
+namespace WarCroft.Entities.Items
+{
+    public class FirePotion : Item
+    {
+        private const int FirePotionWeight = 5;
+        private const int DecreaseHealthByTwenty = 20;
+
+        public FirePotion() : base(FirePotionWeight)
+        {
+        }
+
+        public override void AffectCharacter(Character character)
+        {
+            base.AffectCharacter(character);
+
+            character.Health -= DecreaseHealthByTwenty;
+
+            if (character.Health <= 0)
+            {
+                character.IsAlive = false;
+            }
+        }
+    }
+}
