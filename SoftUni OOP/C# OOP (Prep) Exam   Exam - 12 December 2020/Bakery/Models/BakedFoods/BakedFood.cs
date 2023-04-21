911d4e1534fecf78508ac6655bef8e217e476ff1
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-21 19:35:36 +0300

    Adding Checks for BakedFood properties

--- SoftUni OOP/C# OOP (Prep) Exam   Exam - 12 December 2020/Bakery/Models/BakedFoods/BakedFood.cs
+++ SoftUni OOP/C# OOP (Prep) Exam   Exam - 12 December 2020/Bakery/Models/BakedFoods/BakedFood.cs
@@ -1,10 +1,59 @@
-﻿using System;
+﻿using Bakery.Models.BakedFoods.Contracts;
+using Bakery.Utilities.Messages;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Bakery.Models.BakedFoods
 {
-    internal class BakedFood
+    public abstract class BakedFood : IBakedFood
     {
+        private const int ValueZero = 0;
+
+        private string name;
+        private int portion;
+        private decimal price;
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidName);
+                }
+
+                name = value;
+            }
+        }
+
+        public int Portion
+        {
+            get => portion;
+            private set
+            {
+                if (value <= ValueZero)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidPortion);
+                }
+
+                portion = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get => price;
+            private set
+            {
+                if (value <= ValueZero)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidPrice);
+                }
+
+                price = value;
+            }
+        }
     }
 }
