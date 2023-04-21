bec412d935448ea76ee5da05b9363b7c92f5c212
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-21 19:49:02 +0300

    Adding Child Class Tea, inheriting Base Class Drink

---
+++ SoftUni OOP/C# OOP (Prep) Exam   Exam - 12 December 2020/Bakery/Models/Drinks/Tea.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bakery.Models.Drinks
+{
+    public class Tea : Drink
+    {
+        private const decimal TeaPrice = 2.5M;
+
+        public Tea(string name, int portion, string brand) : base(name, portion, TeaPrice, brand)
+        {
+        }
+    }
+}
