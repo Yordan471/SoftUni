c89d3202853733f11e58672810aea78dd8b26004
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-21 19:43:22 +0300

    Adding Base Class Drink, implementing interface IDrink

---
+++ SoftUni OOP/C# OOP (Prep) Exam   Exam - 12 December 2020/Bakery/Models/Drinks/Drink.cs
@@ -0,0 +1,75 @@
+﻿using Bakery.Models.Drinks.Contracts;
+using Bakery.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Bakery.Models.Drinks
+{
+    public abstract class Drink : IDrink
+    {
+        private const int ValueZero = 0;
+
+        private string name;
+        private int portion;
+        private decimal price;
+        private string brand;
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidName);
+                }
+
+                name = value;
+            }
+        }
+
+        public int Portion
+        {
+            get => portion;
+            private set
+            {
+                if (value <= ValueZero)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidPortion);
+                }
+
+                portion = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get => price;
+            private set
+            {
+                if (value <= ValueZero)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidPrice);
+                }
+
+                price = value;
+            }
+        }
+
+        public string Brand
+        {
+            get => brand;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidBrand);
+                }
+
+                brand = value;
+            }
+        }
+    }
+}
