c64dcfe94182af615b0c84852445e37e30739019
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-04-24 18:11:15 +0300

    Adding Class Table, implementing interface ITable

---
+++ SoftUni OOP (Advanced Module)/C# OOP (Prep) Exam   Exam - 12 December 2020/Bakery/Models/Tables/Table.cs
@@ -0,0 +1,54 @@
+﻿using Bakery.Models.BakedFoods.Contracts;
+using Bakery.Models.Drinks.Contracts;
+using Bakery.Models.Tables.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bakery.Models.Tables
+{
+    public abstract class Table : ITable
+    {
+        public int TableNumber { get; private set; }
+
+        public int Capacity => throw new NotImplementedException();
+
+        public int NumberOfPeople => throw new NotImplementedException();
+
+        public decimal PricePerPerson => throw new NotImplementedException();
+
+        public bool IsReserved => throw new NotImplementedException();
+
+        public decimal Price => throw new NotImplementedException();
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetBill()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFreeTableInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OrderDrink(IDrink drink)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OrderFood(IBakedFood food)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reserve(int numberOfPeople)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
