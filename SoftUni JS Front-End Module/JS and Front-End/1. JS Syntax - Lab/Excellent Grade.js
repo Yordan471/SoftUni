447ec741438533815bd9aab62bbd5d2f82e8fb6d
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-06-19 21:29:16 +0300

    adding solution for task 3. Excellent grade

---
+++ SoftUni JS Front-End Module/JS and Front-End/1. JS Syntax - Lab/Excellent Grade.js
@@ -0,0 +1,12 @@
+function solve(number) {
+    if (number >= 5.50)
+    {
+        console.log('Excellent');
+    }
+    else if (number < 5.50)
+    {
+        console.log('Not excellent');
+    }
+}
+
+solve (4);
