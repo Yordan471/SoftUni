629358a5e2facac57a1c6ee3db20250245bae00f
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-06-22 20:11:15 +0300

    adding solution for task 01. Ages

--- SoftUni JS Front-End Module/JS and Front-End/1. JS Syntax - Lab/16. Count String Occurrences.js
+++ SoftUni JS Front-End Module/JS and Front-End/1. JS Syntax - Lab/16. Count String Occurrences.js
@@ -3,7 +3,7 @@ function solve(text, word) {
 
     let array = text.split(' ');
     for (let index = 0; index < array.length; index++) {
-        if (array[index] === 'is')
+        if (array[index] === word)
         {
             countWord++;
         }       
