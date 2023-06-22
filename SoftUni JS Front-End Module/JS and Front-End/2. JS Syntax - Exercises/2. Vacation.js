0e8d25ce2d2a4f5efcc3fd62b858d7c4a3f1ccfb
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-06-22 20:33:22 +0300

    adding solution for task 02. Vacation

---
+++ SoftUni JS Front-End Module/JS and Front-End/2. JS Syntax - Exercises/2. Vacation.js
@@ -0,0 +1,60 @@
+function solve(numOfPeople, type, day) {
+    let price = 0;
+
+    if (type === 'Students') {
+        if (day === 'Friday') {
+            price = numOfPeople * 8.45;
+        }
+        else if (day === 'Saturday') {
+            price = numOfPeople * 9.80;
+        }
+        else if (day === 'Sunday') {
+            price = numOfPeople * 10.46;
+        }
+    }
+    else if (type === 'Business') {
+        if (day === 'Friday') {
+            price = numOfPeople * 10.90;
+        }
+        else if (day === 'Saturday') {
+            price = numOfPeople * 15.60;
+        }
+        else if (day === 'Sunday') {
+            price = numOfPeople * 16;
+        }
+    }
+    else if (type === 'Regular') {
+        if (day === 'Friday') {
+            price = numOfPeople * 15;
+        }
+        else if (day === 'Saturday') {
+            price = numOfPeople * 20;
+        }
+        else if (day === 'Sunday') {
+            price = numOfPeople * 22.50;
+        }
+    }
+
+    if (type === 'Students') {
+        if (numOfPeople >= 30) {
+            price *= 0.85;
+        }
+    }
+    else if (type === 'Business') {
+        if (numOfPeople >= 100) {
+            price = price - (10 * price);
+        }
+    }
+    else if (type === 'Regular') {
+        if (numOfPeople >= 10 && numOfPeople <= 20) {
+            price *= 0.95;
+        }
+    }
+
+    let actualPrice = price.toFixed(2);
+
+    console.log(`Total price of: ${actualPrice}`);
+}
+
+solve(30, 'Students', 'Sunday');
+solve(40, 'Regular', 'Saturday');
