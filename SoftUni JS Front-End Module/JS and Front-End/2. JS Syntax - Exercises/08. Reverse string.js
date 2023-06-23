1bcae8d2008a017c446194d02204c0f7d19cd39b
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-06-23 19:50:38 +0300

    adding solution for task 08.Reverse String

---
+++ SoftUni JS Front-End Module/JS and Front-End/2. JS Syntax - Exercises/08. Reverse string.js
@@ -0,0 +1,9 @@
+function solve(...chars) {   
+
+    chars = chars.reverse();
+    let string = `${chars[0]}` + `${chars[1]}` + `${chars[2]}`;
+
+    console.log(string);
+}
+
+solve('a', 'b', 'c');
