8437be5e95d9aac8ff87bac2d53c9d69bd62f928
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-06-25 10:27:22 +0300

    adding solution for task 14.

---
+++ SoftUni JS Front-End Module/JS and Front-End/2. JS Syntax - Exercises/14. Print every N-th Element from an Array.js
@@ -0,0 +1,13 @@
+function solve(array, stepNumber) {
+    let outputArr = [];
+    
+    for (let index = 0; index < array.length; index +=stepNumber) {
+        outputArr.push(array[index]);
+    }
+    
+    return outputArr;
+}
+
+solve(['5', '20', '31', '4', '20'], 2);
+solve(['dsa', 'asd', 'test', 'tset'], 2);
+solve(['1', '2', '3', '4', '5'], 6);
