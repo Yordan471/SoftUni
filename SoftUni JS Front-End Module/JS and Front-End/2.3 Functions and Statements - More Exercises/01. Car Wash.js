03c84528b294c01af018e6e9eebe21fab1da7268
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-07-02 09:38:40 +0300

    adding solution for task 01. Car Wash

---
+++ SoftUni JS Front-End Module/JS and Front-End/2.3 Functions and Statements - More Exercises/01. Car Wash.js
@@ -0,0 +1,24 @@
+function CarWash(operations) {
+    let value = 0;
+
+while (operations.length > 0) {
+    switch (operations.shift()) {
+        case 'soap':
+            value += 10;
+            break;
+        case 'water':
+            value *= 1.20;
+            break;
+        case 'vacuum cleaner':
+            value *= 1.25;
+            break;
+        case 'mud':
+            value *= 0.90;
+            break;
+    }
+}
+
+    console.log(`The car is ${value.toFixed(2)}.`);
+}
+
+CarWash(["soap", "water", "mud", "mud", "water", "mud", "vacuum cleaner"]);
