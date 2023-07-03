7860960daf1a172af70c5b09fba581ca8f026de7
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-07-03 19:52:50 +0300

    adding solution for task 07. Meetings

--- SoftUni JS Front-End Module/JS and Front-End/3.1 Objects and Classes - Lab/06. Phone Book.js
+++ SoftUni JS Front-End Module/JS and Front-End/3.1 Objects and Classes - Lab/06. Phone Book.js
@@ -11,9 +11,7 @@ function solve(input) {
 
     for (const key in phoneBook) {
         console.log(`${key} -> ${phoneBook[key]}`);
-    }
-        
-    
+    } 
 }
 
 solve(['Tim 0834212554','Peter 0877547887','Bill 0896543112','Tim 0876566344'])
---
+++ SoftUni JS Front-End Module/JS and Front-End/3.1 Objects and Classes - Lab/07. Meetings.js
@@ -0,0 +1,24 @@
+function solve(array) {
+    let schedule = {};
+
+    for (const info of array) {
+        let entries = info.split(' ');
+        let weekday = entries[0];
+        let name = entries[1];
+
+        if (schedule.hasOwnProperty(weekday)) {
+            console.log(`Conflict on ${weekday}!`);
+            continue;
+        }
+
+        schedule[weekday] = name;
+        
+        console.log(`Sheduled for ${weekday}`);         
+        }
+
+        for (const key in schedule) {
+            console.log(`${key} -> ${schedule[key]}`);
+        }
+    }
+
+    solve(['Monday Peter','Wednesday Bill','Monday Tim','Friday Tim']);
