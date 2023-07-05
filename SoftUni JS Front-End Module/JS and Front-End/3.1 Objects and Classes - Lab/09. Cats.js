5a5435a152165ff35569977b74c3a46beeb28f29
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-07-05 18:06:07 +0300

    Create 09. Cats.js

---
+++ SoftUni JS Front-End Module/JS and Front-End/3.1 Objects and Classes - Lab/09. Cats.js
@@ -0,0 +1,26 @@
+class Cat {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    meow() {
+        console.log(`${this.name}, age ${this.age} says Meow`);
+    }
+}
+
+function solve(inputArray) { 
+    let gatherCats = [];
+
+    for (const line of inputArray) {
+        let splitedLine = line.split(' ');
+        let cat = new Cat(splitedLine[0], splitedLine[1]);
+        gatherCats.push(cat);
+    }
+
+    for (const animal of gatherCats) {
+    animal.meow();
+    }
+}
+
+solve(['Mellow 2', 'Tom 5']);
