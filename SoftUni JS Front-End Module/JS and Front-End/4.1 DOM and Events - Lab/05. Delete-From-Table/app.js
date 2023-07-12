9701f8646acdf4ea901525c244cc050327ba44cd
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-07-12 19:39:17 +0300

    adding solution for task 05. Delete from Table

--- SoftUni JS Front-End Module/JS and Front-End/4.1 DOM and Events - Lab/05. Delete-From-Table/app.js
+++ SoftUni JS Front-End Module/JS and Front-End/4.1 DOM and Events - Lab/05. Delete-From-Table/app.js
@@ -1,3 +1,18 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+  const emailToDelete = document.querySelector('input[name="email"]');
+  const emailBoxes = Array.from(
+    document.querySelectorAll("td:nth-child(even)")
+  );
+
+  const userEmailBox = emailBoxes.find(
+    (email) => email.textContent === emailToDelete.value
+  );
+  const result = document.getElementById("result");
+
+  if (userEmailBox) {
+    userEmailBox.parentElement.remove();
+    result.textContent = "Deleted.";
+  } else {
+    result.textContent = "Not found.:";
+  }
 }
