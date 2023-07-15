e0bace4328e09b81dc8da7181ac4e206c3427425
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-07-15 10:11:15 +0300

    adding solution for task 08. Sum Table

--- SoftUni JS Front-End Module/JS and Front-End/4.1 DOM and Events - Lab/08. Sum-Table/SumTable.js
+++ SoftUni JS Front-End Module/JS and Front-End/4.1 DOM and Events - Lab/08. Sum-Table/SumTable.js
@@ -1,3 +1,10 @@
 function sumTable() {
+    const prices = Array.from(document.querySelectorAll("td:nth-child(even)"));
+    prices.pop();
 
+    const sum = prices.reduce((acc, curr) => {
+       return acc + Number(curr.textContent);
+    }, 0);
+
+    document.getElementById("sum").textContent = sum;
 }
