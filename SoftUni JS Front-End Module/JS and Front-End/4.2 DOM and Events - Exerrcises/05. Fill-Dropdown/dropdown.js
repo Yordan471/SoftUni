5808cb11731549925e68b19f8cc18df69eeff521
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-07-16 15:12:48 +0300

    adding solution for task 05. Fill Dropdown

--- SoftUni JS Front-End Module/JS and Front-End/4.2 DOM and Events - Exerrcises/05. Fill-Dropdown/dropdown.js
+++ SoftUni JS Front-End Module/JS and Front-End/4.2 DOM and Events - Exerrcises/05. Fill-Dropdown/dropdown.js
@@ -1,3 +1,15 @@
 function addItem() {
-    console.log('TODO:...');
+    const textInput = document.querySelector("#newItemText");
+    const valueInput = document.querySelector("#newItemValue");
+
+    const option = document.createElement("option");
+
+    option.textContent = textInput.value;
+    option.setAttribute("value", valueInput.value);
+
+    const select = document.querySelector("#menu");
+    select.appendChild(option);
+
+    textInput.value = "";
+    valueInput.value = "";
 }
