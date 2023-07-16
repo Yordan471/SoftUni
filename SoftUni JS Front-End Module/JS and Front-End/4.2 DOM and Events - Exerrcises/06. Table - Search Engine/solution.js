71395ea842ca34e649cfbc6b26c4e038f80dbea8
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-07-16 15:53:45 +0300

    adding solution for task 06. Table - Search Engine

--- SoftUni JS Front-End Module/JS and Front-End/4.2 DOM and Events - Exerrcises/06. Table - Search Engine/solution.js
+++ SoftUni JS Front-End Module/JS and Front-End/4.2 DOM and Events - Exerrcises/06. Table - Search Engine/solution.js
@@ -1,8 +1,19 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
-
+   
    function onClick() {
-      //   TODO:
+      const searchQuery = document.querySelector("#searchField").value;
+      const cells = Array.from(document.querySelectorAll("tbody td"));
+      const activeRows = Array.from(document.querySelectorAll("tbody tr.select"));
 
+      activeRows.forEach((row) => {
+         row.classList.remove("select");
+      });
+
+      cells.forEach((cell) => {
+         if(cell.textContent.includes(searchQuery)) {
+            cell.parentElement.classList.add("select");
+         }
+      });
    }
 }
