16d4378a6a789068472b5e1442fbf854d68fb0a5
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-07-16 10:17:42 +0300

    adding solution for task 04. Locked Profile

--- SoftUni JS Front-End Module/JS and Front-End/4.2 DOM and Events - Exerrcises/04. Locked-Profile/app.js
+++ SoftUni JS Front-End Module/JS and Front-End/4.2 DOM and Events - Exerrcises/04. Locked-Profile/app.js
@@ -1,3 +1,20 @@
 function lockedProfile() {
-    console.log('TODO...')
+  const buttons = Array.from(document.querySelectorAll("button"));
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      const lockedRadioButton =
+        e.currentTarget.parentElement.querySelector(`input[type="radio"]`);
+
+      if (lockedRadioButton.checked) {
+        return;
+      }
+
+      const isHidden = e.currentTarget.textContent === "Show more";
+      const hiddenInfo = e.currentTarget.parentElement.querySelector("div");
+
+      hiddenInfo.style.display = isHidden ? "block" : "none";
+      e.currentTarget.textContent = isHidden ? "Hide it" : "Show more";
+    });
+  });
 }
