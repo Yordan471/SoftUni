fb5fb4cfa81adf5d7124a047bad27d3dd89ca79e
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-07-16 08:57:34 +0300

    adding solution for task 02. Sections

--- SoftUni JS Front-End Module/JS and Front-End/4.2 DOM and Events - Exerrcises/02. Sections/script.js
+++ SoftUni JS Front-End Module/JS and Front-End/4.2 DOM and Events - Exerrcises/02. Sections/script.js
@@ -1,3 +1,18 @@
 function create(words) {
-   console.log('TODO:...');
+   const container = document.getElementById("content");
+
+   words.forEach((word) => {
+      const div = document.createElement("div");
+      const p = document.createElement("p");
+      p.textContent = word;
+      p.style.display = "none";
+
+      div.appendChild(p);
+      div.addEventListener("click", (e) => {
+          const invParagraph = e.currentTarget.querySelector("p");
+          invParagraph.style.display = "block";
+      });
+
+      container.appendChild(div);
+   });
 }
