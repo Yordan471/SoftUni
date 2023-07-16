0bcba0a91e555fc1cdbd5ee1e6d7044c9a97e7b1
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-07-16 17:14:23 +0300

    adding solution for task 07. Format the Text

--- SoftUni JS Front-End Module/JS and Front-End/4.2 DOM and Events - Exerrcises/07. Format the Text/solution.js
+++ SoftUni JS Front-End Module/JS and Front-End/4.2 DOM and Events - Exerrcises/07. Format the Text/solution.js
@@ -1,3 +1,16 @@
 function solve() {
-  //TODO
+  const textAreaContent = document.querySelector("#input").value.split(".");
+  textAreaContent.pop();
+
+
+  while (textAreaContent.length > 0) {
+    const p = document.createElement("p");
+    p.textContent = textAreaContent
+      .splice(0, 3)
+      .map((text) => text.trim())
+      .join(".");
+
+      p.textContent += ".";
+    document.querySelector("#output").appendChild(p);
+  }
 }
