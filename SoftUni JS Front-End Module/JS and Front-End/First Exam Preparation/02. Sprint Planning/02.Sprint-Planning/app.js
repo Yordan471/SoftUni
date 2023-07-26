b765da5411d630aa46de3d9f7d30111097263a6b
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-07-26 20:16:18 +0300

    adding func createTask

--- SoftUni JS Front-End Module/JS and Front-End/First Exam Preparation/02. Sprint Planning/02.Sprint-Planning/app.js
+++ SoftUni JS Front-End Module/JS and Front-End/First Exam Preparation/02. Sprint Planning/02.Sprint-Planning/app.js
@@ -14,5 +14,24 @@ function solve() {
         deleteTaskBtn: document.querySelector("#delete-task-btn"),
     };
 
-    
+    const tasks = {};
+
+    buttons.createTaskBtn.addEventListener("click", createTask);
+
+    function createTask(e) {
+        if (Object.values(inputFormats).some((input) => input.value === "")) {
+            return;
+        }
+
+        const task = {
+            id: `task-${Object.values(tasks).length + 1}`,
+            title: inputFormats.title.value,
+            description: inputFormats.description.value,
+            label: inputFormats.label.value,
+            points: Number(inputFormats.points.value),
+            asignee: inputFormats.asignee.value,
+        };
+
+        tasks[task.id] = task;
+    }
 }
