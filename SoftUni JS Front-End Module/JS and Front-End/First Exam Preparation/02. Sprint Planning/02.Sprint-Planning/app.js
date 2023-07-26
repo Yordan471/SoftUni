ee5cb803eebf8ccdd02925075a583ea891dd8b47
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-07-26 19:50:16 +0300

    getting all input and btn selectors

--- SoftUni JS Front-End Module/JS and Front-End/First Exam Preparation/02. Sprint Planning/02.Sprint-Planning/app.js
+++ SoftUni JS Front-End Module/JS and Front-End/First Exam Preparation/02. Sprint Planning/02.Sprint-Planning/app.js
@@ -1,5 +1,18 @@
 window.addEventListener('load', solve);
 
 function solve() {
-    // TODO:
+    const inputFormats = {
+        title: document.querySelector("#title"),
+        description: document.querySelector("#description"),
+        label: document.querySelector("#label"),
+        points: document.querySelector("#points"),
+        asignee: document.querySelector("#assignee"),
+    };
+
+    const buttons = {
+        createTaskBtn: document.querySelector("#create-task-btn"),
+        deleteTaskBtn: document.querySelector("#delete-task-btn"),
+    };
+
+    
 }
