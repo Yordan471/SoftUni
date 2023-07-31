03cc9075c420352509094443e4541866e320b830
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-07-31 19:02:57 +0300

    adding code for depart click button

--- SoftUni_JS_Front-End_Module/JS_and_Front-End/5.1_HTTP_and_REST-Exercises/02.Bus-Schedule/app.js
+++ SoftUni_JS_Front-End_Module/JS_and_Front-End/5.1_HTTP_and_REST-Exercises/02.Bus-Schedule/app.js
@@ -1,6 +1,16 @@
 function solve() {
+    const departBtn = document.querySelector("#depart");
+    const arriveBtn = document.querySelector("#arrive");
+    const infoBox = document.querySelector("#info .info");
+
     function depart() {
-        // TODO: 
+        fetch("http://localhost:3030/jsonstore/bus/schedule/depot")
+        .then((res) => res.json())
+        .then((busStop) => {
+            departBtn.disabled = true;
+            arriveBtn.disabled = false;
+            infoBox.textContent = `Next stop ${busStop.name}`
+        });
     }
 
     async function arrive() {
