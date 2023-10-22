3a48af36cd7cdff27fd0c551d491d3c28e39c094
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-10-22 09:27:28 +0300

    adding Project for NuGetPackages that will be used for my projects named - LibrariesForProjects

---
+++ SoftUni Entrity Framework Core/EntityFrameworkCore - Introduction/LibrariesForProjects/LibrariesForProjects/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LibrariesForProjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
