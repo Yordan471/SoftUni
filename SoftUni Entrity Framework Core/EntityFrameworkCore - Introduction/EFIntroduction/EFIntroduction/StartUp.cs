55baa96765c7737ef66c5b7b44caa715ea456d1a
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-10-24 21:34:46 +0300

    Adding Solution - EFIntroduction, Adding Project - Softuni, Changing Class Program to Class StartUp, Adding public static string GetEmployeesFullInformation

---
+++ SoftUni Entrity Framework Core/EntityFrameworkCore - Introduction/EFIntroduction/EFIntroduction/StartUp.cs
@@ -0,0 +1,34 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using SoftUni.Data;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using var context = new SoftUniContext();
+
+            Console.WriteLine(GetEmployeesFullInformation(context));
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = context.Employees;
+
+            var employeesFullInformation = employees.AsNoTracking().OrderBy(e => e.EmployeeId);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var employee in employeesFullInformation)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
