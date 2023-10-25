c876f84679e1020882acd15f411203438c3275d9
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-10-25 20:13:33 +0300

    adding method GetEmployeesWithSalaryOver50000 that takes all employees with salary above 50000

--- SoftUni Entrity Framework Core/EntityFrameworkCore - Introduction/EFIntroduction/EFIntroduction/StartUp.cs
+++ SoftUni Entrity Framework Core/EntityFrameworkCore - Introduction/EFIntroduction/EFIntroduction/StartUp.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Query.Internal;
 using SoftUni.Data;
+using SoftUni.Models;
 using System.Text;
 
 namespace SoftUni
@@ -12,7 +13,9 @@ namespace SoftUni
         {
             using var context = new SoftUniContext();
 
-            Console.WriteLine(GetEmployeesFullInformation(context));
+            //Console.WriteLine(GetEmployeesFullInformation(context));
+
+            Console.WriteLine(GetEmployeesWithSalaryOver50000(context));
         }
 
         public static string GetEmployeesFullInformation(SoftUniContext context)
@@ -26,6 +29,32 @@ namespace SoftUni
             foreach (var employee in employeesFullInformation)
             {
                 sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var employees = context.Employees;
+
+            int aboveSalary = 50000;
+
+            var employeesSalaryOver50000 = employees
+                .Where(e => e.Salary >= aboveSalary)
+                .OrderBy(e => e.FirstName)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.Salary
+                })
+                .ToArray();
+
+            StringBuilder sb = new();
+
+            foreach (var employee in employeesSalaryOver50000)
+            {
+                sb.AppendLine($"{employee.FirstName} - {employee.Salary:F2}");
             }
 
             return sb.ToString().TrimEnd();
