86a712140354ab4712517749d31996456478ccd5
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-11-04 14:32:50 +0200

    adding class StudentSystemContext in P01_StudentSystem.Data for out connection with the sql server

---
+++ SoftUni Entrity Framework Core/EntityFrameworkCore - Entity Relations/P01_StudentSystem/P01_StudentSystem.Data/StudentSystemContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+        //public StudentSystemContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
+
+        private const string ConnectionString = "Server=DESKTOP-U0UT8KF\\SQLEXPRESS;Database=StudentSystem;Trusted_Connection=True;TrustServerCertificate=True";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+    }
+}
