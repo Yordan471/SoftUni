7227c6883d4a268a11788b9242a8ee749a20bd38
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-11-07 14:47:11 +0200

    adding implementation for method GetBooksByAgeRestriction

--- SoftUni Entrity Framework Core/5. Advanced Querying/BookShop/StartUp.cs
+++ SoftUni Entrity Framework Core/5. Advanced Querying/BookShop/StartUp.cs
@@ -1,5 +1,6 @@
 ﻿namespace BookShop
 {
+    using BookShop.Models.Enums;
     using Data;
     using Initializer;
 
@@ -9,6 +10,22 @@
         {
             using var db = new BookShopContext();
             DbInitializer.ResetDatabase(db);
+
+            Console.WriteLine(GetBooksByAgeRestriction(db, "miNor"));
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+
+            AgeRestriction ageRestriction = Enum.Parse<AgeRestriction>(command, true);
+
+            var books = context.Books
+                .Where(b => b.AgeRestriction == ageRestriction)
+                .OrderBy(b => b.Title)
+                .Select(b => b.Title)
+                .ToList();
+
+            return string.Join(Environment.NewLine, books);
         }
     }
 }
