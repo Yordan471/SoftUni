51bf9091b89d999f284bb2b6c729cd61e49e9b07
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-11-07 15:22:42 +0200

    adding implementation for method GetGoldenBooks

--- SoftUni Entrity Framework Core/5. Advanced Querying/BookShop/StartUp.cs
+++ SoftUni Entrity Framework Core/5. Advanced Querying/BookShop/StartUp.cs
@@ -11,7 +11,9 @@
             using var db = new BookShopContext();
             DbInitializer.ResetDatabase(db);
 
-            Console.WriteLine(GetBooksByAgeRestriction(db, "miNor"));
+            //Console.WriteLine(GetBooksByAgeRestriction(db, "miNor"));
+            Console.WriteLine(GetGoldenBooks(db));
+
         }
 
         public static string GetBooksByAgeRestriction(BookShopContext context, string command)
@@ -24,6 +26,21 @@
                 .OrderBy(b => b.Title)
                 .Select(b => b.Title)
                 .ToList();
+
+            return string.Join(Environment.NewLine, books);
+        }
+
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            int copies = 5000;
+
+            EditionType editionType = Enum.Parse<EditionType>("Gold");
+            var books = context.Books
+                .Where(b => b.EditionType == editionType &&
+                            b.Copies < copies)
+                .OrderBy(b => b.BookId)
+                .Select(b => b.Title)
+                .AsEnumerable();
 
             return string.Join(Environment.NewLine, books);
         }
