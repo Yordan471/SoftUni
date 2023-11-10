c929ec14b149adb9958484937e418705aef7ec81
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-11-10 16:12:35 +0200

    for the Prep Exam, adding correct server name for the connection string; Adding Classes - Product and ProductClient + Implementation; Adding enum Class CategoryType

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Prep Exam .NET 6.0 - 11 April 2023/Invoices/Data/Models/ProductClient.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Invoices.Data.Models
+{
+    public class ProductClient
+    {
+        [Required]
+        public int ProductId { get; set; }
+
+        [ForeignKey(nameof(ProductId))]
+        public Product Product { get; set; }
+
+        public int ClientId { get; set; }
+
+        [ForeignKey(nameof(ClientId))]
+        public Client Client { get; set; }
+    }
+}
