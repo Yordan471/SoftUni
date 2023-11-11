a0cdb4142f8b8744cba5791df83d27550d822711
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-11-11 12:45:38 +0200

    adding and implementing Class Invoice

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Prep Exam .NET 6.0 - 11 April 2023/Invoices/Data/Models/Invoice.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Invoices.Data.Models
+{
+    public class Invoice
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(10)]
+        [MaxLength(20)]
+        public string StreetName { get; set; }
+
+        [Required]
+        public int StreetNumber { get; set; }
+
+        [Required]
+        public string PostCode { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        [MaxLength(15)]
+        public string City { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        [MaxLength(15)]
+        public string Country { get; set; }
+
+        public int ClientId { get; set; }
+
+        [ForeignKey(nameof(ClientId))]
+        public Client Client { get; set; }
+    }
+}
