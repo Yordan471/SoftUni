cbd0131c8d357d56943d27631ff76b1fc028dfe6
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-11-18 19:07:33 +0200

    Adding Class ExportProductDto in folder Exports

---
+++ SoftUni Entrity Framework Core/6. JSON Processing/ProductShop/DTOs/Export/ExportProductDto.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductShop.DTOs.Export
+{
+    public class ExportProductDto
+    {
+        [JsonProperty("firstName")]
+        public string? FirstName { get; set; }
+
+        [JsonProperty("lastName")]
+        public string LastName { get; set; } = null!;
+
+        [JsonProperty("age")]
+        public int? Age { get; set; }
+    }
+}
