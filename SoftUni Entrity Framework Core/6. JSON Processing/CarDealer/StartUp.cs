ef3b2de31f4b1d4da58be25d0f88fccb2db42349
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-11-19 12:45:37 +0200

    preparing to work with CarDealer context

--- SoftUni Entrity Framework Core/6. JSON Processing/CarDealer/StartUp.cs
+++ SoftUni Entrity Framework Core/6. JSON Processing/CarDealer/StartUp.cs
@@ -1,10 +1,22 @@
-﻿namespace CarDealer
+﻿using AutoMapper;
+
+namespace CarDealer
 {
     public class StartUp
     {
         public static void Main()
         {
 
+        }
+
+
+
+        public static IMapper Mapper()
+        {
+            IMapper mapper = new Mapper(new MapperConfiguration(cfg =>
+            cfg.AddProfile<CarDealerProfile>()));
+
+            return mapper;
         }
     }
 }
