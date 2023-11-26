6531744a1018d015d950e56c1e568527e4955576
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-11-26 13:32:57 +0200

    Adding Class ImportJSONProductDto and implementing it

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Prep Exam .NET 6.0 - 11 April 2023/Invoices/DataProcessor/ImportDto/ImportJSONProductDto.cs
@@ -0,0 +1,30 @@
+﻿using Invoices.Data.Models.Enums;
+using Invoices.Data.Models;
+using System.ComponentModel.DataAnnotations;
+using static Invoices.Utilities.GlobalConstants;
+using Newtonsoft.Json;
+
+namespace Invoices.DataProcessor.ImportDto
+{
+    public class ImportJSONProductDto
+    {
+        [Required]
+        [MinLength(ProductNameLengthConstraintMin)]
+        [MaxLength(ProductNameLengthConstraintMax)]
+        [JsonProperty("Name")]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [Range((double)ProductPriceRangeMin, (double)ProductPriceRangeMax)]
+        [JsonProperty("Price")]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Range(0, 4)]
+        [JsonProperty("CategoryType")]
+        public CategoryType CategoryType { get; set; }
+
+        [JsonProperty("Clients")]
+        public int[] Clients { get; set; } = null!;
+    }
+}
