9aa14f48cdb0dd202105a523899e42790414c091
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-11-26 20:55:02 +0200

    Adding Class Creator and implementing it

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Exam - 01 April 2023/Boardgames/Data/Models/Creator.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Boardgames.Utilities.GlobalConstraints
+
+namespace Boardgames.Data.Models
+{
+    public class Creator
+    {
+        public Creator() 
+        {
+            BoardGames = new HashSet<BoardGame>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(CreatorNameLengthMax)]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        [StringLength(CreatorNameLengthMax)]
+        public string LastName { get; set; } = null!;
+
+        public ICollection<BoardGame> BoardGames { get; set; }
+    }
+}
