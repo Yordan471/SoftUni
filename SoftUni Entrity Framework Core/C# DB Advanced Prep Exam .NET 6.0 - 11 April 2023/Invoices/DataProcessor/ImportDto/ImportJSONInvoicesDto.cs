d283229d96f1a784ec2dc254f48477df13f5541e
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-11-26 13:06:50 +0200

    adding Class ImportJSONInvoiceto and implementing it

--- SoftUni Entrity Framework Core/C# DB Advanced Prep Exam .NET 6.0 - 11 April 2023/Invoices/DataProcessor/ImportDto/ImportJSONInvoicesDto.cs
+++ SoftUni Entrity Framework Core/C# DB Advanced Prep Exam .NET 6.0 - 11 April 2023/Invoices/DataProcessor/ImportDto/ImportJSONInvoicesDto.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Invoices.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using static Invoices.Utilities.GlobalConstants;
 
 namespace Invoices.DataProcessor.ImportDto
 {
-    internal class ImportJSONInvoicesDto
+    [XmlType("Invoice")]
+    public class ImportJSONInvoicesDto
     {
+        [Required]
+        [Range(InvoicesNumberRangeMin, InvoicesNumberRangeMax)]
+        [XmlElement("Number")]
+        public int Number { get; set; }
+
+        [Required]
+        [XmlElement("IssueDate")]
+        public DateTime IssueDate { get; set; }
+
+        [Required]
+        [XmlElement("DueDate")]
+        public DateTime DueDate { get; set; }
+
+        [Required]
+        [XmlElement("Amount")]
+        public decimal Amount { get; set; }
+
+        [Required]
+        [XmlElement("CurrencyType")]
+        public CurrencyType CurrencyType { get; set; }
+
+        [Required]
+        [XmlElement("ClientId")]
+        public int ClientId { get; set; }
     }
 }
