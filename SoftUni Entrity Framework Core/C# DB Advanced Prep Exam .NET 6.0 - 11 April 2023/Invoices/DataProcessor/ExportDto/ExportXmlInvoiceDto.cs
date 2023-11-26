01320cf268c270f96ec8ffbe52b842d020c85ef5
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-11-26 17:21:07 +0200

    Adding Classes ExportXmlClientDto, ExportXmlInvoiceDto

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Prep Exam .NET 6.0 - 11 April 2023/Invoices/DataProcessor/ExportDto/ExportXmlInvoiceDto.cs
@@ -0,0 +1,27 @@
+﻿using Invoices.Data.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Invoices.DataProcessor.ExportDto
+{
+    [XmlType("Invoice")]
+    public class ExportXmlInvoiceDto
+    {
+        [XmlElement("InvoiceNumber")]
+        public int Number { get; set; }
+
+        [XmlElement("InvoiceAmount")]
+        public decimal Amount { get; set; }
+
+        [XmlElement("DueDate")]
+        public DateTime DueDate { get; set; }
+
+        [XmlElement("Currency")]
+        public CurrencyType CurrencyType { get; set; }
+    }
+}
