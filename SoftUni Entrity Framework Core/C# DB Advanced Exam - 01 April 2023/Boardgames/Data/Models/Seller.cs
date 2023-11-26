43aeed3bc1e9652a3cfab7a0675817054a4eae02
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-11-26 21:03:34 +0200

    Adding Class Seller and Implementing it

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Exam - 01 April 2023/Boardgames/Data/Models/Seller.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Boardgames.Utilities.GlobalConstraints;
+
+namespace Boardgames.Data.Models
+{
+    public class Seller
+    {
+        public Seller() 
+        {
+            BoardgamesSellers = new HashSet<BoardGameSeller>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(SellarNameLengthMax)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [StringLength(SellarAddressLengthMax)]
+        public string Address { get; set; } = null!;
+
+        [Required]
+        public string Country { get; set; } = null!;
+
+        [Required]
+        public string Website { get; set; } = null!;
+
+        public ICollection<BoardgameSeller> BoardgamesSellers  { get; set; }
+    }
+}
