8172d48d69586652fd4ca9c3e0e615d00a036521
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-11-27 20:38:02 +0200

    Adding Class ImportJsonSellerDto

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Exam - 01 April 2023/Boardgames/DataProcessor/ImportDto/ImportJsonSellerDto.cs
@@ -0,0 +1,27 @@
+﻿using Boardgames.Data.Models;
+using System.ComponentModel.DataAnnotations;
+using static Boardgames.Utilities.GlobalConstraints;
+
+namespace Boardgames.DataProcessor.ImportDto
+{
+    public class ImportJsonSellerDto
+    {
+        [Required]
+        [MinLength(SellarNameLengthMin)]
+        [MaxLength(SellarNameLengthMax)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [MinLength(SellarAddressLengthMin)]
+        [MaxLength(SellarAddressLengthMax)]
+        public string Address { get; set; } = null!;
+
+        [Required]
+        public string Country { get; set; } = null!;
+
+        [Required]
+        public string Website { get; set; } = null!;
+
+        public int[] Boardgames { get; set; } = null!;
+    }
+}
