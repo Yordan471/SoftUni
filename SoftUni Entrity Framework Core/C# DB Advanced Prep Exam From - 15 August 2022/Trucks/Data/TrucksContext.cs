b0581f1a990d579b14b6b8635e33bcb76b38f42e
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-11-28 21:43:27 +0200

    Adjusting connection string; Adding Lazy Loading; Adding classes GlobalConstants and XmlHelper (Implementing XmlHelper)

--- SoftUni Entrity Framework Core/C# DB Advanced Prep Exam From - 15 August 2022/Trucks/Data/TrucksContext.cs
+++ SoftUni Entrity Framework Core/C# DB Advanced Prep Exam From - 15 August 2022/Trucks/Data/TrucksContext.cs
@@ -19,7 +19,8 @@
             if (!optionsBuilder.IsConfigured)
             {
                 optionsBuilder
-                    .UseSqlServer(Configuration.ConnectionString);
+                    .UseSqlServer(Configuration.ConnectionString)
+                    .UseLazyLoadingProxies();
             }
         }
 
