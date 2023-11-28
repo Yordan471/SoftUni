d38948bf91d910db13485ca07bca8d04ebc0be99
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-11-28 20:48:52 +0200

    adding implementation for method ExportCreatorsWithTheirBoardgames

--- SoftUni Entrity Framework Core/C# DB Advanced Exam - 01 April 2023/Boardgames/DataProcessor/Serializer.cs
+++ SoftUni Entrity Framework Core/C# DB Advanced Exam - 01 April 2023/Boardgames/DataProcessor/Serializer.cs
@@ -1,6 +1,7 @@
 ﻿namespace Boardgames.DataProcessor
 {
     using Boardgames.Data;
+    using Newtonsoft.Json;
 
     public class Serializer
     {
@@ -11,7 +12,34 @@
 
         public static string ExportSellersWithMostBoardgames(BoardgamesContext context, int year, double rating)
         {
-            throw new NotImplementedException();
+            var sellers = context.Sellers
+                .Where(s => s.BoardgamesSellers.Any(bs => bs.Boardgame.YearPublished >= year &&
+                bs.Boardgame.Rating <= rating))
+                .ToArray()
+                .Select(s => new 
+                {
+                    s.Name,
+                    s.Website,
+                    Boardgames = s.BoardgamesSellers
+                                 .Where(bs => bs.Boardgame.YearPublished >= year &&
+                                 bs.Boardgame.Rating <= rating)
+                                 .Select(bs => new 
+                                 {
+                                     bs.Boardgame.Name,
+                                     bs.Boardgame.Rating,
+                                     bs.Boardgame.Mechanics,
+                                     bs.Boardgame.CategoryType                  
+                                 })
+                                 .OrderByDescending(bs => bs.Rating)
+                                 .ThenBy(bs => bs.Name)
+                                 .ToArray()
+                })
+                .OrderByDescending(s => s.Boardgames.Count())
+                .ThenBy(s => s.Name)
+                .Take(5)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(sellers, Formatting.Indented);
         }
     }
 }
