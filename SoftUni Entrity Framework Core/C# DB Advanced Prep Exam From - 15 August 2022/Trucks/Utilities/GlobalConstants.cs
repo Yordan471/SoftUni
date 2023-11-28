988e03915e083f84fc785b9a1ea71c86397d2e1c
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-11-28 22:09:15 +0200

    implementing class GlobalConstants

--- SoftUni Entrity Framework Core/C# DB Advanced Prep Exam From - 15 August 2022/Trucks/Utilities/GlobalConstants.cs
+++ SoftUni Entrity Framework Core/C# DB Advanced Prep Exam From - 15 August 2022/Trucks/Utilities/GlobalConstants.cs
@@ -1,13 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Trucks.Utilities
 {
     public class GlobalConstants
     {
+        // Truck
+        public const int TruckRegistrationNumberLength = 8;
+        public const string TruckRegistrationNumberRegexValidation = @"[A-Z]{2}\d{4}[A-Z]{2}";
+        public const int TruckVitNumberLength = 17;
+        public const int TruckTankCapacityMin = 950;
+        public const int TruckTankCapacityMax = 1420;
+        public const int TruckCargoCapacityMin = 5000;
+        public const int TruckCargoCapacityMax = 29000;
 
+        // Client
+        public const int ClientNameLengthMin = 3;
+        public const int ClientNameLengthMax = 40;
+        public const int ClientNationalityLengthMin = 2;
+        public const int ClientNationalityLengthMax = 40;
+
+        // Despatcher
+        public const int DespatcherNameLengthMin = 2;
+        public const int DespatcherNameLengthMax = 40;
     }
 }
