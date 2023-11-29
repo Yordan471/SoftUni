41909e8e3ece2ea24eaba9cc0ef5e81144143c5a
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-11-29 19:03:15 +0200

    adding dbsets and adjusting mapping table ClientTruck witch primary key

--- SoftUni Entrity Framework Core/C# DB Advanced Prep Exam From - 15 August 2022/Trucks/Data/TrucksContext.cs
+++ SoftUni Entrity Framework Core/C# DB Advanced Prep Exam From - 15 August 2022/Trucks/Data/TrucksContext.cs
@@ -1,6 +1,7 @@
 ﻿namespace Trucks.Data
 {
     using Microsoft.EntityFrameworkCore;
+    using Trucks.Data.Models;
 
     public class TrucksContext : DbContext
     {
@@ -13,6 +14,13 @@
         { 
         }
 
+        public DbSet<Client> Clients { get; set; }
+
+        public DbSet<Despatcher> Despatchers { get; set; }
+
+        public DbSet<Truck> Trucks { get; set; }
+
+        public DbSet<ClientTruck> ClientsTrucks { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -26,6 +34,8 @@
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<ClientTruck>()
+                .HasKey(ct => new { ct.ClientId, ct.TruckId });
         }
     }
 }
