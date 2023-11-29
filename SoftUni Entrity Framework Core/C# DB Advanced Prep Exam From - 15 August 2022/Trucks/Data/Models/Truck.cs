45d34f1b72924bae1a8c6514cab1fd67cfb23fb4
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-11-29 18:55:09 +0200

    Adding Class Truck and implementing it; Adding enums - CategoryType and MakeType

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Prep Exam From - 15 August 2022/Trucks/Data/Models/Truck.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Trucks.Data.Models.Enums;
+using static Trucks.Utilities.GlobalConstants;
+
+namespace Trucks.Data.Models
+{
+    public class Truck
+    {
+        public Truck() 
+        {
+            ClientsTrucks = new HashSet<ClientTruck>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [RegularExpression(TruckRegistrationNumberRegexValidation)]
+        public string? RegistrationNumber { get; set; }
+
+        [Required]
+        [StringLength(TruckVitNumberLength)]
+        public string VinNumber { get; set; } = null!;
+
+        [Range(TruckTankCapacityMin, TruckTankCapacityMax)]
+        public int TankCapacity { get; set; }
+
+        [Range(TruckCargoCapacityMin, TruckCargoCapacityMax)]
+        public int CargoCapacity { get; set; }
+
+        [Required]
+        [Range(0, 3)]
+        public CategoryType CategoryType { get; set; }
+
+        [Required]
+        [Range(0, 4)]
+        public MakeType MakeType { get; set; }
+
+        [Required]
+        public int DespatcherId { get; set; }
+
+        [ForeignKey(nameof(DespatcherId))]
+        public Despatcher Despatcher { get; set; } = null!;
+
+        public ICollection<ClientTruck> ClientsTrucks { get; set; }
+    }
+}
