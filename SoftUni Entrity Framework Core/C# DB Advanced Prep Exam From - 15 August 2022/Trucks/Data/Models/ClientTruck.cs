b9e4fc0c5bab9e6dab7688bb24e9457180c99fcf
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-11-29 18:57:41 +0200

    Adding Class ClientTruck and implementing it

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Prep Exam From - 15 August 2022/Trucks/Data/Models/ClientTruck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trucks.Data.Models
+{
+    public class ClientTruck
+    {
+        [Required]
+        public int ClientId { get; set; }
+
+        [ForeignKey(nameof(ClientId))]
+        public Client Client { get; set; } = null!;
+
+        [Required]
+        public int TruckId { get; set; }
+
+        [ForeignKey(nameof(TruckId))]
+        public Truck Truck { get; set; } = null!;
+    }
+}
