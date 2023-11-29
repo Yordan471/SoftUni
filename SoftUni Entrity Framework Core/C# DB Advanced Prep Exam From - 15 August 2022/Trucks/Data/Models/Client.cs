d8b420fc0ee55f018e13a613645749fffb22cebd
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-11-29 18:43:05 +0200

    adding Class Despatcher and implementing it

--- SoftUni Entrity Framework Core/C# DB Advanced Prep Exam From - 15 August 2022/Trucks/Data/Models/Client.cs
+++ SoftUni Entrity Framework Core/C# DB Advanced Prep Exam From - 15 August 2022/Trucks/Data/Models/Client.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using static Trucks.Utilities.GlobalConstants;
 
 namespace Trucks.Data.Models
 {
-    internal class Client
+    public class Client
     {
+        public Client() 
+        {
+            ClientsTrucks = new HashSet<ClientTruck>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(ClientNameLengthMax)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [StringLength(ClientNationalityLengthMax)]
+        public string Nationality { get; set; } = null!;
+
+        [Required]
+        public string Type { get; set; } = null!;
+
+        public ICollection<ClientTruck> ClientsTrucks { get; set; }
     }
 }
---
+++ SoftUni Entrity Framework Core/C# DB Advanced Prep Exam From - 15 August 2022/Trucks/Data/Models/Despatcher.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Trucks.Utilities.GlobalConstants;
+
+namespace Trucks.Data.Models
+{
+    public class Despatcher
+    {
+        public Despatcher() 
+        {
+            Trucks = new HashSet<Truck>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(DespatcherNameLengthMax)]
+        public string Name { get; set; } = null!;
+
+        public string? Position { get; set; }
+
+        public ICollection<Truck> Trucks { get; set; }
+    }
+}
