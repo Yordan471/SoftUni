d0f5932186cbf28bb7851312ba507eef62486e7e
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-11-30 19:56:36 +0200

    Adding Class Coach and implementing it

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Prep Exam - 06 August 2022/Footballers/Data/Models/Coach.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Footballers.Utilities.GlobalConstants;
+
+namespace Footballers.Data.Models
+{
+    public class Coach
+    {
+        public Coach() 
+        {
+            FootBallers = new HashSet<Footballer>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(CoachNameLenthMax)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public string Nationality { get; set; } = null!;
+
+        public virtual ICollection<Footballer> FootBallers { get; set; }
+    }
+}
