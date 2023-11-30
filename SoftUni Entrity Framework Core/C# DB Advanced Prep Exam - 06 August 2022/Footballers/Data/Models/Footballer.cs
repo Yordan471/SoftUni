1859bf7abdb79fec8ee2ae73fcdd6cc31774c68f
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-11-30 20:03:57 +0200

    Adding Class Footballer; Adding 2 enums - PositionType, BestSkillType and implementing them

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Prep Exam - 06 August 2022/Footballers/Data/Models/Footballer.cs
@@ -0,0 +1,42 @@
+﻿using Footballers.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static Footballers.Utilities.GlobalConstants;
+
+namespace Footballers.Data.Models
+{
+    public class Footballer
+    {
+        public Footballer() 
+        {
+            TeamsFootballers = new HashSet<TeamFootballer>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(FootballerNameLengthMax)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public DateTime ContractStartDate { get; set; }
+
+        [Required]
+        public DateTime ContractEndDate { get; set; }
+
+        [Required]
+        public PositionType PositionType { get; set; }
+
+        [Required]
+        public BestSkillType BestSkillType { get; set; }
+
+        [Required]
+        public int CoachId { get; set; }
+
+        [ForeignKey(nameof(CoachId))]
+        public virtual Coach Coach { get; set; } = null!;
+
+        public virtual ICollection<TeamFootballer> TeamsFootballers { get; set; }
+    }
+}
