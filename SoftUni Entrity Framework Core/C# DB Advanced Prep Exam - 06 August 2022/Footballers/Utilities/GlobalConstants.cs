0a2733d0ee6b47fecfb7dfa7c1b473a42253f649
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-11-30 19:49:45 +0200

    adding folder Utilities + Classes GlobalConstants, XmlHelper and implementing them

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Prep Exam - 06 August 2022/Footballers/Utilities/GlobalConstants.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Footballers.Utilities
+{
+    public class GlobalConstants
+    {
+        // Footballer
+        public const int FootballerNameLengthMin = 2;
+        public const int FootballerNameLengthMax = 40;
+
+        // Team
+        public const int TeamNameLengthMin = 3;
+        public const int TeamNameLengthMax = 40;
+        public const string TeamNameRegularExpressionValidation = @"[A-Za-z0-9 ._-]";
+        public const int TeamNationalityLengthMin = 2;
+        public const int TeamNationalityLengthMax = 40;
+
+        // Coach
+        public const int CoachNameLenthMin = 2;
+        public const int CoachNameLenthMax = 40;
+    }
+}
