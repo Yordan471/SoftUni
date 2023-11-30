858005654a79aa86c50ba810c5f559f7ab3331af
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-11-30 20:50:52 +0200

    Adding Classes - ImportXmlFootballerDto, ImportXmlCoachDto and implementing them

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Prep Exam - 06 August 2022/Footballers/DataProcessor/ImportDto/ImportXmlCoachDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using static Footballers.Utilities.GlobalConstants;
+
+namespace Footballers.DataProcessor.ImportDto
+{
+    [XmlType("Coach")]
+    public class ImportXmlCoachDto
+    {
+        [Required]
+        [MinLength(CoachNameLenthMin)]
+        [MaxLength(CoachNameLenthMax)]
+        [XmlElement("Name")]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [XmlElement("Nationality")]
+        public string Nationality { get; set; } = null!;
+
+        [XmlArray("Footballers")]
+        public ImportXmlFootballerDto[] Footballers { get; set; }
+    }
+}
---
+++ SoftUni Entrity Framework Core/C# DB Advanced Prep Exam - 06 August 2022/Footballers/DataProcessor/ImportDto/ImportXmlFootballerDto.cs
@@ -0,0 +1,33 @@
+﻿using Footballers.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using static Footballers.Utilities.GlobalConstants;
+
+namespace Footballers.DataProcessor.ImportDto
+{
+    [XmlType("Footballer")]
+    public class ImportXmlFootballerDto
+    {
+        [Required]
+        [MinLength(FootballerNameLengthMin)]
+        [MaxLength(FootballerNameLengthMax)]
+        [XmlElement("Name")]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [XmlElement("ContractStartDate")]
+        public DateTime ContractStartDate { get; set; }
+
+        [Required]
+        [XmlElement("ContractEndDate")]
+        public DateTime ContractEndDate { get; set; }
+
+        [Required]
+        [XmlElement("BestSkillType")]
+        public BestSkillType BestSkillType { get; set; }
+
+        [Required]
+        [XmlElement("PositionType")]
+        public PositionType PositionType { get; set; }     
+    }
+}
