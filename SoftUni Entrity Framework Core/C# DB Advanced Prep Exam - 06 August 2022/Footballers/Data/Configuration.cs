294b7f5cc50d1e3e77c24e53ce7e6c1d739f9466
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-11-30 19:40:35 +0200

    adding Resources for new Prep Exam;  adding lazy loading; Fixing Connection string; Adding needed packages

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Prep Exam - 06 August 2022/Footballers/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Footballers.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server=DESKTOP-U0UT8KF\SQLEXPRES\S;Database=FootballersExam;Trusted_Connection=True";
+    }
+}
---
+++ SoftUni Entrity Framework Core/C# DB Advanced Prep Exam - 06 August 2022/Footballers/Data/FootballersContext.cs
@@ -0,0 +1,27 @@
+﻿namespace Footballers.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class FootballersContext : DbContext
+    {
+        public FootballersContext() { }
+
+        public FootballersContext(DbContextOptions options)
+            : base(options) { }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString)
+                    .UseLazyLoadingProxies();
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
