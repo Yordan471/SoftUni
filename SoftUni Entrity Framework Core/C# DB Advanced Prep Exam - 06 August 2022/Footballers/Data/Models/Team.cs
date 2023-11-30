3a26862e22284a2229e5a140be3cb0815e535b95
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-11-30 19:53:28 +0200

    adding class Team and implementing it

--- SoftUni Entrity Framework Core/C# DB Advanced Prep Exam - 06 August 2022/Footballers/Data/Models/Team.cs
+++ SoftUni Entrity Framework Core/C# DB Advanced Prep Exam - 06 August 2022/Footballers/Data/Models/Team.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using static Footballers.Utilities.GlobalConstants;
 
 namespace Footballers.Data.Models
 {
     public class Team
     {
+        public Team() 
+        {
+            TeamsFootballers = new HashSet<TeamFootballer>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(TeamNameLengthMax)]
+        [RegularExpression(TeamNameRegularExpressionValidation)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [StringLength(TeamNationalityLengthMax)]
+        public string Nationality { get; set; } = null!;
+
+        [Required]
+        public int Trophies { get; set; }
+
+        public virtual TeamFootballer TeamsFootballers { get; set; }
     }
 }
