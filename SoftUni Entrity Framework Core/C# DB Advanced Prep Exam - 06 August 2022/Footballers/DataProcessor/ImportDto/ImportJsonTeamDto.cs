6a6501e60d7b18002c00b20ce4468e2b58062707
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-12-01 20:09:52 +0200

    Adding Class ImportJsonTeamDto

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Prep Exam - 06 August 2022/Footballers/DataProcessor/ImportDto/ImportJsonTeamDto.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using static Footballers.Utilities.GlobalConstants;
+
+namespace Footballers.DataProcessor.ImportDto
+{
+    public class ImportJsonTeamDto
+    {
+        [Required]
+        [MaxLength(TeamNameLengthMax)]
+        [MinLength(TeamNameLengthMin)]
+        [RegularExpression(TeamNameRegularExpressionValidation)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [MaxLength(TeamNationalityLengthMax)]
+        [MinLength(TeamNationalityLengthMin)]
+        public string Nationality { get; set; } = null!;
+
+        [Required]
+        public int Trophies { get; set; }
+
+        [JsonProperty("Footballers")]
+        public int[] Footballers { get; set; } = null!;
+    }
+}
