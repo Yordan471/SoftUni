5f920b21c11d8c10a2816af732f6fb767a1bac71
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-12-06 11:07:20 +0200

    Adding Lazy loading; Adding GlobalConstants ; Adjusting connection string; Adding Class XmlHelper with methods

--- SoftUni Entrity Framework Core/C# DB Advanced Retake Exam - 16 Dec 2021/Artillery/Data/ArtilleryContext.cs
+++ SoftUni Entrity Framework Core/C# DB Advanced Retake Exam - 16 Dec 2021/Artillery/Data/ArtilleryContext.cs
@@ -21,7 +21,8 @@
             if (!optionsBuilder.IsConfigured)
             {
                 optionsBuilder
-                    .UseSqlServer(Configuration.ConnectionString);
+                    .UseSqlServer(Configuration.ConnectionString)
+                    .UseLazyLoadingProxies();
             }
         }
 
---
+++ SoftUni Entrity Framework Core/C# DB Advanced Retake Exam - 16 Dec 2021/Artillery/Utilities/GlobalConstants.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artillery.Utilities
+{
+    public class GlobalConstants
+    {
+        // Country
+        public const int CountryNameLengthMin = 5;
+        public const int CountryNameLengthMax = 60;
+        public const int CountryArmySizeMin = 50000;
+        public const int CountryArmySizeMax = 10000000;
+
+        // Manufacturer
+        public const int ManufacturerNameLengthMin = 5;
+        public const int ManufacturerNameLengthMax = 40;
+        public const int ManufacturerFoundedLengthMin = 10;
+        public const int ManufacturerFoundedLengthMax = 100;
+
+        // Shell
+        public const int ShellWeightMin = 100;
+        public const int ShellWeightMax = 1350000;
+        public const double ShellGunWeightMin = 2.00;
+        public const double ShellGunWeightMax = 35.00;
+        public const int ShellRangeMin = 1;
+        public const int ShellRangeMax = 100000;
+    }
+}
