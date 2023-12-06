a0229ee6baafdf77079ee703eb0e0105a96b2aa6
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-12-06 10:20:59 +0200

    adding resources for prep Exam EF - Core

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Retake Exam - 16 Dec 2021/Artillery/Data/ArtilleryContext.cs
@@ -0,0 +1,32 @@
+﻿namespace Artillery.Data
+{
+    using Artillery.Data.Models;
+    using Microsoft.EntityFrameworkCore;
+    using System.Collections.Generic;
+    using System.Reflection.Emit;
+
+    public class ArtilleryContext : DbContext
+    {
+        public ArtilleryContext() 
+        { 
+        }
+
+        public ArtilleryContext(DbContextOptions options)
+            : base(options) 
+        { 
+        }
+
+         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
