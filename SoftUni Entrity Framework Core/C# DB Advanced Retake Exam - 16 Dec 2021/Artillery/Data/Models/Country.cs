e5bb6330e339895a4fb2986b6f500ee3d4746025
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-12-06 11:10:56 +0200

    Adding Class Country and implementing it

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Retake Exam - 16 Dec 2021/Artillery/Data/Models/Country.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Artillery.Utilities.GlobalConstants;
+
+namespace Artillery.Data.Models
+{
+    public class Country
+    {
+        public Country() 
+        {
+            CountriesGuns = new HashSet<CountryGun>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(CountryNameLengthMax)]
+        public string CountryName { get; set; } = null!;
+
+        [Required]
+        [Range(CountryArmySizeMin, CountryArmySizeMax)]
+        public int ArmySize { get; set; }
+
+        public virtual CountryGun CountriesGuns { get; set; }
+    }
+}
