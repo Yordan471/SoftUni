e2b750f82389b6d242cc2a6a898632791111984e
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-12-06 11:14:36 +0200

    adding Class Gun and implementing it

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Retake Exam - 16 Dec 2021/Artillery/Data/Models/Manufacturer.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Artillery.Utilities.GlobalConstants;
+
+namespace Artillery.Data.Models
+{
+    public class Manufacturer
+    {
+        public Manufacturer() 
+        {
+            Guns = new HashSet<Gun>();
+        }   
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(ManufacturerNameLengthMax)]
+        public string ManufacturerName { get; set; } = null!;
+
+        [Required]
+        [StringLength(ManufacturerFoundedLengthMax)]
+        public string Founded { get; set; } = null!;
+
+        public ICollection<Gun> Guns { get; set; }
+    }
+}
