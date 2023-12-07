c6a69bb43e4b3e2f7264cd3abe7e50283452018b
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-12-07 18:52:24 +0200

    Adding resources for Prep Exam EF Core

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Exam - 04 Dec 2021/Theatre/Data/TheatreContext.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Theatre.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class TheatreContext : DbContext
+    {
+        public TheatreContext() 
+        {
+        }
+
+        public TheatreContext(DbContextOptions options)
+        : base(options) 
+        { 
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+    }
+}
