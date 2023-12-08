66397a1b149381ed0fe16e1df609116937658145
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-12-08 16:54:44 +0200

    implementing method ExportTheatres

--- SoftUni Entrity Framework Core/C# DB Advanced Exam - 04 Dec 2021/Theatre/DataProcessor/Serializer.cs
+++ SoftUni Entrity Framework Core/C# DB Advanced Exam - 04 Dec 2021/Theatre/DataProcessor/Serializer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Theatre.DataProcessor
 {
-
+    using Newtonsoft.Json;
     using System;
     using Theatre.Data;
 
@@ -8,7 +8,33 @@
     {
         public static string ExportTheatres(TheatreContext context, int numbersOfHalls)
         {
-            throw new NotImplementedException();
+            int[] rowNumbersArr = new int[] { 1, 2, 3, 4, 5 };
+
+            var theaters = context.Theatres
+                .Where(t => t.NumberOfHalls >= numbersOfHalls && t.Tickets.Count >= 20)
+                .ToArray()
+                .Select(t => new
+                {
+                    Name = t.Name,
+                    Halls = t.NumberOfHalls,
+                    TotalIncome = t.Tickets
+                    .Where(t => rowNumbersArr.Contains(t.RowNumber)).Select(t => t.Price)
+                    .Sum(),
+                    Tickets = t.Tickets
+                    .Where(t => rowNumbersArr.Contains(t.RowNumber))
+                    .Select(t => new
+                    {
+                        Price = t.Price,
+                        RowNumber = t.RowNumber
+                    })
+                    .OrderByDescending(t => t.Price)
+                    .ToArray()
+                })
+                .OrderByDescending(t => t.Halls)
+                .ThenBy(t => t.Name)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(theaters, Formatting.Indented);
         }
 
         public static string ExportPlays(TheatreContext context, double raiting)
