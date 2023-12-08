966ec03727040a9440fa5587bb97c7a9113f5ef7
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-12-08 12:11:46 +0200

    Adding and implementing Class Cast

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Exam - 04 Dec 2021/Theatre/Data/Models/Cast.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static Theatre.Utilities.GlobalConstants;
+
+namespace Theatre.Data.Models
+{
+    public class Cast
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(CastFullNameLengthMax)]
+        public string FullName { get; set; } = null!;
+
+        [Required]
+        public bool IsMainCharacter { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; } = null!;
+
+        [Required]
+        public int PlayerId { get; set; }
+
+        [ForeignKey(nameof(PlayerId))]
+        public virtual Player Player { get; set; }
+    }
+}
