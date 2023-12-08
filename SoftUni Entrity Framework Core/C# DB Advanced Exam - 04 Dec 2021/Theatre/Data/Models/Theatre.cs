1cf2626c6c2e570754e5fed9d868c85bef886fa9
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-12-08 12:01:36 +0200

    Adding and implementing Class Theatre

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Exam - 04 Dec 2021/Theatre/Data/Models/Theatre.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Theatre.Utilities.GlobalConstants;
+
+namespace Theatre.Data.Models
+{
+    public class Theatre
+    {
+        public Theatre() 
+        {
+            Tickets = new HashSet<Ticket>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(TheatreNameLengthMax)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [Range(TheatreNumberOfHallsMin, TheatreNumberOfHallsMax)]
+        public sbyte NumberOfHalls { get; set; }
+
+        [Required]
+        [StringLength(TheatreDirectorLengthMax)]
+        public string Director { get; set; } = null!;
+
+        public virtual ICollection<Ticket> Tickets { get; set; }
+    }
+}
