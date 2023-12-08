d093e52829e892d814f3aceba7d6c83fb3ef944d
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-12-08 16:21:20 +0200

    adding classes ImportJsonTheatreDto, ImportJsonTicketDto

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Exam - 04 Dec 2021/Theatre/DataProcessor/ImportDto/ImportJsonTheatreDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Theatre.Utilities.GlobalConstants;
+
+namespace Theatre.DataProcessor.ImportDto
+{
+    public class ImportJsonTheatreDto
+    {
+        [Required]
+        [MaxLength(TheatreNameLengthMax)]
+        [MinLength(TheatreNameLengthMin)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [Range(TheatreNumberOfHallsMin, TheatreNumberOfHallsMax)]
+        public sbyte NumberOfHalls { get; set; }
+
+        [Required]
+        [MaxLength(TheatreDirectorLengthMax)]
+        [MinLength(TheatreDirectorLengthMin)]
+        public string Director { get; set; } = null!;
+
+        public ImportJsonTicketDto[] Tickets { get; set; }
+    }
+}
---
+++ SoftUni Entrity Framework Core/C# DB Advanced Exam - 04 Dec 2021/Theatre/DataProcessor/ImportDto/ImportJsonTicketDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Theatre.Utilities.GlobalConstants;
+
+namespace Theatre.DataProcessor.ImportDto
+{
+    public class ImportJsonTicketDto
+    {
+        [Required]
+        [Range((double)TicketPriceMin, (double)TicketPriceMax)]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Range(TicketRowNumberMin, TicketRowNumberMax)]
+        public sbyte RowNumber { get; set; }
+
+        [Required]
+        public int PlayId { get; set; }
+    }
+}
