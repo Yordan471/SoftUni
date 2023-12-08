05d5abb96ac2da5d3b21a5944775ea6abd23fd31
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-12-08 17:06:28 +0200

    Adding two Export Classes

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Exam - 04 Dec 2021/Theatre/DataProcessor/ExportDto/ExportXmlCastDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Theatre.DataProcessor.ExportDto
+{
+    [XmlType("Cast")]
+    public class ExportXmlCastDto
+    {
+        [XmlAttribute("FullName")]
+        public string FullName { get; set; }
+
+        [XmlAttribute("MainCharacter")]
+        public string MainCharacter { get; set; }
+    }
+}
---
+++ SoftUni Entrity Framework Core/C# DB Advanced Exam - 04 Dec 2021/Theatre/DataProcessor/ExportDto/ExportXmlPlayDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Theatre.DataProcessor.ExportDto
+{
+    [XmlType("Play")]
+    public class ExportXmlPlayDto
+    {
+        [XmlAttribute("Title")]
+        public string Title { get; set; }
+
+        [XmlAttribute("Duration")]
+        public string Duration { get; set; }
+
+        [XmlAttribute("Rating")]
+        public float Rating { get; set; }
+
+        [XmlAttribute("Genre")]
+        public string Genre { get; set; }
+
+        [XmlArray("Actors")]
+        public ExportXmlCastDto[] Actors { get; set; }
+    }
+}
