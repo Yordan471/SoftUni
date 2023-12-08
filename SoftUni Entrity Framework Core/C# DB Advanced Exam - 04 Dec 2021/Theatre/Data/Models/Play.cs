f3bc733a2ec9818e48cc2864a6f8f15be2465148
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-12-08 12:08:23 +0200

    Adding and implementing class Play and enum Genre

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Exam - 04 Dec 2021/Theatre/Data/Models/Play.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Theatre.Data.Models.Enums;
+using static Theatre.Utilities.GlobalConstants;
+
+namespace Theatre.Data.Models
+{
+    public class Play
+    {
+        public Play() 
+        {
+            Casts = new HashSet<Cast>();
+            Tickets = new HashSet<Ticket>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(PlayTitleLengthMax)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        public TimeSpan Duration { get; set; }
+
+        [Required]
+        [Range(PlayRatingMin, PlayRatingMax)]
+        public float Rating { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+        [Required]
+        [StringLength(PlayDescriptionLengthMax)]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        [StringLength(PlayScreenwriterLengthMax)]
+        public string Screenwriter { get; set; } = null!;
+
+        public virtual ICollection<Cast> Casts { get; set; }
+
+        public virtual ICollection<Ticket> Tickets { get; set; }
+    }
+}
