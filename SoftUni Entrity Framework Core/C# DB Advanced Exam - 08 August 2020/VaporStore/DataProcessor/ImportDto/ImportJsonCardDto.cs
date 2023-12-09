525db78182202d259c54cda5d715b8beff0323d9
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-12-09 18:18:54 +0200

    adding classes ImportJsonUserDto, ImportJsonCardDto

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Exam - 08 August 2020/VaporStore/DataProcessor/ImportDto/ImportJsonCardDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VaporStore.Data.Models.Enums;
+
+namespace VaporStore.DataProcessor.ImportDto
+{
+    public class ImportJsonCardDto
+    {
+        [Required]
+        [RegularExpression(@"^\d{4} \d{4} \d{4} \d{4}$")]
+        public string Number { get; set; } = null!;
+
+        [Required]
+        [RegularExpression(@"^\d{3}$")]
+        public string Cvc { get; set; } = null!;
+
+        [Required]
+        public string Type { get; set; } = null!;
+    }
+}
---
+++ SoftUni Entrity Framework Core/C# DB Advanced Exam - 08 August 2020/VaporStore/DataProcessor/ImportDto/ImportJsonUserDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VaporStore.Data.Models;
+
+namespace VaporStore.DataProcessor.ImportDto
+{
+    public class ImportJsonUserDto
+    {
+        [Required]
+        [RegularExpression(@"^[A-Z]{1}[a-z]+\s[A-Z]{1}[a-z]+$")]
+        public string FullName { get; set; } = null!;
+
+        [Required]
+        [MaxLength(20)]
+        [MinLength(3)]
+        public string Username { get; set; } = null!;
+
+        [Required]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [Range(3, 103)]
+        public int Age { get; set; }
+
+        public virtual ImportJsonCardDto[] Cards { get; set; }
+    }
+}
