57f2e1f5d50b12e7a6fffaae0d83641616066223
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-12-09 10:40:17 +0200

    Adding classes ImportXmlTaskDto, ImportXmlProjectDto

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Exam - 04 April 2021/TeisterMask/DataProcessor/ImportDto/ImportXmlProjectDto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using static TeisterMask.Utilities.GlobalConstants;
+
+namespace TeisterMask.DataProcessor.ImportDto
+{
+    [XmlType("Project")]
+    public class ImportXmlProjectDto
+    {
+        [Required]
+        [MaxLength(ProjectNameLengthMax)]
+        [MinLength(ProjectNameLengthMin)]
+        [XmlElement("Name")]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [XmlElement("OpenDate")]
+        public string OpenDate { get; set; }
+
+        [XmlElement("DueDate")]
+        public string? DueDate { get; set; }
+
+        [XmlArray("Tasks")]
+        public ImportXmlTaskDto[] Tasks { get; set; }
+    }
+}
---
+++ SoftUni Entrity Framework Core/C# DB Advanced Exam - 04 April 2021/TeisterMask/DataProcessor/ImportDto/ImportXmlTaskDto.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using TeisterMask.Data.Models.Enums;
+using static TeisterMask.Utilities.GlobalConstants;
+
+namespace TeisterMask.DataProcessor.ImportDto
+{
+    [XmlType("Task")]
+    public class ImportXmlTaskDto
+    {
+        [Required]
+        [MaxLength(TaskNameLengthMax)]
+        [MinLength(TaskNameLengthMin)]
+        [XmlElement("Name")]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [XmlElement("OpenDate")]
+        public string OpenDate { get; set; }
+
+        [Required]
+        [XmlElement("DueDate")]
+        public string DueDate { get; set; }
+
+        [Required]
+        [Range(0, 3)]
+        [XmlElement("ExecutionType")]
+        public int ExecutionType { get; set; }
+
+        [Required]
+        [Range(0, 4)]
+        public int LabelType { get; set; }
+    }
+}
