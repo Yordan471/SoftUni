91ba3cc1028b08c9cca70cca39ee231f5f6dea26
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-12-09 11:03:35 +0200

    implementing method ImportProjects

--- SoftUni Entrity Framework Core/C# DB Advanced Exam - 04 April 2021/TeisterMask/DataProcessor/Deserializer.cs
+++ SoftUni Entrity Framework Core/C# DB Advanced Exam - 04 April 2021/TeisterMask/DataProcessor/Deserializer.cs
@@ -6,6 +6,12 @@ namespace TeisterMask.DataProcessor
     using ValidationContext = System.ComponentModel.DataAnnotations.ValidationContext;
 
     using Data;
+    using TeisterMask.Utilities;
+    using TeisterMask.DataProcessor.ImportDto;
+    using TeisterMask.Data.Models;
+    using System.Text;
+    using System.Globalization;
+    using TeisterMask.Data.Models.Enums;
 
     public class Deserializer
     {
@@ -19,7 +25,107 @@ namespace TeisterMask.DataProcessor
 
         public static string ImportProjects(TeisterMaskContext context, string xmlString)
         {
-            throw new NotImplementedException();
+            XmlHelper xmlHelper = new XmlHelper();
+            string xmlRootName = "Projects";
+
+            ImportXmlProjectDto[] projectDtos = xmlHelper.Deserialize<ImportXmlProjectDto[]>(xmlString, xmlRootName);
+
+            ICollection<Project> validProjects = new HashSet<Project>();
+            StringBuilder sb = new();
+
+            foreach (var projectDto in projectDtos)
+            {
+                if (!IsValid(projectDto))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                DateTime parseOpenDate;
+                bool isValidOpenDate = DateTime.TryParseExact(
+                    projectDto.OpenDate, 
+                    "dd/MM/yyyy", 
+                    CultureInfo.InvariantCulture,
+                    DateTimeStyles.None, 
+                    out parseOpenDate);
+
+                if (!isValidOpenDate)
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                DateTime parseDueDate;
+                bool isValidDueDate = DateTime.TryParseExact(
+                    projectDto.OpenDate,
+                    "dd/MM/yyyy",
+                    CultureInfo.InvariantCulture,
+                    DateTimeStyles.None,
+                    out parseDueDate);
+
+                Project validProject = new()
+                {
+                    Name = projectDto.Name,
+                    OpenDate = parseOpenDate,
+                    DueDate = parseDueDate
+                };
+
+                foreach (var taskDto in projectDto.Tasks)
+                {
+                    if (!IsValid(taskDto))
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    DateTime parseTaskOpenDate;
+                    bool isValidTaskOpenDate = DateTime.TryParseExact(
+                        projectDto.OpenDate,
+                        "dd/MM/yyyy",
+                        CultureInfo.InvariantCulture,
+                        DateTimeStyles.None,
+                        out parseTaskOpenDate);
+
+                    DateTime parseTaskDueDate;
+                    bool isValidTaskDueDate = DateTime.TryParseExact(
+                        projectDto.OpenDate,
+                        "dd/MM/yyyy",
+                        CultureInfo.InvariantCulture,
+                        DateTimeStyles.None,
+                        out parseTaskDueDate);
+
+                    if (!isValidTaskOpenDate || !isValidTaskDueDate)
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    if (parseTaskOpenDate < parseOpenDate || parseTaskDueDate > parseDueDate)
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    Task validTask = new()
+                    {
+                        Name = taskDto.Name,
+                        OpenDate = parseTaskOpenDate,
+                        DueDate = parseTaskDueDate,
+                        ExecutionType = (ExecutionType)taskDto.ExecutionType,
+                        LabelType = (LabelType)taskDto.LabelType
+                    };
+
+                    validProject.Tasks.Add(validTask);
+                }
+
+                validProjects.Add(validProject);
+                sb.AppendLine(string.Format(SuccessfullyImportedProject, validProject.Name, validProject.Tasks.Count));
+            }
+
+            context.Projects.AddRange(validProjects);
+            context.SaveChanges();
+
+            return sb.ToString().TrimEnd();
         }
 
         public static string ImportEmployees(TeisterMaskContext context, string jsonString)
