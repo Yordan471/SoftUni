2fc41b096d23245ce26a3d21664a4c2b8998e809
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-12-09 18:45:50 +0200

    adding class ImportXmlPurchaseDto

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Exam - 08 August 2020/VaporStore/DataProcessor/ImportDto/ImportXmlPurchaseDto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using VaporStore.Data.Models.Enums;
+
+namespace VaporStore.DataProcessor.ImportDto
+{
+    [XmlType("Purchase")]
+    public class ImportXmlPurchaseDto
+    {
+        [Required]
+        [XmlAttribute("title")]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [XmlElement("Type")]
+        public string Type { get; set; } = null!;
+
+        [Required]
+        [RegularExpression(@"^[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}$")]
+        [XmlElement("Key")]
+        public string ProductKey { get; set; } = null!;
+
+        [Required]
+        [XmlElement("Card")]
+        public string Card { get; set; } = null!;
+
+        [Required]
+        [XmlElement("Date")]
+        public string Date { get; set; } = null!;
+    }
+}
