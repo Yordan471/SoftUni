240eabe481978f406c54816cb34dff299b470b95
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-12-09 16:05:41 +0200

    adding Class User

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Exam - 08 August 2020/VaporStore/Data/Models/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaporStore.Data.Models
+{
+    public class User
+    {
+        public User() 
+        {
+            Cards = new HashSet<Card>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Username { get; set; } = null!;
+
+        [Required]
+        [RegularExpression(@"^[A-Z]{1}[a-z]+\s[A-Z]{1}[a-z]+$")]
+        public string Fullname { get; set; } = null!;
+
+        [Required]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [Range(3, 103)]
+        public int Age { get; set; }
+
+        public virtual ICollection<Card> Cards { get; set; }
+    }
+}
