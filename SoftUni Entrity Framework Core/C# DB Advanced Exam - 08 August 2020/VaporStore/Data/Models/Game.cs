17583d1c71f5eddeb640df3612c7e1ca16d2bb6d
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-12-09 15:53:10 +0200

    adding class Game

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Exam - 08 August 2020/VaporStore/Data/Models/Game.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaporStore.Data.Models
+{
+    public class Game
+    {
+        public Game() 
+        {
+            Purchases = new HashSet<Purchase>();
+            GameTags = new HashSet<GameTag>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        public int DeveloperId { get; set; }
+
+        [ForeignKey(nameof(DeveloperId))]
+        public virtual Developer Developer { get; set; }
+
+        [Required]
+        public int GenreId { get; set; }
+
+        [ForeignKey(nameof(GenreId))]
+        public virtual Genre Genre { get; set; }
+
+        public virtual ICollection<Purchase> Purchases { get; set; }
+
+        public virtual ICollection<GameTag> GameTags { get; set; }
+    }
+}
