a7001246b7340965f100aa8d3a28f80dcfce6fbf
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-12-09 12:35:12 +0200

    adding class ImportJsonEmployeeDto

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Exam - 04 April 2021/TeisterMask/DataProcessor/ImportDto/ImportJsonEmployeeDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TeisterMask.Data.Models;
+using static TeisterMask.Utilities.GlobalConstants;
+
+namespace TeisterMask.DataProcessor.ImportDto
+{
+    public class ImportJsonEmployeeDto
+    {
+        [Required]
+        [MaxLength(EmployeeUsernameLengthMax)]
+        [MinLength(EmployeeUsernameLengthMin)]
+        [RegularExpression(EmployeeUsernameRegularExpressionValidation)]
+        public string Username { get; set; } = null!;
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(EmployeePhoneRegularExpressionValidation)]
+        public string Phone { get; set; } = null!;
+
+        public virtual int[] Tasks { get; set; }
+    }
+}
