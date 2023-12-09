8984b7ab5a2199b34c2ad28772f220b28fb2f69c
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-12-09 16:53:39 +0200

    adding class ImportJsonGameDto

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Exam - 08 August 2020/VaporStore/DataProcessor/ImportDto/ImportJsonGameDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VaporStore.Data.Models;
+using Newtonsoft.Json;
+
+namespace VaporStore.DataProcessor.ImportDto
+{
+    internal class ImportJsonGameDto
+    {
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [Range(0, (double)Decimal.MaxValue)]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string ReleaseDate { get; set; } = null!;
+
+        [Required]
+        public string Developer { get; set; } = null!;
+
+        [Required]
+        public string Genre { get; set; } = null!;
+
+        public string[] Tags { get; set; }
+    }
+}
