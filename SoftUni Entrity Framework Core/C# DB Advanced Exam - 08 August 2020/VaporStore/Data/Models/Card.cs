2665cf39953f05c492704af814893076721e940e
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-12-09 16:16:00 +0200

    adding Class Card

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Exam - 08 August 2020/VaporStore/Data/Models/Card.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using VaporStore.Data.Models.Enums;
+
+namespace VaporStore.Data.Models
+{
+    public class Card
+    {
+        public Card() 
+        {
+            Purchases = new HashSet<Purchase>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d{4} \d{4} \d{4} \d{4}$")]
+        public string Number { get; set; } = null!;
+
+        [Required]
+        [RegularExpression(@"^\d{3}$")]
+        public string Cvc { get; set; } = null!;
+
+        [Required]
+        public Enums.Type Type { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public virtual User User { get; set; } = null!;
+
+        public virtual ICollection<Purchase> Purchases { get; set; }
+    }
+}
