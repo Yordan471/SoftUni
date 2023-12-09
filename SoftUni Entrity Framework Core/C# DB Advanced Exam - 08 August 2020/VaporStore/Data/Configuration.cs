fb6e8c513b925b2bb26d4b52cc5da744a849d0c0
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-12-09 15:46:29 +0200

    adding resources for Prep Exam EF - Core

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Exam - 08 August 2020/VaporStore/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace VaporStore.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString =
+            @"DESKTOP-U0UT8KF\SQLEXPRESS;Database=VaporStore;Integrated Security=True;Encrypt=False";
+    }
+}
