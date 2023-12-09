fa5d42502b00b2efca9215d6ad5461db228e3c81
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-12-09 09:59:38 +0200

    adding and implementing class Employee

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Exam - 04 April 2021/TeisterMask/Data/Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static TeisterMask.Utilities.GlobalConstants;
+
+namespace TeisterMask.Data.Models
+{
+    public class Employee
+    {
+        public Employee() 
+        {
+            EmployeesTasks = new HashSet<EmployeeTask>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(EmployeeUsernameLengthMax)]
+        public string UserName { get; set; } = null!;
+
+        public string? Email { get; set; }
+
+        [Required]
+        public string Phone { get; set; } = null!;
+
+        public virtual ICollection<EmployeeTask> EmployeesTasks { get; set; }
+    }
+}
