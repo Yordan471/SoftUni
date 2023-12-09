7135ddfc0969c5546ad0e11cc51b09116f53d318
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-12-09 13:07:17 +0200

    adding classes ExportXmlTaskDto, ExportXmlProjectDto

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Exam - 04 April 2021/TeisterMask/DataProcessor/ExportDto/ExportXmlProjectDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace TeisterMask.DataProcessor.ExportDto
+{
+    [XmlType("Project")]
+    public class ExportXmlProjectDto
+    {
+        [XmlAttribute("TaskCount")]
+        public int TaskCount { get; set; }
+
+        [XmlElement("ProjectName")]
+        public string ProjectName { get; set; }
+
+        [XmlElement("HasEndDate")]
+        public string HasEndDate { get; set; }
+    }
+}
--- SoftUni Entrity Framework Core/C# DB Advanced Exam - 04 April 2021/TeisterMask/DataProcessor/Serializer.cs
+++ SoftUni Entrity Framework Core/C# DB Advanced Exam - 04 April 2021/TeisterMask/DataProcessor/Serializer.cs
@@ -1,6 +1,10 @@
 ﻿namespace TeisterMask.DataProcessor
 {
     using Data;
+    using Microsoft.VisualBasic;
+    using Newtonsoft.Json;
+    using System.Globalization;
+    using TeisterMask.Data.Models.Enums;
 
     public class Serializer
     {
@@ -11,7 +15,32 @@
 
         public static string ExportMostBusiestEmployees(TeisterMaskContext context, DateTime date)
         {
-            throw new NotImplementedException();
+            var employees = context.Employees
+                .Where(e => e.EmployeesTasks.Any(et => et.Task.OpenDate >= date))
+                .ToArray()
+                .Select(e => new
+                {
+                    Username = e.Username,
+                    Tasks = e.EmployeesTasks
+                    .Where(et => et.Task.OpenDate >= date)
+                    .Select(et => new
+                    {
+                        TaskName = et.Task.Name,
+                        OpenDate = et.Task.OpenDate.ToString("d", CultureInfo.InvariantCulture),
+                        DueDate = et.Task.DueDate.ToString("d", CultureInfo.InvariantCulture),
+                        LavelType = et.Task.LabelType.ToString(),
+                        ExecutionType = et.Task.ExecutionType.ToString()
+                    })
+                    .OrderByDescending(t => t.DueDate)
+                    .ThenBy(t => t.TaskName)
+                    .ToArray()
+                })
+                .OrderByDescending(e => e.Tasks.Count())
+                .ThenBy(e => e.Username)
+                .Take(10)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(employees, Formatting.Indented);
         }
     }
 }
