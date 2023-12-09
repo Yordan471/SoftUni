5b6d6cff9d334a97179bbcc2ab8957a421db0118
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-12-09 10:15:58 +0200

    Adding class and 2 enumerations; Task, ExecutionType and LabelType

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Exam - 04 April 2021/TeisterMask/Data/Models/Task.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using TeisterMask.Data.Models.Enums;
+using static TeisterMask.Utilities.GlobalConstants;
+
+namespace TeisterMask.Data.Models
+{
+    public class Task
+    {
+        public Task () 
+        {
+            EmployeesTasks = new HashSet<EmployeeTask>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(TaskNameLengthMax)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public DateTime OpenDate { get; set; }
+
+        [Required]
+        public DateTime DueTime { get; set; }
+
+        [Required]
+        public ExecutionType ExecutionType { get; set; }
+
+        [Required]
+        public LabelType LabelType { get; set; }
+
+        [Required]
+        public int ProjectId { get; set; }
+
+        [ForeignKey(nameof(ProjectId))]
+        public virtual Project Project { get; set; } = null!;
+
+        public virtual EmployeeTask EmployeesTasks { get; set; }
+    }
+}
