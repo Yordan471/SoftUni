d889bd3c75b73314aed4edf5ca388eec8d07b442
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-12-10 11:29:22 +0200

    Adding classes ExportXmlGameDto, ExportXmlPurchaseDto and ExportXmlUserDto

---
+++ SoftUni Entrity Framework Core/C# DB Advanced Exam - 08 August 2020/VaporStore/DataProcessor/ExportDto/ExportXmlPurchaseDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace VaporStore.DataProcessor.ExportDto
+{
+    [XmlType("Purchase")]
+    public class ExportXmlPurchaseDto
+    {
+        [XmlElement("Card")]
+        public string Card { get; set; }
+
+        [XmlElement("Cvc")]
+        public int Cvc { get; set; }
+
+        [XmlElement("Date")]
+        public string Date { get; set; }
+
+        public ExportXmlGameDto Game { get; set; }
+    }
+}
---
+++ SoftUni Entrity Framework Core/C# DB Advanced Exam - 08 August 2020/VaporStore/DataProcessor/ExportDto/ExportXmlUserDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace VaporStore.DataProcessor.ExportDto
+{
+    [XmlType("User")]
+    public class ExportXmlUserDto
+    {
+        [XmlAttribute("username")]
+        public string username { get; set; }
+
+        [XmlArray("Purchases")]
+        public ExportXmlPurchaseDto[] Purchases { get; set; }
+    }
+}
