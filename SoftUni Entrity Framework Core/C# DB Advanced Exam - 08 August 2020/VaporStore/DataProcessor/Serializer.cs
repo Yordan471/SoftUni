e994be811155ce887e1b692ef249e342bffd3d7b
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2023-12-10 11:21:48 +0200

    implementing method ExportGamesByGenres

--- SoftUni Entrity Framework Core/C# DB Advanced Exam - 08 August 2020/VaporStore/DataProcessor/Serializer.cs
+++ SoftUni Entrity Framework Core/C# DB Advanced Exam - 08 August 2020/VaporStore/DataProcessor/Serializer.cs
@@ -1,12 +1,37 @@
 ﻿namespace VaporStore.DataProcessor
 { 
     using Data;
+    using Newtonsoft.Json;
 
     public static class Serializer
     {
         public static string ExportGamesByGenres(VaporStoreDbContext context, string[] genreNames)
         {
-            throw new NotImplementedException();
+            var genres = context.Genres
+                .Where(g => genreNames.Contains(g.Name))
+                .ToArray()
+                .Select(g => new
+                {
+                    Id = g.Id,
+                    Genre = g.Name,
+                    Games = g.Games.Where(gg => gg.Purchases.Any()).Select(gg => new
+                    {
+                        Id = gg.Id,
+                        Title = gg.Name,
+                        Developer = gg.Developer.Name,
+                        Tags = gg.GameTags.Select(gt => string.Join(", ", gt.Tag.Name)),
+                        Players = gg.Purchases.Count
+                    })
+                    .OrderByDescending(gg => gg.Players)
+                    .ThenBy(gg => gg.Id)
+                    .ToArray(),
+                    TotalPlayers = g.Games.Sum(ga => ga.Purchases.Count)
+                })
+                .OrderByDescending(g => g.TotalPlayers)
+                .ThenBy(g => g.Id)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(genres, Formatting.Indented);
         }
 
         public static string ExportUserPurchasesByType(VaporStoreDbContext context, string purchaseType)
