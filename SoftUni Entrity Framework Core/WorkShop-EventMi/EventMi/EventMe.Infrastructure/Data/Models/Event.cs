ca06a2d8feef4904aad436ba1deaeb1ad3ec62fe
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-01-13 12:19:36 +0200

    Adding new MVC project with name TaskBoardApp + some initial adjustments

---
+++ SoftUni Entrity Framework Core/WorkShop-EventMi/EventMi/EventMe.Infrastructure/Data/Models/Event.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventMe.Infrastructure.Data.Models
+{
+
+    /// <summary>
+    /// Събитие
+    /// </summary>
+    [Comment("Събитие")]
+    public class Event
+    {
+        /// <summary>
+        /// Идентификатор на събитие
+        /// </summary>
+        [Comment("Идентификатор на събитие")]
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Име на събитието
+        /// </summary>
+        [Required]
+        [Comment("Име на събитието")]
+        [StringLength(50)]
+        public string Name { get; set; } = null!; // това е зло! Прави се required property
+
+        /// <summary>
+        /// Начало на събитието
+        /// </summary>
+        [Required]
+        [Comment("Начало на събитието")]
+        public DateTime Start { get; set; }
+
+        /// <summary>
+        /// Край на събитието
+        /// </summary>
+        [Required]
+        [Comment("Край на събитието")]
+        public DateTime End { get; set; }
+
+        /// <summary>
+        /// Адрес
+        /// </summary>
+        public Address Address { get; set; }
+    }
+}
