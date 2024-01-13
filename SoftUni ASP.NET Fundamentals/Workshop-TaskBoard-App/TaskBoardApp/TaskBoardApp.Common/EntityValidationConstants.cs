a768ddc8b94029131f07b8fc11eaab7337ede414
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-01-13 15:57:21 +0200

    Adding Class Library TaskBoardApp.Common with Class  EntityValidationConstants + constants for every entities property

---
+++ SoftUni ASP.NET Fundamentals/Workshop-TaskBoard-App/TaskBoardApp/TaskBoardApp.Common/EntityValidationConstants.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskBoardApp.Common
+{
+    public static class EntityValidationConstants
+    {
+        public static class TaskValidationConstants
+        {
+            public const int TaskTitleMinLength = 5;
+            public const int TaskTitleMaxLength = 70;
+            public const int TaskDescriptionMinLength = 10;
+            public const int TaskDescriptionMaxLength = 1000;
+        }
+
+        public static class BoardValidationConstants
+        {
+            public const int BoardNameMinLength = 3;
+            public const int BoardNameMaxLength = 30;
+        }
+    }
+}
