b1433941c28b2c16220e726eaeb725dd03edd854
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-01-17 19:55:01 +0200

    Adding IHomeService + 2 view models

---
+++ SoftUni ASP.NET Fundamentals/Workshop-TaskBoard-App/TaskBoardApp/TaskBoardApp.Web.ViewModels/Home/HomeBoardModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskBoardApp.Web.ViewModels.Home
+{
+    public class HomeBoardModel
+    {
+        public string BoardName { get; set; }
+
+        public int TaskCount { get; set; }
+    }
+}
---
+++ SoftUni ASP.NET Fundamentals/Workshop-TaskBoard-App/TaskBoardApp/TaskBoardApp.Web.ViewModels/Home/HomeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskBoardApp.Web.ViewModels.Home
+{
+    public class HomeViewModel
+    {
+        public int AllTasksCount { get; set; }
+
+        public List<HomeBoardModel> BoardsWithTasksCount { get; set; }
+
+        public int UserTasksCount { get; set; }
+    }
+}
