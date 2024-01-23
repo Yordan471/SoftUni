5730bcf6e8034cd5dff0957b26f344650ecbbb40
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-01-23 18:32:22 +0200

    adding Entity Ad with properties

---
+++ SoftUni ASP.NET Fundamentals/Prep Exam ASP .NET Core MVC - SoftUniBazar/SoftUniBazar/Data/Models/Ad.cs
@@ -0,0 +1,54 @@
+﻿namespace SoftUniBazar.Data.Models
+{
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using static SoftUniBazar.Common.EntityValidationConstants.Ad;
+
+    public class Ad
+    {
+        [Comment("Ad identifier")]
+        [Key]
+        public int Id { get; set; }
+
+        [Comment("Ad name")]
+        [Required]
+        [MaxLength(NameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        [Comment("Ad description")]
+        [Required]
+        [MaxLength(DescriptionMaxLength)]
+        public string Description { get; set; } = null!;
+
+        [Comment("Ad price")]
+        [Required]
+        public decimal Price { get; set; }
+
+        [Comment("Owner identifier")]
+        [Required]
+        public string OwnerId { get; set; } = null!;
+
+        [Comment("Ad owner")]
+        [Required]
+        [ForeignKey(nameof(OwnerId))]
+        public IdentityUser Owner { get; set; } = null!;
+
+        [Comment("Ad image")]
+        [Required]
+        public string ImageUrl { get; set; } = null!;
+
+        [Comment("Ad date creation")]
+        [Required]
+        public DateTime CreatedOn { get; set; }
+
+        [Comment("Category identifier")]
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Comment("Ad category")]
+        [Required]
+        public Category Category { get; set; }
+    }
+}
