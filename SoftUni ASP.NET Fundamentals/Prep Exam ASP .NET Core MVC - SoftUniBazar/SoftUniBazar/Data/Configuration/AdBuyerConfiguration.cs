4f56b52749ce77c225e8f882b28c360d5a1c06b8
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-01-23 18:57:11 +0200

    Adding dbSets and PrimaryKey for AdBuyer model

---
+++ SoftUni ASP.NET Fundamentals/Prep Exam ASP .NET Core MVC - SoftUniBazar/SoftUniBazar/Data/Configuration/AdBuyerConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SoftUniBazar.Data.Models;
+
+namespace SoftUniBazar.Data.Configuration
+{
+    public class AdBuyerConfiguration : IEntityTypeConfiguration<AdBuyer>
+    {
+        public void Configure(EntityTypeBuilder<AdBuyer> builder)
+        {
+            builder
+                .HasKey(ab => new { ab.BuyerId, ab.AdId });
+        }
+    }
+}
