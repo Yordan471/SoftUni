cbdb75d5b0873809deaf84f2346d3f90d6d79d21
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-01-23 19:04:28 +0200

    Adding migration + Updating Database

---
+++ SoftUni ASP.NET Fundamentals/Prep Exam ASP .NET Core MVC - SoftUniBazar/SoftUniBazar/Data/Migrations/20240123170227_Add_Custom_Models.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SoftUniBazar.Data.Migrations
+{
+    public partial class Add_Custom_Models : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false, comment: "Category identifier")
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false, comment: "Category name")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ads",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false, comment: "Ad identifier")
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false, comment: "Ad name"),
+                    Description = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false, comment: "Ad description"),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false, comment: "Ad price"),
+                    OwnerId = table.Column<string>(type: "nvarchar(450)", nullable: false, comment: "Owner identifier"),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false, comment: "Ad image"),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false, comment: "Ad date creation"),
+                    CategoryId = table.Column<int>(type: "int", nullable: false, comment: "Category identifier")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ads", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ads_AspNetUsers_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_Ads_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AdsBuyers",
+                columns: table => new
+                {
+                    BuyerId = table.Column<string>(type: "nvarchar(450)", nullable: false, comment: "Buyer identifier"),
+                    AdId = table.Column<int>(type: "int", nullable: false, comment: "Ad identifier")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AdsBuyers", x => new { x.BuyerId, x.AdId });
+                    table.ForeignKey(
+                        name: "FK_AdsBuyers_Ads_AdId",
+                        column: x => x.AdId,
+                        principalTable: "Ads",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_AdsBuyers_AspNetUsers_BuyerId",
+                        column: x => x.BuyerId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Books" },
+                    { 2, "Cars" },
+                    { 3, "Clothes" },
+                    { 4, "Home" },
+                    { 5, "Technology" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ads_CategoryId",
+                table: "Ads",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ads_OwnerId",
+                table: "Ads",
+                column: "OwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AdsBuyers_AdId",
+                table: "AdsBuyers",
+                column: "AdId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AdsBuyers");
+
+            migrationBuilder.DropTable(
+                name: "Ads");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
