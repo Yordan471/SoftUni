85d7b82ce6e348ea366865a7e30e2466b631c5c8
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-01-23 19:02:16 +0200

    adding precision for Price property in Ad model through Fluent API

--- SoftUni ASP.NET Fundamentals/Prep Exam ASP .NET Core MVC - SoftUniBazar/SoftUniBazar/Data/BazarDbContext.cs
+++ SoftUni ASP.NET Fundamentals/Prep Exam ASP .NET Core MVC - SoftUniBazar/SoftUniBazar/Data/BazarDbContext.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SoftUniBazar.Data.Configuration;
+using SoftUniBazar.Data.Models;
+using System.Reflection;
 
 namespace SoftUniBazar.Data
 {
@@ -10,37 +13,47 @@ namespace SoftUniBazar.Data
         {
         }
 
+        public DbSet<Ad> Ads { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<AdBuyer> AdsBuyers { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
-        { 
-            //modelBuilder
-            //    .Entity<Category>()
-            //    .HasData(new Category()
-            //    {
-            //        Id = 1,
-            //        Name = "Books"
-            //    },
-            //    new Category()
-            //    {
-            //        Id = 2,
-            //        Name = "Cars"
-            //    },
-            //    new Category()
-            //    {
-            //        Id = 3,
-            //        Name = "Clothes"
-            //    },
-            //    new Category()
-            //    {
-            //        Id = 4,
-            //        Name = "Home"
-            //    },
-            //    new Category()
-            //    {
-            //        Id = 5,
-            //        Name = "Technology"
-            //    });
-            //
-            // base.OnModelCreating(modelBuilder);
+        {
+            modelBuilder
+                .Entity<Category>()
+                .HasData(new Category()
+                {
+                    Id = 1,
+                    Name = "Books"
+                },
+                new Category()
+                {
+                    Id = 2,
+                    Name = "Cars"
+                },
+                new Category()
+                {
+                    Id = 3,
+                    Name = "Clothes"
+                },
+                new Category()
+                {
+                    Id = 4,
+                    Name = "Home"
+                },
+                new Category()
+                {
+                    Id = 5,
+                    Name = "Technology"
+                });
+
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.
+                GetAssembly(typeof(BazarDbContext)));
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
---
+++ SoftUni ASP.NET Fundamentals/Prep Exam ASP .NET Core MVC - SoftUniBazar/SoftUniBazar/Data/Configuration/AdEntityConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SoftUniBazar.Data.Models;
+
+namespace SoftUniBazar.Data.Configuration
+{
+    public class AdEntityConfiguration : IEntityTypeConfiguration<Ad>
+    {
+        public void Configure(EntityTypeBuilder<Ad> builder)
+        {
+            builder
+                .Property(a => a.Price)
+                .HasPrecision(18, 2);
+        }
+    }
+}
