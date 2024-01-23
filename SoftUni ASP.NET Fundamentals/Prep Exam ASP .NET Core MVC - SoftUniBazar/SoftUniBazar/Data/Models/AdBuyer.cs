be97226d47ade78de9f6241ac97d73e3a1b3740c
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-01-23 18:40:26 +0200

    Adding AdBuyer model + properties

---
+++ SoftUni ASP.NET Fundamentals/Prep Exam ASP .NET Core MVC - SoftUniBazar/SoftUniBazar/Data/Models/AdBuyer.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SoftUniBazar.Data.Models
+{
+    public class AdBuyer
+    {
+        [Comment("Buyer identifier")]
+        [Required]
+        public string BuyerId { get; set; } = null!;
+
+        [Comment("Buyer entity")]
+        [ForeignKey(nameof(BuyerId))]
+        public IdentityUser Buyer { get; set; }
+
+        [Comment("Ad identifier")]
+        [Required]
+        public int AdId { get; set; }
+
+        [Comment("Ad entity")]
+        [ForeignKey(nameof(AdId))]
+        public Ad Ad { get; set; }
+    }
+}
