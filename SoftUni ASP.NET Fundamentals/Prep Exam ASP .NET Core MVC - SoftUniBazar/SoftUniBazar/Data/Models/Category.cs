2d07d000c200c8c0eaa892318198a56ebc9f86cd
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-01-23 18:36:04 +0200

    Adding Entity Category + properties

---
+++ SoftUni ASP.NET Fundamentals/Prep Exam ASP .NET Core MVC - SoftUniBazar/SoftUniBazar/Data/Models/Category.cs
@@ -0,0 +1,26 @@
+﻿namespace SoftUniBazar.Data.Models
+{
+    using Microsoft.EntityFrameworkCore;
+    using System.ComponentModel.DataAnnotations;
+    using static SoftUniBazar.Common.EntityValidationConstants.Category;
+
+    public class Category
+    {
+        public Category() 
+        {
+            Ads = new HashSet<Ad>();
+        }
+
+        [Comment("Category identifier")]
+        [Key]
+        public int Id { get; set; }
+
+        [Comment("Category name")]
+        [Required]
+        [MaxLength(NameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        [Comment("Ad Collection")]
+        public ICollection<Ad> Ads { get; set; }
+    }
+}
