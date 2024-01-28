9a780f17dd68a9fc683ab2427589224a0987498d
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-01-28 21:48:29 +0200

    adding configuration for EventParticipant table + scaffoldng identity Login and Register

---
+++ SoftUni ASP.NET Fundamentals/Prep Exam ASP .NET Core MVC Fundamentals -Homies/Homies/Data/Models/Configurations/EventParticipantConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Homies.Data.Models.Configurations
+{
+    public class EventParticipantConfiguration : IEntityTypeConfiguration<EventParticipant>
+    {
+        public void Configure(EntityTypeBuilder<EventParticipant> builder)
+        {
+            builder
+                .HasKey(ep => new { ep.HelperId, ep.EventId });
+        }
+    }
+}
