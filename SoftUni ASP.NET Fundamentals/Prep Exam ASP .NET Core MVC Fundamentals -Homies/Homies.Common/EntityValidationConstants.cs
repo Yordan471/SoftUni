d2edd41a76c29a0e52d78348fc39b0ecf1ae2f7d
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-01-28 20:11:27 +0200

    adding Homies.Common Layer for EntityValidationConstants and EntityValidationErrorMessages

---
+++ SoftUni ASP.NET Fundamentals/Prep Exam ASP .NET Core MVC Fundamentals -Homies/Homies.Common/EntityValidationConstants.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homies.Common
+{
+    public static class EntityValidationConstants
+    {
+        public static class Event
+        {
+            public const int NameMaxLength = 20;
+            public const int NameMinLength = 5;
+            public const int DescriptionMaxLength = 150;
+            public const int DescriptionMinLength = 15;
+        }
+
+        public static class Type
+        {
+            public const int NameMaxLength = 15;
+            public const int NameMinLength = 5;
+        }
+    }
+}
