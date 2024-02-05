47f7aee8e6173980f2b67bc6cabcb6633c635249
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-02-05 18:28:49 +0200

    adding new project Watchlist.Common + Class EntityValidationConstants

---
+++ SoftUni ASP.NET Fundamentals/Prep Exam ASP .NET Core MVC Fundamentals -Watchlist/Watchlist.Common/EntityValidationsConstants.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Watchlist.Common
+{
+    public static class EntityValidationsConstants
+    {
+        public static class Movie
+        {
+            public const int TitleMaxLength = 50;
+            public const int TitleMinLength = 10;
+            public const int DirectorMaxLength = 50;
+            public const int DirectorMinLength = 5;
+            public const double RatingMaxValue = 10.00;
+            public const double RatingMinValue = 0.00;
+        }
+
+        public static class Genre
+        {
+            public const int NameMaxLength = 50;
+            public const int NameMinLength = 5;
+        }
+    }
+}
