604201b7cf757a1712d9509a1aba47aad49d1f0b
Yordan471 <110678258+Yordan471@users.noreply.github.com>
2024-02-11 13:47:03 +0200

    adding HttpGet and HttpPost for Register

---
+++ SoftUni ASP.NET Fundamentals/Prep Exam ASP .NET Core MVC Fundamentals -Watchlist/Watchlist/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Watchlist.Data.Models;
+using Watchlist.ViewModels.UserViewModels;
+
+namespace Watchlist.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<User> signInManager;
+        private readonly UserManager<User> userManager;
+
+        public AccountController(
+            SignInManager<User> signInManager,
+            UserManager<User> userManager)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            var registerModel = new UserRegisterViewModel();
+
+            return View(registerModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(UserRegisterViewModel registerModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(registerModel);
+            }
+
+            var user = new User() 
+            {
+                UserName = registerModel.UserName,
+                Email = registerModel.Email,
+            };
+
+            var result = await userManager.CreateAsync(user, registerModel.Password);
+
+            if (result.Succeeded)
+            {
+                await signInManager.SignInAsync(user, isPersistent: false);
+            }
+
+            return RedirectToAction("All", "Movies");
+        }
+    }
+}
